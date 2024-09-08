b05757aef04786714260a64b288bbc96b1ae3560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:03:08 +0000

    Add package swmcloudgate/0.1.5/swmcloudgate-0.1.5.tar.gz

---
+++ packages/swmcloudgate/swmcloudgate-0.1.5.tar.gz/swmcloudgate-0.1.5/swmcloudgate/config.py
@@ -0,0 +1,19 @@
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+USER_DIR = os.path.expanduser("~")
+
+
+class Settings(BaseSettings):
+    cache_expire: int = 14 * 24 * 3600
+    cache_dir: str
+
+    class Config:
+        env_file = f"{USER_DIR}/.swm/cloud-gate.conf"
+
+
+@lru_cache()
+def get_settings(cache_dir: str = f"{USER_DIR}/.cache/swm") -> Settings:
+    return Settings(cache_dir=cache_dir)
