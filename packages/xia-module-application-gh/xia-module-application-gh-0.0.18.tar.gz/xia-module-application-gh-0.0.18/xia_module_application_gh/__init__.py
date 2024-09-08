7bdb478df5a1e2380b148f1b3694fce03a304583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:11 +0000

    Add package xia-module-application-gh/0.0.18/xia-module-application-gh-0.0.18.tar.gz

---
+++ packages/xia-module-application-gh/xia-module-application-gh-0.0.18.tar.gz/xia-module-application-gh-0.0.18/xia_module_application_gh/__init__.py
@@ -0,0 +1,15 @@
+from xia_module_application_gh.application import Application
+from xia_module_application_gh.foundation import Foundation
+
+
+modules = {
+    "gh-module-application": "Application",
+    "gh-module-foundation": "Foundation"
+}
+
+__all__ = [
+    "Application",
+    "Foundation"
+]
+
+__version__ = "0.0.18"
