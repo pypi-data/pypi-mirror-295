d033facd3373fea90d5e32c23b569c9bdbb2d939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:02:51 +0000

    Add package asyncdb/2.8.6/asyncdb-2.8.6-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/asyncdb/asyncdb-2.8.6-pp39-pypy39_pp73-win_amd64.whl/asyncdb/connections.py
@@ -0,0 +1,46 @@
+import logging
+from .exceptions import DriverError
+from .interfaces import ConnectionBackend, PoolBackend
+from .utils.modules import module_exists
+from .utils import install_uvloop
+
+
+install_uvloop()
+
+
+class AsyncPool:
+    """
+    AsyncPool.
+       Base class for Asyncio-based DB Pools.
+       Factory interface for Pool-based connectors.
+    """
+
+    def __new__(cls, driver: str = "dummy", **kwargs) -> PoolBackend:
+        classpath = f"asyncdb.drivers.{driver}"
+        pool = f"{driver}Pool"
+        try:
+            mdl = module_exists(pool, classpath)
+            obj = mdl(**kwargs)
+            return obj
+        except Exception as err:
+            logging.exception(err)
+            raise DriverError(message=f"Cannot Load Backend Pool: {pool}") from err
+
+
+class AsyncDB:
+    """AsyncDB.
+
+    Factory Proxy Interface for Database Providers.
+    """
+
+    def __new__(cls, driver: str = "dummy", **kwargs) -> ConnectionBackend:
+        classpath = f"asyncdb.drivers.{driver}"
+        try:
+            mdl = module_exists(driver, classpath)
+            obj = mdl(**kwargs)
+            return obj
+        except Exception as err:
+            logging.exception(err)
+            raise DriverError(
+                message=f"Cannot Load Backend {driver}"
+            ) from err
