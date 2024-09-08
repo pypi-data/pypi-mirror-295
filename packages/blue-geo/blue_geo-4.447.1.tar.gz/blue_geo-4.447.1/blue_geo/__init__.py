745c859612a1276d4e662f8dd7f0d9f8011385a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:07 +0000

    Add package blue-geo/4.447.1/blue_geo-4.447.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.447.1.tar.gz/blue_geo-4.447.1/blue_geo/__init__.py
@@ -0,0 +1,13 @@
+NAME = "blue_geo"
+
+ICON = "🌐"
+
+DESCRIPTION = f"{ICON} AI for precise geospatial data analysis and visualization."
+
+VERSION = "4.447.1"
+
+REPO_NAME = "blue-geo"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/nbs/ukraine-timemap/QGIS.png?raw=true"
+)
