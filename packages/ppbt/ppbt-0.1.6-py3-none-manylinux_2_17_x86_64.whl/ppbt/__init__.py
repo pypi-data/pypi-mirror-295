2c14842d5c5b335dab448a7064a0f72a749895b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:24 +0000

    Add package ppbt/0.1.6/ppbt-0.1.6-py3-none-manylinux_2_17_x86_64.whl

---
+++ packages/ppbt/ppbt-0.1.6-py3-none-manylinux_2_17_x86_64.whl/ppbt/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Broadcom Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""
+Portable Python Build Toolchains.
+"""
+from .common import environ, extract
+
+__version__ = "0.1.6"
+
+ALL = ("environ", "extract")
