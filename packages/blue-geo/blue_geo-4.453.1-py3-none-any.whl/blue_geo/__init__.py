e59115508326675922b61a9737426e24c475fc67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:10 +0000

    Add package blue-geo/4.453.1/blue_geo-4.453.1-py3-none-any.whl

---
+++ packages/blue-geo/blue_geo-4.453.1-py3-none-any.whl/blue_geo/__init__.py
@@ -0,0 +1,13 @@
+ABCLI_PUBLIC_PREFIX = "https://kamangir-public.s3.ca-central-1.amazonaws.com"
+
+NAME = "blue_geo"
+
+ICON = "🌐"
+
+DESCRIPTION = f"{ICON} AI for a Blue Planet."
+
+VERSION = "4.453.1"
+
+REPO_NAME = "blue-geo"
+
+MARQUEE = f"{ABCLI_PUBLIC_PREFIX}/2024-01-06-20-39-46-73614/2024-01-06-20-39-46-73614-2X.gif?raw=true"
