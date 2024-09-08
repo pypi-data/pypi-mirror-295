fde9cd2263a99b5becc1f9dab5162f0638571436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:03:27 +0000

    Add package dream-platform-client/0.9/dream_platform_client-0.9.tar.gz

---
+++ packages/dream-platform-client/dream_platform_client-0.9.tar.gz/dream_platform_client-0.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Read the contents of your README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='dream_platform_client',
+    version='0.9',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    author='Jackson Makl',
+    author_email='jlm487@georgetown.edu',
+    description='A Python client to interact with the DataPlatform API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify Markdown here
+    url='https://github.com/jacksonlmakl/DataPlatform',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
