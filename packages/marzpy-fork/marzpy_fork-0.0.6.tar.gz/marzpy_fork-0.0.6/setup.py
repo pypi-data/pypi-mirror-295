88fecee45464d0eb739c6cc011fffd7bc56704d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:03:27 +0000

    Add package marzpy-fork/0.0.6/marzpy_fork-0.0.6.tar.gz

---
+++ packages/marzpy-fork/marzpy_fork-0.0.6.tar.gz/marzpy_fork-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+setup(
+    name="marzpy-fork", 
+    version="0.0.6",
+    author="Mewhrzad",
+    description="A fork of the original Marzpy library to manage Marzban panel",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vsskx/marzpy",
+    keywords=["marzpy", "Marzban", "Gozargah", "Marzban python", "Marzban API"],
+    packages=find_packages(),
+    install_requires=["aiohttp"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
