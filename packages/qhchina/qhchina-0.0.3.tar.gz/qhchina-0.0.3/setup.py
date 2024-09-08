79edbf11c2a007c506e1a7ddf5ab72d93345ebc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:03 +0000

    Add package qhchina/0.0.3/qhchina-0.0.3.tar.gz

---
+++ packages/qhchina/qhchina-0.0.3.tar.gz/qhchina-0.0.3/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qhchina",
+    version="0.0.3",
+    author="Maciej Kurzynski",
+    author_email="makurz@gmail.com",
+    description="The basic package for Quantitative Humanities · China Lab",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
