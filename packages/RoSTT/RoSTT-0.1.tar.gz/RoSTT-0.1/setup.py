1f09081b6b885e3bf549f5181e5496016bccb498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:03:45 +0000

    Add package RoSTT/0.1/RoSTT-0.1.tar.gz

---
+++ packages/RoSTT/RoSTT-0.1.tar.gz/RoSTT-0.1/setup.py
@@ -0,0 +1,14 @@
+from  setuptools import  setup,find_packages
+
+setup(
+    name='RoSTT',
+    version='0.1',
+    author='Rohan Bhosale',
+    description='this is a speech to text recognition package created by Rohan Bhosale'
+
+)
+packages = find_packages(),
+install_requirement = [
+    'selenium'
+    'webdriver_manager'
+]
