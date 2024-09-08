4754c25649be1452731be39ad19438c738a3a3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 03:04:25 +0000

    Add package NbRisk/40.0.2/NbRisk-40.0.2-py3-none-any.whl

---
+++ packages/NbRisk/NbRisk-40.0.2-py3-none-any.whl/nb_risk/api/views.py
@@ -0,0 +1,40 @@
+from netbox.api.viewsets import NetBoxModelViewSet
+
+from .. import models
+from . import serializers
+
+# ThreatSource ViewSets
+
+class ThreatSourceViewSet(NetBoxModelViewSet):
+    queryset = models.ThreatSource.objects.all()
+    serializer_class = serializers.ThreatSourceSerializer
+    
+# ThreatEvent ViewSets
+
+class ThreatEventViewSet(NetBoxModelViewSet):
+    queryset = models.ThreatEvent.objects.all()
+    serializer_class = serializers.ThreatEventSerializer
+
+# Vulnerability ViewSets
+
+class VulnerabilityViewSet(NetBoxModelViewSet):
+    queryset = models.Vulnerability.objects.all()
+    serializer_class = serializers.VulnerabilitySerializer
+    
+# VulnerabilityAssignment ViewSets
+
+class VulnerabilityAssignmentViewSet(NetBoxModelViewSet):
+    queryset = models.VulnerabilityAssignment.objects.all()
+    serializer_class = serializers.VulnerabilityAssignmentSerializer
+
+# Risk ViewSets
+
+class RiskViewSet(NetBoxModelViewSet):
+    queryset = models.Risk.objects.all()
+    serializer_class = serializers.RiskSerializer
+
+# Control ViewSets
+
+class ControlViewSet(NetBoxModelViewSet):
+    queryset = models.Control.objects.all()
+    serializer_class = serializers.ControlSerializer
