df34e6f69f8661418a154762ff01cc53d7418a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:03:26 +0000

    Add package QPUIQ/0.6.1/qpuiq-0.6.1.tar.gz

---
+++ packages/QPUIQ/qpuiq-0.6.1.tar.gz/qpuiq-0.6.1/PUI/wx/__init__.py
@@ -0,0 +1,18 @@
+from .application import *
+from .button import *
+from .canvas import *
+from .checkbox import *
+from .combobox import *
+from .dialog import *
+from .label import *
+from .layout import *
+from .progressbar import *
+from .radiobutton import *
+from .scroll import *
+from .text import *
+from .textfield import *
+from .window import *
+
+PUIView = WxPUIView
+
+PUI_BACKEND = "wx"
