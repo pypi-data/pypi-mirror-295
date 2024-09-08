93ce156f7b4719d5d95e56c0e8c8498ffee9327a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:02:56 +0000

    Add package MarkerMiner/0.1/MarkerMiner-0.1.tar.gz

---
+++ packages/MarkerMiner/MarkerMiner-0.1.tar.gz/MarkerMiner-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='MarkerMiner',
+    version='0.1',
+    packages=['MarkerMiner'],
+    package_data={
+        'MarkerMiner': ['*.pyd'], 
+    }
+)
