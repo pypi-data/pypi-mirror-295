f284674b659d9008b14277127f0dbf92592fc5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:04 +0000

    Add package opencvfacemodel/0.0.1/opencvfacemodel-0.0.1.tar.gz

---
+++ packages/opencvfacemodel/opencvfacemodel-0.0.1.tar.gz/opencvfacemodel-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'opencv facemodel'
+
+
+# Setting up
+setup(
+    name="opencvfacemodel",
+    version=VERSION,
+    author="Malyoneeeeer",
+    author_email="<malyoneeeeer@yahoo.com>",
+    description=DESCRIPTION,
+    include_package_data=True,
+    packages=['libname'],
+    package_data={'libname': ['models/*.*']},
+    install_requires=['opencv-python'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
