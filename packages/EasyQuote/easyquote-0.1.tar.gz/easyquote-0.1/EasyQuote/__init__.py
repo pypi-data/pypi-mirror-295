f0eb4580f3b589c4df9e8870d9e70808672ab0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:14 +0000

    Add package EasyQuote/0.1/easyquote-0.1.tar.gz

---
+++ packages/EasyQuote/easyquote-0.1.tar.gz/easyquote-0.1/EasyQuote/__init__.py
@@ -0,0 +1,2 @@
+# __init.py__
+from .main import EasyQuote
---
+++ packages/EasyQuote/easyquote-0.1.tar.gz/easyquote-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasyQuote',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'bs4'
+    ],
+)
