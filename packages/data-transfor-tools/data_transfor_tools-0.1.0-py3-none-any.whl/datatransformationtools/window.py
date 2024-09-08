50ca1389033f676676871e71ba13d8f0f5baa4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 15:02:53 +0000

    Add package data-transfor-tools/0.1.0/data_transfor_tools-0.1.0-py3-none-any.whl

---
+++ packages/data-transfor-tools/data_transfor_tools-0.1.0-py3-none-any.whl/datatransformationtools/window.py
@@ -0,0 +1,53 @@
+"""
+Conversions between inches and 
+larger imperial length units
+"""
+
+import numpy as np
+from typing import List, Union
+
+def window1d(input_array: Union[List[float], np.ndarray], size: int, shift: int = 1, stride: int = 1) -> List[np.ndarray]:
+    """
+
+    Parameters
+    ----------
+    input_array: Union[List[float] :
+        
+    np.ndarray] :
+        
+    size: int :
+        
+    shift: int :
+         (Default value = 1)
+    stride: int :
+         (Default value = 1)
+
+    Returns
+    -------
+    numpy.ndarray
+        The time_series as output
+
+    """
+   
+    
+    #  if it's a list Convert input_array to numpy array
+    if isinstance(input_array, list):
+        input_array = np.array(input_array)
+
+    # Check for valid input parameters
+    if size <= 0 or shift <= 0 or stride <= 0:
+        raise ValueError("Size, shift, and stride must be positive integers.")
+    if len(input_array) < size:
+        raise ValueError("Size of the window must be less than or equal to the length of the input array.")
+
+    windows = []
+    for start in range(0, len(input_array) - size + 1, shift):
+        # Slice the window with stride
+        window = input_array[start:start + size:stride]
+        windows.append(window)
+
+    return windows
+
+
+
+
