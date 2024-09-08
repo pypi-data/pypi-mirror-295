4b820a573d7d2da9d55aa5455c5ae604e0d744d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:02:51 +0000

    Add package endmapper/0.2.12/endmapper-0.2.12-py3-none-any.whl

---
+++ packages/endmapper/endmapper-0.2.12-py3-none-any.whl/endmapper/mappers/fastapi_mapper.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, FastAPI
+from fastapi.responses import Response
+
+from endmapper import endpoint_handlers
+from endmapper.mappers.base_mapper import BaseEndpointMapper
+
+
+router = APIRouter()
+main_app = None
+
+
+def connect_app(app: FastAPI):
+    global main_app
+    main_app = app
+
+
+@router.get('/api/endpoints/')
+def get_endpoints():
+    global main_app
+    """
+    ATTENTION: include "router" to main FastAPI app
+
+    This will add new endpoint "api/endpoints/" to your project
+    """
+    if main_app is None:
+        return Response({"error": "No app not connected"}, status_code=200)
+
+    config = BaseEndpointMapper.config()
+    result = endpoint_handlers.FastAPIEndpointHandler(**config.options).result
+    return Response(result, status_code=200)
