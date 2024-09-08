195bc958e93633ba69a0c329cd46ae5f54e07e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-08 14:03:08 +0000

    Add package PySensor-MQTT/0.2.1/pysensor_mqtt-0.2.1.tar.gz

---
+++ packages/PySensor-MQTT/pysensor_mqtt-0.2.1.tar.gz/pysensor_mqtt-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='PySensor-MQTT',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'paho-mqtt',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Biblioteca para simulação de sensores e publicação via MQTT',
+    author='Raunick Vileforte Vieira Generoso',
+    author_email='raunickbhdesign@gmail.com',
+    url='https://github.com/raunick/sensor_library_mqtt',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
