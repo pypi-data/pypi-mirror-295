8dbda4757b1d36c7f8fbef22ac66f60985990f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:51 +0000

    Add package sema4ai-action-server/0.23.1/sema4ai_action_server-0.23.1-cp312-cp312-macosx_13_0_x86_64.whl

---
+++ packages/sema4ai-action-server/sema4ai_action_server-0.23.1-cp312-cp312-macosx_13_0_x86_64.whl/sema4ai/action_server/__main__.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    from sema4ai.action_server.cli import main
+
+    args = None
+    # args = "start --https --ssl-self-signed --full-openapi-spec".split()
+    # args = "start --full-openapi-spec".split()
+    # args = ["start", "--full-openapi"]
+    # import os
+
+    # os.chdir(r"d:\x\temp\check-action-server\oauth-google")
+    # args = ["start", "--expose"]
+
+    main(args)
