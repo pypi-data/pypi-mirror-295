be0e9bccf4849fd3e8d731755be89c8926360072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:24 +0000

    Add package rms-models/0.0.7/rms_models-0.0.7-py3-none-any.whl

---
+++ packages/rms-models/rms_models-0.0.7-py3-none-any.whl/rms_models/models.py
@@ -0,0 +1,49 @@
+from uuid import uuid4
+from rms_models._internal.database import Base
+from sqlalchemy import UUID, Column, Float, ForeignKey, String, Table, Text
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import relationship
+
+
+class Functions(Base):
+    __tablename__ = "functions"
+
+    def __str__(self):
+        return f'Функция {self.name}, тип: {self.type}'
+
+    id = Column(UUID, primary_key=True, default=uuid4)
+    name = Column(String)
+    description = Column(Text)
+    route = Column(String)
+    type = Column(String)
+    input = Column(JSONB)
+
+    robots = relationship(
+        "Robots", secondary="functions_robots", back_populates="functions"
+    )
+
+
+class Robots(Base):
+    __tablename__ = "robots"
+
+    def __str__(self):
+        return f'Робот {self.name}, ip: {self.ip}'
+
+    id = Column(UUID, primary_key=True, default=uuid4)
+    ip = Column(String)
+    name = Column(String)
+    width = Column(Float)
+    length = Column(Float)
+    hash = Column(String)
+
+    functions = relationship(
+        "Functions", secondary="functions_robots", back_populates="robots"
+    )
+
+
+functions_robots = Table(
+    "functions_robots",
+    Base.metadata,
+    Column("function_id", ForeignKey("functions.id")),
+    Column("robot_id", ForeignKey("robots.id")),
+)
