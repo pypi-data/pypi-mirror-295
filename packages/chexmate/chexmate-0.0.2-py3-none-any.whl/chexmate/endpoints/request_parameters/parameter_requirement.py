087b588ca8ad810e78bc005979c2e87c939cfe2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:48 +0000

    Add package chexmate/0.0.2/chexmate-0.0.2-py3-none-any.whl

---
+++ packages/chexmate/chexmate-0.0.2-py3-none-any.whl/chexmate/endpoints/request_parameters/parameter_requirement.py
@@ -0,0 +1,11 @@
+from typing import Any, Callable
+
+
+class ParameterRequirement:
+    """Basically just a stand-in for a lambda function right now. Additional functionality is planned."""
+
+    def __init__(self, requirement: Callable[[Any], bool]):
+        self.requirement = requirement
+
+    def __call__(self, parameter_value: Any, ) -> bool:
+        return self.requirement(parameter_value)
