ae6376b13264a51436296d36b10c54840a13ad03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:09 +0000

    Add package skyfire-sdk/0.6.4/skyfire_sdk-0.6.4-py3-none-any.whl

---
+++ packages/skyfire-sdk/skyfire_sdk-0.6.4-py3-none-any.whl/skyfire-sdk/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from skyfire-sdk.api.api_ninja_api import APINinjaApi
+from skyfire-sdk.api.chat_api import ChatApi
+from skyfire-sdk.api.financial_datasets_api import FinancialDatasetsApi
+from skyfire-sdk.api.financial_datasets_ai_api import FinancialDatasetsAIApi
+from skyfire-sdk.api.gift_card_api import GiftCardApi
+from skyfire-sdk.api.toolkit_api import ToolkitApi
+from skyfire-sdk.api.vetric_api import VetricApi
+from skyfire-sdk.api.wallet_management_api import WalletManagementApi
+
---
+++ packages/skyfire-sdk/skyfire_sdk-0.6.4-py3-none-any.whl/skyfire_api/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from skyfire_api.api.api_ninja_api import APINinjaApi
+from skyfire_api.api.chat_api import ChatApi
+from skyfire_api.api.financial_datasets_api import FinancialDatasetsApi
+from skyfire_api.api.financial_datasets_ai_api import FinancialDatasetsAIApi
+from skyfire_api.api.gift_card_api import GiftCardApi
+from skyfire_api.api.toolkit_api import ToolkitApi
+from skyfire_api.api.vetric_api import VetricApi
+from skyfire_api.api.wallet_management_api import WalletManagementApi
+
