674d8ac5e6a0087ae49af443ea3038ec9c881dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:58 +0000

    Add package cscore/1/cscore-1.tar.gz

---
+++ packages/cscore/cscore-1.tar.gz/cscore-1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+  
+setup(
+    name='cscore',
+    version='1',
+    description='Scoring the commonness from differential analysis',
+    author='Hong Jiang',
+    author_email='scilavisher@gmail.com',
+    packages=['cscore'],
+)
