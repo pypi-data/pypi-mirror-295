4c10ea544e714130586f1f64593cb74dc84d7552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:19 +0000

    Add package scrapeAI/0.2.0/scrapeAI-0.2.0.tar.gz

---
+++ packages/scrapeAI/scrapeAI-0.2.0.tar.gz/scrapeAI-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapeAI',
+    version='0.2.0',
+    description='A Python library to scrape web data using LLMs and Selenium',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Rakesh Kumar',
+    author_email='rakeshparmuwal1436@example.com',
+    url='https://github.com/deBUGger404/Scrape-AI',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'beautifulsoup4',
+        'anthropic',
+        'openai',
+        'google-generativeai'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
