b5295e16a763d9fa5f93b7897a22883f8eb15ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:41 +0000

    Add package lanscape/0.0.7/lanscape-0.0.7.tar.gz

---
+++ packages/lanscape/lanscape-0.0.7.tar.gz/lanscape-0.0.7/src/lanscape/webviewer.py
@@ -0,0 +1,19 @@
+import threading
+import webview
+from app import app
+
+
+def start_server() -> int:
+    app.run(host='0.0.0.0', port=5001, debug=False)
+
+
+    
+if __name__ == "__main__":
+    # Start Flask server in a separate thread
+    server_thread = threading.Thread(target=start_server)
+    server_thread.daemon = True
+    server_thread.start()
+
+    # Start the Pywebview window
+    webview.create_window('LANscape', 'http://127.0.0.1:5001')
+    webview.start()
