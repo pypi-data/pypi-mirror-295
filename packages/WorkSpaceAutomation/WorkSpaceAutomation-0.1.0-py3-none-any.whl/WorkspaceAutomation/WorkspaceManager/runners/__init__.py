42a4fdfc3fdf298b5c1c7b71d65b170962f54c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:03:30 +0000

    Add package WorkSpaceAutomation/0.1.0/WorkSpaceAutomation-0.1.0-py3-none-any.whl

---
+++ packages/WorkSpaceAutomation/WorkSpaceAutomation-0.1.0-py3-none-any.whl/WorkspaceAutomation/WorkspaceManager/runners/__init__.py
@@ -0,0 +1,7 @@
+########################################
+#####  IMPORTING MODULES           #####
+########################################
+
+#####  INTERNAL IMPORTS
+from .apps import run_apps
+from .webs import open_urls
