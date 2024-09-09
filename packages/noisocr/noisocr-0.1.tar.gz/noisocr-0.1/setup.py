bc3d867ef510465cbdcd6aeea60612e255a0d306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:27 +0000

    Add package noisocr/0.1/noisocr-0.1.tar.gz

---
+++ packages/noisocr/noisocr-0.1.tar.gz/noisocr-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='noisocr',
+    version='0.1',
+    description='Tools to simulate post-OCR noisy texts.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sávio Santos',
+    author_email='savi8sant8s@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+)
