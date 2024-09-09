30489617b49aec9abb4309ee95843d6794ec6a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:03:36 +0000

    Add package cefi/4.11.3/cefi-4.11.3-py3-none-any.whl

---
+++ packages/cefi/cefi-4.11.3-py3-none-any.whl/cefi/handler/__init__.py
@@ -0,0 +1,6 @@
+from .capitalcom import CapitalHandler
+from .ccxt import CcxtHandler
+from .degiro import DegiroHandler
+from .ibkr import IbHandler
+
+__all__ = ["CapitalHandler", "CcxtHandler", "DegiroHandler", "IbHandler"]
