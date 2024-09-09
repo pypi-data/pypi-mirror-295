cf2e8352016981f995f51f4b74707e46463e254c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:18 +0000

    Add package ChemMod/0.0.10/ChemMod-0.0.10-py3-none-any.whl

---
+++ packages/ChemMod/ChemMod-0.0.10-py3-none-any.whl/ChemMod/plot/__init__.py
@@ -0,0 +1,16 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+
+from ChemMod.plot import __all__
+from ChemMod.equation import __all__
+
+
+theme('default')
