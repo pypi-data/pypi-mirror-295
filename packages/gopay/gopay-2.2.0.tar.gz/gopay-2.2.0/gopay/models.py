f019d66c7bb6981d824f9c475c38836a2c44b69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:19 +0000

    Add package gopay/2.2.0/gopay-2.2.0.tar.gz

---
+++ packages/gopay/gopay-2.2.0.tar.gz/gopay-2.2.0/gopay/models.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from pydantic import BaseModel, Extra
+from typing import Optional
+from gopay import enums
+
+
+class GopayModel(BaseModel):
+    class Config:
+        use_enum_values = True
+        extra = Extra.forbid
+
+
+class GopayConfig(GopayModel):
+    goid: int
+    client_id: str
+    client_secret: str
+    gateway_url: str
+    timeout: Optional[int] = None
+    scope: enums.TokenScope = enums.TokenScope.ALL
+    language: enums.Language = enums.Language.CZECH
