6ca2ce16a7f48f08f12590e17464a2e1b41bbf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:05 +0000

    Add package marsilea/0.4.6/marsilea-0.4.6-py3-none-any.whl

---
+++ packages/marsilea/marsilea-0.4.6-py3-none-any.whl/marsilea/__init__.py
@@ -0,0 +1,20 @@
+"""Declarative creation of composable visualization"""
+
+__version__ = "0.4.6"
+
+import marsilea.plotter as plotter
+from ._deform import Deformation
+from .base import (
+    WhiteBoard,
+    ClusterBoard,
+    ZeroWidth,
+    ZeroHeight,
+    ZeroWidthCluster,
+    ZeroHeightCluster,
+)
+from .dataset import load_data
+from .dendrogram import Dendrogram, GroupDendrogram
+from .heatmap import Heatmap, SizedHeatmap, CatHeatmap
+from .layers import Piece, Layers
+from .layout import CrossLayout, CompositeCrossLayout
+from .upset import UpsetData, Upset
