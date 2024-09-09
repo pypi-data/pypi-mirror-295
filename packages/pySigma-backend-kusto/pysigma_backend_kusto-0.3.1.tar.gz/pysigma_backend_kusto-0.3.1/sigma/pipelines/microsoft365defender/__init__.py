520f86d12f4a1594d1b34942579fab9ba0d7a634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:49 +0000

    Add package pySigma-backend-kusto/0.3.1/pysigma_backend_kusto-0.3.1.tar.gz

---
+++ packages/pySigma-backend-kusto/pysigma_backend_kusto-0.3.1.tar.gz/pysigma_backend_kusto-0.3.1/sigma/pipelines/microsoft365defender/__init__.py
@@ -0,0 +1,4 @@
+from .microsoft365defender import microsoft_365_defender_pipeline
+pipelines = {
+    "microsoft_365_defender_pipeline": microsoft_365_defender_pipeline,   # TODO: adapt identifier to something approproiate
+}
