7497c443131471e1ef1350988e2ddda20c284ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:43 +0000

    Add package yiddish/0.0.23/yiddish-0.0.23.tar.gz

---
+++ packages/yiddish/yiddish-0.0.23.tar.gz/yiddish-0.0.23/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='yiddish',
+    version='0.0.23',
+    author='Isaac L. Bleaman',
+    author_email='bleaman@berkeley.edu',
+    description='A Python library for processing Yiddish text',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ibleaman/yiddish',
+    license='MIT',
+    packages=['yiddish'],
+    include_package_data=True,
+    test_suite='tests',
+)
---
+++ packages/yiddish/yiddish-0.0.23.tar.gz/yiddish-0.0.23/yiddish/__init__.py
@@ -0,0 +1,13 @@
+from .yiddish import (
+  replace_with_precombined,
+  replace_with_decomposed,
+  replace_punctuation,
+  strip_diacritics,
+  transliterate,
+  detransliterate,
+  romanise_german,
+  respell_loshn_koydesh,
+  spell_loshn_koydesh,
+  hasidify,
+  desovietify
+)
