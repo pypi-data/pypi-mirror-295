2de0ac47f63806d8a4174069bcad6f9c6a4ef12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:28 +0000

    Add package jsave/1.0.3/jsave-1.0.3-py3-none-any.whl

---
+++ packages/jsave/jsave-1.0.3-py3-none-any.whl/jsave/jsave.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+def save(data: dict, filepath: str):
+    jsonData = json.dumps(data, indent=4)
+    with open(filepath, "w") as f:
+        f.write(jsonData)
+    
+
+def read(filepath: str) -> dict:
+    with open(filepath, "r") as f:
+        return json.loads(f.read())
+
+def delete(filepath: str):
+    if os.path.exists(filepath):
+        os.remove(filepath)
