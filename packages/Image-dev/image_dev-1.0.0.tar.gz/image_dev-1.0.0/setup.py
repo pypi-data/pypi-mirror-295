156a81c7496cdad2bc32db52fa8217c7edf0c890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:34 +0000

    Add package Image-dev/1.0.0/image_dev-1.0.0.tar.gz

---
+++ packages/Image-dev/image_dev-1.0.0.tar.gz/image_dev-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Image_dev",              # Module name
+    version="1.0.0",               # Version number
+    packages=find_packages(),      # Automatically find package directories
+    author="Hamdi Emad",
+    author_email="hamdy1721998@gmail.com",
+    description="Image Development project Project",
+    url="https://github.com/HamdEmad/ps-validation-project/tree/main/PS_Validation",  # Project's URL
+    include_package_data=True
+)
