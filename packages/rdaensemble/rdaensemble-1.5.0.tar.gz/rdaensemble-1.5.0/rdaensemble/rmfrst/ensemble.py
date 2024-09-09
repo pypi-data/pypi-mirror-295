bee2a6677fc10152ba3d4b2702e2c811db7fddb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:55 +0000

    Add package rdaensemble/1.5.0/rdaensemble-1.5.0.tar.gz

---
+++ packages/rdaensemble/rdaensemble-1.5.0.tar.gz/rdaensemble-1.5.0/rdaensemble/rmfrst/ensemble.py
@@ -0,0 +1,97 @@
+"""
+GENERATE AN ENSEMBLE OF RANDOM MAPS from RANDOM SPANNING TREES (RMfRST)
+"""
+
+from typing import List, Dict, Tuple
+
+from rdabase import (
+    mkAdjacencies,
+    populations,
+    total_population,
+    Graph,
+    Assignment,
+    calc_population_deviation,
+)
+
+from .random_map import random_map
+
+
+def gen_rmfrst_ensemble(
+    size: int,  # Number of random maps to generate
+    seed: int,  # Starting random seed
+    data: Dict[str, Dict[str, int | str]],
+    graph: Dict[str, List[str]],
+    N: int,  # Number of districts
+    logfile,
+    *,
+    roughly_equal: float = 0.02,
+    verbose: bool = False,
+) -> List[Dict[str, str | float | Dict[str, int | str]]]:
+    """Generate an ensemble of random maps from random spanning trees."""
+
+    start: int = seed
+    plans: List[Dict[str, str | float | Dict[str, int | str]]] = list()
+
+    pairs: List[Tuple[str, str]] = mkAdjacencies(Graph(graph))
+
+    pop_by_geoid: Dict[str, int] = populations(data)
+    total_pop: int = total_population(pop_by_geoid)
+
+    conforming_count: int = 0
+
+    while True:
+        print(f"... {conforming_count} ...")
+        print(
+            f"Conforming count: {conforming_count}, random seed: {seed}", file=logfile
+        )
+
+        plan_name: str = f"{conforming_count:03d}_{seed}"
+
+        try:
+            print("Calling random_map...")
+            assignments: List[Assignment] = random_map(
+                pairs,
+                pop_by_geoid,
+                N,
+                seed,
+            )  # Generate a random contiguous & 'roughly' equal population partitioning of the state.
+
+            print("... random_map done.")
+            popdev: float = calc_population_deviation(
+                assignments, pop_by_geoid, total_pop, N
+            )
+
+            print(f"Population deviation: {popdev}")
+
+            if popdev > roughly_equal:
+                continue
+
+            conforming_count += 1
+            plan: Dict[str, int | str] = {a.geoid: a.district for a in assignments}
+            plans.append({"name": plan_name, "plan": plan})  # No weights.
+
+            if conforming_count == size:
+                break
+
+        except Exception as e:
+            print(f"Failure: {e}", file=logfile)
+            if verbose:
+                print(f"Failure: {e}")
+
+        except:
+            print("Unknown error.", file=logfile)
+            if verbose:
+                print("Unknown error.")
+
+        finally:
+            seed += 1
+
+    print(
+        f"{conforming_count} conforming plans took {seed - start + 1} random seeds.",
+        file=logfile,
+    )
+
+    return plans
+
+
+### END ###
