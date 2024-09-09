f5477fc6bed9192ffcc492385dd46db0dade3ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:36 +0000

    Add package memorag/0.1.2/memorag-0.1.2.tar.gz

---
+++ packages/memorag/memorag-0.1.2.tar.gz/memorag-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='memorag',
+    version='0.1.2',
+    description='A Python package for memory-augmented retrieval-augmented generation',
+    author='Tommy Chien',
+    author_email='tommy@chien.io',
+    packages=find_packages(),
+    install_requires=requirements,  
+    classifiers=[
+        'Programming Language :: Python :: 3.10',  
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',  
+)
