4fbe77302043626044e97246051989998f0aa761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 16:03:45 +0000

    Add package ado_wrapper/1.22.0/ado_wrapper-1.22.0.tar.gz

---
+++ packages/ado_wrapper/ado_wrapper-1.22.0.tar.gz/ado_wrapper-1.22.0/ado_wrapper/plan_resources/colours.py
@@ -0,0 +1,14 @@
+BRIGHT_CYAN = "\u001b[36;1m"
+BRIGHT_BLUE = "\u001b[34;1m"
+BRIGHT_GREEN = "\u001b[38;5;77m"
+BRIGHT_RED = "\u001b[31;1m"
+DARK_BLUE = "\u001b[34m"
+RESET = "\u001b[0m"
+
+ACTIONS = {
+    "create": BRIGHT_GREEN + "+" + RESET,
+    "update": BRIGHT_BLUE + "~" + RESET,
+    "delete": BRIGHT_RED + "-" + RESET,
+}
+
+# https://talyian.github.io/ansicolors/
