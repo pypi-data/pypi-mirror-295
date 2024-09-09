1db7de4bc858591e9e487cb352febead7430bf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:15 +0000

    Add package mkslides-reveal/0.1.9/mkslides_reveal-0.1.9.tar.gz

---
+++ packages/mkslides-reveal/mkslides_reveal-0.1.9.tar.gz/mkslides_reveal-0.1.9/src/mkslides_reveal/__main__.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import shutil
+import livereload
+import logging
+import tempfile
+
+from importlib import metadata
+from pathlib import Path
+from rich.logging import RichHandler
+from urllib.parse import urlparse
+
+from .config import Config
+from .constants import (
+    EXPECTED_CONFIG_LOCATION,
+    DEFAULT_OUTPUT_DIR,
+    REVEALJS_RESOURCE,
+    HIGHLIGHTJS_RESOURCE,
+)
+from .markupgenerator import MarkupGenerator
+
+
+logger = logging.getLogger()
+logger.setLevel("DEBUG")
+logger.addHandler(RichHandler(show_path=False))
+
+
+################################################################################
+
+
+def main() -> argparse.Namespace:
+
+    # Common arguments
+
+    version = metadata.version("mkslides_reveal")
+
+    revealjs_version = None
+    with REVEALJS_RESOURCE.joinpath("package.json").open() as f:
+        revealjs_version = json.load(f)["version"]
+
+    highlightjs_themes_version = None
+    with HIGHLIGHTJS_RESOURCE.joinpath("build", "package.json").open() as f:
+        highlightjs_themes_version = json.load(f)["version"]
+
+    help_argument_data = {
+        "action": "help",
+        "help": "Show this message and exit.",
+    }
+
+    files_argument_data = {
+        "metavar": "FILENAME|PATH",
+        "help": "Path to the Markdown file, or the directory containing Markdown files.",
+    }
+
+    config_file_argument_data = {
+        "metavar": "FILENAME",
+        "default": EXPECTED_CONFIG_LOCATION,
+        "help": "Provide a specific MkSlides-Reveal config file.",
+    }
+
+    site_dir_argument_data = {
+        "metavar": "PATH",
+        "default": DEFAULT_OUTPUT_DIR,
+        "help": "The directory to output the result of the slides build.",
+    }
+
+    # Global arguments
+
+    parser = argparse.ArgumentParser(
+        prog="mkslides-reveal",
+        description="MkSlides-Reveal - Slides with Markdown using the power of Reveal.js.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+    )
+
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"MkSlides-Reveal: {version}, Reveal.js version: {revealjs_version}, Highlight.js themes version: {highlightjs_themes_version}",
+        help="Show the version and exit.",
+    )
+    parser.add_argument("-h", "--help", **help_argument_data)
+
+    subparsers = parser.add_subparsers(title="Commands", dest="command")
+
+    # Build arguments
+
+    build_parser = subparsers.add_parser(
+        "build",
+        help="Build the MkDocs documentation.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+    )
+    build_parser.add_argument("files", **files_argument_data)
+    build_parser.add_argument("-f", "--config-file", **config_file_argument_data)
+    build_parser.add_argument("-d", "--site-dir", **site_dir_argument_data)
+    build_parser.add_argument("-h", "--help", **help_argument_data)
+    build_parser.set_defaults(func=build)
+
+    # Serve arguments
+
+    serve_parser = subparsers.add_parser(
+        "serve",
+        help="Run the builtin development server.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+    )
+    serve_parser.add_argument(
+        "files",
+        **files_argument_data,
+    )
+    serve_parser.add_argument(
+        "-a",
+        "--dev-addr",
+        metavar="<IP:PORT>",
+        default="localhost:8000",
+        help="IP address and port to serve slides locally",
+    )
+    serve_parser.add_argument(
+        "-o",
+        "--open",
+        action="store_true",
+        help="Open the website in a Web browser after the initial build finishes.",
+    )
+    serve_parser.add_argument(
+        "--watch-index-theme",
+        action="store_true",
+        help="Include the index theme in list of files to watch for live reloading.",
+    )
+    serve_parser.add_argument(
+        "--watch-index-template",
+        action="store_true",
+        help="Include the index template in list of files to watch for live reloading.",
+    )
+    serve_parser.add_argument(
+        "--watch-slides-theme",
+        action="store_true",
+        help="Include the slides theme in list of files to watch for live reloading.",
+    )
+    serve_parser.add_argument(
+        "--watch-slides-template",
+        action="store_true",
+        help="Include the slides template in list of files to watch for live reloading.",
+    )
+    serve_parser.add_argument("-f", "--config-file", **config_file_argument_data)
+    serve_parser.add_argument("-h", "--help", **help_argument_data)
+    serve_parser.set_defaults(func=serve)
+
+    # Execute the command
+
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+def read_config(config_location: str) -> Config:
+    config_path = Path(config_location).resolve()
+    config = Config()
+
+    if config_path.exists():
+        logger.info(f'Config file found at "{config_path.absolute()}"')
+        config.merge_config_from_file(config_path)
+
+    return config
+
+
+def parse_ip_port(
+    ip_port_str: str,
+) -> tuple[str, int]:
+    urlparse_result = urlparse(f"//{ip_port_str}")
+    ip = urlparse_result.hostname
+    port = urlparse_result.port
+
+    return ip, port
+
+
+def build(args):
+
+    logger.info("Command: build")
+
+    # Reading configuration
+
+    config = read_config(args.config_file)
+
+    # Configuring paths
+
+    input_path = Path(args.files).resolve(strict=True)
+    md_root_path = input_path if input_path.is_dir() else input_path.parent
+    output_directory = Path(args.site_dir).resolve(strict=False)
+    markup_generator = MarkupGenerator(config, md_root_path, output_directory)
+
+    # Process markdown files
+
+    markup_generator.create_output_directory()
+    markup_generator.process_markdown(input_path)
+
+
+def serve(args):
+
+    logger.info("Command: serve")
+
+    # Reading configuration
+
+    config = read_config(args.config_file)
+
+    # Configuring paths
+
+    input_path = Path(args.files).resolve(strict=True)
+    md_root_path = input_path if input_path.is_dir() else input_path.parent
+    site_dir = tempfile.mkdtemp(prefix="mkslides_")
+    output_directory = Path(site_dir).resolve(strict=False)
+    markup_generator = MarkupGenerator(config, md_root_path, output_directory)
+
+    # Process markdown files
+
+    markup_generator.create_output_directory()
+    markup_generator.process_markdown(input_path)
+
+    # Livereload
+
+    def reload():
+        logger.info("Reloading...")
+        markup_generator.create_output_directory()
+        markup_generator.process_markdown(input_path)
+
+    try:
+        server = livereload.Server()
+        server._setup_logging = (
+            lambda: None
+        )  # https://github.com/lepture/python-livereload/issues/232
+
+        watched_paths = [
+            args.files,
+            args.config_file,  # TODO reload config
+        ]
+
+        if args.watch_index_theme:
+            watched_paths.append(config.get("index", "theme"))
+        if args.watch_index_template:
+            watched_paths.append(config.get("index", "template"))
+        if args.watch_slides_theme:
+            watched_paths.append(config.get("slides", "theme"))
+        if args.watch_slides_template:
+            watched_paths.append(config.get("slides", "template"))
+
+        for path in watched_paths:
+            if path:
+                path = Path(path).resolve(strict=True)
+                logger.info(f'Watching: "{path.absolute()}"')
+                server.watch(filepath=path.absolute().as_posix(), func=reload, delay=1)
+
+        ip, port = parse_ip_port(args.dev_addr)
+
+        try:
+            server.serve(
+                host=ip,
+                port=port,
+                root=output_directory,
+                open_url_delay=0 if args.open else None,
+            )
+        except KeyboardInterrupt:
+            logger.info("Server shut down")
+
+    finally:
+        if output_directory.exists():
+            shutil.rmtree(output_directory)
+            logger.info(f'Removed "{output_directory}"')
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mkslides-reveal/mkslides_reveal-0.1.9.tar.gz/mkslides_reveal-0.1.9/src/mkslides_reveal/constants.py
@@ -0,0 +1,58 @@
+import re
+
+from importlib import resources
+from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
+
+MD_IMAGE_REGEX = re.compile(
+    r"""
+    !                           # Start of the image
+    \[.*?\]                     # Alt text
+    \((?P<location>.*?)\)       # Image location
+    """,
+    re.VERBOSE,
+)
+HTML_IMAGE_REGEX = re.compile(
+    r"""
+    <img                        # Start of the image
+    .+?                         # Any attributes
+    src=                        # src attribute
+        (?P<delimiter>['\"])    # Delimiter
+        (?P<location>.+?)       # Image location
+        (?P=delimiter)          # Delimiter
+    .*?                         # Any attributes
+    >                           # End of the image
+    """,
+    re.VERBOSE,
+)
+
+HTML_BACKGROUND_IMAGE_REGEX = re.compile(
+    r"""
+    <!--                        # Start of the comment
+    .*?                         # Any content
+    data-background-image=      # data-background-image attribute
+        (?P<delimiter>['\"])    # Delimiter
+        (?P<location>.+?)       # Image location
+        (?P=delimiter)          # Delimiter
+    .*?                         # Any content
+    -->                         # End of the comment
+    """,
+    re.VERBOSE,
+)
+
+EXPECTED_CONFIG_LOCATION = "mkslides.yml"
+DEFAULT_OUTPUT_DIR = "site"
+
+ASSETS_RESOURCE = resources.files("assets")
+DEFAULT_CONFIG_RESOURCE = ASSETS_RESOURCE.joinpath("mkslides.default.yml")
+REVEALJS_RESOURCE = ASSETS_RESOURCE.joinpath("reveal.js")
+REVEALJS_THEMES_RESOURCE = REVEALJS_RESOURCE.joinpath("dist", "theme")
+HIGHLIGHTJS_RESOURCE = ASSETS_RESOURCE.joinpath("highlight.js")
+HIGHLIGHTJS_THEMES_RESOURCE = HIGHLIGHTJS_RESOURCE.joinpath("build", "styles")
+
+DEFAULT_JINJA2_ENVIRONMENT = Environment(
+    loader=PackageLoader("assets"),
+    autoescape=select_autoescape()
+)
+DEFAULT_INDEX_TEMPLATE = DEFAULT_JINJA2_ENVIRONMENT.get_template("index.html.jinja")
+DEFAULT_SLIDESHOW_TEMPLATE = DEFAULT_JINJA2_ENVIRONMENT.get_template("slideshow.html.jinja")
+LOCAL_JINJA2_ENVIRONMENT = Environment(loader=FileSystemLoader('.'))
