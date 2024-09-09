73da694a16a0404135fa6783eaff689d57be283e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:09 +0000

    Add package acte/0.0.10/acte-0.0.10-py3-none-any.whl

---
+++ packages/acte/acte-0.0.10-py3-none-any.whl/acte/node/element/inline/button.py
@@ -0,0 +1,51 @@
+from typing import Callable, Awaitable
+
+from acte.node.element.inline.inline import Inline
+from acte.node.implement.interactive import Interactive
+from acte.schema.schema import NullSchema
+from acte.state import Effect, Ref
+
+
+class Button(Inline, Interactive):
+    def __init__(self) -> None:
+        Inline.__init__(self)
+        Interactive.__init__(self)
+
+        self._content: str = ''
+        self._schema: NullSchema | None = None
+        self._on_press: Callable[[], Awaitable[None] | None] | None = None
+
+    @property
+    def content(self) -> str:
+        return self._content
+
+    @property
+    def schema(self) -> NullSchema | None:
+        return self._schema
+
+    @property
+    def on_press(self) -> Callable[[], Awaitable[None] | None] | None:
+        return self._on_press
+
+    async def bind_content(self, content: Ref[str]) -> None:
+        async def _func() -> None:
+            v = content.value
+            if v is None:
+                v = ''
+
+            self._content = v
+
+        effect = await Effect.create(_func)
+
+        self._effect_list.append(effect)
+
+    def set_schema(self, schema: NullSchema) -> None:
+        self._schema = schema
+
+    async def bind_on_press(self, on_press: Ref[Callable[[], Awaitable[None] | None]]) -> None:
+        async def _func() -> None:
+            self._on_press = on_press.value
+
+        effect = await Effect.create(_func)
+
+        self._effect_list.append(effect)
