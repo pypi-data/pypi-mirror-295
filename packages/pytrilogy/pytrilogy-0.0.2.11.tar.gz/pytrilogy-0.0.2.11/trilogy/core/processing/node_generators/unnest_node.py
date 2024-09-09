308edb41feb5d6b49e1b6a81720b8e1f16fd02b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:05 +0000

    Add package pytrilogy/0.0.2.11/pytrilogy-0.0.2.11.tar.gz

---
+++ packages/pytrilogy/pytrilogy-0.0.2.11.tar.gz/pytrilogy-0.0.2.11/trilogy/core/processing/node_generators/unnest_node.py
@@ -0,0 +1,60 @@
+from typing import List
+
+
+from trilogy.core.models import Concept, Function, WhereClause
+from trilogy.core.processing.nodes import UnnestNode, History, StrategyNode
+from trilogy.core.processing.utility import padding
+from trilogy.constants import logger
+
+LOGGER_PREFIX = "[GEN_ROWSET_NODE]"
+
+
+def gen_unnest_node(
+    concept: Concept,
+    local_optional: List[Concept],
+    environment,
+    g,
+    depth: int,
+    source_concepts,
+    history: History | None = None,
+    conditions: WhereClause | None = None,
+) -> StrategyNode | None:
+    arguments = []
+    if isinstance(concept.lineage, Function):
+        arguments = concept.lineage.concept_arguments
+    if arguments or local_optional:
+        parent = source_concepts(
+            mandatory_list=arguments + local_optional,
+            environment=environment,
+            g=g,
+            depth=depth + 1,
+            history=history,
+            conditions=conditions,
+        )
+        if not parent:
+            logger.info(
+                f"{padding(depth)}{LOGGER_PREFIX} could not find unnest node parents"
+            )
+            return None
+
+    base = UnnestNode(
+        unnest_concept=concept,
+        input_concepts=arguments + local_optional,
+        output_concepts=[concept] + local_optional,
+        environment=environment,
+        g=g,
+        parents=([parent] if (arguments or local_optional) else []),
+    )
+    # we need to sometimes nest an unnest node,
+    # as unnest operations are not valid in all situations
+    # TODO: inline this node when we can detect it's safe
+    new = StrategyNode(
+        input_concepts=[concept] + local_optional,
+        output_concepts=[concept] + local_optional,
+        environment=environment,
+        g=g,
+        parents=[base],
+    )
+    qds = new.resolve()
+    assert qds.source_map[concept.address] == {base.resolve()}
+    return new
