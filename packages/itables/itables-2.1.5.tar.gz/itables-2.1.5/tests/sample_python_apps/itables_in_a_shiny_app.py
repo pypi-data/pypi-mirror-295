cf96fb6552ec3db787f80b6faeae859f9e007a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:03:25 +0000

    Add package itables/2.1.5/itables-2.1.5.tar.gz

---
+++ packages/itables/itables-2.1.5.tar.gz/itables-2.1.5/tests/sample_python_apps/itables_in_a_shiny_app.py
@@ -0,0 +1,11 @@
+# Run this app with "shiny run file.py"
+from shiny import App, ui
+
+from itables.sample_dfs import get_countries
+from itables.shiny import DT
+
+df = get_countries()
+
+app_ui = ui.page_fluid(ui.HTML(DT(df)))
+
+app = App(app_ui, server=None)
