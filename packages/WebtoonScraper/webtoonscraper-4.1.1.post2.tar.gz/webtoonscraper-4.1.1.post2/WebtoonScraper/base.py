20d9916c3ed6377bcf5b270b1832e68e125fd1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:07 +0000

    Add package WebtoonScraper/4.1.1.post2/webtoonscraper-4.1.1.post2.tar.gz

---
+++ packages/WebtoonScraper/webtoonscraper-4.1.1.post2.tar.gz/webtoonscraper-4.1.1.post2/WebtoonScraper/base.py
@@ -0,0 +1,18 @@
+"""WebtoonScraper의 기본 정보들을 모아놓은 모듈입니다. circular import를 피하기 위해 필요합니다."""
+
+from __future__ import annotations
+
+import logging
+import os
+
+from rich.logging import RichHandler
+
+__url__ = "https://github.com/ilotoki0804/WebtoonScraper"
+__version__ = "4.1.1.post2"
+
+_CPU_COUNT = os.cpu_count()
+DEFAULT_PROCESS_NUMBER = 1 if _CPU_COUNT is None or _CPU_COUNT < 2 else max(_CPU_COUNT // 2, 10)
+
+logger = logging.getLogger("webtoonscraper_logger")
+logger.handlers = [RichHandler(show_time=False, show_path=False)]
+logger.setLevel(logging.INFO)
