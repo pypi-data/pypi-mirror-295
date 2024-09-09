d8b106bbcb9e9af797a91089901ae20307bc736c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:58 +0000

    Add package deltastudio/1.3/deltastudio-1.3.tar.gz

---
+++ packages/deltastudio/deltastudio-1.3.tar.gz/deltastudio-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deltastudio',
+    version='1.3',
+    description='A package to simpify working with Delta Lake OSS.',
+    author='Morteza Hajipour',
+    author_email='mortezahajipour.works@gmail.com',
+    packages=find_packages(),
+)
