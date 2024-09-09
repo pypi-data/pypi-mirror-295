95f033b393ccd0dd8b5234b256ef25e6fb7308f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:59 +0000

    Add package kronoterm-cloud-api/0.1.0/kronoterm_cloud_api-0.1.0-py3-none-any.whl

---
+++ packages/kronoterm-cloud-api/kronoterm_cloud_api-0.1.0-py3-none-any.whl/kronoterm_cloud_api/enums/hp_enums.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class WorkingFunction(Enum):
+    HP_FUNCTION_HEATING = 0
+    HP_FUNCTION_SANITARY_WATER_HEATING = 1
+    HP_FUNCTION_COOLING = 2
+    HP_FUNCTION_POOL_HEATING = 3
+    HP_FUNCTION_ANTILEGIONELLA = 4
+    HP_FUNCTION_SLEEP = 5
+    HP_FUNCTION_STARTUP = 6
+    HP_FUNCTION_REMOTE_DISCONNECT = 7
+    HP_FUNCTION_ACTIVE_COMPRESSOR_SECURITY = 8
+
+
+class APIEndpoint(Enum):
+    SYSTEM_REVIEW = "TopPage=1&Subpage=2"
+    # HEATING_LOOP_1 = "TopPage=1&Subpage=6"
+    # SET_HEATING_LOOP_1 = "TopPage=1&Subpage=6&Action=1"
+    HEATING_LOOP_2 = "TopPage=1&Subpage=6"
+    SET_HEATING_LOOP_2 = "TopPage=1&Subpage=6&Action=1"
+    TIMELINE_GRAPH = "TopPage=4&Subpage=1&Action=3"
+    CONSUMPTION_HISTOGRAM = "TopPage=4&Subpage=4&Action=4"
+
+
+class HeatingLoop(Enum):
+    HIGH_TEMPERATURE_LOOP = 1  # Radiators
+    LOW_TEMPERATURE_LOOP = 2  # Convectors
+
+
+class HeatingLoopMode(Enum):
+    OFF = 0
+    ON = 1
+    AUTO = 2
