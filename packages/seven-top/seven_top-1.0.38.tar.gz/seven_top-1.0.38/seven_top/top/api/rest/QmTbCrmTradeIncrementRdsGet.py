3cadb431050980fe06431315accd0557924ed73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:30 +0000

    Add package seven-top/1.0.38/seven_top-1.0.38.tar.gz

---
+++ packages/seven-top/seven_top-1.0.38.tar.gz/seven_top-1.0.38/seven_top/top/api/rest/QmTbCrmTradeIncrementRdsGet.py
@@ -0,0 +1,21 @@
+'''
+Created by auto_sdk on 2021.09.14 奇门增量获取卖家订单
+'''
+from seven_top.top.api.base import RestApi
+class QmTbCrmTradeIncrementRdsGet(RestApi):
+	def __init__(self,domain='gw.api.taobao.com',port=80):
+		RestApi.__init__(self,domain, port)
+		self.status = ""
+		self.type = ""
+		self.seller_nick = ""
+		self.buyer_nick = ""
+		self.created = ""
+		self.modified = ""
+		self.jdp_hashcode = ""
+		self.jdp_response = ""
+		self.jdp_created = ""
+		self.jdp_modified = ""
+
+
+	def getapiname(self):
+		return 'taobao.trade.increment.rds.get'
