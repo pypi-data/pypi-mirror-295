f118eed3571743313f085c84e22e658451c66f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:20 +0000

    Add package feature-fabrica/1.1.0/feature_fabrica-1.1.0.tar.gz

---
+++ packages/feature-fabrica/feature_fabrica-1.1.0.tar.gz/feature_fabrica-1.1.0/feature_fabrica/models.py
@@ -0,0 +1,133 @@
+# models.py
+import hashlib
+import re
+from typing import Any, Optional
+
+import numpy as np
+from pydantic import BaseModel, ConfigDict, Field, root_validator, validator
+
+
+class PromiseValue:
+    value: np.ndarray | None = None
+
+
+class FeatureSpec(BaseModel):
+    description: str
+    data_type: str
+    dependencies: list[str] | None = Field(default_factory=list)
+    transformation: dict | None = Field(default_factory=dict)
+
+    @validator("data_type")
+    def validate_data_type(cls, v):
+        try:
+            # Check if the data_type is a valid type
+            if not (getattr(np, v, None)):
+                raise ValueError(
+                    f"Invalid data_type specified: {v}, it should be in numpy"
+                )
+        except Exception as e:
+            raise ValueError(f"Invalid data_type: {v}, error: {str(e)}")
+        return v
+
+
+class FeatureValue(BaseModel, validate_assignment=True):  # type: ignore[call-arg]
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    value: np.ndarray | PromiseValue = Field(default=None)
+    data_type: str
+
+    @root_validator(pre=True)
+    def validate_value(cls, values):
+        v = values.get("value")
+        data_type = values.get("data_type")
+
+        # allow PromiseValue only
+        if isinstance(v, PromiseValue):
+            return values
+        # Get the expected data type from builtins or numpy
+        expected_type = getattr(np, data_type, None)
+
+        if expected_type is None:
+            raise ValueError(f"Unsupported data type '{data_type}', use numpy typing!")
+
+        # Validate that the array has the correct data type
+        actual_type_name = v.dtype.name
+        actual_type, actual_precision = re.findall(r'\D+|\d+', actual_type_name)
+
+        expected_type_list = re.findall(r'\D+|\d+', data_type)
+
+        if actual_type not in data_type:
+            raise ValueError(
+                f"Array dtype '{v.dtype}' does not match expected type '{data_type}'"
+            )
+        elif len(expected_type_list) > 1 and actual_precision != expected_type_list[1]:
+            # convert to desired type if compatible
+            v = v.astype(expected_type)
+
+        # Update the value in the values dictionary
+        values["value"] = v
+        return values
+
+    def __add__(self, other):
+        if isinstance(other, FeatureValue):
+            return self.value + other.value
+        return self.value + other
+
+    def __sub__(self, other):
+        if isinstance(other, FeatureValue):
+            return self.value - other.value
+        return self.value - other
+
+    def __mul__(self, other):
+        if isinstance(other, FeatureValue):
+            return self.value * other.value
+        return self.value * other
+
+    def __truediv__(self, other):
+        if isinstance(other, FeatureValue):
+            return self.value / other.value
+        return self.value / other
+
+    def __getattr__(self, name):
+        # Delegate attribute access to the `value` attribute
+        return getattr(self.value, name)
+
+    def __repr__(self):
+        return f"FeatureValue(value={self.value})"
+
+
+class TNode(BaseModel):
+    transformation_name: str
+    start_time: float
+    end_time: float
+    shape: tuple | None = None
+    time_taken: float | None = None
+    output_hash: str | None = None
+    next: Optional["TNode"] = None  # Forward reference
+
+    def to_dict(self) -> dict[str, Any]:
+        # Convert the node and its next nodes to a dictionary
+        node_dict = {
+            "value": self.value,
+            "transformation_name": self.transformation_name,
+        }
+        if self.next:
+            node_dict["next"] = self.next.to_dict()
+        else:
+            node_dict["next"] = None
+        return node_dict
+
+    def compute_hash(self, data: np.ndarray) -> str:
+        data_bytes = data.tobytes()
+        return hashlib.sha256(data_bytes).hexdigest()
+
+    def store_hash_and_shape(self, output_data: np.ndarray):
+        self.shape = output_data.shape
+        self.output_hash = self.compute_hash(output_data)
+
+    def finilize_metrics(self) -> None:
+        self.time_taken = self.end_time - self.start_time
+
+
+class THead(BaseModel):
+    next: TNode | None = None
---
+++ packages/feature-fabrica/feature_fabrica-1.1.0.tar.gz/feature_fabrica-1.1.0/feature_fabrica/utils.py
@@ -0,0 +1,59 @@
+# utils.py
+import sys
+
+from beartype import beartype
+from loguru import logger
+
+from feature_fabrica.exceptions import CyclicDependencyError
+
+logger_set = False
+
+@beartype
+def setup_logger(
+    log_format: str = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+                      "<level>{level: <8}</level> | "
+                      "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+                      "<level>{message}</level>",
+    log_level: str = "DEBUG",
+    log_file: str | None = None,
+    log_rotation: str | None = None,
+    log_retention: str | None = None,
+    log_compression: str | None = None,
+    colorize_stdout: bool = True
+):
+    global logger_set
+
+    # Remove the default logger
+    logger.remove()
+
+    # Add stdout with formatting and color
+    logger.add(sys.stdout, format=log_format, level=log_level, colorize=colorize_stdout)
+
+    # Add file handler if log_file is provided
+    if log_file:
+        logger.add(
+            log_file,
+            format=log_format.replace('<level>', '').replace('</level>', ''),
+            level=log_level,
+            rotation=log_rotation,
+            retention=log_retention,
+            compression=log_compression,
+        )
+
+    logger_set = True
+
+
+def get_logger(**kwargs):
+    if not logger_set:
+        setup_logger(**kwargs)
+    return logger
+
+
+def verify_dependencies(dependencies_count: dict[str, int]):
+    logger = get_logger()
+    if 0 in dependencies_count.values():
+        loop_features = [f_name for f_name, c in dependencies_count.items() if c == 0]
+        logger.debug(
+            f"Cyclic dependency detected! The following features form a cycle: {loop_features}"
+        )
+        raise CyclicDependencyError(loop_features)
