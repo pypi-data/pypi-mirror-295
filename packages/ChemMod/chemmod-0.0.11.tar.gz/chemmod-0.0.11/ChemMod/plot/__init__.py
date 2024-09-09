86310faa384c83a0c85e8ce1fe3629b38256648d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 05:03:24 +0000

    Add package ChemMod/0.0.11/chemmod-0.0.11.tar.gz

---
+++ packages/ChemMod/chemmod-0.0.11.tar.gz/chemmod-0.0.11/ChemMod/plot/__init__.py
@@ -0,0 +1,15 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+
+from ChemMod.plot import __all__
+from ChemMod.equation import __all__
+
+theme_list_for_formatting_of_plots = ['teal', 1, 'teal', 'teal', 'viridis', 'teal', 'teal', 'paleturquoise', 'lightcyan', 'paleturquoise']
