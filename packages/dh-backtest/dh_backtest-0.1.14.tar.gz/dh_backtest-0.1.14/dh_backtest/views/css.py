da137816b1a3da5d8a4176bb8c04565b9fdfb59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:46 +0000

    Add package dh-backtest/0.1.14/dh_backtest-0.1.14.tar.gz

---
+++ packages/dh-backtest/dh_backtest-0.1.14.tar.gz/dh_backtest-0.1.14/dh_backtest/views/css.py
@@ -0,0 +1,52 @@
+style_root_div = {
+    'backgroundColor': '#D0B8A8', 
+    'width': '100%', 
+    'display': 'flex', 
+    'flex-direction': 'column', 
+    'justify-content': 'space-between', 
+    # 'border': '1px solid red', 
+}
+
+style_header = {
+    'backgroundColor': '#D0B8A8', 
+    'height': '60px',
+    'width': '100%',
+    'max-height': '10vh',
+    'justify-content': 'center', 
+    'fontSize': 30, 
+    'fontfamily': 'Arial', 
+    'margin': 'auto', 
+    'padding': '10px',
+    # 'border': '1px solid blue', 
+}
+
+style_body = {
+    'backgroundColor': '#DFD3C3', 
+    'height': '90vh',
+    'min-height': '800px',
+    'width': '100%', 
+    # 'border': '1px solid blue', 
+    'display': 'flex', 
+    'justify-content': 'space-around'
+}
+
+style_body_sub_div = {
+    'backgroundColor': '#F8EDE3', 
+    'display': 'inline-block', 
+    'height': '80vh',
+    'width': '35%', 
+    'border-radius': '10px',
+    'margin-top': '10px',
+    'margin-left': 'auto', 
+    'margin-right': 'auto',
+    # 'overflow-x': 'hidden',
+    # 'overflow-y': 'scroll',
+
+}
+
+style_element = {
+    'border-radius': '5px',
+    'padding': '10px',
+    'width': '100%',
+    'margin-top': '10px',
+}
