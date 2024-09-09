f8ec22e85b8b384a3c1947cef464af77dd050dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:01 +0000

    Add package scrapeanything/0.9.223/scrapeanything-0.9.223.tar.gz

---
+++ packages/scrapeanything/scrapeanything-0.9.223.tar.gz/scrapeanything-0.9.223/src/scrapeanything/utils/api_service.py
@@ -0,0 +1,23 @@
+from scrapeanything.utils.config import Config
+from scrapeanything.database.repository import Repository
+from scrapeanything.utils.requests import Methods
+from scrapeanything.utils.requests import ResponseTypes
+from scrapeanything.utils.constants import *
+import requests
+
+class ApiService:
+
+    def wget(self, url: str, parameters: dict=None, method: Methods=Methods.GET, response_type: ResponseTypes=ResponseTypes.JSON) -> any:
+        if method == Methods.GET:
+            response = requests.get(url=url, data=parameters)
+        elif method == Methods.POST:
+            response = requests.post(url=url, data=parameters)
+        else:
+            raise Exception(f'{method} method is not supported')
+
+        if response_type == ResponseTypes.JSON:
+            return response.json()
+        elif response_type == ResponseTypes.TEXT:
+            return response.text
+        else:
+            raise Exception(f'{response_type} response type is not supported')
