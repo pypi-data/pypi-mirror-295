03323876a6e2fd51f2968e81a686322478ca763c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:30 +0000

    Add package py-data-validator/0.1.4/py_data_validator-0.1.4.tar.gz

---
+++ packages/py-data-validator/py_data_validator-0.1.4.tar.gz/py_data_validator-0.1.4/py_data_validator/rules/nullable.py
@@ -0,0 +1,11 @@
+from py_data_validator.rule import Rule
+
+
+class Nullable(Rule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def validate(self):
+        self.require_params_count(0)
+
+        return True
