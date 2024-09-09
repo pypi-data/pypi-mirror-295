31944f6ff52c3f8157def1e7110490be7293d07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:16 +0000

    Add package lanscape/0.0.12/lanscape-0.0.12.tar.gz

---
+++ packages/lanscape/lanscape-0.0.12.tar.gz/lanscape-0.0.12/src/lanscape/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Import Blueprints
+from .blueprints.api import api_bp
+from .blueprints.web import web_bp
+
+# Register Blueprints
+app.register_blueprint(api_bp)
+app.register_blueprint(web_bp)
+
+    
+# Custom Jinja filter
+def is_substring_in_values(results: dict, substring: str) -> bool:
+    return any(substring.lower() in str(v).lower() for v in results.values()) if substring else True
+
+app.jinja_env.filters['is_substring_in_values'] = is_substring_in_values
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
