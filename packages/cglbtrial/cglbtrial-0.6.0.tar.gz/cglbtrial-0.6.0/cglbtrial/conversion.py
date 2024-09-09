06d2385446b8c41d648c67685dd2459e14d07e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:57 +0000

    Add package cglbtrial/0.6.0/cglbtrial-0.6.0.tar.gz

---
+++ packages/cglbtrial/cglbtrial-0.6.0.tar.gz/cglbtrial-0.6.0/cglbtrial/conversion.py
@@ -0,0 +1,12 @@
+
+
+def fahrenheit_2_kelvin(fahrenheit):
+    '''
+    takes a temperature `temp` in fahrenheit and returns it in Kelvin
+    '''
+
+    kelvin = 5./9. * (fahrenheit - 32.) + 273.15
+
+    return kelvin
+
+
