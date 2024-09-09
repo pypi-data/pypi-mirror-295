a167847c5a83a0195ed0fe3490c64d94198483d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:34 +0000

    Add package zid/1.1.0/zid-1.1.0-cp38-abi3-musllinux_1_2_aarch64.whl

---
+++ packages/zid/zid-1.1.0-cp38-abi3-musllinux_1_2_aarch64.whl/zid/_lib.pyi
@@ -0,0 +1,16 @@
+def zid() -> int:
+    """
+    Generate a unique identifier.
+    """
+
+def parse_zid_timestamp(zid: int) -> int:
+    """
+    Extract the UNIX timestamp in milliseconds from a ZID.
+    """
+
+def set_random_buffer_size(size: int) -> None:
+    """
+    Set the size of the buffer when requesting random bytes from the operating system.
+
+    Default: 128 KiB
+    """
