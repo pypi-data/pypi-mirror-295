62f4aa30d2fd7c47d5cd1728d2e366a43309c73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:14 +0000

    Add package nb-service/3.0.1/nb_service-3.0.1-py3-none-any.whl

---
+++ packages/nb-service/nb_service-3.0.1-py3-none-any.whl/nb_service/navigation.py
@@ -0,0 +1,15 @@
+from netbox.plugins import PluginMenuItem
+
+
+menu_items = (
+    PluginMenuItem(
+        permissions=["nb_service.view_service"],
+        link="plugins:nb_service:service_list",
+        link_text="Services",
+    ),
+    PluginMenuItem(
+        permissions=["nb_service.view_application"],
+        link="plugins:nb_service:application_list",
+        link_text="Applications",
+    ),
+)
