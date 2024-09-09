fffe5134ae53b868224b282cb5f2e06ebf0ebca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:48 +0000

    Add package rahhscidow/1.0/rahhscidow-1.0.tar.gz

---
+++ packages/rahhscidow/rahhscidow-1.0.tar.gz/rahhscidow-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='rahhscidow',  # Your package name
+    version='1.0',
+    description='A Scihub paper downloader.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Use 'text/markdown' if you're using Markdown
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[],  # Add your dependencies here
+    # other setup parameters
+)
