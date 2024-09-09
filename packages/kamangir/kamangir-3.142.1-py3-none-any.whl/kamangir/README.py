3ef9eeb6ce5bf52acd606a39eec57742a7f26e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:30 +0000

    Add package kamangir/3.142.1/kamangir-3.142.1-py3-none-any.whl

---
+++ packages/kamangir/kamangir-3.142.1-py3-none-any.whl/kamangir/README.py
@@ -0,0 +1,43 @@
+import os
+from blueness import module
+from abcli import file
+from abcli.plugins.README import build as build_README
+from kamangir import NAME, VERSION, REPO_NAME, ICON
+from kamangir.content import content
+from kamangir.logger import logger
+
+MY_NAME = module.name(__file__, NAME)
+
+
+def build():
+    logger.info(
+        "{}.build {} item(s): {}".format(
+            MY_NAME,
+            len(content["items"]),
+            ", ".join(list(content["items"].keys())),
+        )
+    )
+
+    items = [
+        "{}[`{}`]({}) [![image]({})]({}) {} {}".format(
+            item["icon"],
+            item["name"].replace("_", "-"),
+            f"https://github.com/kamangir/{name}",
+            item["image"],
+            f"https://github.com/kamangir/{name}",
+            item["description"],
+            item["pypi"],
+        )
+        for name, item in content["items"].items()
+        if name != "template"
+    ]
+
+    return build_README(
+        items=items,
+        cols=content["cols"],
+        path=os.path.join(file.path(__file__), ".."),
+        ICON=ICON,
+        NAME=NAME,
+        VERSION=VERSION,
+        REPO_NAME=REPO_NAME,
+    )
---
+++ packages/kamangir/kamangir-3.142.1-py3-none-any.whl/kamangir/logger.py
@@ -0,0 +1,4 @@
+from abcli.logger import get_logger
+from kamangir import ICON
+
+logger = get_logger(ICON)
