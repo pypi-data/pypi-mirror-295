99d00d02930e05d26b91e998801539c20a4392a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:56 +0000

    Add package rage-toolkit/0.0.1/rage_toolkit-0.0.1.tar.gz

---
+++ packages/rage-toolkit/rage_toolkit-0.0.1.tar.gz/rage_toolkit-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='rage_toolkit',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    author='Vinzent Penzkofer, Timo Baumann',
+    author_email='vinzent.penzkofer@outlook.de, timo.baumann@oth-regensburg.de',
+    description='A framework for retrieval augmented generation evaluation (RAGE)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/othr-nlp/rage_toolkit',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords='retrieval, augmented generation, evaluation, RAG, NLP',
+    project_urls={
+        'Documentation': 'https://github.com/othr-nlp/rage_toolkit',
+    },
+    include_package_data=True,
+)
