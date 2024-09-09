bc770ca4c5110e8e86299fdfa9b6e09aefee9264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 05:03:29 +0000

    Add package ONTraC/2.0rc5/ontrac-2.0rc5.tar.gz

---
+++ packages/ONTraC/ontrac-2.0rc5.tar.gz/ontrac-2.0rc5/src/ONTraC/bin/ONTraC_analysis.py
@@ -0,0 +1,186 @@
+import os
+import sys
+from optparse import OptionGroup, OptionParser, Values
+
+from ..analysis.cell_type import cell_type_visualization
+from ..analysis.data import AnaData
+from ..analysis.niche_cluster import niche_cluster_visualization
+from ..analysis.niche_net import embedding_adjust_visualization
+from ..analysis.spatial import spatial_visualization
+from ..analysis.train_loss import train_loss_visualiztion
+from ..log import *
+from ..optparser._IO import add_IO_options_group, validate_io_options, write_io_options_memo
+from ..utils import *
+from ..version import __version__
+
+# ------------------------------------
+# Constants
+# ------------------------------------
+IO_OPTIONS = ['input', 'preprocessing_dir', 'GNN_dir', 'NTScore_dir']
+
+
+# ------------------------------------
+# Functions
+# ------------------------------------
+def analysis_pipeline(options: Values) -> None:
+    # 0. load data class
+    ana_data = AnaData(options)
+
+    # part 1: niche network construction
+    embedding_adjust_visualization(ana_data=ana_data)
+
+    # part 2: train loss
+    train_loss_visualiztion(ana_data=ana_data)
+
+    # part 3: spatial based output
+    spatial_visualization(ana_data=ana_data)
+
+    # part 4: niche cluster
+    niche_cluster_visualization(ana_data=ana_data)
+
+    # part 5: cell type based output
+    if not options.suppress_cell_type:
+        cell_type_visualization(ana_data=ana_data)
+
+
+# TODO: move to optparser
+def add_suppress_group(optparser: OptionParser) -> None:
+    group = OptionGroup(optparser, 'Suppress options')
+    group.add_option('--suppress-cell-type-composition',
+                     dest='suppress_cell_type_composition',
+                     action='store_true',
+                     default=False,
+                     help='Suppress the cell type composition visualization.')
+    group.add_option('--suppress-niche-cluster-loadings',
+                     dest='suppress_niche_cluster_loadings',
+                     action='store_true',
+                     default=False,
+                     help='Suppress the niche cluster loadings visualization.')
+    group.add_option('--suppress-cell-type',
+                     dest='suppress_cell_type',
+                     action='store_true',
+                     default=False,
+                     help='Suppress the cell type visualization.')
+    group.add_option('--suppress-niche-trajectory',
+                        dest='suppress_niche_trajectory',
+                        action='store_true',
+                        default=False,
+                        help='Suppress the niche trajectory related visualization.')
+    optparser.add_option_group(group)
+
+
+def add_embedding_adjust_group(optparser: OptionParser) -> None:
+    group = OptionGroup(optparser, 'Embedding adjust options')
+    group.add_option(
+        '--embedding-adjust',
+        dest='embedding_adjust',
+        action='store_true',
+        default=False,
+        help=
+        'Adjust the cell type coding according to embeddings. Default is False. At least two (Embedding_1 and Embedding_2) should be in the original data if embedding_adjust is True.'
+    )
+    group.add_option(
+        '--sigma',
+        dest='sigma',
+        type='float',
+        default=1,
+        help=
+        'Sigma for the exponential function to control the similarity between different cell types or clusters. Default is 1.'
+    )
+    optparser.add_option_group(group)
+
+
+def prepare_optparser() -> OptionParser:
+    """Prepare optparser object. New options will be added in this function first.
+
+    Ret: OptParser object.
+    """
+    usage = "USAGE: %prog <-d DATASET> <--preprocessing-dir PREPROCESSING_DIR> <--GNN-dir GNN_DIR> <--NTScore-dir NTSCORE_DIR> <-o OUTPUT_DIR> [-l LOG_FILE] [-r REVERSE]"
+    description = "Analysis the results of ONTraC."
+    optparser = OptionParser(version=f'%prog {__version__}',
+                             usage=usage,
+                             description=description,
+                             add_help_option=False)
+    optparser.add_option('-h', '--help', action='help', help='Show this help message and exit.')
+    add_IO_options_group(optparser=optparser, io_options=IO_OPTIONS)
+    optparser.add_option('-l', '--log', dest='log', type='string', help='Log file.')
+    optparser.add_option('-o', '--output', dest='output', type='string', help='Output directory.')
+    optparser.add_option('-r',
+                         '--reverse',
+                         dest='reverse',
+                         action='store_true',
+                         default=False,
+                         help='Reverse the NT score.')
+    optparser.add_option('-s',
+                         '--sample',
+                         dest='sample',
+                         action='store_true',
+                         default=False,
+                         help='Plot each sample separately.')
+    add_embedding_adjust_group(optparser)
+    add_suppress_group(optparser)
+    return optparser
+
+
+def opt_validate(optparser: OptionParser) -> Values:
+    """Validate options from a OptParser object.
+
+    Args:
+        optparser: OptParser object.
+    """
+    (options, args) = optparser.parse_args()
+
+    validate_io_options(optparser, options, IO_OPTIONS, overwrite_validation=False)
+
+    if not options.output:
+        error('Output directory is required.')
+        sys.exit(1)
+    if not os.path.isdir(options.output):
+        info(f'Output directory not found: {options.output}, will create it.')
+        os.makedirs(options.output)
+    else:
+        warning(f'Output directory already exists: {options.output}, will overwrite it.')
+
+    if not os.path.exists(options.log):
+        error(f'Log file not found: {options.log}')
+        sys.exit(1)
+
+    options.yaml = f'{options.preprocessing_dir}/samples.yaml'
+    if not os.path.exists(options.yaml):
+        error(f'File not found: {options.yaml}')
+        sys.exit(1)
+    options.device = 'cpu'
+
+    info('------------------ RUN params memo ------------------ ')
+    # print parameters to stdout
+    write_io_options_memo(options, IO_OPTIONS)
+    info(f'Output directory: {options.output}')
+    info(f'Log file: {options.log}')
+    info(f'Reverse: {options.reverse}')
+    info(f'Sample: {options.sample}')
+    info(f'Embedding adjust: {options.embedding_adjust}')
+    if options.embedding_adjust:
+        info(f'Sigma: {options.sigma}')
+    if hasattr(options, 'suppress_cell_type_composition'):
+        info(f'Suppress cell type composition: {options.suppress_cell_type_composition}')
+    if hasattr(options, 'suppress_niche_cluster_loadings'):
+        info(f'Suppress niche cluster loadings: {options.suppress_niche_cluster_loadings}')
+    if hasattr(options, 'suppress_niche_trajectory'):
+        info(f'Suppress niche trajectory: {options.suppress_niche_trajectory}')
+
+    return options
+
+
+def main():
+
+    # write version information
+    write_version_info()
+
+    optparser = prepare_optparser()
+    options = opt_validate(optparser)
+
+    analysis_pipeline(options)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ONTraC/ontrac-2.0rc5.tar.gz/ontrac-2.0rc5/src/ONTraC/optparser/_IO.py
@@ -0,0 +1,232 @@
+import os
+import sys
+from optparse import OptionGroup, OptionParser, Values
+from typing import List, Optional
+
+from ..log import *
+
+
+def add_IO_options_group(optparser: OptionParser, io_options: Optional[List[str]]) -> None:
+    """
+    Add I/O options group to optparser.
+    :param optparser: OptionParser object.
+    :param io_options: List of I/O options.
+    :return: OptionGroup object.
+    """
+    if io_options is None:
+        return
+    # I/O options group
+    group_io = OptionGroup(optparser, "IO")
+    if 'input' in io_options:
+        group_io.add_option(  # TODO: deprecated warning
+            '-d',
+            '--dataset',
+            dest='dataset',
+            type='string',
+            help='Meta data file in csv format. Each row is a cell and each column is a meta data. The first column should be the cell name. Coordinates (x, y), cell type and sample should be included.'
+        )
+        group_io.add_option(
+            '--meta-input',
+            dest='meta_input',
+            type='string',
+            help=
+            'Meta data file in csv format. Each row is a cell and each column is a meta data. The first column should be the cell name. Coordinates (x, y) and sample should be included. Cell type is optional.'
+        )
+        group_io.add_option(
+            '--exp-input',
+            dest='exp_input',
+            type='string',
+            help=
+            'Normalized expression file in csv format. Each row is a cell and each column is a gene. The first column should be the cell name. If not provided, cell type should be included in the meta data file.'
+        )
+        group_io.add_option('--embedding-input',
+                            dest='embedding_input',
+                            type='string',
+                            help='Embedding file in csv format. The first column should be the cell name.')
+        group_io.add_option(
+            '--decomposition-cell-type-composition-input',
+            dest='decomposition_cell_type_composition_input',
+            type='string',
+            help=
+            'Decomposition outputed cell type compostion of each spot in csv format. The first column should be the spot name.'
+        )
+        group_io.add_option(
+            '--decomposition-expression-input',
+            dest='decomposition_expression_input',
+            type='string',
+            help=
+            'Decomposition outputed expression of each cell type in csv format. The first column should be the cell type name corresponding to the columns name of decomposition outputed cell type compostion.'
+        )
+    if 'preprocessing_dir' in io_options:
+        group_io.add_option('--preprocessing-dir',
+                            dest='preprocessing_dir',
+                            type='string',
+                            help='Directory for preprocessing outputs.')
+    if 'GNN_dir' in io_options:
+        group_io.add_option('--GNN-dir', dest='GNN_dir', type='string', help='Directory for the GNN output.')
+    if 'NTScore_dir' in io_options:
+        group_io.add_option('--NTScore-dir',
+                            dest='NTScore_dir',
+                            type='string',
+                            help='Directory for the NTScore output.')
+
+    optparser.add_option_group(group_io)
+
+
+def validate_io_options(optparser: OptionParser,
+                        options: Values,
+                        io_options: Optional[List[str]],
+                        overwrite_validation: bool = True) -> None:
+    """Validate IO options from a OptParser object.
+    :param optparser: OptionParser object.
+    :param options: Options object.
+    :param io_options: List of I/O options.
+    :param overwrite_validation: Overwrite validation flag.
+    :return: None
+    """
+    if io_options is None:
+        return
+    if 'input' in io_options:
+        # dataset
+        if options.dataset and not options.meta_input:
+            warning(FutureWarning('The -d/--dataset option is deprecated. Please use --meta-input instead.').__str__())
+            options.meta_input = options.dataset
+        # meta data
+        if not options.meta_input:
+            error('Please provide a meta data file in csv format.')
+            optparser.print_help()
+            sys.exit(1)
+        if not os.path.isfile(options.meta_input):
+            error(f'The meta data file ({options.meta_input}) you given does not exist.')
+            optparser.print_help()
+            sys.exit(1)
+        if not options.meta_input.endswith(('csv', 'csv.gz')):
+            error(f'The meta data file ({options.meta_input}) should be in csv format.')
+            optparser.print_help()
+            sys.exit(1)
+        # expression data
+        if options.exp_input:
+            if not os.path.isfile(options.exp_input):
+                error(f'The expression data file ({options.exp_input}) you given does not exist.')
+                optparser.print_help()
+                sys.exit(1)
+            if not options.exp_input.endswith(('csv', 'csv.gz')):
+                error(f'The expression data file ({options.exp_input}) should be in csv format.')
+                optparser.print_help()
+                sys.exit(1)
+        # embedding
+        if options.embedding_input:
+            if not os.path.isfile(options.embedding_input):
+                error(f'The embedding file ({options.embedding_input}) you given does not exist.')
+                optparser.print_help()
+                sys.exit(1)
+            if not options.embedding_input.endswith(('csv', 'csv.gz')):
+                error(f'The embedding file ({options.embedding_input}) should be in csv format.')
+                optparser.print_help()
+                sys.exit(1)
+        # decomposition
+        # two decomposition input files should be provided together
+        if options.decomposition_cell_type_composition_input and not options.decomposition_expression_input:
+            error('Please provide both decomposition cell type compostion file and decomposition expression file.')
+            optparser.print_help()
+            sys.exit(1)
+        if not options.decomposition_cell_type_composition_input and options.decomposition_expression_input:
+            error('Please provide both decomposition cell type compostion file and decomposition expression file.')
+            optparser.print_help()
+            sys.exit(1)
+        # check decomposition input files
+        if options.decomposition_cell_type_composition_input:
+            if not os.path.isfile(options.decomposition_cell_type_composition_input):
+                error(
+                    f'The decomposition outputed cell type compostion file ({options.decomposition_cell_type_composition_input}) you given does not exist.'
+                )
+                optparser.print_help()
+                sys.exit(1)
+            if not options.decomposition_cell_type_composition_input.endswith(('csv', 'csv.gz')):
+                error(
+                    f'The decomposition outputed cell type compostion file ({options.decomposition_cell_type_composition_input}) should be in csv format.'
+                )
+                optparser.print_help()
+                sys.exit(1)
+        if options.decomposition_expression_input:
+            if not os.path.isfile(options.decomposition_expression_input):
+                error(
+                    f'The decomposition outputed expression file ({options.decomposition_expression_input}) you given does not exist.'
+                )
+                optparser.print_help()
+                sys.exit(1)
+            if not options.decomposition_expression_input.endswith(('csv', 'csv.gz')):
+                error(
+                    f'The decomposition outputed expression file ({options.decomposition_expression_input}) should be in csv format.'
+                )
+                optparser.print_help()
+
+    if 'preprocessing_dir' in io_options:
+        if not options.preprocessing_dir:
+            error('Please provide a directory for preprocessing outputs.')
+            optparser.print_help()
+            sys.exit(1)
+        if os.path.isdir(options.preprocessing_dir):
+            if overwrite_validation:
+                warning(
+                    f'The directory ({options.preprocessing_dir}) you given already exists. It will be overwritten.')
+            else:
+                pass
+        else:
+            info(f'Creating directory: {options.preprocessing_dir}')
+            os.makedirs(options.preprocessing_dir, exist_ok=True)
+
+    if 'GNN_dir' in io_options:
+        if not options.GNN_dir:
+            error('Please provide a directory for the GNN output.')
+            optparser.print_help()
+            sys.exit(1)
+        if os.path.isdir(options.GNN_dir):
+            if overwrite_validation:
+                warning(f'The directory ({options.GNN_dir}) you given already exists. It will be overwritten.')
+            else:
+                pass
+        else:
+            info(f'Creating directory: {options.GNN_dir}')
+            os.makedirs(options.GNN_dir, exist_ok=True)
+
+    if 'NTScore_dir' in io_options:
+        if not options.NTScore_dir:
+            error('Please provide a directory for the NTScore output.')
+            optparser.print_help()
+            sys.exit(1)
+        if os.path.isdir(options.NTScore_dir):
+            if overwrite_validation:
+                warning(f'The directory ({options.NTScore_dir}) you given already exists. It will be overwritten.')
+            else:
+                pass
+        else:
+            info(f'Creating directory: {options.NTScore_dir}')
+            os.makedirs(options.NTScore_dir, exist_ok=True)
+
+
+def write_io_options_memo(options: Values, io_options: Optional[List[str]]) -> None:
+    """Write IO options to stdout.
+    :param options: Options object.
+    :param io_options: List of I/O options.
+    :return: None
+    """
+    if io_options is None:
+        return
+    info('            -------- I/O options -------             ')
+    if 'input' in io_options:
+        info(f'meta data file:  {options.meta_input}')
+        if options.exp_input:
+            info(f'expression data file:  {options.exp_input}')
+        if options.embedding_input:
+            info(f'embedding file:  {options.embedding_input}')
+        if options.decomposition_cell_type_composition_input:
+            info(f'decomposition cell type compostion file:  {options.decomposition_cell_type_composition_input}')
+        if options.decomposition_expression_input:
+            info(f'decomposition expression file:  {options.decomposition_expression_input}')
+    if 'preprocessing_dir' in io_options:
+        info(f'preprocessing output directory:  {options.preprocessing_dir}')
+    if 'GNN_dir' in io_options:
+        info(f'GNN output directory:  {options.GNN_dir}')
+    if 'NTScore_dir' in io_options:
+        info(f'NTScore output directory:  {options.NTScore_dir}')
