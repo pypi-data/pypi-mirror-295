aabb018c1bba97c4744d290f35cbda595ea021d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 16:03:24 +0000

    Add package mns-common/1.2.4.3/mns_common-1.2.4.3-py3-none-any.whl

---
+++ packages/mns-common/mns_common-1.2.4.3-py3-none-any.whl/mns_common/constant/self_choose_constant.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 16
+project_path = file_path[0:end]
+sys.path.append(project_path)
+
+# 同花顺概念
+SELF_CHOOSE_THS_CONCEPT = 'ths_concept'
+# 同花顺行业
+SELF_CHOOSE_THS_INDUSTRY = 'ths_industry'
+
+SELF_CHOOSE_KPL_CONCEPT = 'kpl_concept'
+
+# 开盘啦一级概念
+SELF_CHOOSE_KPL_FIRST_CONCEPT = 'kpl_first_concept'
+
+# 开盘啦二级概念
+SELF_CHOOSE_KPL_SECOND_CONCEPT = 'kpl_second_concept'
+
+# 个股打板自选
+SELF_CHOOSE_SYMBOL_DA_BAN = 'da_ban'
+
+# 个股普通自选
+SELF_CHOOSE_SYMBOL_DA_BAN = 'normal'
