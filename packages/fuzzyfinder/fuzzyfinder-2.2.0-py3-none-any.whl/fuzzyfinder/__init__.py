dacbf6c249dbfd20323c866652d96d2a820312be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:01 +0000

    Add package fuzzyfinder/2.2.0/fuzzyfinder-2.2.0-py3-none-any.whl

---
+++ packages/fuzzyfinder/fuzzyfinder-2.2.0-py3-none-any.whl/fuzzyfinder/__init__.py
@@ -0,0 +1,15 @@
+# ruff: noqa
+
+__version__ = "2.2.0"
+
+__all__ = []
+
+
+def export(defn):
+    """Decorator to explicitly mark functions that are exposed in a lib."""
+    globals()[defn.__name__] = defn
+    __all__.append(defn.__name__)
+    return defn
+
+
+from . import main
