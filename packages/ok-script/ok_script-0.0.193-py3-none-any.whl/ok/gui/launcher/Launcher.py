e9eb4c57d13f7daddb7a426e66db2efed53b4bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:41 +0000

    Add package ok-script/0.0.193/ok_script-0.0.193-py3-none-any.whl

---
+++ packages/ok-script/ok_script-0.0.193-py3-none-any.whl/ok/gui/launcher/Launcher.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from ok.gui.launcher.LauncherWindow import LauncherWindow
+from ok.gui.util.app import init_app_config, center_window
+from ok.logging.Logger import get_logger, config_logger
+from ok.update.GitUpdater import GitUpdater
+from ok.util.exit_event import ExitEvent
+
+logger = get_logger(__name__)
+
+
+class Launcher:
+
+    def __init__(self, config):
+        self.app = None
+        self.locale = None
+        self.config = config
+        self.exit_event = ExitEvent()
+        config_logger(self.config, name='launcher')
+        self.app, self.locale = init_app_config()
+        self.updater = GitUpdater(config, self.exit_event)
+        self.updater.list_all_versions()
+
+    def start(self):
+        try:
+            logger.info(f'launcher start pid {os.getpid()} {self.locale}')
+            w = LauncherWindow(self.config, self.updater, self.exit_event)
+            center_window(self.app, w)
+            w.show()
+            self.app.exec_()
+        except Exception as e:
+            logger.error('launcher start error', e)
+            self.exit_event.set()
+            sys.exit(0)
