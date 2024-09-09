d7b2bb9090e7ac613a4945cf0f7201757272e448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:41 +0000

    Add package idatagate/0.0.2/idatagate-0.0.2-py3-none-any.whl

---
+++ packages/idatagate/idatagate-0.0.2-py3-none-any.whl/idatagate/cli.py
@@ -0,0 +1,117 @@
+import logging
+import os
+import shutil
+from typing import Optional
+
+import typer
+
+from idatagate.collect.collect import collect_data
+from idatagate.collect.google.key import initialize_service
+from idatagate.collect.google.storage import (
+    download_all_files,
+    download_file,
+    list_files,
+    upload_file,
+)
+from idatagate.process.dataset import process_all
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+logger = logging.getLogger("htt")
+
+app = typer.Typer()
+
+DEFAULT_KEY = None
+
+key_opt = typer.Option(
+    DEFAULT_KEY, "--key", help="Encryption key for the service account"
+)
+
+
+@app.command()
+def collect(
+    draw: bool = typer.Option(
+        True, "--draw/--no-draw", help="Draw hand landmarks connections"
+    ),
+    zip: bool = typer.Option(True, "--zip/--no-zip", help="Get output as zip"),
+    clean: bool = typer.Option(
+        False, "--clean/--no-clean", help="Remove images dir but keep only zip"
+    ),
+    data_dir: str = typer.Option(
+        "/tmp/data", "--data-dir", "-d", help="Directory to store images"
+    ),
+    classes_number: int = typer.Option(
+        26, "--classes-number", "-cn", help="Number of classes"
+    ),
+    samples_number: int = typer.Option(
+        100, "--samples-number", "-sn", help="Number of images per class"
+    ),
+    capture_device: int = typer.Option(0, "--capture-device", help="Camera device ID"),
+):
+    """Take hand images."""
+    collect_data(data_dir, classes_number, samples_number, capture_device, draw)
+    if zip:
+        shutil.make_archive("output", "zip", data_dir)
+    if clean:
+        shutil.rmtree(data_dir)
+
+
+@app.command()
+def process(
+    data_dir: str = typer.Option(
+        "./data", "--data-dir", "-d", help="Directory with images"
+    ),
+    output: str = typer.Option(
+        "data.pickle", "--output", "-o", help="Output pickle file"
+    ),
+):
+    """Transform images into dataset pickle."""
+    process_all(data_dir)
+
+
+@app.command()
+def upload(
+    file: str = typer.Argument(
+        "output.zip",
+        help="Path to zip file to upload to Google Drive",
+    ),
+    key: Optional[str] = key_opt,
+):
+    """Upload files to Google Storage."""
+    if os.path.exists(file):
+        service = initialize_service(key)
+        upload_file(file, service)
+    else:
+        logging.info(f"File {file} does not exist")
+
+
+@app.command()
+def list(
+    key: Optional[str] = key_opt,
+):
+    """List files in Google Storage."""
+    service = initialize_service(key)
+    list_files(service)
+
+
+@app.command()
+def download(
+    file_id: Optional[str] = typer.Option(
+        None, "--file-id", help="File ID to download"
+    ),
+    key: Optional[str] = key_opt,
+):
+    """Download files from Google Drive."""
+    service = initialize_service(key)
+    if file_id:
+        download_file(file_id, f"./{file_id}.zip", service)
+    else:
+        download_all_files("./dataset.zip", service)
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/idatagate/idatagate-0.0.2-py3-none-any.whl/idatagate/process/dataset.py
@@ -0,0 +1,63 @@
+import os
+import pickle
+import random
+from pathlib import Path
+
+import cv2
+from mediapipe.python.solutions.hands import Hands
+
+
+def process_image(hands: Hands, img_path: str):
+    """Process a single image and extract hand landmarks."""
+    data_aux = []
+    x_, y_ = [], []
+    img = cv2.imread(img_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark:
+                x_.append(landmark.x)
+                y_.append(landmark.y)
+
+            # Normalize landmarks to the top-left corner
+            for landmark in hand_landmarks.landmark:
+                data_aux.append(landmark.x - min(x_))
+                data_aux.append(landmark.y - min(y_))
+        return data_aux, True
+
+    return None, False
+
+
+def process_all(
+    dir: str,
+    output: str = "data.pickle",
+    batch: int = 100,
+):
+    """Process all classes across all sample directories in batches."""
+    hands = Hands(
+        max_num_hands=1,
+        static_image_mode=True,
+        min_detection_confidence=0.3,
+    )
+    result = {"data": [], "labels": []}
+    # all_labels = set()
+    # for sample_dir in os.listdir(dir):
+    #     all_labels.update(os.listdir(os.path.join(dir, sample_dir)))
+
+    all_images = [str(file) for file in Path(dir).rglob("*") if file.is_file()]
+    all_images = random.shuffle(all_images)
+    for img_path in all_images:
+        data_aux, valid = process_image(hands, img_path)
+        if valid:
+            label = img_path.split(os.path.sep)[-2]
+            result["data"].append(data_aux)
+            result["labels"].append(label)
+
+    # # Display stats after processing
+    # unique_labels, counts = np.unique(all_labels, return_counts=True)
+    # print_class_stats(unique_labels, counts)
+    # draw_class_bar_plot(unique_labels, counts)
+    with open(output, "wb") as f:
+        pickle.dump(result, f)
