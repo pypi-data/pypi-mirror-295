c75f1944bf07a03d4e0a9eaee39c16adcab9b50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 16:03:42 +0000

    Add package galigeopy/0.0.17/galigeopy-0.0.17-py3-none-any.whl

---
+++ packages/galigeopy/galigeopy-0.0.17-py3-none-any.whl/galigeopy/__init__.py
@@ -0,0 +1,13 @@
+# GaligeoPy
+from galigeopy.galigeopy import about, check
+
+# Org
+from galigeopy.org.org import Org
+
+# Model
+from galigeopy.model.network import Network
+from galigeopy.model.poi import Poi
+from galigeopy.model.zone_type import ZoneType
+from galigeopy.model.zone import Zone
+from galigeopy.model.zone_geounit import ZoneGeounit
+from galigeopy.model.geolevel import Geolevel
