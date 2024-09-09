d0b259a963939a4a00290a31cc49774915578952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:04 +0000

    Add package ChemMod/0.0.9/chemmod-0.0.9.tar.gz

---
+++ packages/ChemMod/chemmod-0.0.9.tar.gz/chemmod-0.0.9/ChemMod/equation/__init__.py
@@ -0,0 +1,16 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from ChemMod.equation import __all__
+
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+
+
---
+++ packages/ChemMod/chemmod-0.0.9.tar.gz/chemmod-0.0.9/ChemMod/plot/__init__.py
@@ -0,0 +1,15 @@
+
+# from ChemMod.equation import equation
+# from ChemMod.plot import plot
+# from ChemMod import ChemMod
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ChemMod.plot import __all__
+from ChemMod.equation import __all__
+
+__all__ = [s for s in dir() if not s.startswith("_")]
+
+
+theme('default')
