0f22b4576d592c011421fb16d7ea952c3b9622e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:04:52 +0000

    Add package mtmai/0.3.686/mtmai-0.3.686-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.686-py3-none-any.whl/mtmai/cli/release.py
@@ -0,0 +1,28 @@
+"""部署相关的子命令"""
+
+import logging
+
+from mtmai.cli.cli import Cli
+from mtmlib.mtutils import bash, is_in_gitpod
+
+logger = logging.getLogger()
+
+
+class CliRelease(Cli):
+    """释放版本"""
+
+    def run(self) -> None:
+        import argparse
+
+        parser = argparse.ArgumentParser(description="Release options")
+        parser.add_argument("--options1", help="Option 1 for release")
+        args, unknown = parser.parse_known_args()
+
+        if args.options1:
+            logger.info(f"Release option 1: {args.options1}")
+        from mtmai.mtlibs import dev_helper
+
+        if is_in_gitpod():
+            bash("git pull")
+            pass
+        dev_helper.release_py()
