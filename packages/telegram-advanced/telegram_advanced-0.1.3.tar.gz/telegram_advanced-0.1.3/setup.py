b88efa138683a3854c20f988daa0f1982859a95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:12 +0000

    Add package telegram-advanced/0.1.3/telegram_advanced-0.1.3.tar.gz

---
+++ packages/telegram-advanced/telegram_advanced-0.1.3.tar.gz/telegram_advanced-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telegram_advanced",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        # Add other dependencies here
+    ],
+    author="LCF",
+    author_email="your.email@example.com",
+    description="An advanced Telegram bot library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/telegram_advanced",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
