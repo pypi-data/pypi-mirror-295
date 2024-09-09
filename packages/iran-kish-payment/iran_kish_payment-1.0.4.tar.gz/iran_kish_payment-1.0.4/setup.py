7175425a1a198e2d6be64d57f5e9371f5dfdce63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:13 +0000

    Add package iran-kish-payment/1.0.4/iran_kish_payment-1.0.4.tar.gz

---
+++ packages/iran-kish-payment/iran_kish_payment-1.0.4.tar.gz/iran_kish_payment-1.0.4/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="iran_kish_payment",
+    version="1.0.4",
+    packages=find_packages(),
+    install_requires=[
+        'pycryptodome',
+        'requests',
+        'urllib3<2'
+    ],
+    tests_require=[
+        'unittest',
+    ],
+    test_suite='tests',
+    description="Iran Kish payment portal",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="parsbytes",
+    author_email="parsbytes@gmail.com",
+    url="https://github.com/parsbytes/iran_kish_payment",
+)
