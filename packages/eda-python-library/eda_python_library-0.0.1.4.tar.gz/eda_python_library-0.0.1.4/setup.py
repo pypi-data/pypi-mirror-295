1e3d374ac62ab53f44dc5d16ad4013cb674624d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:45 +0000

    Add package eda-python-library/0.0.1.4/eda_python_library-0.0.1.4.tar.gz

---
+++ packages/eda-python-library/eda_python_library-0.0.1.4.tar.gz/eda_python_library-0.0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open(r"C:\Users\Devadarsan\Desktop\Karthik_projects\EDA_Package\EDA_python_package_library\README.md", "r") as f:
+    long_description = f.read()
+
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.10'
+]
+ 
+setup(
+  name='eda-python-library',
+  version='0.0.1.4',
+  description='A Library for Making the Explorartory Data Analysis process easy in single line of codes',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url="https://github.com/KaRtHiK-56/EDA_python_package_library",  
+  author='Karthik',
+  author_email='karthiksurya611@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='eda', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
