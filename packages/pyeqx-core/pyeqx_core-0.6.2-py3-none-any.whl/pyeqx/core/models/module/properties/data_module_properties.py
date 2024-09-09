1ac72f29fa08a17061ff567dcecdbe0143136fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:25 +0000

    Add package pyeqx-core/0.6.2/pyeqx_core-0.6.2-py3-none-any.whl

---
+++ packages/pyeqx-core/pyeqx_core-0.6.2-py3-none-any.whl/pyeqx/core/models/module/properties/data_module_properties.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Any
+
+from pyeqx.core.common import from_str
+
+
+@dataclass
+class DataModuleProperties(dict):
+    storage: str
+
+    def __init__(self, storage: str, obj: dict[str, Any]):
+        obj.update({"storage": storage})
+
+        self.__dict__ = obj
+
+    def to_dict(self) -> dict:
+        result = {
+            "storage": self.storage,
+        }
+        return result
