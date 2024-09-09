5823114d53610dad0172c51e2f2ec0c731215451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:13 +0000

    Add package basedpyright/1.17.3/basedpyright-1.17.3.tar.gz

---
+++ packages/basedpyright/basedpyright-1.17.3.tar.gz/basedpyright-1.17.3/packages/pyright-internal/src/tests/samples/any1.py
@@ -0,0 +1,38 @@
+# This sample tests certain uses of Any that should be flagged as illegal.
+
+import typing
+from typing import Any, cast
+
+# This should generate an error because Any is not valid for isinstance.
+isinstance(0, Any)
+
+# This should generate an error because Any is not valid for isinstance.
+isinstance(0, typing.Any)
+
+v1 = cast(Any, 0)
+v2 = cast(typing.Any, 0)
+
+
+class A(Any):
+    ...
+
+
+class B(typing.Any):
+    ...
+
+
+# This should generate an error because Any is not callable.
+Any()
+
+# This should generate an error because Any is not callable.
+typing.Any()
+
+
+def func1() -> int:
+    # This should generate an error because Any cannot be used as a value.
+    return Any
+
+
+def func2() -> int:
+    # This should generate an error because Any cannot be used as a value.
+    return typing.Any
---
+++ packages/basedpyright/basedpyright-1.17.3.tar.gz/basedpyright-1.17.3/packages/pyright-internal/src/tests/samples/call7.py
@@ -0,0 +1,71 @@
+# This sample tests the handling of an unpacked TypedDict passed as
+# an argument to a function.
+
+from typing import TypedDict, Unpack
+
+
+class TD1(TypedDict):
+    arg1: int
+    arg2: str
+
+
+class TD2(TD1):
+    arg3: float
+
+
+def func1(arg1: int, arg2: str):
+    pass
+
+
+def func2(arg1: int, arg2: str, arg3: float):
+    pass
+
+
+def func3(arg1: int, arg2: str, **kwargs: float):
+    pass
+
+
+def func4(arg1: int, arg2: str, **kwargs: int):
+    pass
+
+
+td1: TD1 = {"arg1": 10, "arg2": "something"}
+td2: TD2 = {"arg1": 10, "arg2": "something", "arg3": 3.4}
+
+func1(**td1)
+
+# This should generate an error because "arg1" is already assigned
+func1(arg1=3, **td1)
+
+# This should generate an error because "arg3" isn't provided
+func1(**td2)
+
+# This should generate an error because "arg3" isn't matched
+func2(**td1)
+
+func2(**td2)
+
+
+func3(**td1)
+
+func3(**td2)
+
+func4(**td1)
+
+# This should generate an error because "arg3" cannot be matched
+# due to the type of the **kwargs parameter.
+func4(**td2)
+
+
+class Options(TypedDict, total=False):
+    opt1: bool
+    opt2: str
+
+
+def func5(code: str | None = None, **options: Unpack[Options]):
+    pass
+
+
+func5(**{})
+func5(**{"opt1": True})
+func5(**{"opt2": "hi"})
