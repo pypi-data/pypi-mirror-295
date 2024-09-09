8bf3e5b412886fb1ce365d4ca138eb5d4bcfc793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:42 +0000

    Add package qldebugger/0.3.0/qldebugger-0.3.0.tar.gz

---
+++ packages/qldebugger/qldebugger-0.3.0.tar.gz/qldebugger-0.3.0/src/qldebugger/cli.py
@@ -0,0 +1,143 @@
+import json
+import logging
+from pathlib import Path
+from typing import TYPE_CHECKING, Mapping
+
+import click
+
+from . import actions
+from .config import load_config
+
+if TYPE_CHECKING:
+    from mypy_boto3_sns.type_defs import MessageAttributeValueTypeDef
+
+CONFIG_FILENAME = Path('qldebugger.toml')
+
+
+@click.group()
+def cli() -> None:
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s:qldebugger:%(message)s')
+
+
+@cli.command()
+def init() -> None:
+    config = """[aws]
+profile = ""
+access_key_id = "secret"
+secret_access_key = "secret"
+session_token = ""
+region = "us-east-1"
+endpoint_url = "http://localhost:4566/"
+
+[queues]
+myqueue = {}
+
+[lambdas]
+print = {handler = "qldebugger.example.lambdas.print_messages"}
+[lambdas.fail]
+handler = "qldebugger.example.lambdas.exec_fail"
+[lambdas.fail.environment]
+VARIABLE = "VALUE"
+
+[event_source_mapping]
+a = {queue = "myqueue", function_name = "print"}
+b = {queue = "myqueue", function_name = "fail", batch_size = 1, maximum_batching_window = 20}
+"""
+    if CONFIG_FILENAME.exists():
+        click.echo('Configuration file already exists')
+        return
+    with CONFIG_FILENAME.open('w') as fp:
+        fp.write(config)
+    click.echo('Configuration file created')
+
+
+@cli.command()
+@click.argument('event_source_mapping_name')
+def run(event_source_mapping_name: str) -> None:
+    load_config(CONFIG_FILENAME)
+    actions.event_source_mapping.receive_messages_and_run_lambda(event_source_mapping_name=event_source_mapping_name)
+
+
+# Infra
+
+
+@cli.group()
+def infra() -> None: ...
+
+
+@infra.command('create-secrets')
+def infra_create_secrets() -> None:
+    load_config(CONFIG_FILENAME)
+    actions.infra.create_secrets()
+
+
+@infra.command('create-topics')
+def infra_create_topics() -> None:
+    load_config(CONFIG_FILENAME)
+    actions.infra.create_topics()
+
+
+@infra.command('create-queues')
+def infra_create_queues() -> None:
+    load_config(CONFIG_FILENAME)
+    actions.infra.create_queues()
+
+
+@infra.command('create-all')
+def infra_create_all() -> None:
+    load_config(CONFIG_FILENAME)
+    actions.infra.create_secrets()
+    actions.infra.create_topics()
+    actions.infra.create_queues()
+    actions.infra.subscribe_topics()
+
+
+@infra.command('subscribe-topics')
+def infra_subscribe_topics() -> None:
+    load_config(CONFIG_FILENAME)
+    actions.infra.subscribe_topics()
+
+
+# Msg
+
+
+@cli.group()
+def msg() -> None: ...
+
+
+@msg.command('publish')
+@click.argument('topic_name')
+@click.argument('message')
+@click.argument('attributes', default='{}', type=json.loads)
+def msg_publish(topic_name: str, message: str, attributes: Mapping[str, 'MessageAttributeValueTypeDef']) -> None:
+    load_config(CONFIG_FILENAME)
+    actions.message.publish_message(topic_name=topic_name, message=message, attributes=attributes)
+
+
+@msg.command('send')
+@click.argument('queue_name')
+@click.argument('message')
+def msg_send(queue_name: str, message: str) -> None:
+    load_config(CONFIG_FILENAME)
+    actions.message.send_message(queue_name=queue_name, message=message)
+
+
+@msg.command('receive')
+@click.argument('queue_name')
+@click.option('--batch-size', default=1, type=int, show_default=True)
+@click.option('--wait-seconds', default=0, type=int, show_default=True)
+def msg_receive(queue_name: str, batch_size: int, wait_seconds: int) -> None:
+    load_config(CONFIG_FILENAME)
+    messages = actions.message.receive_message(
+        queue_name=queue_name, batch_size=batch_size, maximum_batching_window=wait_seconds
+    )
+    for message in messages['Messages']:
+        click.echo(repr(message.get('Body')))
+    actions.message.delete_messages(queue_name=queue_name, messages=messages)
+
+
+@msg.command('purge')
+@click.argument('queue_name')
+def msg_purge(queue_name: str) -> None:
+    load_config(CONFIG_FILENAME)
+    actions.message.purge_messages(queue_name=queue_name)
---
+++ packages/qldebugger/qldebugger-0.3.0.tar.gz/qldebugger-0.3.0/src/qldebugger/config/file_parser.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from typing import Any, BinaryIO, Dict, List, NamedTuple, Optional, Tuple, Union
+
+import tomli
+from pydantic import BaseModel, Field, PositiveInt, field_validator
+
+
+class ConfigAWS(BaseModel):
+    profile: Optional[str] = None
+    access_key_id: Optional[str] = None
+    secret_access_key: Optional[str] = None
+    session_token: Optional[str] = None
+    region: Optional[str] = None
+    endpoint_url: Optional[str] = None
+
+
+class ConfigSecret(BaseModel, ABC):
+    @abstractmethod
+    def get_value(self) -> Union[str, bytes]:
+        raise NotImplementedError
+
+
+class ConfigSecretString(ConfigSecret):
+    string: str
+
+    def get_value(self) -> str:
+        return self.string
+
+
+class ConfigSecretBinary(ConfigSecret):
+    binary: bytes
+
+    def get_value(self) -> bytes:
+        return self.binary
+
+
+class ConfigTopicSubscriber(BaseModel):
+    queue: str
+    raw_message_delivery: bool = False
+    filter_policy: Optional[str] = None
+
+
+class ConfigTopic(BaseModel):
+    subscribers: List[ConfigTopicSubscriber] = Field(default_factory=list)
+
+
+class ConfigQueueRedrivePolicy(BaseModel):
+    dead_letter_queue: str
+    max_receive_count: PositiveInt
+
+
+class ConfigQueue(BaseModel):
+    redrive_policy: Optional[ConfigQueueRedrivePolicy] = None
+
+
+class NameHandlerTuple(NamedTuple):
+    module: str
+    function: str
+
+
+class ConfigLambda(BaseModel):
+    handler: NameHandlerTuple
+    environment: Dict[str, str] = Field(default_factory=dict)
+
+    @field_validator('handler', mode='before')
+    @classmethod
+    def _split_handler(cls, v: Any) -> Tuple[str, str]:
+        if not isinstance(v, str):
+            raise ValueError('should be a str')
+        if '.' not in v:
+            raise ValueError('should have a module and function names')
+        module, function = v.rsplit('.', maxsplit=1)
+        return module, function
+
+
+class ConfigEventSourceMapping(BaseModel):
+    queue: str
+    batch_size: int = 10
+    maximum_batching_window: int = 0
+    function_name: str
+
+
+class Config(BaseModel):
+    aws: ConfigAWS = Field(default_factory=ConfigAWS)
+    secrets: Dict[str, Union[ConfigSecretString, ConfigSecretBinary]] = Field(default_factory=dict)
+    topics: Dict[str, ConfigTopic] = Field(default_factory=dict)
+    queues: Dict[str, ConfigQueue]
+    lambdas: Dict[str, ConfigLambda]
+    event_source_mapping: Dict[str, ConfigEventSourceMapping]
+
+    @classmethod
+    def from_toml(cls, fp: BinaryIO, /) -> 'Config':
+        return cls.model_validate(tomli.load(fp))
