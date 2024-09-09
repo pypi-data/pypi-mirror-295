3ac324c9712fb255275d5109dcd7ae37a739c8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 03:05:30 +0000

    Add package tbc-video-export/0.1.4/tbc_video_export-0.1.4.tar.gz

---
+++ packages/tbc-video-export/tbc_video_export-0.1.4.tar.gz/tbc_video_export-0.1.4/src/tbc_video_export/common/consts.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import importlib.metadata
+from datetime import datetime
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from tbc_video_export.common.utils.metadata import get_url_from_metadata
+
+if TYPE_CHECKING:
+    from typing import Final
+
+_metadata = importlib.metadata.metadata("tbc-video-export")
+
+# substituted by poetry-dynamic-versioning when doing pyinstaller builds
+__version__ = "0.1.4"
+
+APPLICATION_NAME: Final = _metadata["Name"]
+PROJECT_VERSION: Final = _metadata["Version"] if __version__ == "0.0.0" else __version__
+
+PROJECT_CREDITS: Final = (
+    "Credits:\n"
+    "  Jitterbug\tDevelopment (https://github.com/JuniorIsAJitterbug)\n"
+    "  Harry Munday\tProject Management (https://github.com/harrypm)\n"
+)
+
+PROJECT_SUMMARY: Final = f"{_metadata['Summary']}\n\n{PROJECT_CREDITS}"
+PROJECT_URL: Final = _metadata["Home-Page"]
+PROJECT_URL_ISSUES: Final = get_url_from_metadata("Issues")
+PROJECT_URL_WIKI: Final = get_url_from_metadata("Wiki")
+PROJECT_URL_DISCORD: Final = get_url_from_metadata("Discord")
+
+PROJECT_URL_WIKI_COMMANDLIST: Final = f"{PROJECT_URL_WIKI}/Command-List"
+PROJECT_URL_WIKI_PROFILES: Final = f"{PROJECT_URL_WIKI}/FFmpeg-Profiles"
+
+
+CURRENT_TIMESTAMP: Final = datetime.now().strftime("%y-%m-%d_%H%M%S%f")[:-3]
+EXPORT_CONFIG_FILE_NAME: Final = Path(f"{APPLICATION_NAME}.json")
+
+PIPE_BUFFER_SIZE: Final = 4 * 1024 * 1024  # 4MB
+
+MINIMUM_TERMINAL_HEIGHT: Final = 30
+MINIMUM_TERMINAL_WIDTH: Final = 120
+
+TWO_STEP_OUT_FILE_LUMA_SUFFIX: Final = "luma"
+
+FFMPEG_VIDEO_MAP: Final = "[v_out]"
+
+# Ubuntu 22.04 uses FFmpeg 4.4.1 which does not support the new format
+FFMPEG_USE_OLD_MERGEPLANES: Final = True
+
+
+# Windows 10 conhost uses unicode-less fonts by default :'(
+# SUCCESS_SYMBOL = "✓"
+# ERROR_SYMBOL = "🗙"
+# RUNNING_SYMBOLS: tuple[str, ...] = ("⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏")
+
+SUCCESS_SYMBOL: Final = "●"
+ERROR_SYMBOL: Final = "○"
+RUNNING_SYMBOLS: Final[tuple[str, ...]] = ("/", "-", "\\", "|")
+
+# for NT ANSI enabling
+NT_STD_OUTPUT_HANDLE: Final = -11
+NT_ENABLE_PROCESSED_OUTPUT: Final = 0x1
+NT_ENABLE_WRAP_AT_EOL_OUTPUT: Final = 0x2
+NT_ENABLE_VIRTUAL_TERMINAL_PROCESSING: Final = 0x4
+
+# for NT named pipes
+NT_NAMED_PIPE_MAX_INSTANCES: Final = 1
+NT_NAMED_PIPE_TIMEOUT: Final = 0
+NT_NAMED_PIPE_BUFFER_SIZE: Final = 65536
+
+# for NT proc snapshots
+NT_TH32CS_SNAPPROCESS: Final = 0x2
---
+++ packages/tbc-video-export/tbc_video_export-0.1.4.tar.gz/tbc_video_export-0.1.4/src/tbc_video_export/opts/opts_ffmpeg.py
@@ -0,0 +1,149 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from tbc_video_export.common.enums import FieldOrder
+from tbc_video_export.opts import opt_types, opt_validators
+
+if TYPE_CHECKING:
+    import argparse
+
+
+def add_ffmpeg_opts(parent: argparse.ArgumentParser) -> None:
+    """Add FFmpeg opts to the parent arg parser."""
+    # ffmpeg arguments
+    ffmpeg_opts = parent.add_argument_group("ffmpeg")
+
+    ffmpeg_opts.add_argument(
+        "--audio-track",
+        dest="audio_track",
+        action="append",
+        default=[],
+        type=opt_validators.validate_audio_track_opts,
+        metavar="file_name",
+        help="Audio track to mux.\nYou can use this option multiple times.\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--audio-track-advanced",
+        dest="audio_track",
+        action="append",
+        default=[],
+        type=opt_validators.validate_audio_track_advanced_opts,
+        metavar=(
+            "["
+            "file_name, "
+            "title, "
+            "language, "
+            "rate, "
+            "sample_format, "
+            "channels, "
+            "channel_layout, "
+            "offset"
+            "]"
+        ),
+        help="Audio track to mux (advanced).\n"
+        "You can use this option multiple times.\n"
+        "Only file_name is required. None must be used when skipping an argument.\n\n"
+        "Examples:\n"
+        '\'["/path/to/file.flac", "HiFi", "eng", 192000]\'\n'
+        '\'["/path/to/file.flac", "Linear", "eng", None, None, None, None, 0.15]\'\n'
+        '\'["/path/to/file.pcm", "Analog Audio", None, 44100, "s16le", 2]\'\n'
+        '\'["/path/to/file.dts", "PCM Surround", "eng", 44100, "s16le", 6, "5.1"]\'\n'
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--metadata",
+        nargs=2,
+        default=[],
+        action="append",
+        metavar=("key", "value"),
+        help="Add metadata to output the file.\n"
+        "You can use this option multiple times.\n"
+        "Example:\n\n"
+        "--metadata Title foo --metadata Year 2024"
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--metadata-file",
+        type=opt_validators.valiate_metadata_file_exists,
+        default=[],
+        action="append",
+        metavar="filename",
+        help="Add metadata to the output of the file using ffmetadata files.\n"
+        "You can use this option multiple times.\n\n"
+        "See https://ffmpeg.org/ffmpeg-formats.html#Metadata-1 for details.\n\n"
+        "Note: When using --export-metadata the generated ffmetadata file is also "
+        "used.\n"
+        "Files defined here are used before any generated metadata and take "
+        "priority.\n"
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--field-order",
+        type=opt_types.TypeFieldOrder(parent),
+        choices=list(FieldOrder),
+        default=FieldOrder.TFF,
+        metavar="order",
+        help="Set a field order. (default: tff)\n"
+        "Available formats:\n\n  "
+        + "\n  ".join(f"{e.name.lower()!s:<5s} {e.value}" for e in FieldOrder)
+        + "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--force-anamorphic",
+        action="store_true",
+        default=False,
+        help="Force widescreen aspect ratio.\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--force-black-level",
+        type=opt_validators.validate_black_levels_opts,
+        metavar="R[,G,B]",
+        default=None,
+        help="Force black levels using the colorlevels filter.\n"
+        "Use a comma-separated list of numbers to provide values for colorlevels.\n"
+        "If a single number is provided, it is used for all 3."
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--hwaccel-device",
+        type=str,
+        help="Hardware acceleration device.\n"
+        "Specify a hardware device to use when a supported video profile is selected."
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--no-attach-json",
+        action="store_true",
+        default=False,
+        help="Disable embedding the TBC json in the video file. (default: no).\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--thread-queue-size",
+        type=int,
+        default=1024,
+        metavar="int",
+        help="Set the thread queue size for FFmpeg. (default: 1024)\n"
+        "Reduce this if you are having out-of-memory issues as a higher value\n"
+        "will allow the decoder to consume more memory."
+        "\n\n",
+    )
+
+    ffmpeg_opts.add_argument(
+        "--checksum",
+        action="store_true",
+        default=False,
+        help="Enable SHA256 checksumming on the output streams. (default: no)\n"
+        "This will create a .sha256 file next to your output file.\n"
+        "This may reduce export FPS slightly. FFmpeg must be used to verify checksums."
+        "\n\n",
+    )
