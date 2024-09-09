3009bf54553d0cc99e7956ae0da6beb388caed26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 04:04:41 +0000

    Add package xia-module-application-gh/0.0.19/xia-module-application-gh-0.0.19.tar.gz

---
+++ packages/xia-module-application-gh/xia-module-application-gh-0.0.19.tar.gz/xia-module-application-gh-0.0.19/xia_module_application_gh/templates/gh-module-foundation/module/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+    }
+  }
+}
+
+locals {
+  github_config = yamldecode(file(var.config_file))
+  cosmos_name = var.landscape["settings"]["cosmos_name"]
+}
+
+resource "github_repository" "foundation-repository" {
+  for_each = var.foundations
+
+  name        = each.value["repository_name"]
+  description = "Foundation: ${each.value["name"]}"
+
+  visibility = each.value["visibility"]
+
+  template {
+    owner                = each.value["template_owner"]
+    repository           = each.value["template_name"]
+  }
+}
+
+resource "github_actions_variable" "action_var_cosmos_name" {
+  for_each = var.foundations
+
+  repository       = each.value["repository_name"]
+  variable_name    = "COSMOS_NAME"
+  value            = local.cosmos_name
+
+  depends_on = [github_repository.foundation-repository]
+}
+
+resource "github_actions_variable" "action_var_realm_name" {
+  for_each = var.foundations
+
+  repository       = each.value["repository_name"]
+  variable_name    = "REALM_NAME"
+  value            = each.value["parent"]
+
+  depends_on = [github_repository.foundation-repository]
+}
+
+resource "github_actions_variable" "action_var_foundation_name" {
+  for_each = var.foundations
+
+  repository       = each.value["repository_name"]
+  variable_name    = "FOUNDATION_NAME"
+  value            = each.value["name"]
+
+  depends_on = [github_repository.foundation-repository]
+}
