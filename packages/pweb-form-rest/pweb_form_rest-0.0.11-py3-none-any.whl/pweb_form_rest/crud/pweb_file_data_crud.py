12483152c422af5a5706304ddb6a2f7ea2ed2737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:38 +0000

    Add package pweb-form-rest/0.0.11/pweb_form_rest-0.0.11-py3-none-any.whl

---
+++ packages/pweb-form-rest/pweb_form_rest-0.0.11-py3-none-any.whl/pweb_form_rest/crud/pweb_file_data_crud.py
@@ -0,0 +1,58 @@
+from pweb_form_rest.crud.pweb_crud_common import PWebCRUDCommon
+from pweb_form_rest.crud.pweb_upload_processor import PWebUploadProcessor
+from pweb_form_rest.schema.pweb_rest_schema import PWebDataDTO
+from pweb_orm import PWebBaseModel
+from ppy_common import PyCommon
+
+
+class FileDataCRUD(PWebCRUDCommon):
+    pweb_upload_processor: PWebUploadProcessor = PWebUploadProcessor()
+
+    def __init__(self, model: PWebBaseModel):
+        self.model = model
+
+    def upload_and_save_file(self, form_data, model, request_dto: PWebDataDTO, upload_path, override_names: dict = None, before_save=None, after_save=None):
+        override_names = self.pweb_upload_processor.get_file_override_names(uuid=model.uuid, request_dto=request_dto, override_names=override_names)
+        uploaded_file_names = self.pweb_upload_processor.validate_and_upload_multiple(form_data=form_data, request_dto=request_dto, upload_path=upload_path, override_name=override_names)
+        for name in override_names:
+            if hasattr(model, name) and name in uploaded_file_names:
+                file_name = uploaded_file_names[name][override_names[name]]
+                setattr(model, name, file_name)
+
+        model = self.pweb_upload_processor.delete_orphan_files(override_names=override_names, form_data=form_data, model=model, upload_path=upload_path)
+        model = self.perform_save(model=model, data=form_data, before_save=before_save, after_save=after_save)
+        return model
+
+    def get_model_by_upload_file_data(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, existing_model=None, form_data: dict = None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        if not form_data:
+            form_data = self.pweb_crud.get_form_data(request_dto, before_validate=before_validate, after_validate=after_validate)
+        model = self.pweb_crud.populate_model(form_data, request_dto, instance=existing_model, ignore_load=ignore_load)
+        if not model.uuid:
+            model.uuid = PyCommon.uuid()
+        return self.upload_and_save_file(form_data=form_data, model=model, request_dto=request_dto, upload_path=upload_path, override_names=override_names, before_save=before_save, after_save=after_save)
+
+    def upload_file_data_and_save(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, form_data=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        model = self.get_model_by_upload_file_data(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+        return model
+
+    def upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully created!", form_data=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        model = self.upload_file_data_and_save(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+        return self.message_or_data_response(model=model, response_dto=response_dto, response_message=response_message)
+
+    def update_upload_file_data_save(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, existing_model=None, form_data: dict = None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        if not form_data:
+            form_data = self.pweb_crud.get_form_data(data_dto=request_dto, before_validate=before_validate, after_validate=after_validate)
+
+        if not existing_model:
+            existing_model = self.get_by_id(form_data['id'], query=query, exception=True)
+
+        model = self.get_model_by_upload_file_data(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, existing_model=existing_model, before_save=before_save, after_save=after_save, ignore_load=ignore_load)
+        return model
+
+    def update_upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = "Successfully updated!", existing_model=None, form_data: dict = None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        model = self.update_upload_file_data_save(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, existing_model=existing_model, query=query, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+        return self.message_or_data_response(model=model, response_dto=response_dto, response_message=response_message)
+
+    def delete_file(self, upload_path, filename, response_message: str = "Successfully deleted!"):
+        self.pweb_upload_processor.delete_file(upload_path=upload_path, filename=filename)
+        return self.message_or_data_response(model=None, response_message=response_message)
---
+++ packages/pweb-form-rest/pweb_form_rest-0.0.11-py3-none-any.whl/pweb_form_rest/crud/pweb_rest_data_crud.py
@@ -0,0 +1,63 @@
+from pweb_form_rest.common.pweb_com_fr_message import ComFRMessage
+from pweb_form_rest.crud.pweb_crud_common import PWebCRUDCommon
+from pweb_form_rest.schema.pweb_rest_schema import PWebDataDTO
+from pweb_form_rest.crud.pweb_file_data_crud import FileDataCRUD
+from pweb_orm import PWebBaseModel
+
+
+class RESTDataCRUD(PWebCRUDCommon):
+    file_data_crud = None
+
+    def __init__(self, model: PWebBaseModel):
+        self.model = model
+        self.file_data_crud = FileDataCRUD(model=model)
+
+    def update_and_get_model(self, request_dto: PWebDataDTO, data: dict = None, existing_model=None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        if not data:
+            data = self.get_json_data(request_dto, before_validate=before_validate, after_validate=after_validate)
+        return self.edit(model_id=data['id'], data=data, request_dto=request_dto, existing_model=existing_model, query=query, before_save=before_save, after_save=after_save, ignore_load=ignore_load)
+
+    def create_and_get_model(self, request_dto: PWebDataDTO, data: dict = None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        if not data:
+            data = self.get_json_data(request_dto, before_validate=before_validate, after_validate=after_validate)
+        return self.save(data=data, request_dto=request_dto, before_save=before_save, after_save=after_save, ignore_load=ignore_load)
+
+    def create(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = ComFRMessage.create_success, data: dict = None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        model = self.create_and_get_model(data=data, request_dto=request_dto, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+        return self.message_or_data_response(model, response_dto, response_message)
+
+    def details(self, model_id: int, response_dto: PWebDataDTO, query=None):
+        existing_model = self.get_by_id(model_id, exception=True, query=query)
+        return self.response_maker.data_response(existing_model, response_dto)
+
+    def update(self, request_dto: PWebDataDTO, response_dto: PWebDataDTO = None, response_message: str = ComFRMessage.update_success, existing_model=None, data: dict = None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        model = self.update_and_get_model(request_dto=request_dto, existing_model=existing_model, query=query, data=data, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+        return self.message_or_data_response(model, response_dto, response_message)
+
+    def delete(self, model_id: int, response_message: str = ComFRMessage.delete_success, query=None, before_delete=None, after_delete=None):
+        self.soft_remove(model_id=model_id, query=query, before_delete=before_delete, after_delete=after_delete)
+        return self.response_maker.success_message(response_message)
+
+    def hard_delete(self, model_id: int, response_message: str = ComFRMessage.delete_success, query=None):
+        self.remove(model_id, query=query)
+        return self.response_maker.success_message(response_message)
+
+    def paginated_list(self, response_dto: PWebDataDTO, query=None, search_fields: list = None, sort_field=None, sort_order=None, item_per_page=None):
+        data_list = self.read_all(query=query, search_fields=search_fields, sort_field=sort_field, sort_order=sort_order, item_per_page=item_per_page)
+        return self.response_maker.paginate_response(data_list, response_dto)
+
+    def list(self, response_dto: PWebDataDTO, query=None, search_fields: list = None, sort_field=None, sort_order=None):
+        data_list = self.read_all(query=query, search_fields=search_fields, sort_field=sort_field, sort_order=sort_order, enable_pagination=False)
+        return self.response_maker.list_data_type_response(data_list, response_dto=response_dto)
+
+    def upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = ComFRMessage.create_success, form_data=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        return self.file_data_crud.upload_file_data(request_dto=request_dto, upload_path=upload_path, response_dto=response_dto, override_names=override_names, response_message=response_message, form_data=form_data, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+
+    def update_upload_file_data(self, request_dto: PWebDataDTO, upload_path, response_dto: PWebDataDTO = None, override_names: dict = None, response_message: str = ComFRMessage.update_success, existing_model=None, form_data: dict = None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        return self.file_data_crud.update_upload_file_data(request_dto=request_dto, upload_path=upload_path, response_dto=response_dto, override_names=override_names, response_message=response_message, existing_model=existing_model, form_data=form_data, query=query, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+
+    def upload_file_data_save(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, form_data=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        return self.file_data_crud.upload_file_data_and_save(request_dto=request_dto, upload_path=upload_path, override_names=override_names, form_data=form_data, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
+
+    def update_upload_file_data_save(self, request_dto: PWebDataDTO, upload_path, override_names: dict = None, existing_model=None, form_data: dict = None, query=None, before_save=None, after_save=None, before_validate=None, after_validate=None, ignore_load: list = None):
+        return self.file_data_crud.update_upload_file_data_save(request_dto=request_dto, upload_path=upload_path, override_names=override_names, existing_model=existing_model, form_data=form_data, query=query, before_save=before_save, after_save=after_save, before_validate=before_validate, after_validate=after_validate, ignore_load=ignore_load)
