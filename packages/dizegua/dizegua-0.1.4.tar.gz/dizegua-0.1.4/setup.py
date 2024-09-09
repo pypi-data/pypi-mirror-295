8d7fde531bc89ee65239e86f7e6ede1beb09a5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 14:03:45 +0000

    Add package dizegua/0.1.4/dizegua-0.1.4.tar.gz

---
+++ packages/dizegua/dizegua-0.1.4.tar.gz/dizegua-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.4'
+DESCRIPTION = 'Esse pacote só diz égua'
+LONG_DESCRIPTION = ""
+
+with open("./README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+        name="dizegua",
+        version=VERSION,
+        author="Thalyson Wilker",
+        author_email="thalison.wilker@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
