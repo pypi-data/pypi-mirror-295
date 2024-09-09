9d83a18132252a26773c04c374d8599d0e4722e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 16:03:48 +0000

    Add package mtmai/0.3.701/mtmai-0.3.701-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.701-py3-none-any.whl/mtmai/toolsv2/web_search.py
@@ -0,0 +1,57 @@
+import logging
+from textwrap import dedent
+
+import httpx
+
+from mtmai.core.config import settings
+
+logger = logging.getLogger()
+search_result_limit = 3
+
+
+async def search_web_by_keywords(keywords: str):
+    searxng_url = f"{settings.SEARXNG_URL_BASE}/search"
+    logger.info("调用 search ( %s ), %s", searxng_url, keywords)
+
+    with httpx.Client() as client:
+        params = {"q": keywords, "format": "json"}
+        r = client.get(searxng_url, params=params)
+        # r.raise_for_status()
+
+        if r.status_code == 429:
+            return ("大模型调用每分钟token数量限制, 请在1分钟后重试", {})
+        r.raise_for_status()
+
+        search_results = r.json()
+
+        result_list2 = search_results.get("results", [])[:search_result_limit]
+
+        result_list3 = [
+            {
+                "title": x.get("title", ""),
+                "url": x.get("url", ""),
+                "content": x.get("content", ""),
+            }
+            for x in result_list2
+        ]
+
+        content_lines = ["搜索结果:"]
+        for x in result_list3:
+            content_lines.append(
+                dedent(f"""title: {x.get("title")}
+                      content: {x.get("content")}
+                      """)
+            )
+
+        return (
+            "\n".join(content_lines),
+            {
+                "artifaceType": "ArtifactSearchResults",
+                "props": {
+                    "title": f"{keywords}的搜索结果",
+                    "results": result_list3,
+                    "suggestions": search_results.get("suggestions", []),
+                    # "infoboxes": search_results.get("infoboxes", []),
+                },
+            },
+        )
