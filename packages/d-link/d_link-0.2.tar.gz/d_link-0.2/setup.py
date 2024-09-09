c389f4f588020a05f6c254b6e53f04eeb3333dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 16:03:50 +0000

    Add package d-link/0.2/d_link-0.2.tar.gz

---
+++ packages/d-link/d_link-0.2.tar.gz/d_link-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='d-link',
+    version='0.2',
+    author='Pg Network',
+    author_email='gamerzp780@gmail.com',
+    description='A Python library to extract data from a Python script and push it to a GitHub Pages repository.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PgNetwork01/d-link',  # Update with your repository URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # List any dependencies here
+        # Example: 'requests>=2.25.1'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
