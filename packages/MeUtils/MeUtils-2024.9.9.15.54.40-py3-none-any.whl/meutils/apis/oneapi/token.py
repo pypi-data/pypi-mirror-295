476e2009c12f861601462b17ab5620d232b1466a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-09 15:05:07 +0000

    Add package MeUtils/2024.9.9.15.54.40/MeUtils-2024.9.9.15.54.40-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.9.9.15.54.40-py3-none-any.whl/meutils/apis/oneapi/token.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : token
+# @Time         : 2024/7/19 13:56
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+import os
+
+from meutils.pipe import *
+from meutils.schemas.oneapi_types import BASE_URL
+
+
+async def get_api_key_money(api_key):
+    headers = {
+        'authorization': f'Bearer {api_key}'
+    }
+    async with httpx.AsyncClient(base_url=BASE_URL, headers=headers, timeout=30) as client:
+        response1 = await client.get("/v1/dashboard/billing/usage")
+        response2 = await client.get("/v1/dashboard/billing/subscription")
+
+        # logger.debug(response2.json())
+
+        # {"object":"list","total_usage":13665.746200000001}
+        # {"object":"billing_subscription","has_payment_method":true,"soft_limit_usd":2000,"hard_limit_usd":2000,"system_hard_limit_usd":2000,"access_until":0}
+
+        if response1.is_success and response2.is_success:
+            total_usage = response1.json()['total_usage'] / 100
+            hard_limit_usd = response2.json()['hard_limit_usd']
+            return hard_limit_usd - total_usage
+        else:
+            return 1
+
+
+if __name__ == '__main__':
+    arun(get_api_key_money(os.getenv("OPENAI_API_KEY_GUOCHAN")))
+
+    arun(get_api_key_money("sk-kBc2cacPZNcPIjwE4c461b796aAd4702A9B2C34cAfBe64Af"))
