7ab9855c21153d87b6a8084800d44a57d6c70212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:04:47 +0000

    Add package udata-front/5.1.3.dev27493/udata_front-5.1.3.dev27493-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.1.3.dev27493-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/api/schemas.ts
@@ -0,0 +1,56 @@
+import type { AxiosResponse } from "axios";
+import { api } from "./api";
+/**
+ * A schema version.
+ */
+export type SchemaVersion = {
+  version_name: string;
+  schema_url: string;
+}
+
+/**
+ * A schema from the schema catalog
+ */
+export type RegisteredSchema = {
+  name: string;
+  schema_type: string;
+  versions: Array<SchemaVersion>;
+}
+
+/**
+ * A schema associated with a resource.
+ */
+export type Schema = {
+  name?: string | null;
+  url ?: string | null;
+  version ?: string | null;
+} | null;
+
+export interface ValidataError {
+  code: string;
+  description: string;
+  fieldName: string;
+  fieldNumber: number;
+  fieldPosition: number;
+  message: string;
+  name: string;
+  note: string;
+  rowNumber: number;
+  rowPosition: number;
+}
+export type SchemaResponseData = Array<RegisteredSchema>;
+export type SchemaResponse = AxiosResponse<SchemaResponseData>;
+
+let catalogRequest: Promise<Array<RegisteredSchema>> | null = null;
+
+/**
+ * Get Schema Catalog
+ */
+export function getCatalog() {
+  if (catalogRequest) {
+    return catalogRequest;
+  }
+  catalogRequest = api.get<SchemaResponseData>("datasets/schemas/")
+  .then((resp) => resp.data)
+  return catalogRequest;
+}
---
+++ packages/udata-front/udata_front-5.1.3.dev27493-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/helpers/index.ts
@@ -0,0 +1,69 @@
+import { useI18n } from "vue-i18n";
+import { dayjs } from "./i18n";
+import markdown from "./markdown";
+import { toggleAccordion } from "./toggleAccordion";
+import RemoveMarkdown from "remove-markdown";
+import { readonly } from "vue";
+
+export const RESOURCE_TYPE = readonly(["main", "documentation", "update", "api", "code", "other"] as const);
+
+export const filesize = (val: number) => {
+  const { t } = useI18n();
+  const suffix = t('B')
+  const units = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']
+  for (let unit of units) {
+      if (Math.abs(val) < 1024.0) {
+        return `${val.toFixed(1)}${unit}${suffix}`
+      }
+      val /= 1024.0
+  }
+  return `${val.toFixed(1)}Y${suffix}`
+}
+
+/**
+ * Format date based on locale.
+ */
+export const formatDate = (date: Date | string, format = 'LL') => {
+  return dayjs(date).format(format);
+}
+
+/**
+ * Format date as relative from now.
+ * It displays "today" or dayjs.fromNow content, based on date.
+ */
+export const formatFromNow = (date: Date | string) => {
+  const { t } = useI18n();
+  const today = dayjs().startOf("date");
+  const dateWithoutTime = dayjs(date).startOf("date");
+  if(today.isSame(dateWithoutTime)) {
+    return t("today");
+  }
+  return dayjs(date).fromNow();
+}
+
+/**
+ * Format date relative form now if date is less than a month ago.
+ * Otherwise, show a formatted date.
+ */
+export const formatRelativeIfRecentDate = (date: Date | string) => {
+  const { t } = useI18n();
+  const today = dayjs().startOf("date");
+  const dateWithoutTime = dayjs(date).startOf("date");
+  const DONT_ROUND_DIFF = true;
+  if(today.diff(dateWithoutTime, 'month', DONT_ROUND_DIFF) > 1) {
+    return t("on {date}", {date: formatDate(date)});
+  }
+  return formatFromNow(date);
+}
+
+export const truncate = (val: string, length = 300) => {
+  if (typeof val !== "string") return;
+  return val.length > length ? val.slice(0, length) + "…" : val;
+};
+
+export const excerpt = (val: string, length = 300) => {
+  if (typeof val !== "string") return;
+  return truncate(RemoveMarkdown(val), length);
+};
+
+export { markdown, toggleAccordion };
