2ffeb934287934d0321683a197ec2ace21940c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:04:41 +0000

    Add package hgraph/0.3.16/hgraph-0.3.16.tar.gz

---
+++ packages/hgraph/hgraph-0.3.16.tar.gz/hgraph-0.3.16/src/hgraph/_operators/_to_json.py
@@ -0,0 +1,26 @@
+from hgraph._types._scalar_types import DEFAULT
+from hgraph._wiring._decorators import operator
+from hgraph._types import TIME_SERIES_TYPE, TS, OUT
+
+
+__all__ = ["to_json", "from_json"]
+
+
+@operator
+def to_json(ts: DEFAULT[TIME_SERIES_TYPE]) -> TS[str]:
+    """
+    Converts the ``ts`` to a JSON string.
+    """
+
+
+@operator
+def from_json(ts: TS[str]) -> DEFAULT[OUT]:
+    """
+    Converts the ``ts`` JSON string to the OUT type.
+    Usage would be along the lines of:
+
+    ::
+
+        value = from_json[TS[MySchema]](ts)
+
+    """
