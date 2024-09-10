763faa76fe03bdafa36825a01da65069856f3ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:56 +0000

    Add package mtmai/0.3.725/mtmai-0.3.725-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.725-py3-none-any.whl/mtmai/cli/gen.py
@@ -0,0 +1,39 @@
+"""客户端代码生成"""
+
+import json
+import logging
+from pathlib import Path
+
+from mtmai.cli.cli import Cli
+from mtmai.mtlibs import mtutils
+from mtmlib.mtutils import bash
+
+logger = logging.getLogger()
+
+
+class CliGen(Cli):
+    """openapi 客户端代码生成 含 typescript 和 python"""
+
+    def run(self, *args, **kwargs) -> None:
+        from mtmai.cli.serve import app
+        from mtmai.core.config import settings
+
+        openapi = app.openapi()
+        with Path(settings.OPENAPI_JSON_PATH).open("w") as f:
+            logger.info(
+                "openapi.json exported %s to %s",
+                openapi.get("openapi", "unknown version"),
+                settings.OPENAPI_JSON_PATH,
+            )
+            json.dump(openapi, f, indent=2)
+        # dev_helper.gen()
+        if not mtutils.command_exists("openapi-python-client"):
+            bash(
+                "pip install openapi-python-client && openapi-python-client --install-completion"
+            )
+        bash(
+            "openapi-python-client generate --path mtmai/mtmai/openapi.json --overwrite"
+        )
+
+        # typescript 客户端库
+        bash("cd packages/mtmaiapi && bun run gen")
