ea3e66ae6d47222e2ce219461a793abe540645bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:05:18 +0000

    Add package syndesi/0.2.3/syndesi-0.2.3-py3-none-any.whl

---
+++ packages/syndesi/syndesi-0.2.3-py3-none-any.whl/syndesi/adapters/visa.py
@@ -0,0 +1,57 @@
+
+from pyvisa import ResourceManager
+
+from .adapter import Adapter
+from ..tools.types import to_bytes
+from typing import Union
+
+class VISA(Adapter):
+    def __init__(self, resource : str):
+        """
+        USB VISA stack adapter
+
+        Parameters
+        ----------
+        resource : str
+            resource address string
+        """
+        self._resource = resource
+        self._rm = ResourceManager()
+        self._inst = self._rm.open_resource(self._resource)
+        self._inst.write_termination = ''
+        self._inst.read_termination = ''
+
+    def list_devices(self=None):
+        """
+        Returns a list of VISA devices
+        """
+        rm = ResourceManager()
+
+        return rm.list_resources()
+
+    def flushRead(self):
+        pass
+
+    def open(self):
+        self._inst.open()
+
+    def close(self):
+        self._inst.close()
+            
+    def write(self, data : Union[bytes, str]):
+        data = to_bytes(data)
+        self._inst.write_raw(data)
+
+    def read(self) -> bytes:
+        return self._inst.read_raw()
+
+    def query(self, data : Union[bytes, str], timeout=None, continuation_timeout=None) -> bytes:
+        """
+        Shortcut function that combines
+        - flush_read
+        - write
+        - read
+        """
+        # TODO : implement timeouts
+        self.write(data)
+        return self.read()
