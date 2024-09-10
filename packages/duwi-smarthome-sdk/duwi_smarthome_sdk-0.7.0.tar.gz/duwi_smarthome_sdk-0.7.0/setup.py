17ce0b1fef0608548b9012c9899de8b3447707ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:21 +0000

    Add package duwi-smarthome-sdk/0.7.0/duwi_smarthome_sdk-0.7.0.tar.gz

---
+++ packages/duwi-smarthome-sdk/duwi_smarthome_sdk-0.7.0.tar.gz/duwi_smarthome_sdk-0.7.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.7.0'
+DESCRIPTION = 'sdk for duwi third platform'
+
+with open("README.md", encoding="utf-8") as fh:
+    doc_long_description = fh.read()
+
+setup(
+    name="duwi_smarthome_sdk",
+    version=VERSION,
+    author="duwi",
+    author_email="duwitech@163.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['websockets', 'aiohttp'],
+    keywords=['python', 'duwi', 'sdk', 'third', 'platform'],
+    entry_points={
+        'console_scripts': [
+            'duwi = duwi_smarthome_sdk.main:main'
+        ]
+    },
+    license="MIT",
+    url="https://github.com/duwi2024/homeassistant-sdk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
