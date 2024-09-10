5b02397f5c83ecd33e747e8b197c9ad0c1cfd955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:04:30 +0000

    Add package pyplaywright/0.0.2/pyplaywright-0.0.2.tar.gz

---
+++ packages/pyplaywright/pyplaywright-0.0.2.tar.gz/pyplaywright-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyplaywright',
+    version='0.0.2',
+    description='Python Playwright Utilities.',
+    author='Satyam Sinha',
+    packages=find_packages(),
+)
