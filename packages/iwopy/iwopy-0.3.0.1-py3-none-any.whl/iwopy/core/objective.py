b3587b3d78c9d3b23f0e68b2ce090ae623e359e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:24 +0000

    Add package iwopy/0.3.0.1/iwopy-0.3.0.1-py3-none-any.whl

---
+++ packages/iwopy/iwopy-0.3.0.1-py3-none-any.whl/iwopy/core/objective.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+
+from .function import OptFunction
+
+
+class Objective(OptFunction):
+    """
+    Abstract base class for objective functions.
+
+    :group: core
+
+    """
+
+    @abstractmethod
+    def maximize(self):
+        """
+        Returns flag for maximization of each component.
+
+        Returns
+        -------
+        flags: np.array
+            Bool array for component maximization,
+            shape: (n_components,)
+
+        """
+        pass
