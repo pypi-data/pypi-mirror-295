206165bb0fcca369c7039aa6b70ff9259b403d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:54 +0000

    Add package pawnlib/2.0.32/pawnlib-2.0.32.tar.gz

---
+++ packages/pawnlib/pawnlib-2.0.32.tar.gz/pawnlib-2.0.32/pawnlib/typing/constants.py
@@ -0,0 +1,264 @@
+class TimeConstants:
+    MINUTE_IN_SECONDS = 60
+    HOUR_IN_SECONDS = 60 * MINUTE_IN_SECONDS
+    DAY_IN_SECONDS = 24 * HOUR_IN_SECONDS
+    WEEK_IN_SECONDS = 7 * DAY_IN_SECONDS
+    MONTH_IN_SECONDS = 30 * DAY_IN_SECONDS
+    YEAR_IN_SECONDS = 365 * DAY_IN_SECONDS
+
+    MILLISECOND_IN_SECONDS = 0.001
+    MICROSECOND_IN_SECONDS = 0.000001
+    NANOSECOND_IN_SECONDS = 0.000000001
+
+    HOUR_IN_MINUTES = 60
+    DAY_IN_MINUTES = HOUR_IN_MINUTES * 24
+    WEEK_IN_MINUTES = 7 * DAY_IN_MINUTES
+    MONTH_IN_MINUTES = 30 * DAY_IN_MINUTES
+    YEAR_IN_MINUTES = 365 * DAY_IN_MINUTES
+
+
+class BooleanConstants:
+    TRUE = 1
+    FALSE = 0
+
+
+class NumericConstants:
+    TINT = 10 ** 18
+    ICX_IN_LOOP = 10**18
+    EXA = 10 ** 18
+    PETA = 10 ** 15
+    TERA = 10 ** 12
+    GIGA = 10 ** 9
+    MEGA = 10 ** 6
+    KILO = 10 ** 3
+    MILLI = 10 ** -3
+    MICRO = 10 ** -6
+    NANO = 10 ** -9
+    SQRT_2 = 2 ** 0.5
+    EULER_MASCHERONI = 0.57721  # Euler-Mascheroni
+    PI = 3.14159
+    E = 2.71828
+    GOLDEN_RATIO = (1 + 5 ** 0.5) / 2
+
+
+class AddressConstants:
+    ICON_ADDRESS = 42
+    ICON_ADDRESS_WITHOUT_PREFIX = 40
+    # CHAIN_SCORE_ADDRESS = f"cx{'0'*39}0"
+    # GOVERNANCE_ADDRESS = f"cx{'0'*39}1"
+    CHAIN_SCORE_ADDRESS = "cx0000000000000000000000000000000000000000"
+    GOVERNANCE_ADDRESS = "cx0000000000000000000000000000000000000001"
+    ETH_ADDRESS = 40  # Ethereum address length without '0x' prefix
+    ETH_ADDRESS_WITH_PREFIX = 42  # Ethereum address length with '0x' prefix
+    BTC_ADDRESS_P2PKH = 34  # Bitcoin P2PKH address length
+    BTC_ADDRESS_BECH32 = 42  # Bitcoin Bech32 address length
+
+
+class TimeStampDigits:
+    SECONDS_DIGITS = 10
+    MILLI_SECONDS_DIGITS = 13
+    MICRO_SECONDS_DIGITS = 16
+    NANOSECONDS_DIGITS = 19
+    PICOSECONDS_DIGITS = 22
+    FEMTOSECONDS_DIGITS = 25
+
+
+class ICONConstants:
+    ICON_METHODS = {
+        "cx0000000000000000000000000000000000000001": [
+            "getRevision", "getVersion", "getStepPrice", "getStepCosts", "getMaxStepLimit",
+            "getScoreStatus", "acceptScore", "rejectScore", "addAuditor", "removeAuditor", "isInScoreBlackList",
+            "getProposal", "getProposals", "registerProposal", "voteProposal", "applyProposal",
+            "cancelProposal", "onTimer"
+        ],
+        "cx0000000000000000000000000000000000000000": [
+            "disableScore", "enableScore", "acceptScore", "rejectScore", "blockScore", "unblockScore",
+            "getBlockedScores", "blockAccount", "unblockAccount", "isBlocked", "setRevision", "setStepPrice",
+            "setStepCost", "setMaxStepLimit", "getRevision", "getStepPrice", "getStepCost", "getStepCosts",
+            "getMaxStepLimit", "getScoreStatus", "getServiceConfig", "getFeeSharingConfig", "getNetworkInfo",
+            "getIISSInfo", "setStake", "getStake", "setDelegation", "getDelegation", "claimIScore",
+            "queryIScore", "registerPRep", "getPRep", "unregisterPRep", "setPRep", "getPReps", "getMainPReps",
+            "getSubPReps", "setBond", "getBond", "setBonderList", "getBonderList", "estimateUnstakeLockPeriod",
+            "getPRepTerm", "getPRepStats", "getPRepStatsOf", "disqualifyPRep", "burn", "validateRewardFund",
+            "setRewardFund", "setRewardFundAllocation2", "getScoreOwner", "setScoreOwner", "setNetworkScore",
+            "getNetworkScores", "addTimer", "removeTimer", "penalizeNonvoters", "setSlashingRates",
+            "getSlashingRates", "setUseSystemDeposit", "getUseSystemDeposit", "getBTPNetworkTypeID",
+            "getPRepNodePublicKey", "setPRepNodePublicKey", "registerPRepNodePublicKey", "openBTPNetwork",
+            "closeBTPNetwork", "sendBTPMessage", "getMinimumBond", "setMinimumBond", "initCommissionRate",
+            "setCommissionRate", "requestUnjail", "handleDoubleSignReport", "setPRepCountConfig",
+            "getPRepCountConfig", "setBondRequirementRate"
+        ]
+    }
+
+
+class HAVAHConstants:
+    HAVAH_METHODS = {
+        "cx0000000000000000000000000000000000000001": [
+            "setRevision", "setStepPrice", "setStepCost", "setMaxStepLimit", "grantValidator",
+            "revokeValidator", "setTimestampThreshold", "setRoundLimitFactor", "setUSDTPrice",
+            "setUSDTPriceOracle", "getUSDTPriceOracle", "addPlanetManager", "removePlanetManager",
+            "startRewardIssue", "setPrivateClaimableRate", "withdrawLostTo", "registerValidator",
+            "enableValidator", "unregisterValidator", "setBlockVoteCheckParameters", "setActiveValidatorCount",
+            "name", "openBTPNetwork", "delegate", "setDelegate"
+        ],
+        "cx0000000000000000000000000000000000000000": [
+            "setRevision", "setStepPrice", "setStepCost", "setMaxStepLimit", "getRevision",
+            "getStepPrice", "getStepCost", "getStepCosts", "getMaxStepLimit", "getServiceConfig",
+            "getScoreOwner", "setScoreOwner", "setRoundLimitFactor", "getRoundLimitFactor", "setUSDTPrice",
+            "getUSDTPrice", "getIssueInfo", "startRewardIssue", "addPlanetManager", "removePlanetManager",
+            "isPlanetManager", "registerPlanet", "unregisterPlanet", "setPlanetOwner", "getPlanetInfo",
+            "reportPlanetWork", "claimPlanetReward", "getRewardInfoOf", "getRewardInfo",
+            "setPrivateClaimableRate", "getPrivateClaimableRate", "addDeployer", "removeDeployer", "isDeployer",
+            "getDeployers", "setTimestampThreshold", "getTimestampThreshold", "grantValidator",
+            "revokeValidator", "getValidators", "getRewardInfosOf", "withdrawLostTo", "getLost",
+            "getBTPNetworkTypeID", "getBTPPublicKey", "openBTPNetwork", "closeBTPNetwork", "sendBTPMessage",
+            "setBTPPublicKey", "setBlockVoteCheckParameters", "getBlockVoteCheckParameters",
+            "registerValidator", "unregisterValidator", "getNetworkStatus", "setValidatorInfo",
+            "setNodePublicKey", "enableValidator", "getValidatorInfo", "getValidatorStatus",
+            "setActiveValidatorCount", "getActiveValidatorCount", "getValidatorsOf", "getValidatorsInfo",
+            "getDisqualifiedValidatorsInfo"
+        ]
+    }
+
+
+class CryptographicConstants:
+    SHA256_DIGEST_SIZE = 32
+    SHA3_256_DIGEST_SIZE = 32
+    KECCAK_256_DIGEST_SIZE = 32
+    SECP256K1_PRIVATE_KEY_SIZE = 32
+    SECP256K1_PUBLIC_KEY_SIZE = 64
+    SECP256K1_SIGNATURE_SIZE = 64
+    ED25519_PRIVATE_KEY_SIZE = 32
+    ED25519_PUBLIC_KEY_SIZE = 32
+    ED25519_SIGNATURE_SIZE = 64
+
+
+class NetworkConstants:
+    DEFAULT_HTTP_PORT = 80
+    DEFAULT_HTTPS_PORT = 443
+    DEFAULT_ICON_RPC_PORT = 9000
+    DEFAULT_ICON_P2P_PORT = 7100
+    DEFAULT_ETHEREUM_PORT = 8545
+    DEFAULT_BITCOIN_PORT = 8333
+    LOCALHOST = "127.0.0.1"
+    LOCALHOST_IPV6 = "::1"
+    IPV4_LOOPBACK = "127.0.0.1"
+    IPV6_LOOPBACK = "::1"
+    IPV4_BROADCAST = "255.255.255.255"
+    IPV6_BROADCAST = "ff02::1"
+    MAC_ADDRESS_BROADCAST = "ff:ff:ff:ff:ff:ff"
+
+class AWSRegionConstants:
+    REGIONS = {
+        "us-east-1": "US East (N. Virginia)",
+        "us-east-2": "US East (Ohio)",
+        "us-west-1": "US West (N. California)",
+        "us-west-2": "US West (Oregon)",
+        "ca-central-1": "Canada (Central)",
+        "eu-west-1": "EU (Ireland)",
+        "eu-west-2": "EU (London)",
+        "eu-west-3": "EU (Paris)",
+        "eu-central-1": "EU (Frankfurt)",
+        "eu-north-1": "EU (Stockholm)",
+        "eu-south-1": "EU (Milan)",
+        "ap-southeast-1": "Asia Pacific (Singapore)",
+        "ap-southeast-2": "Asia Pacific (Sydney)",
+        "ap-northeast-1": "Asia Pacific (Tokyo)",
+        "ap-northeast-2": "Asia Pacific (Seoul)",
+        "ap-northeast-3": "Asia Pacific (Osaka)",
+        "ap-east-1": "Asia Pacific (Hong Kong)",
+        "ap-south-1": "Asia Pacific (Mumbai)",
+        "sa-east-1": "South America (São Paulo)",
+        "me-south-1": "Middle East (Bahrain)",
+        "af-south-1": "Africa (Cape Town)",
+    }
+
+    @staticmethod
+    def region_name(code: str) -> str:
+        """
+        Get the human-readable name of an AWS region based on its code.
+
+        :param code: AWS region code (e.g., "us-east-1").
+        :return: Human-readable region name (e.g., "US East (N. Virginia)").
+        """
+        return AWSRegionConstants.REGIONS.get(code, "Unknown region")
+
+    @staticmethod
+    def region_list() -> list:
+        """
+        Get a list of all AWS region codes.
+
+        :return: List of AWS region codes.
+        """
+        print(AWSRegionConstants.REGIONS.keys())
+        return list(AWSRegionConstants.REGIONS.keys())
+
+
+class GradeMappingConstants:
+    GRADE_MAPPING = {
+        "0x0": {"name": "Main", "color": "[blue]\[Main][/blue]"},
+        "0x1": {"name": "Sub", "color": "[green]\[Sub][/green]"},
+        "0x2": {"name": "Cand", "color": "[Cand]"}
+    }
+
+    @staticmethod
+    def grade_name(grade_code: str) -> str:
+        """
+        Get the name associated with the grade code.
+        :param grade_code: Grade code (e.g., "0x0").
+        :return: Grade name (e.g., "Main").
+        """
+        return GradeMappingConstants.GRADE_MAPPING.get(grade_code, {}).get("name", "Unknown grade")
+
+    @staticmethod
+    def grade_color(grade_code: str) -> str:
+        """
+        Get the color representation associated with the grade code.
+        :param grade_code: Grade code (e.g., "0x0").
+        :return: Color representation (e.g., "[blue]\\[Main][/blue]").
+        """
+        return GradeMappingConstants.GRADE_MAPPING.get(grade_code, {}).get("color", "Unknown color")
+
+
+class YesNoConstants:
+    YES_NO_MAPPING = {
+        "0x0": "no",
+        "0x1": "yes"
+    }
+
+    @staticmethod
+    def yes_no(grade_code: str) -> str:
+        """
+        Get 'yes' or 'no' based on the grade code.
+        :param grade_code: Grade code (e.g., "0x0" for 'no', "0x1" for 'yes').
+        :return: 'yes' or 'no'.
+        """
+        return YesNoConstants.YES_NO_MAPPING.get(grade_code, "Unknown code")
+
+
+class AllConstants(
+    TimeConstants,
+    BooleanConstants,
+    NumericConstants,
+    AddressConstants,
+    TimeStampDigits,
+    ICONConstants,
+    HAVAHConstants,
+    CryptographicConstants,
+    NetworkConstants,
+    AWSRegionConstants,
+    GradeMappingConstants,
+    YesNoConstants
+):
+    __slots__ = ()
+
+    def __setattr__(self, name, value):
+        raise TypeError("Constants are read-only")
+
+    def get_aws_region_name(self, code: str = "") -> str:
+        return self.region_name(code)
+
+    def get_aws_region_list(self) -> list:
+        return self.region_list()
+
+const = AllConstants()
---
+++ packages/pawnlib/pawnlib-2.0.32.tar.gz/pawnlib-2.0.32/pawnlib/utils/notify.py
@@ -0,0 +1,413 @@
+import os
+import re
+import requests
+from pawnlib.config.globalconfig import pawnlib_config as pawn
+from pawnlib.output import color_print
+from pawnlib.resource import net
+from pawnlib.typing import date_utils, shorten_text
+from pawnlib.utils import http
+import json
+
+class TelegramBot:
+    """
+    A class to interact with the Telegram Bot API.
+
+    :param bot_token: Telegram bot token. If not provided, it will be fetched from the 'TELEGRAM_BOT_TOKEN' environment variable.
+    :param chat_id: Chat ID to send messages to. If not provided, it will be fetched from the 'TELEGRAM_CHAT_ID' environment variable or determined dynamically.
+
+    :raises ValueError: If the bot token is not provided either as an argument or an environment variable.
+
+    Example:
+
+        .. code-block:: python
+
+            bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+            bot.send_message("Hello, world!")
+            bot.send_html_message("<b>Hello, world!</b>")
+            bot.send_plain_text_message("Just plain text.")
+            bot.send_dict_message({"key": "value"})
+
+    """
+
+    def __init__(self, bot_token=None, chat_id=None):
+        self.bot_token = (bot_token or os.getenv('TELEGRAM_BOT_TOKEN', '')).strip('\'"')
+        if not self.bot_token:
+            raise ValueError("Telegram bot token is required. Please set it as an argument or in the 'TELEGRAM_BOT_TOKEN' environment variable.")
+
+        self.chat_id = chat_id
+        self.api_url = f"https://api.telegram.org/bot{self.bot_token}"
+        self.chat_id = (chat_id or os.getenv('TELEGRAM_CHAT_ID', '')).strip('\'"')
+
+        if not self.chat_id:
+            self.chat_id = self.get_chat_id()
+
+        pawn.console.debug(f"bot_token={self.bot_token}, chat_id={self.chat_id}")
+
+    def escape_markdown(self, text):
+        """
+        Escape Markdown special characters for MarkdownV2.
+
+        :param text: The text to escape.
+        :return: The escaped text.
+
+        Example:
+
+            .. code-block:: python
+
+                bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+                escaped_text = bot.escape_markdown("Hello *world*!")
+                print(escaped_text)  # Output: Hello \*world\*!
+        """
+        escape_chars = r'_*[]()~`>#+-=|{}.!'
+        return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', str(text))
+
+    def send_message(self, message, parse_mode="Markdown", disable_web_page_preview=False):
+        """
+        Send a message to the Telegram chat.
+
+        :param message: The message to send.
+        :param parse_mode: The parse mode for the message ('MarkdownV2' or 'HTML').
+        :param disable_web_page_preview: Whether to disable web page preview.
+        :return: The response from the Telegram API if successful, None otherwise.
+
+        Example:
+
+            .. code-block:: python
+
+                bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+                response = bot.send_message("Hello, world!")
+                print(response)
+        """
+        payload = {
+            "chat_id": self.chat_id,
+            "text": self.escape_markdown(message) if parse_mode == "MarkdownV2" else message,
+            "parse_mode": parse_mode,
+            "disable_web_page_preview": disable_web_page_preview
+        }
+        response = requests.post(f"{self.api_url}/sendMessage", json=payload)
+        if response.status_code == 200:
+            return response.json()  # 성공적으로 메시지를 보낸 경우
+        else:
+            print(f"Failed to send message: {response.status_code} - {response.text}")
+            return None
+
+    def send_html_message(self, message):
+        """
+        Send a message using HTML formatting.
+
+        :param message: The message to send.
+        :return: The response from the Telegram API if successful, None otherwise.
+
+        Example:
+
+            .. code-block:: python
+
+                bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+                response = bot.send_html_message("<b>Hello, world!</b>")
+                print(response)
+        """
+        return self.send_message(message, parse_mode="HTML")
+
+    def send_plain_text_message(self, message):
+        """
+        Send a plain text message without any formatting.
+
+        :param message: The message to send.
+        :return: The response from the Telegram API if successful, None otherwise.
+
+        Example:
+
+            .. code-block:: python
+
+                bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+                response = bot.send_plain_text_message("Just plain text.")
+                print(response)
+        """
+        return self.send_message(message)
+
+    def send_dict_message(self, message_dict):
+        """
+        Send a dictionary as a JSON formatted message.
+
+        :param message_dict: The dictionary to send.
+        :return: The response from the Telegram API if successful, None otherwise.
+
+        Example:
+
+            .. code-block:: python
+
+                bot = TelegramBot(bot_token="your_bot_token", chat_id="your_chat_id")
+                response = bot.send_dict_message({"key": "value"})
+                print(response)
+        """
+        message = json.dumps(message_dict, indent=2)
+        return self.send_plain_text_message(message)
+
+    def get_chat_id(self):
+        """Retrieve chat_id by getting updates from the Telegram bot API"""
+        response = requests.get(f"{self.api_url}/getUpdates")
+        if response.status_code == 200:
+            data = response.json()
+            if "result" in data and len(data["result"]) > 0:
+                chat_id = data["result"][-1]["message"]["chat"]["id"]
+                pawn.console.debug(f"Retrieved chat_id: {chat_id}")
+                return chat_id
+            else:
+                raise ValueError("No messages found in bot updates to retrieve chat_id.")
+        else:
+            raise ConnectionError(f"Failed to retrieve updates: {response.status_code} - {response.text}")
+
+    def save_chat_id(self, chat_id_file="chat_id.txt"):
+        """Save the chat_id to a file for later use"""
+        with open(chat_id_file, "w") as file:
+            file.write(str(self.chat_id))  # chat_id를 문자열로 변환하여 저장
+        pawn.console.debug(f"chat_id saved to {chat_id_file}")
+
+    def load_chat_id(self, chat_id_file="chat_id.txt"):
+        """Load the chat_id from a file"""
+        if os.path.exists(chat_id_file):
+            with open(chat_id_file, "r") as file:
+                self.chat_id = file.read().strip()
+            pawn.console.debug(f"Loaded chat_id from {chat_id_file}")
+        else:
+            pawn.console.debug(f"chat_id file {chat_id_file} not found. Retrieving chat_id using getUpdates...")
+            self.chat_id = self.get_chat_id()
+
+    def send_auto_message(self, message):
+        """Automatically detect the type of message and send it appropriately"""
+        if isinstance(message, dict):
+            self.send_dict_message(message)
+        elif isinstance(message, str):
+            if self.is_html(message):
+                self.send_html_message(message)
+            elif self.is_markdown(message):
+                self.send_message(message, parse_mode="Markdown")
+            else:
+                self.send_plain_text_message(message)
+        else:
+            raise ValueError("Unsupported message type")
+
+    def is_html(self, message):
+        """Check if the string contains HTML tags"""
+        return bool(re.search(r'<[^>]+>', message))
+
+    def is_markdown(self, message):
+        """Check if the string contains MarkdownV2 special characters"""
+        # MarkdownV2에서 사용하는 특수 문자를 검사
+        markdown_special_chars = r'[_*[\]()~`>#+-=|{}.!]'
+        return bool(re.search(f'[{re.escape(markdown_special_chars)}]', message))
+
+
+def get_level_color(c_level):
+    default_color = "5be312"
+    return dict(
+        info="5be312",
+        warn="f2c744",
+        warning="f2c744",
+        error="f70202",
+    ).get(c_level, default_color)
+
+
+def send_slack(url, msg_text, title=None, send_user_name="CtxBot", msg_level='info'):
+    """
+
+    Send to slack message
+
+    :param url: webhook url
+    :param msg_text:
+    :param title:
+    :param send_user_name:
+    :param msg_level:
+    :return:
+    """
+
+    if title:
+        msg_title = title
+    else:
+        msg_title = shorten_text(msg_text, width=50)
+
+    msg_level = msg_level.lower()
+
+    if url is None:
+        pawn.error_logger.error("[ERROR] slack webhook url is None")
+        return False
+    p_color = get_level_color(msg_level)
+
+    payload = {
+        # https://app.slack.com/block-kit-builder
+        "username": send_user_name,
+        "text": msg_title,
+        "blocks": [
+            {"type": "divider"}
+        ],
+        "attachments": [
+            {
+                "color": "#" + p_color,
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": msg_title
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": f'{"+ [HOST]":^12s} : {net.get_hostname()}, {net.get_public_ip()}'
+                        }
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": f'{"+ [DATE]":^12s} : {(date_utils.todaydate("log"))}'
+                        }
+                    },
+                    # {
+                    #     "type": "section",
+                    #     "text": {
+                    #         "type": "plain_text",
+                    #         "text": f'{"+ [DESC]":^12s} : {msg_text}'
+                    #     }
+                    # }
+                ]
+            }
+        ]
+    }
+
+    def _make_attachment(key=None, value=None):
+        if key:
+            text = f'💡{key:<12s}: {value}'
+        elif not key:
+            text = f'{"+ [DESC]":^12s} : {msg_text}'
+        else:
+            text = ""
+
+        return {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": text
+            },
+        }
+    _attachments = []
+    for attachment in payload["attachments"]:
+        if isinstance(msg_text, dict):
+            for key, value in msg_text.items():
+                if key:
+                    attachment['blocks'].append(_make_attachment(key, value))
+        elif isinstance(msg_text, list):
+            for value_in_list in msg_text:
+                if value_in_list:
+                    attachment['blocks'].append(_make_attachment(value=value_in_list))
+        elif msg_text:
+            attachment['blocks'].append(_make_attachment(value=msg_text))
+        _attachments.append(attachment)
+    payload["attachments"] = _attachments
+    try:
+        post_result = requests.post(url, json=payload, verify=False, timeout=15)
+        if post_result and post_result.status_code == 200 and post_result.text == "ok":
+            pawn.app_logger.info("[OK][Slack] Send slack")
+            return True
+        else:
+            pawn.error_logger.error(f"[ERROR][Slack] Got errors, status_code={post_result.status_code}, text={shorten_text(post_result.text, 50)}")
+            return False
+
+    except Exception as e:
+        pawn.error_logger.error(f"[ERROR][Slack] Got errors -> {e}")
+        return False
+
+
+def send_slack_token(title=None, message=None, token=None, channel_name=None, send_user="python_app", msg_level="info"):
+    if title:
+        msg_title = title
+    else:
+        msg_title = message
+
+    p_color = get_level_color(msg_level)
+
+    attachments = [
+        {
+            # "pretext": f"[{send_user}] {title}",
+            "title": str(title).capitalize(),
+            # "text": f"{converter.todaydate('ms')}  {message}",
+            # "mrkdwn_in": ["pretext"]
+        },
+        {
+            "color": "#" + p_color,
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f'Title : {msg_title}'
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f'{"+ [HOST]":^12s} : {net.get_hostname()}, {net.get_public_ip()}'
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f'{"+ [DATE]":^12s} : {(date_utils.todaydate("log"))}'
+                    }
+                },
+            ],
+            # "mrkdwn_in": ["blocks"],
+        }
+    ]
+    if isinstance(message, dict):
+        if attachments[-1].get("blocks"):
+            for message_k, message_v in message.items():
+                attachments[-1]['blocks'].append(
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": f"+ [{message_k:<12s}]: {message_v}"
+                        }
+                    }
+                )
+    else:
+        attachments[-1]['blocks'].append(
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": f"{'+ [DESC]':^12s} : {message}"
+                }
+            }
+        )
+
+    payload = {
+        "channel": channel_name,
+        "attachments": attachments
+    }
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        res = http.jequest(url='https://slack.com/api/chat.postMessage', method="post", payload=payload, headers=headers)
+
+        if res and res.get('status_code') == 200 and res['json']['ok'] == True:
+            pawn.app_logger.info(f"[OK][Slack] Send slack with token")
+            return True
+        else:
+            pawn.error_logger.error(f"[ERROR][Slack] Got errors, status_code={res.get('status_code')}, text={res.get('text')}")
+            return False
+
+    except Exception as e:
+        pawn.error_logger.error(f"[ERROR][Slack] Got errors -> {e}")
+        return False
+
