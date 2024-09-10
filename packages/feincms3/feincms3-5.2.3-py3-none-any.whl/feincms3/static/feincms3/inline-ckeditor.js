6e34c803978f20b12a2a2fd86520a113e75396d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:05:08 +0000

    Add package feincms3/5.2.3/feincms3-5.2.3-py3-none-any.whl

---
+++ packages/feincms3/feincms3-5.2.3-py3-none-any.whl/feincms3/static/feincms3/inline-ckeditor.js
@@ -0,0 +1,46 @@
+/* global CKEDITOR, django */
+;(() => {
+  function onReady(fn) {
+    if (
+      document.readyState === "complete" ||
+      document.readyState === "interactive"
+    ) {
+      // call on next available tick
+      setTimeout(fn, 1)
+    } else {
+      document.addEventListener("DOMContentLoaded", fn)
+    }
+  }
+
+  onReady(() => {
+    const configs = {}
+    const scripts = document.querySelectorAll("[data-inline-cke-config]")
+    scripts.forEach(function parseConfig(script) {
+      configs[script.dataset.inlineCkeId] = JSON.parse(
+        script.dataset.inlineCkeConfig,
+      )
+    })
+
+    function initializeInlineCKE() {
+      document.querySelectorAll("textarea[data-inline-cke]").forEach((el) => {
+        if (
+          el.dataset.inlineCke !== "active" &&
+          !el.id.includes("__prefix__")
+        ) {
+          CKEDITOR.replace(el, configs[el.dataset.inlineCke])
+          el.dataset.inlineCke = "active"
+        }
+      })
+    }
+
+    function addFormsetAddedHandler() {
+      django.jQuery(document).on("formset:added", initializeInlineCKE)
+    }
+
+    initializeInlineCKE()
+
+    if (window.django) {
+      addFormsetAddedHandler()
+    }
+  })
+})()
