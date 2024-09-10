5eaebb7c3c8d979c8be408cf57d16bca9a61bb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:04:36 +0000

    Add package tcrutils/12.0.262/tcrutils-12.0.262.tar.gz

---
+++ packages/tcrutils/tcrutils-12.0.262.tar.gz/tcrutils-12.0.262/tcrutils/dr/__init__.py
@@ -0,0 +1,12 @@
+try:
+  from . import placeholder_sets as placeholder_set
+  from . import placeholders as placeholder
+  from .core import (
+    DynamicResponseBuilder,
+    DynamicResponseError,
+    DynamicResponseMissingPlaceholderError,
+    DynamicResponsePlaceholderInvalidReturnError,
+    DynamicResponseRecursionError,
+    DynamicResponseSyntaxError,
+  )
+except ImportError: ...
