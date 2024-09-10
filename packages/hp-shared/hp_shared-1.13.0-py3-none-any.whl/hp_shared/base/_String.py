5c58cce11b0b8920f6407b9cf9f2f0e5ff2bb04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:04:50 +0000

    Add package hp-shared/1.13.0/hp_shared-1.13.0-py3-none-any.whl

---
+++ packages/hp-shared/hp_shared-1.13.0-py3-none-any.whl/hp_shared/base/_String.py
@@ -0,0 +1,6 @@
+from .es import String
+
+class _String(String):
+  pass
+
+__all__ = ['_String']
---
+++ packages/hp-shared/hp_shared-1.13.0-py3-none-any.whl/hp_shared/base/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+
+from ._Array import *
+from ._console import *
+from ._Date import *
+from ._Function import *
+from ._Math import *
+from ._Number import *
+from ._Object import *
+from ._Set import *
+from ._String import *
