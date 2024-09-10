c2cb0212bc40722030caf1e76fc4bc4aa30e0753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:05:01 +0000

    Add package tapenade/0.0.2/tapenade-0.0.2-py3-none-any.whl

---
+++ packages/tapenade/tapenade-0.0.2-py3-none-any.whl/tapenade/preprocessing/__init__.py
@@ -0,0 +1,27 @@
+from ._preprocessing import (
+    change_array_pixelsize,
+    compute_mask,
+    local_image_equalization,
+    normalize_intensity,
+    align_array_major_axis,
+    align_array_major_axis_from_files,
+    crop_array_using_mask,
+    crop_array_using_mask_from_files,
+    masked_gaussian_smooth,
+    masked_gaussian_smooth_dense_two_arrays_gpu,
+    masked_gaussian_smooth_sparse
+)
+
+__all__ = [
+    "change_array_pixelsize",
+    "compute_mask",
+    "local_image_equalization",
+    "normalize_intensity",
+    "align_array_major_axis",
+    "align_array_major_axis_from_files",
+    "crop_array_using_mask",
+    "crop_array_using_mask_from_files",
+    "masked_gaussian_smooth",
+    "masked_gaussian_smooth_dense_two_arrays_gpu",
+    "masked_gaussian_smooth_sparse",
+]
