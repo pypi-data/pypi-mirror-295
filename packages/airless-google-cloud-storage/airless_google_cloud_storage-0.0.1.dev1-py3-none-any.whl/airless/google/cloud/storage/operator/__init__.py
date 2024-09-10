818dd7ab70f582fc15a5b31ee62bd2575990e90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:03:31 +0000

    Add package airless-google-cloud-storage/0.0.1.dev1/airless_google_cloud_storage-0.0.1.dev1-py3-none-any.whl

---
+++ packages/airless-google-cloud-storage/airless_google_cloud_storage-0.0.1.dev1-py3-none-any.whl/airless/google/cloud/storage/operator/__init__.py
@@ -0,0 +1,10 @@
+from .file import (FileUrlToGcsOperator)
+from .ftp import (FtpToGcsOperator)
+from .storage import (
+    FileDetectOperator,
+    FileToBigqueryOperator,
+    BatchWriteDetectOperator,
+    BatchWriteProcessOperator,
+    FileDeleteOperator,
+    FileMoveOperator
+)
