49fcef02331632c006a292f8d5d370589d152bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:04:16 +0000

    Add package oauthAzureFastApi/0.4.6/oauthazurefastapi-0.4.6.tar.gz

---
+++ packages/oauthAzureFastApi/oauthazurefastapi-0.4.6.tar.gz/oauthazurefastapi-0.4.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oauthAzureFastApi',
+    version='0.4.6',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'authlib',
+        'httpx',
+        'python-dotenv',
+        'python-jose',
+        'uvicorn',
+        'starlette',
+        'itsdangerous'
+    ],
+    description='A FastAPI module for Azure OAuth integration',
+    long_description=open("README.md").read(),  # or README.rst
+    long_description_content_type="text/markdown",  # or "text/x-rst"
+    author='Saubhik Bhadra,Kaushik Bhadra',
+    author_email='saubhik.bhadra@gmail.com,kaushik.bhadra@gmail.com',
+    maintainer='Saubhik Bhadra, Kaushik Bhadra',  
+    maintainer_email='saubhik.bhadra@gmail.com, kaushik.bhadra@gmail.com',  
+    url='https://github.com/saubhik1/oauthAzureFastApi',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
