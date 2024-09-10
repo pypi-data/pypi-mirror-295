14d7bf6debedad4898a3ef056dfcc42155735f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:59 +0000

    Add package LoggerManager/0.1.3/loggermanager-0.1.3.tar.gz

---
+++ packages/LoggerManager/loggermanager-0.1.3.tar.gz/loggermanager-0.1.3/LoggerManager/utils/__init__.py
@@ -0,0 +1,19 @@
+from LoggerManager.utils.errors import LoggerErrors
+from LoggerManager.utils.logger_typing import LogLevel, ColorName
+from LoggerManager.utils.internal_logger import InternalLogger
+from LoggerManager.utils.formatter import ConsoleFormatter, FileFormatter
+from LoggerManager.utils.filter import MultiLevelFilter, LevelFilter
+from LoggerManager.utils.level_decorator import level_setting_decorator
+from LoggerManager.utils.time_execution import TimeExecutionLogger
+
+
+
+
+
+
+
+
+
+
+
+
