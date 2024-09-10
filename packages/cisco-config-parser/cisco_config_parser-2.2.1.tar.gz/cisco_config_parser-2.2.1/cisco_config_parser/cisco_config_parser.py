cba2c9092eb41f506ce98e49daff894cc782dff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:04:04 +0000

    Add package cisco-config-parser/2.2.1/cisco_config_parser-2.2.1.tar.gz

---
+++ packages/cisco-config-parser/cisco_config_parser-2.2.1.tar.gz/cisco_config_parser-2.2.1/cisco_config_parser/cisco_config_parser.py
@@ -0,0 +1,3 @@
+
+from .parser import ConfigParser
+from .parser import ConfigParserOld
---
+++ packages/cisco-config-parser/cisco_config_parser-2.2.1.tar.gz/cisco_config_parser-2.2.1/cisco_config_parser/helper/helper.py
@@ -0,0 +1,26 @@
+from .help import *
+
+
+class IOSRouteParser:
+    def __repr__(self):
+        return RTP_IOS_HELP
+
+
+
+class ParentChild:
+    def __repr__(self):
+        return PARENT_CHILD_HELP
+
+
+class L3Interface:
+    def __repr__(self):
+        return LAYER3_INTERFACE_HELP
+
+
+class L2Interface:
+    def __repr__(self):
+        return LAYER2_INTERFACE_HELP
+
+
+
+
