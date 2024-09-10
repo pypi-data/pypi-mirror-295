0db810841903df391b2be309c11e952b1cc58cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 06:03:57 +0000

    Add package edgeiq/2.6.0/edgeiq-2.6.0-cp310-none-macosx_11_0_arm64.whl

---
+++ packages/edgeiq/edgeiq-2.6.0-cp310-none-macosx_11_0_arm64.whl/edgeiq/object_detection/__init__.pyi
@@ -0,0 +1,6 @@
+from .object_detection import ObjectDetection as ObjectDetection, _build_object_detection_results as _build_object_detection_results, filter_predictions_by_label as filter_predictions_by_label, markup_image as markup_image
+from .object_detection_analytics import ObjectDetectionAnalytics as ObjectDetectionAnalytics
+from .object_detection_prediction import ObjectDetectionPrediction as ObjectDetectionPrediction
+from .object_detection_results import ObjectDetectionResults as ObjectDetectionResults
+
+__all__ = ['ObjectDetectionPrediction', 'ObjectDetectionResults', 'ObjectDetection', 'ObjectDetectionAnalytics', 'filter_predictions_by_label', 'markup_image', '_build_object_detection_results']
