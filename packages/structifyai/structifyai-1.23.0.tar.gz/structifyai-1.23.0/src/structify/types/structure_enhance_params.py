2173bfa81199bc8743073647bcaee21f5877d024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:04:12 +0000

    Add package structifyai/1.23.0/structifyai-1.23.0.tar.gz

---
+++ packages/structifyai/structifyai-1.23.0.tar.gz/structifyai-1.23.0/src/structify/types/structure_enhance_params.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["StructureEnhanceParams"]
+
+
+class StructureEnhanceParams(TypedDict, total=False):
+    entity_id: Required[str]
+
+    property_name: Required[str]
