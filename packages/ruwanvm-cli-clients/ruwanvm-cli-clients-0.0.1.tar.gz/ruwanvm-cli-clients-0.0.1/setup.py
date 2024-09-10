2c29c691d29cac8408da8251e50f61c63b688358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:05:08 +0000

    Add package ruwanvm-cli-clients/0.0.1/ruwanvm-cli-clients-0.0.1.tar.gz

---
+++ packages/ruwanvm-cli-clients/ruwanvm-cli-clients-0.0.1.tar.gz/ruwanvm-cli-clients-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'ruwanvm cli clients'
+LONG_DESCRIPTION = 'ruwanvm cli use to various miscellaneous tasks'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="ruwanvm-cli-clients",
+    version=VERSION,
+    author="Ruwan Vimukthi",
+    author_email="ruwanvm@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(include=['ruwanvm', 'ruwanvm.misc']),
+    install_requires=[
+        'pytz>=2024.1',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
