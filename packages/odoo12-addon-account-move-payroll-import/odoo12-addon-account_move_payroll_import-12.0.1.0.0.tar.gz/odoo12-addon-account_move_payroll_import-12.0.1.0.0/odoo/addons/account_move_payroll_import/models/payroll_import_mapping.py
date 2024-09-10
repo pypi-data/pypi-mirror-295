35f73e0e914934038fcd84c391f472fb11a86471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:03:37 +0000

    Add package odoo12-addon-account-move-payroll-import/12.0.1.0.0/odoo12-addon-account_move_payroll_import-12.0.1.0.0.tar.gz

---
+++ packages/odoo12-addon-account-move-payroll-import/odoo12-addon-account_move_payroll_import-12.0.1.0.0.tar.gz/odoo12-addon-account_move_payroll_import-12.0.1.0.0/odoo/addons/account_move_payroll_import/models/payroll_import_mapping.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, _
+
+
+class PayrollImportMapping(models.Model):
+    _name = "payroll.import.mapping"
+    _description = "Payroll Import Mapping"
+
+    name = fields.Char(string="Name", required=True, default="payroll.import.setup")
+    res_model = fields.Char(string=_("Model"))
+    res_field = fields.Char(string=_("Field"))
+    column_field = fields.Char(string=_("Column Field"), required=True)
+    account_field = fields.Char(string=_("Account Field"))
+    tax_field = fields.Char(string=_("Tax Field"))
+    aggregate_field = fields.Char(string=_("Aggregate Flag Field"))
+    payroll_import_setup_id = fields.Many2one(
+        comodel_name="payroll.import.setup",
+        string=_("Payroll Import Setup"),
+        required=True,
+        ondelete="cascade",
+        default=lambda self: self.env.ref(
+            "account_move_payroll_import.payroll_import_setup_a3_2024", False
+        ),
+    )
+
+    _sql_constraints = [
+        (
+            "unique_mapping",
+            "unique(name, res_model, res_field, column_field)",
+            "This mapping already exists.",
+        )
+    ]
