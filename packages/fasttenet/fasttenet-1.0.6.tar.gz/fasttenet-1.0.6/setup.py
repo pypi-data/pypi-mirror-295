0359de2fbec891861f1ecd5fcaafd48cf04e1059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:54 +0000

    Add package fasttenet/1.0.6/fasttenet-1.0.6.tar.gz

---
+++ packages/fasttenet/fasttenet-1.0.6.tar.gz/fasttenet-1.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='fasttenet',
+      version='1.0.6',
+      description='FastTENET',
+      url='http://github.com/cxinsys/fasttenet',
+      author='Complex Intelligent Systems Laboratory (CISLAB)',
+      author_email='daewon4you@gmail.com',
+      license='BSD-3-Clause',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=['numpy', 'statsmodels', 'networkx', 'tqdm', 'matplotlib', 'omegaconf', 'mate-cxinsys'],
+      zip_safe=False,)
