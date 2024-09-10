a8823fe487bfcd3e82c08de23e35559f098c4633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 06:04:34 +0000

    Add package coingecko-openapi-client/1.3.0/coingecko-openapi-client-1.3.0.tar.gz

---
+++ packages/coingecko-openapi-client/coingecko-openapi-client-1.3.0.tar.gz/coingecko-openapi-client-1.3.0/coingecko_openapi_client/api/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from coingecko_openapi_client.api.asset_platforms_api import AssetPlatformsApi
+from coingecko_openapi_client.api.categories_api import CategoriesApi
+from coingecko_openapi_client.api.coins_api import CoinsApi
+from coingecko_openapi_client.api.companies__beta_api import CompaniesBetaApi
+from coingecko_openapi_client.api.contract_api import ContractApi
+from coingecko_openapi_client.api.derivatives_api import DerivativesApi
+from coingecko_openapi_client.api.exchange_rates_api import ExchangeRatesApi
+from coingecko_openapi_client.api.exchanges_api import ExchangesApi
+from coingecko_openapi_client.api.global_api import GlobalApi
+from coingecko_openapi_client.api.nfts__beta_api import NftsBetaApi
+from coingecko_openapi_client.api.ping_api import PingApi
+from coingecko_openapi_client.api.search_api import SearchApi
+from coingecko_openapi_client.api.simple_api import SimpleApi
+from coingecko_openapi_client.api.trending_api import TrendingApi
