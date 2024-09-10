69132788f1ef19328e82cfb2290a9b11edbcbd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:06:02 +0000

    Add package disnake-jishaku-ru/1.5.5/disnake-jishaku-ru-1.5.5.tar.gz

---
+++ packages/disnake-jishaku-ru/disnake-jishaku-ru-1.5.5.tar.gz/disnake-jishaku-ru-1.5.5/jishaku/meta.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MIT
+
+from collections import namedtuple
+
+import pkg_resources
+
+__all__ = (
+    '__author__',
+    '__copyright__',
+    '__docformat__',
+    '__license__',
+    '__title__',
+    '__version__',
+    'version_info'
+)
+
+# pylint: disable=invalid-name
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+version_info = VersionInfo(major=1, minor=5, micro=5, releaselevel='final', serial=0)
+
+__author__ = 'Darkness800'
+__copyright__ = 'Copyright 2021 Devon (Gorialis) R, Copyright (c) 2024 MayaTeam'
+__docformat__ = 'restructuredtext en'
+__license__ = 'MIT'
+__title__ = 'jishaku'
+__version__ = '.'.join(map(str, (version_info.major, version_info.minor, version_info.micro)))
+
+# This ensures that when jishaku is reloaded, pkg_resources requeries it to provide correct version info
+pkg_resources.working_set.by_key.pop('jishaku', None)
