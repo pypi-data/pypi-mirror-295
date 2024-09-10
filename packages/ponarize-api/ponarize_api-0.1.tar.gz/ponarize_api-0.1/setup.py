2bc62f3e2e9b91039facb1824d10d98691cfd911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:12 +0000

    Add package ponarize-api/0.1/ponarize_api-0.1.tar.gz

---
+++ packages/ponarize-api/ponarize_api-0.1.tar.gz/ponarize_api-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ponarize_api',
+    version='0.1',
+    description='Relevant, accurate and fast classification of Domains. Blacklist control for aggressive IPs.',
+    author='PonaTech',
+    author_email='info@pona.com.tr',
+    packages=find_packages(),
+    install_requires=[],
+)
