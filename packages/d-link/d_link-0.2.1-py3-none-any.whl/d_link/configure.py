6bb4fc49d164048ce5fa138728a47778f7dde1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:04:44 +0000

    Add package d-link/0.2.1/d_link-0.2.1-py3-none-any.whl

---
+++ packages/d-link/d_link-0.2.1-py3-none-any.whl/d_link/configure.py
@@ -0,0 +1,12 @@
+# d_link/configure.py
+from settings_manager import SettingsManager
+
+def configure():
+    """Script to configure d_link settings."""
+    settings_manager = SettingsManager()
+    settings_manager.display_settings()
+    settings_manager.set_repository()
+    print("Configuration complete. Your settings have been saved to environment variables.")
+
+if __name__ == "__main__":
+    configure()
