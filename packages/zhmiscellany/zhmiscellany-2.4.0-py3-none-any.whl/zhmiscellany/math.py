9a8dc2ec48174520e80f410093e6301488633e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:34 +0000

    Add package zhmiscellany/2.4.0/zhmiscellany-2.4.0-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-2.4.0-py3-none-any.whl/zhmiscellany/math.py
@@ -0,0 +1,23 @@
+
+
+def smart_percentage(things, total_things):
+    if total_things == 0:
+        return total_things
+    else:
+        div = 0
+        while 100/total_things < 1/(10**div):
+            div += 1
+        percentage = round((things/total_things)*100, div)
+        if str(percentage).endswith('.0'):
+            return round(percentage)
+        else:
+            return percentage
+
+
+def calculate_evenly_spaced_points(duration, segments, offset=0):
+    interval = duration / (segments - 1)
+    return [(interval * i)+offset for i in range(segments)]
+
+
+def clamp(value, minimum, maximum):
+    return max(min(value, maximum), minimum)
