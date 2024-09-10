77344776ddf3184e34cbc526c79ab61ad4eb4e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:03:59 +0000

    Add package benchmarker-py/0.0.0.1/benchmarker-py-0.0.0.1.tar.gz

---
+++ packages/benchmarker-py/benchmarker-py-0.0.0.1.tar.gz/benchmarker-py-0.0.0.1/README.md
@@ -0,0 +1,8 @@
+
+# Security Holding Package
+
+This is a security holding package, a placeholder for a deleted package susceptible to hijacking.
+
+(The package is completely empty)
+
+For inquiries: [sec_reporting@service.jfrog.com](mailto:sec_reporting@service.jfrog.com)
---
+++ packages/benchmarker-py/benchmarker-py-0.0.0.1.tar.gz/benchmarker-py-0.0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='benchmarker-py',
+    version='0.0.0.1',
+    packages=['benchmarker-py'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
+    
