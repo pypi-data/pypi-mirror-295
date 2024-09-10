712370c34133da8d31366fc349d0c9a19bce76a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:06:31 +0000

    Add package lced-utils/0.0.1/lced_utils-0.0.1-py3-none-any.whl

---
+++ packages/lced-utils/lced_utils-0.0.1-py3-none-any.whl/lced_utils/file_utils.py
@@ -0,0 +1,35 @@
+import os
+
+
+def create_required_directories(directories):
+    """
+    检查并创建必需的目录
+    :param directories: 必需的目录列表
+    """
+    project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    for directory in directories:
+        directory_full_path = os.path.join(project_root_path, directory)
+        if not os.path.exists(directory_full_path):
+            try:
+                os.makedirs(directory)
+            except OSError as e:
+                pass
+
+
+def get_file_extensions(directory):
+    """
+    获取目录中所有文件的扩展名
+    :param directory: 目录路径
+    :return: 文件扩展名的集合
+    """
+    extensions = set()
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            _, ext = os.path.splitext(filename)
+            if ext:
+                extensions.add(ext)
+    return extensions
+
+
+if __name__ == "__main__":
+    create_required_directories(["logs"])
