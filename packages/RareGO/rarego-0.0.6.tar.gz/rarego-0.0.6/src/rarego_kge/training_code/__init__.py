e5dc8fa7efca9d973e0449d30ac32ccc91b7e862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:04:49 +0000

    Add package RareGO/0.0.6/rarego-0.0.6.tar.gz

---
+++ packages/RareGO/rarego-0.0.6.tar.gz/rarego-0.0.6/src/rarego_kge/training_code/__init__.py
@@ -0,0 +1,11 @@
+from .config import config 
+from .KGEtrainclass import KnowledgeGraphEmbedding
+
+from .kgio import data_process
+
+__all__ = [
+    "data_process",
+"config","KnowledgeGraphEmbedding",
+    # Add other functions or classes that should be accessible from the top level
+]
+
