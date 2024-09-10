e627a8efd8943140ed2bd2e191d32d3ae32314f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:05:24 +0000

    Add package fkg-utils/0.1.7/fkg_utils-0.1.7.tar.gz

---
+++ packages/fkg-utils/fkg_utils-0.1.7.tar.gz/fkg_utils-0.1.7/fkg_utils/utils/utils.py
@@ -0,0 +1,9 @@
+import hashlib
+
+
+def encode_md5_text(text: str):
+    sha1_hash = hashlib.sha1(text.encode()).digest()
+    sha1_hex = sha1_hash.hex()
+    md5_hash = hashlib.md5(sha1_hex.lower().encode()).digest()
+    md5_hex = md5_hash.hex()
+    return md5_hex.lower()
