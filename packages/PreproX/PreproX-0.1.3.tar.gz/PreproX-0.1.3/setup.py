81bd8503e7251d81841d09d08f834b5ed617db5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:14 +0000

    Add package PreproX/0.1.3/PreproX-0.1.3.tar.gz

---
+++ packages/PreproX/PreproX-0.1.3.tar.gz/PreproX-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# Read the README.md file for a long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PreproX',  # Your package name
+    version='0.1.3',
+    description='A Python library for data preprocessing suggestions and transformations',
+    long_description=long_description,  # This uses README.md as the long description
+    long_description_content_type='text/markdown',
+    author='Samama Farooq',
+    author_email='samama4200@gmail.com',
+    url='https://github.com/Sam-Coding77/DataWiz',  # Optional, replace with your repo URL
+    packages=find_packages(),  # Automatically find all packages in your project
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+        'category-encoders',
+        'psutil',
+        'scipy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
