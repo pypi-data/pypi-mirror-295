56644e7f568d13018b72ced3b7110d52831f5cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:45 +0000

    Add package mtmai/0.3.724/mtmai-0.3.724-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.724-py3-none-any.whl/mtmai/initial_data.py
@@ -0,0 +1,21 @@
+from sqlmodel import Session
+
+from mtmai.core.db import engine, init_db
+from mtmai.core.logging import get_logger
+
+logger = get_logger()
+
+
+def init() -> None:
+    with Session(engine) as session:
+        init_db(session)
+
+
+def main() -> None:
+    logger.info("Creating initial data")
+    init()
+    logger.info("Initial data created")
+
+
+if __name__ == "__main__":
+    main()
