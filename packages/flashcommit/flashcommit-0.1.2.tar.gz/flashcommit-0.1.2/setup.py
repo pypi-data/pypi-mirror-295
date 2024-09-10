8a3ba5418f18c74e1e66da2f613d349bd5cb51e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:04:40 +0000

    Add package flashcommit/0.1.2/flashcommit-0.1.2.tar.gz

---
+++ packages/flashcommit/flashcommit-0.1.2.tar.gz/flashcommit-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flashcommit',
+    version='0.1.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'fcommit=flashcommit.main:main',
+        ],
+    },
+    python_requires=">=3.8.0",
+    install_requires=[
+        'setuptools',
+        'requests',
+        'argparse',
+        'pydantic',
+        'gitpython',
+        'pydriller',
+        'python-dotenv~=1.0.1',
+        'websocket-client',
+        'rich',
+        'prompt_toolkit',
+    ],
+)
