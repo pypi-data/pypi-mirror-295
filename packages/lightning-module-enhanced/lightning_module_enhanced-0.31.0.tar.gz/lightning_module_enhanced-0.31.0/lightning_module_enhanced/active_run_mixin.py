8dc28c92b8644ccf7d43dc606297cce60a94941b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:13 +0000

    Add package lightning-module-enhanced/0.31.0/lightning_module_enhanced-0.31.0.tar.gz

---
+++ packages/lightning-module-enhanced/lightning_module_enhanced-0.31.0.tar.gz/lightning_module_enhanced-0.31.0/lightning_module_enhanced/active_run_mixin.py
@@ -0,0 +1,78 @@
+"""ActiveRunMixin. Helper class to track stuff during runs (training, testing, predicting)"""
+from __future__ import annotations
+from copy import deepcopy
+import torch as tr
+from torch import nn
+from .metrics import CoreMetric
+from .utils import tr_detach_data
+
+# pylint: disable=abstract-method
+class ActiveRunMixin(nn.Module):
+    """
+    Helper class to keep track of properties that are updated during a run started via Trainer.fit, Trainer.test or
+    Trainer.predict.
+    Note: "loss" keys is always included here, as it's supposed that any Trainer active run must have a loss function.
+    """
+    def __init__(self):
+        super().__init__()
+        # Updated during the epochs of an actieve run (i.e. Trainer.fit, Trainer.test or Trainer.predict).
+        self._active_run_metrics: dict[str, dict[str, CoreMetric]] = {}
+
+    def _setup_active_metrics(self):
+        """sets up self.active_run_metrics based on metrics for this train run. Called at on_fit_start."""
+        if len(self._active_run_metrics) > 0:
+            raise ValueError("TODO: add breakpoint here to understand if/where it's hit")
+        self._active_run_metrics = {"": {"loss": self.criterion_fn, **self.metrics}}
+        if hasattr(self, "trainer") and self.trainer.enable_validation:
+            self._active_run_metrics["val_"] = deepcopy(self._active_run_metrics[""])
+
+    def _reset_all_active_metrics(self):
+        """ran at epoch end to reset the metrics"""
+        for prefix in self._active_run_metrics.keys():
+            for metric in self._active_run_metrics[prefix].values():
+                metric.reset()
+
+    def _set_metrics_running_model(self):
+        """ran at fit/test start to set the running model"""
+        for prefix in self._active_run_metrics.keys():
+            for metric in self._active_run_metrics[prefix].values():
+                metric.running_model = lambda: self
+
+    def _unset_metrics_running_model(self):
+        """ran at fit/test end to unset the running model"""
+        for prefix in self._active_run_metrics.keys():
+            for metric in self._active_run_metrics[prefix].values():
+                metric.running_model = None
+        self._active_run_metrics = {}
+
+    def _update_metrics_at_batch_end(self, batch_results: dict[str, tr.Tensor]):
+        assert isinstance(batch_results, dict), f"Expected dict, got {type(batch_results)}"
+        assert "loss" in batch_results.keys(), f"Loss must be in batch_metrics. Got: {list(batch_results.keys())}"
+        batch_metrics = set(batch_results.keys()) - {"loss"}
+
+        if batch_metrics != (expected_metrics := set(self.metrics.keys())):
+            raise ValueError(f"Expected metrics: {expected_metrics} vs. this batch: {batch_results.keys()}")
+
+        batch_results_detach = tr_detach_data(batch_results)
+        prefix = self._prefix_from_trainer()
+        for metric_name, metric in self._active_run_metrics[prefix].items():
+            metric.batch_update(batch_results_detach[metric_name])
+
+    def _run_and_log_metrics_at_epoch_end(self):
+        """Runs and logs a given list of logged metrics. Assume they all exist in self.metrics"""
+        all_prefixes = self._active_run_metrics.keys()
+        metrics_to_log = list(self._active_run_metrics[""].keys())
+        for metric_name in metrics_to_log:
+            for prefix in all_prefixes:
+                metric_fn: CoreMetric = self._active_run_metrics[prefix][metric_name]
+                # Get the metric's epoch result
+                metric_epoch_result = metric_fn.epoch_result()
+                # Log the metric at the end of the epoch. Only log on pbar the val_loss, loss is tracked by default
+                prog_bar = (metric_name == "loss" and prefix == "val_")
+
+                value_reduced = metric_fn.epoch_result_reduced(metric_epoch_result)
+                if value_reduced is not None:
+                    self.log(f"{prefix}{metric_name}", value_reduced, prog_bar=prog_bar, on_epoch=True)
+                # Call the metadata callback for the full result, since it can handle any sort of metrics
+                self.metadata_callback.log_epoch_metric(metric_name, metric_epoch_result,
+                                                        self.trainer.current_epoch, prefix)
---
+++ packages/lightning-module-enhanced/lightning_module_enhanced-0.31.0.tar.gz/lightning_module_enhanced-0.31.0/lightning_module_enhanced/callbacks/metrics_history.py
@@ -0,0 +1,69 @@
+"""MetricsHistory callback. Keeps track of the metrics throughout a train run"""
+from __future__ import annotations
+from typing import Any
+from overrides import overrides
+import pytorch_lightning as pl
+
+from ..logger import lme_logger as logger
+
+class MetricsHistory(pl.Callback):
+    """MetricsHistory callback implementation"""
+    def __init__(self):
+        self.history: dict[str, list[float]] = None
+        self.expected_metrics = []
+        self.higher_is_better: dict[str, bool] = None
+
+    def _setup_metrics(self, pl_module: "LME"):
+        self.expected_metrics = [*list(pl_module.metrics.keys()), "loss"]
+        if self.history is not None:
+            assert len(self.history) > 0
+            for k in self.history.keys():
+                assert len(self.history[k]["train"]) > 0
+            assert (a := set(self.expected_metrics)) == (b := set(self.history.keys())), (a, b)
+        # see test_metrics_history_2 as to why we reset here
+        self.history = {metric_name: {"train": [], "val": []} for metric_name in self.expected_metrics}
+        self.higher_is_better = {name: metric.higher_is_better for name, metric in pl_module.metrics.items()}
+        self.higher_is_better["loss"] = False
+
+    @overrides
+    def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: "LME"):
+        if trainer.current_epoch == 0:
+            self._setup_metrics(pl_module)
+        else:
+            if self.history is not None and self.higher_is_better is None: # for old models TODO: remove at some point
+                self.higher_is_better = {name: metric.higher_is_better for name, metric in pl_module.metrics.items()}
+                self.higher_is_better["loss"] = False
+        assert self.history is not None, "self.history is None: on_train_epoch_end somehow called before on_fit_start."
+
+        for metric_name in self.expected_metrics:
+            if metric_name not in self.history:
+                logger.debug(f"Metric '{metric_name}' not in original metrics, probably added afterwards. Skipping")
+                continue
+            metric = pl_module._active_run_metrics[""][metric_name] # pylint: disable=protected-access
+            metric_score = metric.epoch_result_reduced(metric.epoch_result())
+            if metric_score is None:
+                logger.debug2(f"Metric '{metric_name}' cannot be reduced to a single number. Skipping")
+                continue
+            self.history[metric_name]["train"].append(metric_score.item())
+
+            if trainer.enable_validation:
+                val_metric = pl_module._active_run_metrics["val_"][metric_name] # pylint: disable=protected-access
+                val_metric_score = val_metric.epoch_result_reduced(val_metric.epoch_result())
+                self.history[metric_name]["val"].append(val_metric_score.item())
+
+    @overrides
+    def state_dict(self) -> dict[str, Any]:
+        return {"history": self.history, "expected_metrics": self.expected_metrics,
+                "higher_is_better": self.higher_is_better}
+
+    @overrides
+    def load_state_dict(self, state_dict: dict[str, Any]):
+        self.history = state_dict["history"]
+        self.expected_metrics = state_dict["expected_metrics"]
+        self.higher_is_better = state_dict.get("higher_is_better", None) # TODO: remove this
+
+    def __repr__(self):
+        return f"""[MetricsHistory]. { {k: f"{len(v['train'])} entries" for k, v in self.history.items()} }"""
+
+    def __getitem__(self, key: str):
+        return self.history[key]
