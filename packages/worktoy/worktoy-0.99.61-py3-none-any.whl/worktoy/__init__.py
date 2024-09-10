586aa13e4f220e3de756f48a9730279d472700e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:03:56 +0000

    Add package worktoy/0.99.61/worktoy-0.99.61-py3-none-any.whl

---
+++ packages/worktoy/worktoy-0.99.61-py3-none-any.whl/worktoy/__init__.py
@@ -0,0 +1,9 @@
+"""The 'worktoy' package provides a collection of utilities leveraging
+advanced python features including custom metaclasses and the descriptor
+protocol. The readme file included provides detailed documentation on the
+included features. """
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+__all__ = ['desc', 'ezdata', 'keenum', 'meta', 'parse', 'text']
