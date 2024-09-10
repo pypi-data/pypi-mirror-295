384a082a8a638a4957bdf73e6400542012883b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:05:10 +0000

    Add package argrelay/0.7.16.dev1/argrelay-0.7.16.dev1.tar.gz

---
+++ packages/argrelay/argrelay-0.7.16.dev1.tar.gz/argrelay-0.7.16.dev1/src/argrelay/runtime_data/StaticData.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+from argrelay.runtime_data.EnvelopeCollection import EnvelopeCollection
+
+
+@dataclass(frozen = True)
+class StaticData:
+    """
+    All data which normally does not change between requests.
+
+    See also `StaticDataSchema`.
+    """
+
+    envelope_collections: dict[str, EnvelopeCollection] = field()
+    """
+    MondoDB collection name to its `EnvelopeCollection`.
+
+    Collection name is normally (but not necessarily) matching
+    the value of `ReservedPropName.envelope_class` prop.
+    """
