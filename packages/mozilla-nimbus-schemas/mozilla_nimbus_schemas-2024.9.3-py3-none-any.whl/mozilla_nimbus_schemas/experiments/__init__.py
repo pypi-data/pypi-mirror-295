e891152ae1717f2cc580023d436fac3840985e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:03:50 +0000

    Add package mozilla-nimbus-schemas/2024.9.3/mozilla_nimbus_schemas-2024.9.3-py3-none-any.whl

---
+++ packages/mozilla-nimbus-schemas/mozilla_nimbus_schemas-2024.9.3-py3-none-any.whl/mozilla_nimbus_schemas/experiments/__init__.py
@@ -0,0 +1,4 @@
+from .experiments import NimbusExperiment, RandomizationUnit
+from .feature_manifests import FeatureManifest
+
+__all__ = ("NimbusExperiment", "RandomizationUnit", "FeatureManifest")
