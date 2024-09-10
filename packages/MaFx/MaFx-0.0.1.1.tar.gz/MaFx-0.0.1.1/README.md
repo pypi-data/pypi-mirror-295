b0f37f4af36fad662bce83620c63355f5ae9a2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:03:35 +0000

    Add package MaFx/0.0.1.1/MaFx-0.0.1.1.tar.gz

---
+++ packages/MaFx/MaFx-0.0.1.1.tar.gz/MaFx-0.0.1.1/README.md
@@ -0,0 +1,4 @@
+# MaFx
+## The All In One Math Tool
+MaFx is a math tool that can do a lot of things. It can do basic math, algebra, calculus, and more. It is a great tool for students and teachers. It is easy to use and has a simple interface. It is also free and open source
+
---
+++ packages/MaFx/MaFx-0.0.1.1.tar.gz/MaFx-0.0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MaFx',
+    version='0.0.1.1',
+    packages=find_packages(),
+    description='A Complex Math Library',
+    long_description=open('README.md').read(),
+    license='MIT',
+    author='FedxD',
+    author_email="abbaskazim135@gmail.com",
+)
