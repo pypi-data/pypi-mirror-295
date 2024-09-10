43cdea6a66e28e135cd5f4eedf60134e9ac98d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 06:04:34 +0000

    Add package Kqlmagic/0.1.114.post26/Kqlmagic-0.1.114.post26.tar.gz

---
+++ packages/Kqlmagic/Kqlmagic-0.1.114.post26.tar.gz/Kqlmagic-0.1.114.post26/azure/Kqlmagic/__init__.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+try:
+    # to avoid this warnig to be dispayed:
+    # UserWarning: Distutils was imported before Setuptools. This usage is discouraged and may exhibit undesirable behaviors or errors. Please use Setuptools' objects directly or at least import Setuptools first.
+    import setuptools
+except: # pylint: disable=bare-except
+    pass
+
+from .magic_extension import load_ipython_extension, unload_ipython_extension, _register_kqlmagic_magic
+from ._version import __version__
+from .kql_magic import kql, kql_stop
+
+__all__ = ['__version__', 'kql', 'kql_stop', 'load_ipython_extension', 'unload_ipython_extension', '_register_kqlmagic_magic']
