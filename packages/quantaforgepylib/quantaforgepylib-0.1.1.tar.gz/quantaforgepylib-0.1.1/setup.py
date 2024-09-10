545c5de3cb352a0fa0c8933bd0c6152fade6ba04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:06:31 +0000

    Add package quantaforgepylib/0.1.1/quantaforgepylib-0.1.1.tar.gz

---
+++ packages/quantaforgepylib/quantaforgepylib-0.1.1.tar.gz/quantaforgepylib-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quantaforgepylib',  # Replace with your package’s name
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Smisch',  
+    author_email='ceo@quantaforge.dev',
+    description='A library of fancy python tools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
