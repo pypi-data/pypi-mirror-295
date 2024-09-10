3f7f1e0070f8da9d48886cf5fedaf268d44bd5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:03:50 +0000

    Add package splot/1.1.7/splot-1.1.7.tar.gz

---
+++ packages/splot/splot-1.1.7.tar.gz/splot-1.1.7/splot/libpysal.py
@@ -0,0 +1,18 @@
+"""
+``splot.libpysal``
+==================
+
+Provides visualisations for all core components of
+Python Spatial Analysis Library in `libpysal`.
+
+libpysal weights
+----------------
+
+.. autosummary::
+   :toctree: generated/
+
+   plot_spatial_weights
+
+"""
+
+from ._viz_libpysal_mpl import plot_spatial_weights  # noqa F401
