f5580c3ac8d5956e477f426ef6339202030cc71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:04:47 +0000

    Add package cbr-website-beta/0.175.0/cbr_website_beta-0.175.0.tar.gz

---
+++ packages/cbr-website-beta/cbr_website_beta-0.175.0.tar.gz/cbr_website_beta-0.175.0/cbr_website_beta/global_vars.py
@@ -0,0 +1,4 @@
+# use this class to create references for objects that need to be shared between different parts of the code
+from osbot_fast_api.api.Fast_API__Http_Events import Fast_API__Http_Events
+
+fast_api_http_events = Fast_API__Http_Events
