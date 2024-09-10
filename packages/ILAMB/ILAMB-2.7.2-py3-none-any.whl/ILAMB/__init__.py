f41cb2b91d8523d1f03891cb16e0e4eb9424a245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 06:04:18 +0000

    Add package ILAMB/2.7.2/ILAMB-2.7.2-py3-none-any.whl

---
+++ packages/ILAMB/ILAMB-2.7.2-py3-none-any.whl/ILAMB/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "Nathan Collier"
+__date__ = "Sept 2024"
+__version__ = "2.7.2"
---
+++ packages/ILAMB/ILAMB-2.7.2-py3-none-any.whl/ILAMB/generated_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ILAMB SETUP.PY
+short_version = '2.7.2'
+version = '2.7.2'
+git_revision = 'RELEASE'
+full_version = '2.7.2 (%(git_revision)s)' % {
+    'git_revision': git_revision}
+release = True
+if not release:
+    version = full_version
