7f7b3fbb2043b4626771fa789b315fffd1422f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:40 +0000

    Add package yosafe-packages/0.11.19/yosafe_packages-0.11.19-py3-none-any.whl

---
+++ packages/yosafe-packages/yosafe_packages-0.11.19-py3-none-any.whl/yosafe_packages/yosafe_subpackage_1/yosafe_subpackage_1_functions.py
@@ -0,0 +1,22 @@
+from requests import get
+
+
+def yosafe_get_yosafe_subpackage_1():
+    text = "yosafe_subpackage_1"
+    url = "https://cowsay.morecode.org/say"
+    params = {
+        "message": text,
+        "format": "text",
+    }
+    response = get(url, params=params)
+    if response.status_code == 200:
+        return response.text 
+    else:
+        return f"Error: {response.status_code} - Could not retrieve ASCII art."
+
+
+def yosafe_add(a, b):
+    return a + b
+
+
+
