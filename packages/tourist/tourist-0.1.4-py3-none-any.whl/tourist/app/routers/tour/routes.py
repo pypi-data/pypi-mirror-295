55ad4c9369e129c1f57a56e144a443e1e0419487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:59 +0000

    Add package tourist/0.1.4/tourist-0.1.4-py3-none-any.whl

---
+++ packages/tourist/tourist-0.1.4-py3-none-any.whl/tourist/app/routers/tour/routes.py
@@ -0,0 +1,47 @@
+import logging
+
+from fastapi import APIRouter, HTTPException, status
+
+from tourist.app.base import BaseResponse, BaseRequest
+from tourist.service import get_page, get_page_with_actions
+
+tour = APIRouter(prefix="/tour", redirect_slashes=True, tags=["Tour"])
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class TouristViewRequest(BaseRequest):
+    target_url: str
+    warmup_url: str | None = None
+    cookies: list | None = None
+    screenshot: bool | None = False
+
+
+class TouristActionsRequest(BaseRequest):
+    actions: str
+
+
+class TouristViewResponse(BaseResponse):
+    current_url: str | None = None
+    cookies: list | None = None
+    source_html: str
+    b64_screenshot: str | None = None
+
+
+class TouristActionsResponse(BaseResponse): ...
+
+
+@tour.post("/view", response_model=TouristViewResponse)
+def view_page(tourist_view_request: TouristViewRequest):
+    if page := get_page(**tourist_view_request.model_dump()):
+        return page
+    else:
+        raise HTTPException(status_code=400, detail="something went wrong")
+
+
+@tour.post("/actions")
+def do_actions(tourist_actions_request: TouristActionsRequest):
+    if page := get_page_with_actions(**tourist_actions_request.model_dump()):
+        return page
+    else:
+        raise HTTPException(status_code=status.HTTP_4, detail="something went wrong")
