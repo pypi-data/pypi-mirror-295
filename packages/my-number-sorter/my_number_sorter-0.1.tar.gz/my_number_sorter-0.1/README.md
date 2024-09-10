7ea2758794c7d3de024614cadd03946497c22db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:03:45 +0000

    Add package my-number-sorter/0.1/my_number_sorter-0.1.tar.gz

---
+++ packages/my-number-sorter/my_number_sorter-0.1.tar.gz/my_number_sorter-0.1/README.md
@@ -0,0 +1,11 @@
+# My Number Sorter
+
+A Python library that sorts numbers based on their alphabetical word representation.
+
+## Installation
+
+To install the package:
+
+```bash
+pip install my_number_sorter
+```
---
+++ packages/my-number-sorter/my_number_sorter-0.1.tar.gz/my_number_sorter-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_number_sorter',
+    version='0.1',
+    description='A python library that sorts numbers based on their alpahbetic representation',
+    author='Adedotun',
+    author_email='adedotunabimbola2@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
