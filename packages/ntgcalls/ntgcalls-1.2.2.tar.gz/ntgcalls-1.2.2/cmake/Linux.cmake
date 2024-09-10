f929edd59d3c947b4494086ca625e09468d02bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:59 +0000

    Add package ntgcalls/1.2.2/ntgcalls-1.2.2.tar.gz

---
+++ packages/ntgcalls/ntgcalls-1.2.2.tar.gz/ntgcalls-1.2.2/cmake/Linux.cmake
@@ -0,0 +1,19 @@
+target_compile_definitions(${target_name} PUBLIC
+    WEBRTC_POSIX
+    WEBRTC_LINUX
+    _LIBCPP_ABI_NAMESPACE=Cr
+    _LIBCPP_ABI_VERSION=2
+    _LIBCPP_DISABLE_AVAILABILITY
+    BOOST_NO_CXX98_FUNCTION_BASE
+    NDEBUG
+    RTC_ENABLE_H265
+)
+
+set_target_properties(${target_name} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+target_link_libraries(${target_name} PRIVATE
+    X11
+    dl
+    rt
+    m
+    Threads::Threads
+)
