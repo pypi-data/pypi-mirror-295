a53be1898f80d7295efa6a5035f8c7d2b222ca22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:52 +0000

    Add package kskimgeometry/0.1/kskimgeometry-0.1.tar.gz

---
+++ packages/kskimgeometry/kskimgeometry-0.1.tar.gz/kskimgeometry-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kskimgeometry',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='kskim',
+    packages=find_packages(),
+)
