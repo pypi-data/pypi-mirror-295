c0021e94014e68fa7cf79330935c320679aef60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:44 +0000

    Add package formant/1.185.71/formant-1.185.71.tar.gz

---
+++ packages/formant/formant-1.185.71.tar.gz/formant-1.185.71/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationTemplateTags, AnnotationType, ScopeFilter, FocusedDatapoint, AnnotationStreamType, TagParameters, AnnotationFieldValueTags, AnnotationTags, AnnotationFieldValue, UserParameters, AnnotationTemplateListResponse, ScopeFilterTypesItem, Annotation, AnnotationFieldType, UserParametersRolesItem, AnnotationFieldValuesRequest, AnnotationFieldValuesRequestTags, AnnotationTemplate, SheetParameters, AnnotationFieldValuesResponse, AnnotationMetadata, AnnotationField
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.185.71.tar.gz/formant-1.185.71/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandProgress, CommandDeliveryEvent, Filter, CommandDeliveryEventTags, ScopeFilter, CommandRequest, CommandResponseTags, Command, CommandTemplate, CommandTemplateParameterMeta, CommandDeliveryEventMetadata, CommandListResponse, CommandDeliveryEventStreamType, CommandParameterMeta, CommandQuery, ScopeFilterTypesItem, CommandTemplateListResponse, CommandRequestListResponse, CommandTags, CommandParameter, CommandDeliveryEventType, CommandResponseStreamType, CommandStreamType, CommandResponse, CommandTemplateTags, FileInfo
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
