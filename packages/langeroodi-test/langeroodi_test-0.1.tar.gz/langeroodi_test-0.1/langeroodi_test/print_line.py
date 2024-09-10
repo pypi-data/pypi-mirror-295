24749a5fa731ec25814d9afe21aaa16c12d288d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:52 +0000

    Add package langeroodi-test/0.1/langeroodi_test-0.1.tar.gz

---
+++ packages/langeroodi-test/langeroodi_test-0.1.tar.gz/langeroodi_test-0.1/langeroodi_test/print_line.py
@@ -0,0 +1,2 @@
+def print_line():
+    print('This is just to test my PyPi account! (D.Langeroodi)')
---
+++ packages/langeroodi-test/langeroodi_test-0.1.tar.gz/langeroodi_test-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+       name='langeroodi_test',
+       version='0.1',
+       packages=find_packages(),
+       description='PyPi test package!',
+       author='Danial Langeroodi',
+       author_email='dlangaroudi@gmail.com',
+       classifiers=['Programming Language :: Python :: 3',
+                    'Operating System :: OS Independent'],
+       python_requires='>=3.6',
+)
