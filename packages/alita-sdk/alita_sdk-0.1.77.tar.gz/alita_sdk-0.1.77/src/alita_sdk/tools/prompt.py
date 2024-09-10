bf307bdfad8a2f686574ccb22b0baab72d310ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:05:07 +0000

    Add package alita-sdk/0.1.77/alita_sdk-0.1.77.tar.gz

---
+++ packages/alita-sdk/alita_sdk-0.1.77.tar.gz/alita_sdk-0.1.77/src/alita_sdk/tools/prompt.py
@@ -0,0 +1,16 @@
+from langchain_core.tools import BaseTool
+from pydantic import validator
+from typing import Any
+
+class Prompt(BaseTool):
+    name: str
+    description: str
+    prompt: Any
+    
+    @validator('name', pre=True, allow_reuse=True)
+    def remove_spaces(cls, v):
+        return v.replace(' ', '')
+    
+    def _run(self, **kwargs):
+        return self.prompt.predict(variables=kwargs)
+    
