55315f95f8aad4afb6e605dc95c901e7cea15573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:04:16 +0000

    Add package uv/0.4.8/uv-0.4.8.tar.gz

---
+++ packages/uv/uv-0.4.8.tar.gz/uv-0.4.8/crates/uv-distribution/src/download.rs
@@ -0,0 +1,80 @@
+use std::path::{Path, PathBuf};
+
+use crate::Error;
+use distribution_filename::WheelFilename;
+use distribution_types::{CachedDist, Dist, Hashed};
+use pypi_types::{HashDigest, Metadata23};
+use uv_cache_info::CacheInfo;
+
+/// A locally available wheel.
+#[derive(Debug, Clone)]
+pub struct LocalWheel {
+    /// The remote distribution from which this wheel was downloaded.
+    pub(crate) dist: Dist,
+    /// The parsed filename.
+    pub(crate) filename: WheelFilename,
+    /// The canonicalized path in the cache directory to which the wheel was downloaded.
+    /// Typically, a directory within the archive bucket.
+    pub(crate) archive: PathBuf,
+    /// The cache index of the wheel.
+    pub(crate) cache: CacheInfo,
+    /// The computed hashes of the wheel.
+    pub(crate) hashes: Vec<HashDigest>,
+}
+
+impl LocalWheel {
+    /// Return the path to the downloaded wheel's entry in the cache.
+    pub fn target(&self) -> &Path {
+        &self.archive
+    }
+
+    /// Return the [`Dist`] from which this wheel was downloaded.
+    pub fn remote(&self) -> &Dist {
+        &self.dist
+    }
+
+    /// Return the [`WheelFilename`] of this wheel.
+    pub fn filename(&self) -> &WheelFilename {
+        &self.filename
+    }
+
+    /// Read the [`Metadata23`] from a wheel.
+    pub fn metadata(&self) -> Result<Metadata23, Error> {
+        read_flat_wheel_metadata(&self.filename, &self.archive)
+    }
+}
+
+impl Hashed for LocalWheel {
+    fn hashes(&self) -> &[HashDigest] {
+        &self.hashes
+    }
+}
+
+/// Convert a [`LocalWheel`] into a [`CachedDist`].
+impl From<LocalWheel> for CachedDist {
+    fn from(wheel: LocalWheel) -> CachedDist {
+        CachedDist::from_remote(
+            wheel.dist,
+            wheel.filename,
+            wheel.hashes,
+            wheel.cache,
+            wheel.archive,
+        )
+    }
+}
+
+impl std::fmt::Display for LocalWheel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.remote())
+    }
+}
+
+/// Read the [`Metadata23`] from an unzipped wheel.
+fn read_flat_wheel_metadata(
+    filename: &WheelFilename,
+    wheel: impl AsRef<Path>,
+) -> Result<Metadata23, Error> {
+    let dist_info = install_wheel_rs::metadata::find_flat_dist_info(filename, &wheel)?;
+    let metadata = install_wheel_rs::metadata::read_dist_info_metadata(&dist_info, &wheel)?;
+    Ok(Metadata23::parse_metadata(&metadata)?)
+}
---
+++ packages/uv/uv-0.4.8.tar.gz/uv-0.4.8/crates/uv/src/commands/tool/upgrade.rs
@@ -0,0 +1,170 @@
+use std::{collections::BTreeSet, fmt::Write};
+
+use anyhow::Result;
+use owo_colors::OwoColorize;
+use tracing::debug;
+
+use uv_cache::Cache;
+use uv_client::Connectivity;
+use uv_configuration::Concurrency;
+use uv_normalize::PackageName;
+use uv_requirements::RequirementsSpecification;
+use uv_settings::{Combine, ResolverInstallerOptions, ToolOptions};
+use uv_tool::InstalledTools;
+
+use crate::commands::pip::loggers::{SummaryResolveLogger, UpgradeInstallLogger};
+use crate::commands::project::{update_environment, EnvironmentUpdate};
+use crate::commands::tool::common::remove_entrypoints;
+use crate::commands::{tool::common::install_executables, ExitStatus, SharedState};
+use crate::printer::Printer;
+use crate::settings::ResolverInstallerSettings;
+
+/// Upgrade a tool.
+pub(crate) async fn upgrade(
+    name: Vec<PackageName>,
+    connectivity: Connectivity,
+    args: ResolverInstallerOptions,
+    filesystem: ResolverInstallerOptions,
+    concurrency: Concurrency,
+    native_tls: bool,
+    cache: &Cache,
+
+    printer: Printer,
+) -> Result<ExitStatus> {
+    let installed_tools = InstalledTools::from_settings()?.init()?;
+    let _lock = installed_tools.lock().await?;
+
+    let names: BTreeSet<PackageName> = {
+        if name.is_empty() {
+            installed_tools
+                .tools()
+                .unwrap_or_default()
+                .into_iter()
+                .map(|(name, _)| name)
+                .collect()
+        } else {
+            name.into_iter().collect()
+        }
+    };
+
+    if names.is_empty() {
+        writeln!(printer.stderr(), "Nothing to upgrade")?;
+        return Ok(ExitStatus::Success);
+    }
+
+    // Determine whether we applied any upgrades.
+    let mut did_upgrade = false;
+
+    for name in names {
+        debug!("Upgrading tool: `{name}`");
+
+        // Ensure the tool is installed.
+        let existing_tool_receipt = match installed_tools.get_tool_receipt(&name) {
+            Ok(Some(receipt)) => receipt,
+            Ok(None) => {
+                let install_command = format!("uv tool install {name}");
+                writeln!(
+                    printer.stderr(),
+                    "`{}` is not installed; run `{}` to install",
+                    name.cyan(),
+                    install_command.green()
+                )?;
+                return Ok(ExitStatus::Failure);
+            }
+            Err(_) => {
+                let install_command = format!("uv tool install --force {name}");
+                writeln!(
+                    printer.stderr(),
+                    "`{}` is missing a valid receipt; run `{}` to reinstall",
+                    name.cyan(),
+                    install_command.green()
+                )?;
+                return Ok(ExitStatus::Failure);
+            }
+        };
+
+        let existing_environment = match installed_tools.get_environment(&name, cache) {
+            Ok(Some(environment)) => environment,
+            Ok(None) => {
+                let install_command = format!("uv tool install {name}");
+                writeln!(
+                    printer.stderr(),
+                    "`{}` is not installed; run `{}` to install",
+                    name.cyan(),
+                    install_command.green()
+                )?;
+                return Ok(ExitStatus::Failure);
+            }
+            Err(_) => {
+                let install_command = format!("uv tool install --force {name}");
+                writeln!(
+                    printer.stderr(),
+                    "`{}` is missing a valid environment; run `{}` to reinstall",
+                    name.cyan(),
+                    install_command.green()
+                )?;
+                return Ok(ExitStatus::Failure);
+            }
+        };
+
+        // Resolve the appropriate settings, preferring: CLI > receipt > user.
+        let options = args.clone().combine(
+            ResolverInstallerOptions::from(existing_tool_receipt.options().clone())
+                .combine(filesystem.clone()),
+        );
+        let settings = ResolverInstallerSettings::from(options.clone());
+
+        // Resolve the requirements.
+        let requirements = existing_tool_receipt.requirements();
+        let spec = RequirementsSpecification::from_requirements(requirements.to_vec());
+
+        // Initialize any shared state.
+        let state = SharedState::default();
+
+        // TODO(zanieb): Build the environment in the cache directory then copy into the tool
+        // directory.
+        let EnvironmentUpdate {
+            environment,
+            changelog,
+        } = update_environment(
+            existing_environment,
+            spec,
+            &settings,
+            &state,
+            Box::new(SummaryResolveLogger),
+            Box::new(UpgradeInstallLogger::new(name.clone())),
+            connectivity,
+            concurrency,
+            native_tls,
+            cache,
+            printer,
+        )
+        .await?;
+
+        did_upgrade |= !changelog.is_empty();
+
+        // If we modified the target tool, reinstall the entrypoints.
+        if changelog.includes(&name) {
+            // At this point, we updated the existing environment, so we should remove any of its
+            // existing executables.
+            remove_entrypoints(&existing_tool_receipt);
+
+            install_executables(
+                &environment,
+                &name,
+                &installed_tools,
+                ToolOptions::from(options),
+                true,
+                existing_tool_receipt.python().to_owned(),
+                requirements.to_vec(),
+                printer,
+            )?;
+        }
+    }
+
+    if !did_upgrade {
+        writeln!(printer.stderr(), "Nothing to upgrade")?;
+    }
+
+    Ok(ExitStatus::Success)
+}
