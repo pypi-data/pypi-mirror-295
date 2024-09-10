8b248a5957f43ff93ae2188109f04eb10c91b07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:04:12 +0000

    Add package localgit/1.0.2/localgit-1.0.2.tar.gz

---
+++ packages/localgit/localgit-1.0.2.tar.gz/localgit-1.0.2/setup.py
@@ -0,0 +1,32 @@
+import io
+
+import setuptools
+
+__version__ = "1.0.2"
+
+description = "localgit is a tool for managing local git repo clones."
+
+setuptools.setup(
+    name="localgit",
+    version=__version__,
+    url="https://github.com/natibek/localgit/tree/main",
+    author="Nathnael Bekele",
+    author_email="nwtbekele@gmail.com",
+    python_requires=(">=3.11.0"),
+    license="Apache 2.0",
+    description=description,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["src"],
+    entry_points={
+        "console_scripts": [
+            "localgit=src.localgit:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
