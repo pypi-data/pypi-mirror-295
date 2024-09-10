be99eced8e43a29a9ae9ffb7d88ea9d56c2d1600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 15:04:05 +0000

    Add package syncservers/0.0.3/syncservers-0.0.3.tar.gz

---
+++ packages/syncservers/syncservers-0.0.3.tar.gz/syncservers-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+version = "0.0.2"
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name='syncservers',
+    version=version,
+    description='instantly sync data among home servers',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Zhuo Wang',
+    author_email='zhuowang10@gmail.com',
+    url='https://github.com/zhuowang10/syncservers',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['syncservers'],
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'easyschedule>=0.107',
+        'asyncinotify>=4.0.9',
+    ],
+)
