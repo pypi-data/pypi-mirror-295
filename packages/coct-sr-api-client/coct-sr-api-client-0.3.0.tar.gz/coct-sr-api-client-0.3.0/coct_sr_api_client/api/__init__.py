4d4c878b53af14f681fa74697d25128da04e87fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:51 +0000

    Add package coct-sr-api-client/0.3.0/coct-sr-api-client-0.3.0.tar.gz

---
+++ packages/coct-sr-api-client/coct-sr-api-client-0.3.0.tar.gz/coct-sr-api-client-0.3.0/coct_sr_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from coct_sr_api_client.api.auth_group_api import AuthGroupApi
+from coct_sr_api_client.api.config_group_api import ConfigGroupApi
+from coct_sr_api_client.api.service_request_group_api import ServiceRequestGroupApi
+
