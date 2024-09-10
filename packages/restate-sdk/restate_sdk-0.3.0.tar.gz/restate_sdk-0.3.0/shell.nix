376090be8ab2900e1c29c0c82876317261ca87b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:13 +0000

    Add package restate-sdk/0.3.0/restate_sdk-0.3.0.tar.gz

---
+++ packages/restate-sdk/restate_sdk-0.3.0.tar.gz/restate_sdk-0.3.0/shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSUserEnv {
+  name = "my-python-env";
+  targetPkgs = pkgs: (with pkgs; [
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    just
+
+		# rust
+		rustup
+    clang
+    llvmPackages.bintools
+    protobuf
+    cmake
+		liburing
+		pkg-config
+  ]);
+
+	RUSTC_VERSION =
+	    builtins.elemAt
+	    (builtins.match
+	     ".*channel *= *\"([^\"]*)\".*"
+	     (pkgs.lib.readFile ./rust-toolchain.toml)
+	    )
+	    0;
+
+  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+
+  runScript = ''
+    bash
+  '';
+}).env
