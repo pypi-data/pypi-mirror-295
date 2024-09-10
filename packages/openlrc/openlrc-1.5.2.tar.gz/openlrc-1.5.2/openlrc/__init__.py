cc789292ca87dff2805f4c662e01a3fae80aa3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:17 +0000

    Add package openlrc/1.5.2/openlrc-1.5.2.tar.gz

---
+++ packages/openlrc/openlrc-1.5.2.tar.gz/openlrc-1.5.2/openlrc/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (C) 2024. Hao Zheng
+#  All rights reserved.
+
+from openlrc.openlrc import LRCer
+from openlrc.translate import list_chatbot_models
+
+__all__ = ('LRCer', 'list_chatbot_models')
+__version__ = '1.5.2'
+__author__ = 'zh-plus'
