bb5397e0a529fd24dc72744c89f9222f70ba2fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:04:04 +0000

    Add package truss/0.9.33/truss-0.9.33.tar.gz

---
+++ packages/truss/truss-0.9.33.tar.gz/truss-0.9.33/truss/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from single_source import get_version
+
+__version__ = get_version(__name__, Path(__file__).parent.parent)
+
+
+def version():
+    return __version__
+
+
+from truss.api import login, push
+from truss.build import from_directory, init, kill_all, load
+
+__all__ = ["from_directory", "init", "kill_all", "load", "push", "login"]
