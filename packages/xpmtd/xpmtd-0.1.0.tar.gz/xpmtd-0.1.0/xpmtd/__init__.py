00ce30e599a03b3479e7ef422065d76af762eeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 16:04:40 +0000

    Add package xpmtd/0.1.0/xpmtd-0.1.0.tar.gz

---
+++ packages/xpmtd/xpmtd-0.1.0.tar.gz/xpmtd-0.1.0/xpmtd/__init__.py
@@ -0,0 +1,10 @@
+# xpmtd/xpmtd/__init__.py
+
+# Import sorting_metadata module
+from .sorting_metadata import SortingMetadata, SortingMetadataSession
+
+# Alternatively, you could import everything from sorting_metadata if needed
+# from .sorting_metadata import *
+
+# Define __all__ if you want to control what gets exposed with 'from xpmtd import *'
+__all__ = ['sorting_metadata']
