f7ac79b14257bc242ba418274712aa30de48e808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 06:03:51 +0000

    Add package ystu/0.0.3/ystu-0.0.3.tar.gz

---
+++ packages/ystu/ystu-0.0.3.tar.gz/ystu-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='ystu',
+  version='0.0.3',
+  author='aleksejeei',
+  author_email='aleksejeei@gmail.com',
+  description='Auth on site YSTU',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/aleksejeei/ystu_lib',
+  packages=find_packages(),
+  install_requires=['requests', 'bs4'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/aleksejeei'
+  },
+  python_requires='>=3.10'
+)
