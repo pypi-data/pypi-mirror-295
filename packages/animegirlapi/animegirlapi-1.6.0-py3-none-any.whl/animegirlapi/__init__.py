c0163176b26b728d12816d82b3a2692826bc34ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:04:05 +0000

    Add package animegirlapi/1.6.0/animegirlapi-1.6.0-py3-none-any.whl

---
+++ packages/animegirlapi/animegirlapi-1.6.0-py3-none-any.whl/animegirlapi/__init__.py
@@ -0,0 +1,6 @@
+from animegirlapi.main import get_random_image
+from animegirlapi.main import get_all_ratings
+from animegirlapi.main import get_all_images
+from animegirlapi.main import download_image
+from animegirlapi.main import get_all_tags
+from animegirlapi.main import get_info
