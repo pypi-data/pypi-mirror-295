a888a50516d08b964e506289afa3ad2d7281f480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 03:04:42 +0000

    Add package data-guard/0.0.3/data_guard-0.0.3-py3-none-any.whl

---
+++ packages/data-guard/data_guard-0.0.3-py3-none-any.whl/data_guard/rules/accepted_if.py
@@ -0,0 +1,34 @@
+from data_guard.rule import Rule
+
+
+class AcceptedIf(Rule):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.accepts = ["yes", "on", 1, "1", True, "true"]
+
+    def validate(self) -> bool:
+        self.require_params_count(2)
+
+        self.set_params(
+            {"required_if_key": self.args[0], "required_if_value": self.args[1]}
+        )
+
+        required_if_key = self.params.get("required_if_key")
+
+        required_if_value = self.params.get("required_if_value")
+
+        expression = f"'{self.data.get(required_if_key)}' == {repr(required_if_value)}"
+
+        if eval(expression):
+            value = self.params.get("value")
+
+            if self.is_countable(value):
+                return value in self.accepts
+
+            return False
+
+        return True
+
+    def get_message(self) -> str:
+        return "{field} is required when {required_if_key} is {required_if_value}"
