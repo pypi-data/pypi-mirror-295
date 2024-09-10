36891717276fe10c28a565323f731e3a15593840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:04:19 +0000

    Add package oursin/0.7.0/oursin-0.7.0-py3-none-any.whl

---
+++ packages/oursin/oursin-0.7.0-py3-none-any.whl/oursin/atlas/__init__.py
@@ -0,0 +1,10 @@
+from .ontology import *
+
+ccf25 = Atlas('ccf25')
+ccf50 = Atlas('ccf50')
+ccf100 = Atlas('ccf100')
+waxholm39 = Atlas('waxholm39')
+waxholm78 = Atlas('waxholm78')
+princeton20 = Atlas('princeton20')
+azba8 = Atlas('azba8')
+human500 = Atlas('human500')
