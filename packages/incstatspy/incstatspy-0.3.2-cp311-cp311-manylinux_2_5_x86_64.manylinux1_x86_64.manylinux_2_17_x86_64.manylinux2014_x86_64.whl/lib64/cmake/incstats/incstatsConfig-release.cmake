aa40d05a63237a385c45322c97e9fc22d5a1437e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 04:04:11 +0000

    Add package incstatspy/0.3.2/incstatspy-0.3.2-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/incstatspy/incstatspy-0.3.2-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/lib64/cmake/incstats/incstatsConfig-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "incstats" for configuration "Release"
+set_property(TARGET incstats APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(incstats PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libincstats.so"
+  IMPORTED_SONAME_RELEASE "libincstats.so"
+  )
+
+list(APPEND _cmake_import_check_targets incstats )
+list(APPEND _cmake_import_check_files_for_incstats "${_IMPORT_PREFIX}/lib64/libincstats.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
