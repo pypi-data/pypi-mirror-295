f7e575949557e0dbe08923598d76323f11916f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 07:04:12 +0000

    Add package flake8-async/24.9.2/flake8_async-24.9.2.tar.gz

---
+++ packages/flake8-async/flake8_async-24.9.2.tar.gz/flake8_async-24.9.2/tests/eval_files/async102_trio.py
@@ -0,0 +1,33 @@
+# NOASYNCIO
+# NOANYIO - since anyio.Cancelled does not exist
+import trio
+
+
+async def foo(): ...
+
+
+# except cancelled/baseexception are also critical
+async def foo4():
+    try:
+        ...
+    except trio.Cancelled:
+        await foo()  # error: 8, Statement("trio.Cancelled", lineno-1)
+    except:
+        await foo()  # safe, since after trio.Cancelled
+
+    try:
+        ...
+    except trio.Cancelled:
+        await foo()  # error: 8, Statement("trio.Cancelled", lineno-1)
+    except BaseException:
+        await foo()  # safe, since after trio.Cancelled
+
+
+async def foo5():
+    try:
+        ...
+    except trio.Cancelled:
+        with trio.CancelScope(deadline=30, shield=True):
+            await foo()  # safe
+    except BaseException:
+        await foo()  # safe, since after trio.Cancelled
