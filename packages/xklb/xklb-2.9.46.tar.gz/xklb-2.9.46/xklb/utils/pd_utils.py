db9b1089e872a62c056a29f68098bacd40abe8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:57 +0000

    Add package xklb/2.9.46/xklb-2.9.46.tar.gz

---
+++ packages/xklb/xklb-2.9.46.tar.gz/xklb-2.9.46/xklb/utils/pd_utils.py
@@ -0,0 +1,28 @@
+import re
+from contextlib import suppress
+
+
+def kebab_camel_snake(col):
+    col = re.sub(r"(?<!^)(?=[A-Z])", "_", col)
+    col = col.lower()
+    col = re.sub(r"\s+", "_", col)
+    col = re.sub(r"[\]\[\)\(\{\}]+", "", col)
+    col = re.sub(r"[\:\-\.]+", "_", col)
+    col = re.sub(r"__+", "_", col)
+    return col
+
+
+def columns_snake_case(df):
+    df.columns = [kebab_camel_snake(x) if isinstance(x, str) else x for x in df.columns]
+    return df
+
+
+def convert_dtypes(df, clean=False):
+    for col in df.columns:
+        with suppress(Exception):
+            if clean:
+                df[col] = df[col].str.replace(r"\[.*|\(.*|\/.*", "", regex=True)
+            df.loc[:, col] = df[col].str.replace(",", "").astype(float)
+
+    df = df.convert_dtypes()
+    return df
