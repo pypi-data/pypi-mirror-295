b4e77a1997ba74ff939bbfa5e4563d89e60aca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 05:04:26 +0000

    Add package yta-multimedia/0.0.75/yta-multimedia-0.0.75.tar.gz

---
+++ packages/yta-multimedia/yta-multimedia-0.0.75.tar.gz/yta-multimedia-0.0.75/yta_multimedia/__init__.py
@@ -0,0 +1,9 @@
+from yta_multimedia.experimental.text.ai_prompt_handler import AIPromptHandler
+
+def test():
+    aiph = AIPromptHandler()
+
+    aiph.generate_youtube_video_title('test')
+
+
+test()
