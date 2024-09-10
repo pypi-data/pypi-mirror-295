4127b6e217a9b4a080d166aa76fc7c5076485706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-10 14:03:59 +0000

    Add package DadosAbertosBrasil/1.2.0/dadosabertosbrasil-1.2.0.tar.gz

---
+++ packages/DadosAbertosBrasil/dadosabertosbrasil-1.2.0.tar.gz/dadosabertosbrasil-1.2.0/DadosAbertosBrasil/__init__.py
@@ -0,0 +1,49 @@
+"""Dados Abertos Brasil é uma iniciativa para facilitar o acesso a dados
+abertos e APIs do governo brasileiro.
+
+É um pacote open-source para Python e Pandas e a forma mais simples de acessar
+dados de instituições como IGBE, IPEA, Banco Central, etc.
+
+Módulos
+-------
+- DadosAbertosBrasil.ibge
+- DadosAbertosBrasil.ipea
+- DadosAbertosBrasil.camara
+- DadosAbertosBrasil.senado
+- DadosAbertosBrasil.bacen
+- DadosAbertosBrasil.favoritos
+- DadosAbertosBrasil.UF
+
+Sobre
+-----
+- Página Oficial: https://www.gustavofurtado.com/dab.html
+- Documentação: https://www.gustavofurtado.com/DadosAbertosBrasil
+
+Instalação
+----------
+- `pip install DadosAbertosBrasil`
+
+Dependências
+------------
+- Python 3.6 ou superior
+- pandas
+- requests
+
+Licença
+-------
+- MIT
+
+"""
+
+from . import bacen
+from . import camara
+from . import ibge
+from . import ipea
+from . import senado
+from .favoritos import *
+from .uf import Governador, UF
+from ._utils.errors import *
+
+
+__version__ = "1.2.0"
+__author__ = "Gustavo Furtado da Silva"
