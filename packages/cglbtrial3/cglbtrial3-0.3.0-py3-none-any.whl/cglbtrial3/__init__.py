148c85755494e460bbf35c6250be2b23db87628b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:21 +0000

    Add package cglbtrial3/0.3.0/cglbtrial3-0.3.0-py3-none-any.whl

---
+++ packages/cglbtrial3/cglbtrial3-0.3.0-py3-none-any.whl/cglbtrial3/__init__.py
@@ -0,0 +1,2 @@
+§
+ÐTáfãó—dS)N©róúcglbtrial3/__init__.pyú<module>rsðððr
---
+++ packages/cglbtrial3/cglbtrial3-0.3.0-py3-none-any.whl/cglbtrial3/conversion.py
@@ -0,0 +1,4 @@
+§
+ĞTáfSãó—d„ZdS)có—d|dz
+zdz}|S)NgÃõ(\Â	@g(@g×£p=
+Ç[@©)Úvalue_unit_oneÚAs  úcglbtrial3/conversion.pyÚunit_one_2_unit_twors€¨$°¸sÑ0BÑ*CÀFÑ*J¨ĞRSÈ8óN)rrrrú<module>r	sğØSĞSĞSĞSĞSr
