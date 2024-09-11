16956194f63d0a43639e481474dd184d732e8fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:04:52 +0000

    Add package Cirron/0.4/Cirron-0.4.tar.gz

---
+++ packages/Cirron/Cirron-0.4.tar.gz/Cirron-0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+# Read the contents of README
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="Cirron",
+    version="0.4",
+    packages=find_packages(),
+    package_data={
+        "cirron": [
+            "cirronlib.cpp",
+            "apple_arm_events.h",
+            "tracer.py",
+            "cirron.py",
+            "injector.py",
+        ],
+    },
+    author="Matt Stuchlik",
+    author_email="matej.stuchlik@gmail.com",
+    description="Cirron measures how many CPU instructions and system calls a piece of Python code executes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/s7nfo/Cirron",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/Cirron/Cirron-0.4.tar.gz/Cirron-0.4/tests/tests.py
@@ -0,0 +1,57 @@
+import unittest
+import os
+import time
+import errno
+
+from cirron import Tracer, Collector, Injector
+
+# GITHUB_ACTIONS "should" be defined, but turns out it's not.
+IN_GITHUB_ACTIONS = os.getenv("POWERSHELL_DISTRIBUTION_CHANNEL") and os.getenv(
+    "POWERSHELL_DISTRIBUTION_CHANNEL"
+).startswith("GitHub")
+
+
+class TestTracer(unittest.TestCase):
+    def test_tracer(self):
+        with Tracer() as t:
+            time.sleep(0.1)
+
+        self.assertEqual(len(t.trace), 1)
+
+
+class TestCollector(unittest.TestCase):
+    @unittest.skipIf(
+        IN_GITHUB_ACTIONS,
+        "As of 02/07/2024, GitHub Actions does not support perf_event_open.",
+    )
+    def test_collector(self):
+        with Collector() as c:
+            time.sleep(0.1)
+
+        self.assertGreater(c.counters.instruction_count, 0)
+
+    @unittest.skipIf(
+        IN_GITHUB_ACTIONS,
+        "As of 02/07/2024, GitHub Actions does not support perf_event_open.",
+    )
+    def test_collector_empty(self):
+        # The collector should remove overhead from the measurements,
+        # so this should theoretically be 0. This may be flaky.
+        with Collector() as c:
+            pass
+
+        self.assertEqual(c.counters.instruction_count, 0)
+
+
+class TestInjector(unittest.TestCase):
+    def test_injector(self):
+        injector = Injector()
+        injector.inject("openat", "error", "ENOSPC")
+        with injector:
+            with self.assertRaises(OSError) as cm:
+                f = open("test.txt", "w")
+
+            # Check if the error code matches ENOSPC
+            self.assertEqual(cm.exception.errno, errno.ENOSPC)
+            # Optionally, check the error message
+            self.assertIn("No space left on device", str(cm.exception))
