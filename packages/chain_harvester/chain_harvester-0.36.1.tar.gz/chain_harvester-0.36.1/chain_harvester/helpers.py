9d35cc5e4a9ddaf68f41d8e6a18e494811aa25ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:38 +0000

    Add package chain_harvester/0.36.1/chain_harvester-0.36.1.tar.gz

---
+++ packages/chain_harvester/chain_harvester-0.36.1.tar.gz/chain_harvester-0.36.1/chain_harvester/helpers.py
@@ -0,0 +1,24 @@
+from chain_harvester.networks.arbitrum.mainnet import ArbitrumMainnetChain
+from chain_harvester.networks.base.mainnet import BaseMainnetChain
+from chain_harvester.networks.ethereum.mainnet import EthereumMainnetChain
+from chain_harvester.networks.optimism.mainnet import OptimismMainnetChain
+from chain_harvester.networks.polygon.mainnet import PolygonMainnetChain
+from chain_harvester.networks.scroll.mainnet import ScrollMainnetChain
+
+
+def get_chain(network, *args, **kwargs):
+    match network:
+        case "ethereum":
+            return EthereumMainnetChain(*args, **kwargs)
+        case "polygon":
+            return PolygonMainnetChain(*args, **kwargs)
+        case "optimism":
+            return OptimismMainnetChain(*args, **kwargs)
+        case "arbitrum":
+            return ArbitrumMainnetChain(*args, **kwargs)
+        case "base":
+            return BaseMainnetChain(*args, **kwargs)
+        case "scroll":
+            return ScrollMainnetChain(*args, **kwargs)
+        case _:
+            raise ValueError(f"Unknown network: {network}")
