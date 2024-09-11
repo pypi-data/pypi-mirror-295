b11125748d900df86967d42a3baa3e17505147ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:33 +0000

    Add package djing/0.2.1/djing-0.2.1.tar.gz

---
+++ packages/djing/djing-0.2.1.tar.gz/djing-0.2.1/djing/ui/src/Pages/Index.tsx
@@ -0,0 +1,7 @@
+export default function IndexPage(props: string[]) {
+  return (
+    <div className="text-center h-screen flex justify-center flex-col items-center p-4">
+      welcome
+    </div>
+  );
+}
