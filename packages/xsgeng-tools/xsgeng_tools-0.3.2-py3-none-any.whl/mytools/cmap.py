25ee394f1f581e691b0cb50de6ae62cb4fdff5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:06:01 +0000

    Add package xsgeng-tools/0.3.2/xsgeng_tools-0.3.2-py3-none-any.whl

---
+++ packages/xsgeng-tools/xsgeng_tools-0.3.2-py3-none-any.whl/mytools/cmap.py
@@ -0,0 +1,110 @@
+from matplotlib.colors import LinearSegmentedColormap as _LinearSegmentedColormap
+bwr_alpha = _LinearSegmentedColormap(
+    'bwr_alpha', 
+    dict(
+        red=[
+            (0, 0, 0), 
+            (0.5, 1, 1), 
+            (1, 1, 1)
+        ],
+        green=[
+            (0, 0.5, 0),
+            (0.5, 1, 1),
+            (1, 0, 0)
+        ],
+        blue=[
+            (0, 1, 1),
+            (0.5, 1, 1),
+            (1, 0, 0)
+        ],
+        alpha = [
+            (0, 1, 1),
+            (0.5, 0, 0),
+            (1, 1, 1)
+        ],
+    ), 
+)
+gold_alpha = _LinearSegmentedColormap(
+    'gold_alpha', 
+    dict(
+        red=[
+            (0, 1, 1), 
+            (1, 1, 1), 
+        ],
+        green=[
+            (0, 1, 1),
+            (1, 0.9, 1),
+        ],
+        blue=[
+            (0, 1, 1),
+            (1, 0, 1),
+        ],
+        alpha = [
+            (0, 0, 0),
+            (1, 1, 1)
+        ],
+    )
+)
+grey_alpha = _LinearSegmentedColormap(
+    'grey_alpha', 
+    dict(
+        red=[
+            (0, 0, 1), 
+            (1, 0, 1), 
+        ],
+        green=[
+            (0, 0, 1),
+            (1, 0, 1),
+        ],
+        blue=[
+            (0, 0, 1),
+            (1, 0, 1),
+        ],
+        alpha = [
+            (0, 0, 0),
+            (1, 1, 1)
+        ],
+    )
+)
+red_alpha = _LinearSegmentedColormap(
+    'red_alpha', 
+    dict(
+        red=[
+            (0, 0, 1), 
+            (1, 1, 1)
+        ],
+        green=[
+            (0, 0, 1),
+            (1, 0, 0)
+        ],
+        blue=[
+            (0, 0, 1),
+            (1, 0, 0)
+        ],
+        alpha = [
+            (0, 0, 0),
+            (1, 1, 1)
+        ],
+    ), 
+)
+blue_alpha = _LinearSegmentedColormap(
+    'blue_alpha', 
+    dict(
+        red=[
+            (0, 0, 1), 
+            (1, 0, 1)
+        ],
+        green=[
+            (0, 0, 1),
+            (1, 0, 0)
+        ],
+        blue=[
+            (0, 0, 1),
+            (1, 1, 0)
+        ],
+        alpha = [
+            (0, 0, 0),
+            (1, 1, 1)
+        ],
+    ), 
+)
