870fc317fa963c00f1b8f4f7429928bd48a862a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:13 +0000

    Add package pipzz/0.1/pipzz-0.1.tar.gz

---
+++ packages/pipzz/pipzz-0.1.tar.gz/pipzz-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pipzz',
+      version='0.1',
+      description='pipzz: I sleep',
+      url='http://example.com',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pipzz'],
+      zip_safe=False)
