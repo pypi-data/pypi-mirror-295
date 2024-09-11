939d4b32e8b63ad9359d157ddc588383448842d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 14:04:53 +0000

    Add package mtmai/0.3.737/mtmai-0.3.737-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.737-py3-none-any.whl/mtmai/api/main.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter
+
+from mtmai.api.routes import (
+    agent,
+    agent_blog,
+    agent_task,
+    auth,
+    blog,
+    completions,
+    dataset,
+    doccolls,
+    image,
+    items,
+    metrics,
+    mtmcrawler,
+    tasks_queue,
+    uimessages,
+    users,
+    utils,
+    webhook,
+    workspace,
+)
+from mtmai.core.coreutils import is_in_vercel
+
+api_router = APIRouter()
+
+api_router.include_router(auth.router, tags=["auth"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(blog.router, prefix="/posts", tags=["posts"])
+api_router.include_router(image.router, prefix="/image", tags=["image"])
+api_router.include_router(uimessages.router, prefix="/uimessages", tags=["uimessages"])
+api_router.include_router(completions.router, tags=["completions"])
+
+api_router.include_router(agent_task.router, prefix="/agent_task", tags=["agent_task"])
+api_router.include_router(doccolls.router, prefix="/doccolls", tags=["doccolls"])
+api_router.include_router(dataset.router, prefix="/dataset", tags=["dataset"])
+api_router.include_router(webhook.router, prefix="/webhook", tags=["webhook"])
+api_router.include_router(metrics.router, prefix="/metrics", tags=["metrics"])
+api_router.include_router(workspace.router, prefix="/workspace", tags=["workspace"])
+api_router.include_router(agent.router, prefix="/agent", tags=["agent"])
+api_router.include_router(agent_blog.router, prefix="/blog_agent", tags=["blog_agent"])
+api_router.include_router(mtmcrawler.router, prefix="/mtmcrawler", tags=["mtmcrawler"])
+
+
+api_router.include_router(
+    tasks_queue.router, prefix="/tasks_queue", tags=["tasks_queue"]
+)
+
+
+if not is_in_vercel():
+    from mtmai.api.routes import admin
+
+    api_router.include_router(
+        admin.router,
+        prefix="/admin",
+        tags=["admin"],
+    )
+
+    from mtmai.api.demos import demo_pgmq
+
+    api_router.include_router(demo_pgmq.router, prefix="/demo_pgmq", tags=["demo_pgmq"])
+
+    from mtmai.api.routes import server
+
+    api_router.include_router(server.router, prefix="/server", tags=["server"])
+
+    from mtmai.api.demos import demos
+
+    api_router.include_router(demos.router, prefix="/demos/demos", tags=["demos_demos"])
+
+    from mtmai.trans import trans_api
+
+    api_router.include_router(trans_api.router, prefix="/trans", tags=["trans"])
---
+++ packages/mtmai/mtmai-0.3.737-py3-none-any.whl/mtmai/mtlibs/mtmcrawler/mtmcrawler.py
@@ -0,0 +1,54 @@
+"""
+爬虫主要的功能
+"""
+
+from sqlmodel import Session
+
+from mtmai.core.logging import get_logger
+from mtmai.models.mtcrawl import MTCrawlPage
+
+logger = get_logger()
+
+
+async def _fetch_page_html(url: str):
+    """
+    爬取指定页面
+    """
+    import httpx
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.text
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP error occurred: {e}")
+            return None
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            return None
+
+
+async def crawl_page(db: Session, site_id: str, url: str):
+    """
+    爬取一个页面并建立页面索引
+    """
+    logger.info(f"crawl_page: {url}")
+    html = await _fetch_page_html(url)
+    if html is None:
+        return None
+    logger.info(f"crawl_page: {len(html)}")
+
+    db.exec(
+        MTCrawlPage(
+            site_id=site_id,
+            url=url,
+            title="",
+            description="",
+            keywords="",
+            author="fake_author",
+            copyright="fake_copyright",
+        )
+    )
+    db.commit()
+    return html
