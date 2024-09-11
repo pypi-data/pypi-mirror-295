15c8648da175bf7a0093272c316f698086fe8f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:09 +0000

    Add package ShelfSimplify/0.4.0/ShelfSimplify-0.4.0.tar.gz

---
+++ packages/ShelfSimplify/ShelfSimplify-0.4.0.tar.gz/ShelfSimplify-0.4.0/ShelfSimplify/__init__.py
@@ -0,0 +1,4 @@
+from .main import createfile, appendfile, readfile
+from .main import renamefile, replacefile, removefile
+from .main import getsysdetails
+from .main import getsys_node, getsys_machine, getsys_system, getsys_version, getsys_processor, getsys_release
