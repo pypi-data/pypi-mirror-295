71de3d901574af15ec0c83641ac65845957d1e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:05:17 +0000

    Add package qxmt/0.0.0/qxmt-0.0.0.tar.gz

---
+++ packages/qxmt/qxmt-0.0.0.tar.gz/qxmt-0.0.0/qxmt/experiment/schema.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from pydantic import BaseModel, ConfigDict
+
+from qxmt.datasets.schema import Dataset
+from qxmt.models.base import BaseMLModel
+
+
+class RunRecord(BaseModel):
+    run_id: int
+    desc: str
+    execution_time: str
+    commit_id: str
+    config_path: Path | str
+    evaluation: dict[str, float]
+
+
+class RunArtifact(BaseModel):
+    run_id: int
+    dataset: Dataset
+    model: BaseMLModel
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class ExperimentDB(BaseModel):
+    name: str
+    desc: str
+    working_dirc: Path
+    experiment_dirc: Path
+    runs: list[RunRecord]
