8e3b6811ae4925a0b04bcf707c09bc3823000af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:49 +0000

    Add package genesis-metallicity/0.6/genesis_metallicity-0.6.tar.gz

---
+++ packages/genesis-metallicity/genesis_metallicity-0.6.tar.gz/genesis_metallicity-0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='genesis_metallicity',
+    version='0.6',
+    packages=find_packages(),
+    description='non-parametric gas-phase metallicity and electron temperature estimation',
+    author='Danial Langeroodi',
+    author_email='dlangaroudi@gmail.com',
+    url='https://github.com/langeroodi/genesis_metallicity',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'],
+    include_package_data=True,
+    package_data={'genesis_metallicity': ['data/*.pkl']},
+    python_requires='>=3.6',
+    install_requires=required,
+)
