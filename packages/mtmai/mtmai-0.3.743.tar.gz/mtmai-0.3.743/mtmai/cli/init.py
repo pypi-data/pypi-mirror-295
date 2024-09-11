78a366c8ca2cb31971ecd97f37b7d093618134e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 15:05:24 +0000

    Add package mtmai/0.3.743/mtmai-0.3.743.tar.gz

---
+++ packages/mtmai/mtmai-0.3.743.tar.gz/mtmai-0.3.743/mtmai/cli/init.py
@@ -0,0 +1,33 @@
+"""部署相关的子命令"""
+
+import asyncio
+
+from mtmai.core.logging import get_logger
+
+logger = get_logger()
+
+
+class CliInit:
+    """
+    开发环境项目一键初始化，可在github workspace ,huggingface, gitpod.io 等免费平台一件初始化开发环境。
+
+    """
+
+    def run(self, *args, **kwargs) -> None:
+        from mtmai.mtlibs import dev_helper
+        from mtmlib.mtutils import is_in_gitpod
+
+        dev_helper.init_project()
+
+        if is_in_gitpod():
+            import threading
+
+            from mtmlib import tunnel
+
+            threading.Thread(
+                target=lambda: asyncio.run(tunnel.start_cloudflared())
+            ).start()
+
+            from mtmai.mtlibs.server.kasmvnc import run_kasmvnc
+
+            threading.Thread(target=run_kasmvnc).start()
