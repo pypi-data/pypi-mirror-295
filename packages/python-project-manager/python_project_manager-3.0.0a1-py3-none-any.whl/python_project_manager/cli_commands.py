7bf9bc42749defb7fb9bac9427466ad8dd1fcff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:44 +0000

    Add package python-project-manager/3.0.0a1/python_project_manager-3.0.0a1-py3-none-any.whl

---
+++ packages/python-project-manager/python_project_manager-3.0.0a1-py3-none-any.whl/python_project_manager/cli_commands.py
@@ -0,0 +1,18 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+# Commands
+from python_project_manager.ppm_commands.ppm_init import init
+from python_project_manager.ppm_commands.ppm_run import run
+from python_project_manager.ppm_commands.ppm_version import version
+from python_project_manager.ppm_commands.ppm_install import install
+from python_project_manager.ppm_commands.ppm_list import list
+
+cli.add_command(init)
+cli.add_command(run)
+cli.add_command(version)
+cli.add_command(install)
+cli.add_command(list)
