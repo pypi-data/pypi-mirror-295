feb15639901cf54a673519fed6863bb9b03c2072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 06:04:24 +0000

    Add package fraclib/1.2/fraclib-1.2.tar.gz

---
+++ packages/fraclib/fraclib-1.2.tar.gz/fraclib-1.2/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fraclib",
+    version="1.2",
+    packages=find_packages(),
+    install_requires=[],  # Si necesitas dependencias adicionales
+    description="Librería para trabajar con fracciones",
+    author="Rodrigo Enrique Guzman Roldan",
+    author_email="rodriguzrol1@gmail.com",
+)
