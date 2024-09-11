a750c3dd6acb372e6113819c6e1b610ccab5f848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 06:04:44 +0000

    Add package dipdup/8.0.0/dipdup-8.0.0.tar.gz

---
+++ packages/dipdup/dipdup-8.0.0.tar.gz/dipdup-8.0.0/src/dipdup/__init__.py
@@ -0,0 +1,9 @@
+"""Root DipDup package.
+
+Contains software versions and other metadata.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version('dipdup')
+__spec_version__ = '3.0'
