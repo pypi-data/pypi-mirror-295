4912e55681bc26bc44e8ae424d5042fdd59a3eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:35 +0000

    Add package djing/0.2.2/djing-0.2.2.tar.gz

---
+++ packages/djing/djing-0.2.2.tar.gz/djing-0.2.2/djing/ui/src/main.tsx
@@ -0,0 +1,27 @@
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import MainLayout from "./Layout/MainLayout";
+import "vite/modulepreload-polyfill";
+import "./index.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  createInertiaApp({
+    resolve: async (name) => {
+      const pages = import.meta.glob("./Pages/**/*.tsx");
+
+      let page: any = pages[`./Pages/${name}.tsx`];
+
+      const module = await page();
+
+      const Component = module.default;
+
+      Component.layout =
+        Component.layout || ((page: any) => <MainLayout>{page}</MainLayout>);
+
+      return module;
+    },
+    setup({ el, App, props }) {
+      createRoot(el).render(<App {...props} />);
+    },
+  });
+});
