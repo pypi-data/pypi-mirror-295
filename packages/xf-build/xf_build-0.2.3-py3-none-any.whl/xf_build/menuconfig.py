42ea94dc681f1f1f6a1758c0ff10f413ac9afff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:04:42 +0000

    Add package xf-build/0.2.3/xf_build-0.2.3-py3-none-any.whl

---
+++ packages/xf-build/xf_build-0.2.3-py3-none-any.whl/xf_build/menuconfig.py
@@ -0,0 +1,87 @@
+import os
+from pathlib import Path
+from kconfiglib import Kconfig
+from menuconfig import menuconfig
+import logging
+
+
+class MenuConfig(Kconfig):
+    """
+    menuconfig相关操作内容
+    """
+
+    CONFIG_NAME: str = "xfconfig"
+    HEADER_DIR: str = "header_config"
+    HEADER_NAME: str = "xfconfig.h"
+    XFKCONFIG_NAME: str = "XFKconfig"
+    DEFAULT_CONFIG: str = "xfconfig.defaults"
+    KCONFIG_PREFIX: str = "CONFIG_"
+    KCONFIG_CONFIG_HEADER: str = "# Welcome to xfusion\n"
+    MENUCONFIG_STYLE: str = "default selection=fg:white,bg:blue"
+    HEADER_TEMPLATE: str = ("#ifndef __XF_CONFIG_H__\n"
+                            "#define __XF_CONFIG_H__\n"
+                            "\n{0}\n\n"
+                            "#endif // __XF_CONFIG_H__\n")
+
+    def __init__(self,
+                 config_in: Path,
+                 target_path: Path,
+                 build_path: Path) -> None:
+        logging.debug(f"config_in:{config_in}")
+        super().__init__(str(config_in), True, True, "utf-8", False)
+        project_path = build_path / ".."
+        proj_config_path = project_path / self.CONFIG_NAME
+        self.header_path = build_path / self.HEADER_DIR / self.HEADER_NAME
+
+        os.environ["MENUCONFIG_STYLE"] = self.MENUCONFIG_STYLE
+        os.environ["KCONFIG_CONFIG"] = proj_config_path.as_posix()
+        os.environ["KCONFIG_CONFIG_HEADER"] = self.KCONFIG_CONFIG_HEADER
+        os.environ["CONFIG_"] = self.KCONFIG_PREFIX
+
+        proj_default_config_path = project_path / self.DEFAULT_CONFIG
+        target_default_config_path = target_path / self.DEFAULT_CONFIG
+
+        if proj_config_path.exists():
+            logging.debug(f"load config: {proj_config_path}")
+            self.load_config(proj_config_path.as_posix())
+        elif proj_default_config_path.exists():
+            logging.debug(f"load config: {proj_default_config_path}")
+            self.load_config(proj_default_config_path.as_posix())
+        elif target_default_config_path.exists():
+            logging.debug(f"load config: {target_default_config_path}")
+            self.load_config(target_default_config_path.as_posix())
+
+        # 防止文件夹及文件夹没被建立
+        if not self.header_path.is_file():
+            header_dirs = self.header_path.parent
+            if not header_dirs.is_dir():
+                header_dirs.mkdir(parents=True, exist_ok=True)
+            with self.header_path.open("w", encoding="utf-8") as f:
+                f.write("")
+
+        self.write_autoconf(self.header_path.as_posix())
+        self.__add_header()
+
+    def __add_header(self) -> None:
+        with open(self.header_path, "r", encoding="utf-8") as f:
+            header_contents = f.read()
+        with open(self.header_path, "w", encoding="utf-8") as f:
+            header_contents = self.HEADER_TEMPLATE.format(header_contents)
+            f.write(header_contents)
+
+    def start(self) -> None:
+        """
+        运行menuconfig配置页面，并生成头文件
+        """
+        menuconfig(self)
+        self.write_autoconf(self.header_path)
+        self.__add_header()
+
+    def get_macro(self, macro):
+        """
+        获取menuconfig产生的宏
+        """
+        value = self.syms.get(macro)
+        if value is None:
+            return None
+        return value.str_value
