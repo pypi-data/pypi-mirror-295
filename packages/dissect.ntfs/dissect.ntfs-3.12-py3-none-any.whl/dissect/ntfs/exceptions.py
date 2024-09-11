198ff4c57b3c4a662ceefd68a924b581109da4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:38 +0000

    Add package dissect.ntfs/3.12/dissect.ntfs-3.12-py3-none-any.whl

---
+++ packages/dissect.ntfs/dissect.ntfs-3.12-py3-none-any.whl/dissect/ntfs/exceptions.py
@@ -0,0 +1,34 @@
+class Error(Exception):
+    pass
+
+
+class BrokenIndexError(Error):
+    pass
+
+
+class BrokenMftError(Error):
+    pass
+
+
+class FilenameNotAvailableError(Error):
+    pass
+
+
+class FileNotFoundError(Error):
+    pass
+
+
+class MftNotAvailableError(Error):
+    pass
+
+
+class NotADirectoryError(Error):
+    pass
+
+
+class NotAReparsePointError(Error):
+    pass
+
+
+class VolumeNotAvailableError(Error):
+    pass
