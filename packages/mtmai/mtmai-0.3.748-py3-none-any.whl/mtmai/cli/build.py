56906486b7175a3b4c50ff3075c628db694f15cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:06:10 +0000

    Add package mtmai/0.3.748/mtmai-0.3.748-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.748-py3-none-any.whl/mtmai/cli/build.py
@@ -0,0 +1,18 @@
+def register_build_commands(cli):
+    print("aaa")
+
+    @cli.group()
+    def build():
+        """Build related commands"""
+        pass
+
+    @build.command()
+    def docker_base():
+        """Build Docker base image"""
+        # CliBuild().run("docker_base")
+        print("build docker_base")
+
+    @build.command()
+    def frontend():
+        """Build frontend assets"""
+        print("build frontend")
