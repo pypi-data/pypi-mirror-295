c9f8972f7f835d455816b0bb733cb136d51faafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:05:23 +0000

    Add package dendro/0.6.4/dendro-0.6.4.tar.gz

---
+++ packages/dendro/dendro-0.6.4.tar.gz/dendro-0.6.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# read version from dendro/version.txt
+with open('dendro/version.txt') as f:
+    __version__ = f.read().strip()
+
+setup(
+    name='dendro',
+    version=__version__,
+    author="Jeremy Magland, Luiz Tauffer, Alessio Buccino, Ben Dichter",
+    author_email="jmagland@flatironinstitute.org",
+    url="https://github.com/magland/dendro",
+    description="",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'dendro': ['version.txt']},
+    install_requires=[
+        'click',
+        'simplejson',
+        'numpy',
+        'PyYAML',
+        'pydantic', # intentionally do not specify version 1 or 2 since we support both
+        'psutil',
+        'requests'
+    ],
+    extras_require={
+        'compute_client': [
+            'pubnub>=7.2.0'
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "dendro=dendro.cli:main",
+        ],
+    }
+)
