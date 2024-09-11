f444b12219376acabb76385edc5ad611afc09ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:05:38 +0000

    Add package splight-lib/5.10.2/splight_lib-5.10.2.tar.gz

---
+++ packages/splight-lib/splight_lib-5.10.2.tar.gz/splight_lib-5.10.2/splight_lib/models/asset.py
@@ -0,0 +1,77 @@
+import warnings
+from typing import Any, List, Optional, Tuple
+
+from geojson_pydantic import GeometryCollection
+from pydantic import BaseModel, Field
+
+from splight_lib.constants import DESCRIPTION_MAX_LENGTH
+from splight_lib.models.attribute import Attribute
+from splight_lib.models.database_base import (
+    ResourceSummary,
+    SplightDatabaseBaseModel,
+)
+from splight_lib.models.exceptions import MethodNotAllowed
+from splight_lib.models.metadata import Metadata
+from splight_lib.models.tag import Tag
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+class AssetRelationship(BaseModel):
+    id: str
+    name: str
+    description: Optional[str] = None
+    related_asset_kind: Optional[ResourceSummary] = None
+    asset: ResourceSummary
+    related_asset: Optional[ResourceSummary] = None
+
+
+class AssetKind(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    name: str
+
+    def save(self):
+        raise MethodNotAllowed("AssetKind objects are read-only")
+
+    def delete(self):
+        raise MethodNotAllowed("AssetKind objects are read-only")
+
+
+class Asset(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    name: str
+    description: Optional[str] = Field(
+        default=None, max_length=DESCRIPTION_MAX_LENGTH
+    )
+    tags: List[Tag] = []
+    attributes: List[Attribute] = []
+    metadata: List[Metadata] = []
+    geometry: Optional[GeometryCollection] = None
+    centroid_coordinates: Optional[Tuple[float, float]] = None
+    kind: Optional[AssetKind] = None
+    actions: Optional[List[ResourceSummary]] = None
+    related_to: List[AssetRelationship] = []
+    related_from: List[AssetRelationship] = []
+
+    def set_attribute(self, attribute: Attribute, value: Any, value_type: str):
+        new_value = self._db_client.operate(
+            resource_name="set-asset-attribute",
+            instance={
+                "asset": self.id,
+                "attribute": attribute.id,
+                "value": value,
+                "type": value_type,
+            },
+        )
+        return new_value
+
+    def get_attribute(self, attribute: Attribute, value_type: str):
+        new_value = self._db_client.operate(
+            resource_name="get-asset-attribute",
+            instance={
+                "asset": self.id,
+                "attribute": attribute.id,
+                "type": value_type,
+            },
+        )
+        return new_value
---
+++ packages/splight-lib/splight_lib-5.10.2.tar.gz/splight_lib-5.10.2/splight_lib/models/dashboard.py
@@ -0,0 +1,93 @@
+from typing import Any, Dict, List, Optional
+
+from pydantic import Field
+
+from splight_lib.constants import DESCRIPTION_MAX_LENGTH
+from splight_lib.models.database_base import SplightDatabaseBaseModel
+
+
+class Filter(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    chart_item: str
+    operator: Optional[str] = None
+    key: Optional[str] = None
+    value: Optional[str] = None
+    label: Optional[str] = None
+
+
+class AdvancedFilter(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    chart_item: str
+    operator: Optional[str] = None
+    key: Optional[str] = None
+    field: Optional[str] = None
+    value: Optional[str] = None
+
+
+class ChartItem(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    chart: str
+    label: str
+    order: Optional[int] = None
+    color: Optional[str] = None
+    position_x: Optional[str] = None
+    position_y: Optional[str] = None
+    width: Optional[str] = None
+    height: Optional[str] = None
+    split_by: Optional[str] = None
+    aggregate_criteria: Optional[str] = None
+    aggregate_period: Optional[str] = None
+    source: Optional[str] = None
+    source_label: Optional[str] = None
+    source_type: Optional[str] = None
+    source_component_label: Optional[str] = None
+    source_component_id: Optional[str] = None
+    output_format: Optional[str] = None
+    target: Optional[str] = None
+    old_source: Optional[str] = None
+    old_source_label: Optional[str] = None
+    filters: Optional[List[Filter]] = None
+    advanced_filters: Optional[List[AdvancedFilter]] = None
+    query_params: Optional[str] = None
+
+
+class Chart(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    name: str
+    description: Optional[str] = Field(
+        default=None, max_length=DESCRIPTION_MAX_LENGTH
+    )
+    items: Optional[List[ChartItem]] = None
+    tab: str
+    position_x: Optional[str] = 0
+    position_y: Optional[str] = 0
+    height: Optional[str] = 28
+    width: Optional[str] = 80
+    min_height: Optional[str] = 14
+    min_width: Optional[str] = 14
+    type: Optional[str] = None
+    timestamp_gte: Optional[str] = None
+    timestamp_lte: Optional[str] = None
+    refresh_interval: Optional[str] = None
+    relative_window_time: Optional[str] = None
+    external_resource: Optional[str] = None
+    external_resource_type: Optional[str] = None
+    y_axis_max_limit: Optional[str] = None
+    y_axis_min_limit: Optional[str] = None
+    config: Optional[Dict[str, Any]] = None
+
+
+class Tab(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    name: str
+    charts: Optional[List[Chart]] = None
+    order: Optional[int] = None
+    dashboard: str
+
+
+class Dashboard(SplightDatabaseBaseModel):
+    id: Optional[str] = None
+    name: str
+    description: Optional[str] = Field(
+        default=None, max_length=DESCRIPTION_MAX_LENGTH
+    )
