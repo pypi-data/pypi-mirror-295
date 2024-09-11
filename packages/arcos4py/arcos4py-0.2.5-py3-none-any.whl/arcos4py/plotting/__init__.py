3e34fcbb00318ca1ee7b0666a4befc5cfb255046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:04:40 +0000

    Add package arcos4py/0.2.5/arcos4py-0.2.5-py3-none-any.whl

---
+++ packages/arcos4py/arcos4py-0.2.5-py3-none-any.whl/arcos4py/plotting/__init__.py
@@ -0,0 +1,9 @@
+"""Tools for plotting collective events."""
+
+__author__ = """Benjamin Graedel"""
+__email__ = "benjamin.graedel@unibe.ch"
+__version__ = '0.2.5'
+
+from ._plotting import NoodlePlot, dataPlots, plotOriginalDetrended, statsPlots
+
+__all__ = ["plotOriginalDetrended", "dataPlots", "statsPlots", "NoodlePlot"]
