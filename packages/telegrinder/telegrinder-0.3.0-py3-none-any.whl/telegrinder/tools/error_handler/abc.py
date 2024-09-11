7118116fd359f3df5e234dc97ad72a120394707a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:38 +0000

    Add package telegrinder/0.3.0/telegrinder-0.3.0-py3-none-any.whl

---
+++ packages/telegrinder/telegrinder-0.3.0-py3-none-any.whl/telegrinder/tools/error_handler/abc.py
@@ -0,0 +1,33 @@
+import typing
+from abc import ABC, abstractmethod
+
+from fntypes.result import Result
+
+from telegrinder.api import API
+from telegrinder.bot.dispatch.context import Context
+
+EventT = typing.TypeVar("EventT")
+Handler = typing.Callable[typing.Concatenate[EventT, ...], typing.Awaitable[typing.Any]]
+
+
+class ABCErrorHandler(ABC, typing.Generic[EventT]):
+    @abstractmethod
+    def __call__(
+        self,
+        *args: typing.Any,
+        **kwargs: typing.Any,
+    ) -> typing.Callable[[typing.Callable[..., typing.Any]], typing.Callable[..., typing.Any]]:
+        """Decorator for registering callback as an error handler."""
+
+    @abstractmethod
+    async def run(
+        self,
+        handler: Handler[EventT],
+        event: EventT,
+        api: API,
+        ctx: Context,
+    ) -> Result[typing.Any, typing.Any]:
+        """Run error handler."""
+
+
+__all__ = ("ABCErrorHandler",)
