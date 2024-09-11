b7b2508ba24e530ab2739f0948261ec1f10c443c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:05:10 +0000

    Add package echion/0.3.3/echion-0.3.3.tar.gz

---
+++ packages/echion/echion-0.3.3.tar.gz/echion-0.3.3/.github/workflows/cibw_before_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. scripts/build_libunwind.sh
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+# Install xz
+yum makecache
+yum install -y gettext-devel
+
+git clone https://git.tukaani.org/xz.git
+cd xz
+./autogen.sh
+./configure CFLAGS='-fPIC' CXXFLAGS='-fPIC'
+make
+make install
+cd ..
+rm -rf xz
