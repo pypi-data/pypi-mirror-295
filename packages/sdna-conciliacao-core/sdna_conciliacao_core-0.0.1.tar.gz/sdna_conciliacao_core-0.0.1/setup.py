9cdd84949a93d11f0c7a18e8f3ebae5ef5d6cdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 06:04:41 +0000

    Add package sdna-conciliacao-core/0.0.1/sdna_conciliacao_core-0.0.1.tar.gz

---
+++ packages/sdna-conciliacao-core/sdna_conciliacao_core-0.0.1.tar.gz/sdna_conciliacao_core-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sdna_conciliacao_core",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "psycopg2-binary>=2.9.5",
+        "pyspark>=3.3.2",
+    ],
+    author="Fernando Miranda Calil",
+    author_email="fcalil@sdnadigital.com",
+    description="Utilitários comuns para projetos de conciliação",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/sdna-team/okto/sdna-conciliacao",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
