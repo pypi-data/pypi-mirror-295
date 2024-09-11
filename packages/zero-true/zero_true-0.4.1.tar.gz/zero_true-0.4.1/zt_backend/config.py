f8df9eb7c097bb3252a4d2cc7ee2bcd4a4621dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 06:04:31 +0000

    Add package zero-true/0.4.1/zero_true-0.4.1.tar.gz

---
+++ packages/zero-true/zero_true-0.4.1.tar.gz/zero_true-0.4.1/zt_backend/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    run_mode: str = "app"
+    project_name: str = ""
+    user_name: str = ""
+    ws_url: str = ""
+    local_url: str = ""
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
