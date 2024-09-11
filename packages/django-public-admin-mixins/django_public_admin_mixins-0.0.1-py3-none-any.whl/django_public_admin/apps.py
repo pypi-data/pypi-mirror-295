14592e9a14a08c76a0a3bc1bb3fe8e09be4541d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:04:32 +0000

    Add package django-public-admin-mixins/0.0.1/django_public_admin_mixins-0.0.1-py3-none-any.whl

---
+++ packages/django-public-admin-mixins/django_public_admin_mixins-0.0.1-py3-none-any.whl/django_public_admin/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PublicAdminConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_public_admin"
+    label ="public_admin"
