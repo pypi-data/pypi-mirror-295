9ecfc02748ad56834bf63bf13084f6483a283587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:42 +0000

    Add package pywithmysql/0.1.3/pywithmysql-0.1.3.tar.gz

---
+++ packages/pywithmysql/pywithmysql-0.1.3.tar.gz/pywithmysql-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pywithmysql',
+    version='0.1.3',
+    description='The library to work with MySQL databases for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hardusss/pywithmysql',
+    author='DanikBlatota777',
+    author_email='danylo29bro@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pymysql'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
