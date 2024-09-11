4880363d85bf86fd009a405ccfedb2a17e69d900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:08 +0000

    Add package cmake-file-api/0.0.8.1/cmake_file_api-0.0.8.1-py3-none-any.whl

---
+++ packages/cmake-file-api/cmake_file_api-0.0.8.1-py3-none-any.whl/cmake_file_api/kinds/configureLog/v1.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from cmake_file_api.kinds.common import VersionMajorMinor
+from cmake_file_api.kinds.kind import ObjectKind
+
+
+class ConfigureLogV1:
+    KIND = ObjectKind.CONFIGURELOG
+
+    __slots__ = ("version", "path", "eventKindNames")
+
+    def __init__(self, version: VersionMajorMinor, path: Path, eventKindNames: list[str]):
+        self.version = version
+        self.path = path
+        self.eventKindNames = eventKindNames
+
+    @classmethod
+    def from_dict(cls, dikt: dict[str, Any], reply_path: Path) -> "ConfigureLogV1":
+        if dikt["kind"] != cls.KIND.value:
+            raise ValueError
+        path = Path(dikt["path"])
+        version = VersionMajorMinor.from_dict(dikt["version"])
+        event_kind_names = dikt["eventKindNames"]
+        return cls(version, path, event_kind_names)
+
+    @classmethod
+    def from_path(cls, path: Path, reply_path: Path) -> "ConfigureLogV1":
+        dikt = json.load(path.open())
+        return cls.from_dict(dikt, reply_path)
+
+    def __repr__(self) -> str:
+        return "{}(version={}, paths={}, configurations={})".format(
+            type(self).__name__,
+            self.version,
+            self.path,
+            self.eventKindNames,
+        )
