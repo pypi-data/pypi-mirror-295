f8d8da8e4cf7c6514c3792ede41395121aa1ce05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:04:37 +0000

    Add package analysta-index/0.2.49/analysta_index-0.2.49-py3-none-any.whl

---
+++ packages/analysta-index/analysta_index-0.2.49-py3-none-any.whl/analysta_index/document_loaders/constants.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2023 Artem Rozumenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from langchain_community.document_loaders import (TextLoader,
+        UnstructuredMarkdownLoader,
+        PyPDFLoader,
+        UnstructuredPDFLoader,UnstructuredWordDocumentLoader,
+        JSONLoader, AirbyteJSONLoader, UnstructuredHTMLLoader,
+        UnstructuredPowerPointLoader, PythonLoader)
+from langchain_unstructured import UnstructuredLoader
+
+from .AnalystaCSVLoader import AnalystaCSVLoader
+from .AnalystaExcelLoader import AnalystaExcelLoader
+
+loaders_map = {
+    '.txt': {
+        'class': TextLoader,
+        'kwargs': {
+            'autodetect_encoding': True
+        }
+    },
+    '.md': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.csv': {
+        'class': AnalystaCSVLoader,
+        'kwargs': {
+            'encoding': 'utf-8',
+            'raw_content': False
+        }
+    },
+    '.xlsx': {
+        'class': AnalystaExcelLoader,
+        'kwargs': {
+            'raw_content': False
+        }
+    },
+    '.xls': {
+        'class': AnalystaExcelLoader,
+        'kwargs': {
+            'raw_content': False
+        }
+    },
+    '.pdf': {
+        'class': PyPDFLoader,
+        'kwargs': {}
+    },
+    '.docx': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.json': {
+        'class': TextLoader,
+        'kwargs': {
+            'autodetect_encoding': True
+        }
+    },
+    '.jsonl': {
+        'class': AirbyteJSONLoader,
+        'kwargs': {}
+    },
+    '.htm': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.html': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.ppt': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.pptx': {
+        'class': UnstructuredLoader,
+        'kwargs': {}
+    },
+    '.py': {
+        'class': PythonLoader,
+        'kwargs': {}
+    }
+}
