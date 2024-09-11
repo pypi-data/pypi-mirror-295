bb9851cff7d58b7aac478125a1f1578706149710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:51 +0000

    Add package optimodel-guard-server/0.4.1/optimodel_guard_server-0.4.1.tar.gz

---
+++ packages/optimodel-guard-server/optimodel_guard_server-0.4.1.tar.gz/optimodel_guard_server-0.4.1/src/optimodel_guard/Guards/GuardBaseClass.py
@@ -0,0 +1,36 @@
+from typing import Dict, Any
+from optimodel_types.providerTypes import QueryParams, QueryResponse
+from pydantic import BaseModel
+
+
+class GuardEvalResponse(BaseModel):
+    failure: bool
+    metadata: Dict[str, Any] | None = {}
+
+
+class GuardBaseClass:
+    """
+    Common interface for all guards to implement
+    """
+
+    def handlePreQuery(self, query: QueryParams) -> GuardEvalResponse:
+        """
+        Handle a pre-query event.
+
+        @return bool: True if the query failed the check, False if it passed the check
+
+        @NOTE This is only called for preQuery guards
+        """
+        pass
+
+    def handlePostQuery(
+        self, query: QueryParams, response: QueryResponse
+    ) -> GuardEvalResponse:
+        """
+        Handle a post-query event.
+
+        @return bool: True if the response failed the check, False if it passed the check
+
+        @NOTE This is only called for postQuery guards
+        """
+        pass
---
+++ packages/optimodel-guard-server/optimodel_guard_server-0.4.1.tar.gz/optimodel_guard_server-0.4.1/src/optimodel_guard/index.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from huggingface_hub import login
+
+from optimodel_guard.Guards import GuardBaseClass, GuardMapping
+from optimodel_types import GuardBody, GuardResponse
+
+import logging
+import sys
+import os
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+"""
+Login to hugging face so we can use models that require authentication
+"""
+HF_TOKEN = os.environ.get("HF_TOKEN")
+if HF_TOKEN:
+    logger.info(f"Logging in to hugging face...")
+    login(HF_TOKEN)
+
+
+app = FastAPI()
+
+baseURL = "/optimodel-guard/api/v1"
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info(f"🔐 Starting Optimodel Guard Server...")
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logger.error(request, exc_str)
+    content = {"status_code": 10422, "message": exc_str, "data": None}
+    return JSONResponse(content=content, status_code=422)
+
+
+@app.post(f"{baseURL}/guard")
+async def query(data: GuardBody):
+    logger.info(f"New request to check guards...")
+
+    """
+    Get an instance of the guard passed
+    """
+    guard: GuardBaseClass = GuardMapping[data.guard.guardName]
+
+    """
+    Then check the guard
+    """
+    if data.guard.guardType == "preQuery":
+        guardResponse = guard.handlePreQuery(messages=data.messages, config=data.guard)
+    elif data.guard.guardType == "postQuery":
+        guardResponse = guard.handlePostQuery(messages=data.messages, config=data.guard)
+
+    toReturn = GuardResponse(
+        failure=guardResponse.failure, metadata=guardResponse.metadata
+    )
+    return toReturn
+
+
+@app.get(f"{baseURL}/health")
+async def getHealth():
+    return {"status": "ok"}
