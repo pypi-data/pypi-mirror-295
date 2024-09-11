ef47665c1a5cb73376cc9a8e629d73c1a6fa61a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:05:20 +0000

    Add package MeowCore/0.0.1/MeowCore-0.0.1-py3-none-any.whl

---
+++ packages/MeowCore/MeowCore-0.0.1-py3-none-any.whl/MeowCore/main.py
@@ -0,0 +1,11 @@
+
+import logging
+
+logger = logging.getLogger('MeowCore')
+logger.setLevel(logging.DEBUG)
+
+
+class MeowCore:
+    def __init__(self):
+        print("meowcore working")
+        return
