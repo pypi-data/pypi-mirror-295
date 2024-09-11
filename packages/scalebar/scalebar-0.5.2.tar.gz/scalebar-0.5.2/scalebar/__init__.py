82fa765c6f82369d54b40a79ff55beb6059e9c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:05:20 +0000

    Add package scalebar/0.5.2/scalebar-0.5.2.tar.gz

---
+++ packages/scalebar/scalebar-0.5.2.tar.gz/scalebar-0.5.2/scalebar/__init__.py
@@ -0,0 +1,9 @@
+from scalebar.core.image_wrapper import Images
+from scalebar.core.position import Position
+from scalebar.core.estimation import Result
+
+__all__ = [
+    "Images",
+    "Result",
+    "Position",
+]
