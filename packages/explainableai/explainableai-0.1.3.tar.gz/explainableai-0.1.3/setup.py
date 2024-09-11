1abb3e8d1afab8cab08226cea39722dbd32c15a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:55 +0000

    Add package explainableai/0.1.3/explainableai-0.1.3.tar.gz

---
+++ packages/explainableai/explainableai-0.1.3.tar.gz/explainableai-0.1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='explainableai',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'shap',
+        'matplotlib',
+        'seaborn',
+        'plotly',
+        'ipywidgets',
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define command-line scripts here if needed
+            # "explain=explainableai:main",
+        ],
+    },
+    author=['Om Bhojane', 'Palak Boricha'],
+    author_email='ombhojane05@gmail.com',
+    description='A package for Explainable AI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ombhojane/explainableai',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
