2d9530ee59c788492d770d1748b073315ee7ecae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:39 +0000

    Add package pytimer2/0.2.1/pytimer2-0.2.1.tar.gz

---
+++ packages/pytimer2/pytimer2-0.2.1.tar.gz/pytimer2-0.2.1/README.md
@@ -0,0 +1,17 @@
+# PyTimer
+
+PyTimer is a simple Python package that provides countdown timer functionality. It is designed to be lightweight and easy to use, making it ideal for timing events, tracking intervals, and managing delays in various Python applications.
+
+## Features
+
+- **Easy-to-Use Interface:** Quickly create and start countdown timers with minimal code.
+- **Multithreading Support:** Runs the countdown in a separate thread, allowing the main program to remain responsive.
+- **Real-Time Monitoring:** Check the remaining time at any point using the `get_countdown()` method.
+- **Customizable Duration:** Set the countdown to any number of seconds as needed.
+
+## Installation
+
+You can install PyTimer directly from GitHub or from PyPI once published:
+
+```bash
+pip install pytimer
---
+++ packages/pytimer2/pytimer2-0.2.1.tar.gz/pytimer2-0.2.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pytimer2",
+    version="0.2.1",
+    author="Atiqur Rahman",
+    author_email="rahman.md.attiq@gmail.com",
+    description="PyTimer is a lightweight and easy-to-use Python package designed to provide countdown timer functionality. It offers a simple class-based approach to manage countdowns, making it perfect for various use cases such as timing events, tracking intervals, or managing delays in scripts and applications.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/attiqRahman/pytimer",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
