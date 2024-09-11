938cccfab94a9b4cb64801b42b079fe930a9caf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:04:36 +0000

    Add package rustPlusPushReceiver/0.6.1/rustPlusPushReceiver-0.6.1.tar.gz

---
+++ packages/rustPlusPushReceiver/rustPlusPushReceiver-0.6.1.tar.gz/rustPlusPushReceiver-0.6.1/README.rst
@@ -0,0 +1,5 @@
+Subscribe to GCM/FCM and receive notifications
+
+python implementation of https://github.com/MatthieuLemoine/push-receiver
+
+Used by https://github.com/olijeffers0n/rustplus
---
+++ packages/rustPlusPushReceiver/rustPlusPushReceiver-0.6.1.tar.gz/rustPlusPushReceiver-0.6.1/setup.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+from setuptools import setup, find_packages
+
+push_receiver_classifiers = [
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers",
+    "License :: Public Domain",
+    "Topic :: Software Development :: Libraries"
+]
+
+with open("README.rst", "r") as f:
+    push_receiver_readme = f.read()
+
+setup(
+    name="rustPlusPushReceiver",
+    version="0.6.1",
+    author="Franc[e]sco & olijeffers0n",
+    url="https://github.com/olijeffers0n/push_receiver",
+    packages=find_packages("."),
+    description="Subscribe to GCM/FCM and receive notifications",
+    long_description=push_receiver_readme,
+    license="Unlicense",
+    classifiers=push_receiver_classifiers,
+    keywords="fcm gcm push notification firebase google",
+    install_requires=["oscrypto", "protobuf", "cryptography", "betterproto", "requests"],
+    extras_require={
+        "example": ["appdirs"]
+    }
+)
