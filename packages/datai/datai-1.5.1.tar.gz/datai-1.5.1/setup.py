056a408e5112a7ff5ad0e1031d27267d3d964dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:38 +0000

    Add package datai/1.5.1/datai-1.5.1.tar.gz

---
+++ packages/datai/datai-1.5.1.tar.gz/datai-1.5.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'datai',
+    version = '1.5.1',
+    description = 'A library for data visualization and cleaning',
+    author = 'M Ans',
+    author_email = 'm.ans.cs@outlook.com',
+    packages = find_packages(),
+    install_requires = [
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'setuptools',
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = '>=3.7',    # Python version requirement
+)
