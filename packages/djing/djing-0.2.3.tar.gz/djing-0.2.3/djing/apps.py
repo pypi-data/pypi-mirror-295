feadc359c30a9c4e172a6607ad1dc4ba0f57a770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:05:21 +0000

    Add package djing/0.2.3/djing-0.2.3.tar.gz

---
+++ packages/djing/djing-0.2.3.tar.gz/djing-0.2.3/djing/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+from djing.core.inertia_app import InertiaApp
+from djing.core.application import Application
+
+
+class DjingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+
+    name = "djing"
+
+    def ready(self):
+        inertia_app = InertiaApp()
+
+        application = Application(inertia_app)
+
+        application.run()
---
+++ packages/djing/djing-0.2.3.tar.gz/djing-0.2.3/djing/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+
+from djing.views import catch_all_view, index, login, process_login
+
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("login", login, name="login"),
+    path("login", process_login, name="process_login"),
+    re_path(r"^.*$", catch_all_view),
+]
