c72a4eb46442b9a28e3f89648967b1194eb942d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:37 +0000

    Add package partitioneer/0.2.13/partitioneer-0.2.13.tar.gz

---
+++ packages/partitioneer/partitioneer-0.2.13.tar.gz/partitioneer-0.2.13/partitioneer/__init__.py
@@ -0,0 +1,9 @@
+from .core import (
+    PartitionFilter,
+    read_data_from_partitions,
+    write_data_to_partitions,
+    get_latest_partition_date,
+    get_first_partition_date,
+    read_latest_partition,
+    read_first_partition,
+)
