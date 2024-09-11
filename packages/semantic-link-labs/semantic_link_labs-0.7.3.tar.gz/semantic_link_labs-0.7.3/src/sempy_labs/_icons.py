7e7a56fd417956268e3aa91854626b5c074acb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:21 +0000

    Add package semantic-link-labs/0.7.3/semantic_link_labs-0.7.3.tar.gz

---
+++ packages/semantic-link-labs/semantic_link_labs-0.7.3.tar.gz/semantic_link_labs-0.7.3/src/sempy_labs/_icons.py
@@ -0,0 +1,33 @@
+green_dot = "\U0001F7E2"
+yellow_dot = "\U0001F7E1"
+red_dot = "\U0001F534"
+in_progress = "⌛"
+checked = "\u2611"
+unchecked = "\u2610"
+start_bold = "\033[1m"
+end_bold = "\033[0m"
+bullet = "\u2022"
+warning = "⚠️"
+error = "\u274C"
+info = "ℹ️"
+data_type_mapping = {
+    "string": "String",
+    "int": "Int64",
+    "tinyint": "Int64",
+    "smallint": "Int64",
+    "bigint": "Int64",
+    "boolean": "Boolean",
+    "timestamp": "DateTime",
+    "date": "DateTime",
+    "decimal(38,18)": "Decimal",
+    "decimal(19,4)": "Decimal",
+    "double": "Double",
+    "float": "Double",
+}
+measure_icon = "\u2211"
+table_icon = "\u229E"
+column_icon = "\u229F"
+model_bpa_name = "ModelBPA"
+report_bpa_name = "ReportBPA"
+severity_mapping = {warning: "Warning", error: "Error", info: "Info"}
+special_characters = ['"', "/", '"', ":", "|", "<", ">", "*", "?", "'", "!"]
