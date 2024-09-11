222da6517bef9a7835dd8efb933e6ba85b3a3201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 14:05:02 +0000

    Add package promptsy/0.1.3.4/promptsy-0.1.3.4.tar.gz

---
+++ packages/promptsy/promptsy-0.1.3.4.tar.gz/promptsy-0.1.3.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promptsy',
+    version='0.1.3.4',
+    description='A Python library for managing and organizing prompts for Large Language Models (LLMs)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Felipe Rafael de Souza',
+    author_email='felipe@evakub.com.br',
+    url='https://github.com/feliperafael/promptsy',
+    packages=find_packages(),
+    include_package_data=True,  
+    package_data={
+        'promptsy': ['prompts/**/*.yaml'],  
+    },
+    install_requires=[
+        'PyYAML',
+        'colorama',
+        'openai',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='prompts llm-prompts prompt-management large-language-models',
+)
