cd124a9c741fd5dcbaf0ae52f00a10df7cf5dd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:06:14 +0000

    Add package lunyamwi/1.0.6/lunyamwi-1.0.6.tar.gz

---
+++ packages/lunyamwi/lunyamwi-1.0.6.tar.gz/lunyamwi-1.0.6/lunyamwi/model_setup.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import json
+from constants import LUNYAMWI_ML_BASE_URL
+
+
+def setup_agent(payload = None):
+    url = LUNYAMWI_ML_BASE_URL + '/agentSetup/'
+    print(url)
+    resp = requests.post(url, data=json.dumps(payload),headers = {'Content-Type': 'application/json'})
+    response = resp.json()
+    return response
+
