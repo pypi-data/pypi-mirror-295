4421ac35f1aaffb42276aa5fc2079900b65ac9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:30 +0000

    Add package PorkofQRCode/1.3/porkofqrcode-1.3.tar.gz

---
+++ packages/PorkofQRCode/porkofqrcode-1.3.tar.gz/porkofqrcode-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+requirements = ["colorama<=0.4.6", "qrcode<=6.1"]
+
+setup(name='PorkofQRCode',
+      version='1.3',
+      description='',
+      packages=['PorkofQRCode'],
+      author_email='porkof@mail2tor.com',
+      zip_safe=False,
+      install_requires=requirements)
