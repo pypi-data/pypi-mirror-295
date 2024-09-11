0fd05353d6ff33fa260430b7ac126126917c1a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:06:04 +0000

    Add package deepmodeling-sphinx/0.2.10/deepmodeling_sphinx-0.2.10.tar.gz

---
+++ packages/deepmodeling-sphinx/deepmodeling_sphinx-0.2.10.tar.gz/deepmodeling_sphinx-0.2.10/deepmodeling_sphinx/banner.js
@@ -0,0 +1,37 @@
+/* The initial version is taken from
+https://github.com/pytorch/pytorch_sphinx_theme
+under MIT license
+*/
+window.mobileMenu = {
+  bind: function () {
+    $("[data-behavior='open-mobile-menu']").on("click", function (e) {
+      e.preventDefault();
+      $(".mobile-main-menu").addClass("open");
+      $("body").addClass("no-scroll");
+
+      mobileMenu.listenForResize();
+    });
+
+    $("[data-behavior='close-mobile-menu']").on("click", function (e) {
+      e.preventDefault();
+      mobileMenu.close();
+    });
+  },
+
+  listenForResize: function () {
+    $(window).on("resize.ForMobileMenu", function () {
+      if ($(this).width() > 768) {
+        mobileMenu.close();
+      }
+    });
+  },
+
+  close: function () {
+    $(".mobile-main-menu").removeClass("open");
+    $("body").removeClass("no-scroll");
+    $(window).off("resize.ForMobileMenu");
+  },
+};
+$(document).ready(function () {
+  mobileMenu.bind();
+});
