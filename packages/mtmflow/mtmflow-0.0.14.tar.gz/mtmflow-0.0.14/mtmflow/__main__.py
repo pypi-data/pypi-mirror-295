c5462e61545d6e2af93d0334ab2f175d96ab575c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 15:05:32 +0000

    Add package mtmflow/0.0.14/mtmflow-0.0.14.tar.gz

---
+++ packages/mtmflow/mtmflow-0.0.14.tar.gz/mtmflow-0.0.14/mtmflow/__main__.py
@@ -0,0 +1,34 @@
+import argparse
+
+from mtmflow.cli.serve import CliServe
+from mtmflow.cli.install import CliInstall
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="mtmflow")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Serve
+    serve_parser = subparsers.add_parser("serve", help="Start the server")
+    cli_serve = CliServe()
+    serve_parser.set_defaults(func=cli_serve.run)
+
+    # Install
+    install_parser = subparsers.add_parser("install", help="Install the server")
+    cli_install = CliInstall()
+    install_parser.set_defaults(func=cli_install.run)
+
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        positional_args = [getattr(args, arg) for arg in vars(args) if arg != 'func']
+        keyword_args = {arg: getattr(args, arg) for arg in vars(args) if arg != 'func'}
+        args.func(*positional_args, **keyword_args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
