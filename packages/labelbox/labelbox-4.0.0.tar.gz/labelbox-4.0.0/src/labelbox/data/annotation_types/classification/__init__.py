19e86fbfdb1b2fbd1db118607de8959649103274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 14:05:04 +0000

    Add package labelbox/4.0.0/labelbox-4.0.0.tar.gz

---
+++ packages/labelbox/labelbox-4.0.0.tar.gz/labelbox-4.0.0/src/labelbox/data/annotation_types/classification/__init__.py
@@ -0,0 +1,2 @@
+from .classification import (Checklist, ClassificationAnswer, Radio,
+                             Text)
---
+++ packages/labelbox/labelbox-4.0.0.tar.gz/labelbox-4.0.0/src/labelbox/data/annotation_types/types.py
@@ -0,0 +1,40 @@
+import sys
+from typing import Generic, TypeVar, Any
+
+from typing_extensions import Annotated
+from packaging import version
+import numpy as np
+
+from pydantic import StringConstraints, Field
+
+Cuid = Annotated[str, StringConstraints(min_length=25, max_length=25)]
+
+DType = TypeVar('DType')
+DShape = TypeVar('DShape')
+
+
+class _TypedArray(np.ndarray, Generic[DType, DShape]):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, val, field: Field):
+        if not isinstance(val, np.ndarray):
+            raise TypeError(f"Expected numpy array. Found {type(val)}")
+        return val
+
+
+if version.parse(np.__version__) >= version.parse('1.25.0'):
+    from typing import GenericAlias
+    TypedArray = GenericAlias(_TypedArray, (Any, DType))
+elif version.parse(np.__version__) >= version.parse('1.23.0'):
+    from numpy._typing import _GenericAlias
+    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
+elif version.parse('1.22.0') <= version.parse(
+        np.__version__) < version.parse('1.23.0'):
+    from numpy.typing import _GenericAlias
+    TypedArray = _GenericAlias(_TypedArray, (Any, DType))
+else:
+    TypedArray = _TypedArray[Any, DType]
