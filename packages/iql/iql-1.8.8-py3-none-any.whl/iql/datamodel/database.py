0d2dd633102050c69a0cd4d19ee23dc0907eb6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:05:27 +0000

    Add package iql/1.8.8/iql-1.8.8-py3-none-any.whl

---
+++ packages/iql/iql-1.8.8-py3-none-any.whl/iql/datamodel/database.py
@@ -0,0 +1,37 @@
+import logging
+from abc import abstractmethod
+from typing import Iterable, Optional
+
+from .result import IqlResult
+
+logger = logging.getLogger(__name__)
+
+
+class IqlDatabase:
+    @abstractmethod
+    def execute_query(
+        self,
+        query: str,
+        completed_results: Iterable[IqlResult],
+        raw: bool = False,
+        parameters: Optional[Iterable] = None,
+    ) -> Optional[IqlResult]:
+        pass
+
+    @abstractmethod
+    def get_connection(self) -> object:
+        pass
+
+    @abstractmethod
+    def close_db(self):
+        pass
+
+
+class IqlDatabaseConnector:
+    @abstractmethod
+    def create_database(self) -> IqlDatabase:
+        pass
+
+    @abstractmethod
+    def create_database_from_con(self, con: object) -> IqlDatabase:
+        pass
