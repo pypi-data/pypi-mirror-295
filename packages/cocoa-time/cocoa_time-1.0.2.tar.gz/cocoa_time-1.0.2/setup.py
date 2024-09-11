410c0f4378e239e4434491ef00d160c0b289fa93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:06:02 +0000

    Add package cocoa-time/1.0.2/cocoa_time-1.0.2.tar.gz

---
+++ packages/cocoa-time/cocoa_time-1.0.2.tar.gz/cocoa_time-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(
+    name="cocoa_time",
+    version="1.0.2",
+    author="Edanick",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+)
