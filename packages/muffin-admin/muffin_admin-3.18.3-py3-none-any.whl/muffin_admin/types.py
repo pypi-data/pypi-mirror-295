b4c3e9a198329d390a98d3c579ce4d2816900008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:13 +0000

    Add package muffin-admin/3.18.3/muffin_admin-3.18.3-py3-none-any.whl

---
+++ packages/muffin-admin/muffin_admin-3.18.3-py3-none-any.whl/muffin_admin/types.py
@@ -0,0 +1,33 @@
+"""Custom types."""
+
+from typing import Any, Callable, Literal, TypedDict
+
+from marshmallow.fields import Field
+from typing_extensions import NotRequired, TypeAlias
+
+TActionView = Literal["show", "edit", "list", "bulk"]
+TRAProps = dict[str, Any]
+TRAInfo = tuple[str, TRAProps]
+TRAConverter = Callable[[Field], TRAInfo]
+TRAFields: TypeAlias = tuple[tuple[str, TRAInfo], ...]
+TRAInputs: TypeAlias = tuple[tuple[str, TRAInfo], ...]
+
+
+class TRAReference(TypedDict):
+    key: NotRequired[str]
+    source: NotRequired[str]
+    reference: NotRequired[str]
+    searchKey: NotRequired[str]
+
+
+TRARefs = tuple[tuple[str, TRAReference], ...]
+
+
+class TRAActionLink(TypedDict):
+    label: NotRequired[str]
+    icon: NotRequired[str]
+    title: NotRequired[str]
+    field: NotRequired[str]
+
+
+TRALinks = tuple[tuple[str, TRAActionLink], ...]
