7655abe7dc99753fc9c6d35f3c6e8c11a0305899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:05:01 +0000

    Add package digneapy/0.2.3/digneapy-0.2.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/digneapy/digneapy-0.2.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/digneapy/domains/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2023/10/30 12:35:54
+@Author  :   Alejandro Marrero
+@Version :   1.0
+@Contact :   amarrerd@ull.edu.es
+@License :   (C)Copyright 2023, Alejandro Marrero
+@Desc    :   None
+"""
+
+from digneapy.domains.bin_packing import BPP, BPPDomain
+from digneapy.domains.knapsack import Knapsack, KPDomain
+
+__all__ = ["Knapsack", "KPDomain", "BPP", "BPPDomain"]
