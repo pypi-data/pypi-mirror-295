b24a51fb73624a3932eceb463f987ec38cd40fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:34 +0000

    Add package dash-functional-component/0.0.2/dash_functional_component-0.0.2.tar.gz

---
+++ packages/dash-functional-component/dash_functional_component-0.0.2.tar.gz/dash_functional_component-0.0.2/dash_functional_component/PropsInjector.py
@@ -0,0 +1,37 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class PropsInjector(Component):
+    """A PropsInjector component.
+inject extra props to children from a global register
+
+Keyword arguments:
+
+- children (a list of or a singular dash component, string or number; optional):
+    children.
+
+- id (string; required):
+    The ID used to identify this component in Dash callbacks."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_functional_component'
+    _type = 'PropsInjector'
+    @_explicitize_args
+    def __init__(self, children=None, id=Component.REQUIRED, **kwargs):
+        self._prop_names = ['children', 'id']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in ['id']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(PropsInjector, self).__init__(children=children, **args)
