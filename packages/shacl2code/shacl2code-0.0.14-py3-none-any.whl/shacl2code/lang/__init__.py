660440ef75ba3fffafcd9da90a6bd77b7836e901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:04:35 +0000

    Add package shacl2code/0.0.14/shacl2code-0.0.14-py3-none-any.whl

---
+++ packages/shacl2code/shacl2code-0.0.14-py3-none-any.whl/shacl2code/lang/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2024 Joshua Watt
+#
+# SPDX-License-Identifier: MIT
+
+from .lang import LANGUAGES  # noqa: F401
+
+# All renderers must be imported here to be registered
+from .jinja import JinjaRender  # noqa: F401
+from .python import PythonRender  # noqa: F401
+from .jsonschema import JsonSchemaRender  # noqa: F401
