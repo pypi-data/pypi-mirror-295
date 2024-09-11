9db00a9f695ee201a9912adac68b2d354652fcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:04:45 +0000

    Add package udata-front/5.1.3.dev27921/udata_front-5.1.3.dev27921-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.1.3.dev27921-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/components/Toggletip/Toggletip.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="toggletip__wrapper text-grey-500"
+    ref="wrapper"
+    @focusout="hideIfOutside"
+    @keydown.esc="hide"
+  >
+    <button
+      v-bind="$attrs"
+      :aria-expanded="shown"
+      :aria-controls="id"
+      ref="button"
+      role="button"
+      @click.prevent="toggle"
+    >
+      <slot></slot>
+    </button>
+    <div class="toggletip" :id="id" v-show="shown" ref="toggletip" :class="{
+      'fr-p-0': noMargin,
+    }">
+      <slot name="toggletip" :hide></slot>
+    </div>
+  </div>
+</template>
+    
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getRandomId } from "@gouvminint/vue-dsfr";
+withDefaults(defineProps<{
+  noMargin?: boolean;
+}>(), {
+  noMargin: false,
+});
+defineOptions({inheritAttrs: false})
+const button = ref<HTMLButtonElement | null>(null);
+const toggletip = ref<HTMLElement | null>(null);
+const wrapper = ref<HTMLElement | null>(null);
+const shown = ref(false);
+const id = getRandomId("toggletip");
+const toggle = () => {
+  shown.value = !shown.value;
+}
+const hide = () => {
+  shown.value = false;
+}
+const hideIfOutside = (e: FocusEvent) => {
+  const target = (e.relatedTarget || e.target) as HTMLElement | null;
+  if(button.value !== target &&
+    !button.value?.contains(target) &&
+    !toggletip.value?.contains(target)
+  ) {
+    hide();
+  }
+};
+onMounted(() => {
+  document.addEventListener("click", hideIfOutside);
+});
+</script>
