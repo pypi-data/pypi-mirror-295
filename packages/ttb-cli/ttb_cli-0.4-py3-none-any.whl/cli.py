328651363015262da4e9e9c543b274b33941d5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:05:58 +0000

    Add package ttb-cli/0.4/ttb_cli-0.4-py3-none-any.whl

---
+++ packages/ttb-cli/ttb_cli-0.4-py3-none-any.whl/cli.py
@@ -0,0 +1,26 @@
+#  Copyright (c) 2024.  - All Rights Reserved
+#  You may use, distribute and modify this code under the terms of the TAMM license, which unfortunately won't be written for another century.
+#  You should have received a copy of the XYZ license with this file. If not, please contact the legal authority.
+#   */
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.join(os.path.abspath(__file__)))
+
+import click
+from src.commands.performance import lighthouse_report, metrics
+from src.utils import config
+
+@click.group()
+def cli():
+    """TAMM Toolbox CLI"""
+    config.load_config()
+
+
+# noinspection PyTypeChecker
+cli.add_command(metrics, 'performance')
+# noinspection PyTypeChecker
+cli.add_command(lighthouse_report, 'lighthouse')
+
+if __name__ == "__main__":
+    cli()
