b8c5801bc6ec84b7d906969ca884b8cf8b221c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:31 +0000

    Add package metalmental-logger/0.3/metalmental_logger-0.3.tar.gz

---
+++ packages/metalmental-logger/metalmental_logger-0.3.tar.gz/metalmental_logger-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="metalmental_logger",
+    version="0.3",
+    author="MetalMental",
+    author_email="flupino@metalmental.net",
+    license="MIT",
+    url="https://github.com/Flupinochan/MetalMentalPythonLogger",
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
