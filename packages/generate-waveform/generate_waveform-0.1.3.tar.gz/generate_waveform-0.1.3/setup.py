4edbd2da396c69bad8af64fbd388fd88d8f89f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:43 +0000

    Add package generate-waveform/0.1.3/generate_waveform-0.1.3.tar.gz

---
+++ packages/generate-waveform/generate_waveform-0.1.3.tar.gz/generate_waveform-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='generate_waveform',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.24.4",
+        "matplotlib==3.7.5",
+        "fastapi==0.114.0",
+        "uvicorn==0.30.6"
+    ],
+    test_suite='tests',
+    tests_require=[
+        'pytest',
+    ],
+    author='Zahra Shamsi',
+    author_email='zahra.shamsi@gmail.com',
+    description='Generate waves',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
