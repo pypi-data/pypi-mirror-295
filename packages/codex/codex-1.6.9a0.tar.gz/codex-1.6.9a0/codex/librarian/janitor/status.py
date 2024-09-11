29cfd4e791461febe9aeb636c74a627ef54b1997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 15:05:31 +0000

    Add package codex/1.6.9a0/codex-1.6.9a0.tar.gz

---
+++ packages/codex/codex-1.6.9a0.tar.gz/codex-1.6.9a0/codex/librarian/janitor/status.py
@@ -0,0 +1,17 @@
+"""Janitor Status Types."""
+
+from django.db.models import Choices
+
+
+class JanitorStatusTypes(Choices):
+    """Janitor Status Types."""
+
+    CLEANUP_FK = "JTD"
+    CODEX_LATEST_VERSION = "JLV"
+    CODEX_UPDATE = "JCU"
+    CODEX_RESTART = "JCR"
+    CODEX_STOP = "JCS"
+    DB_OPTIMIZE = "JDO"
+    DB_BACKUP = "JDB"
+    CLEANUP_SESSIONS = "JSD"
+    CLEANUP_COVERS = "JCD"
