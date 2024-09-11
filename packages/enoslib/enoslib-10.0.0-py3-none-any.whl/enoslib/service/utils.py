642c04dae9b63ddfd0e27d246fb8f1472683e81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:08 +0000

    Add package enoslib/10.0.0/enoslib-10.0.0-py3-none-any.whl

---
+++ packages/enoslib/enoslib-10.0.0-py3-none-any.whl/enoslib/service/utils.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Optional, Union
+
+
+def _to_abs(path: Path) -> Path:
+    """Make sure the path is absolute."""
+    _path = Path(path)
+    if not _path.is_absolute():
+        # prepend the cwd
+        _path = Path(Path.cwd(), _path)
+    return _path
+
+
+def _check_path(backup_dir: Path) -> Path:
+    """Make sure the backup_dir is created somewhere."""
+    backup_path = _to_abs(backup_dir)
+    # make sure it exists
+    backup_path.mkdir(parents=True, exist_ok=True)
+    return backup_path
+
+
+def _set_dir(
+    one_dir: Optional[Union[Path, str]], default_dir: Union[Path, str]
+) -> Path:
+    if one_dir is None:
+        _dir = Path(default_dir)
+    else:
+        _dir = Path(one_dir)
+
+    _dir = _check_path(_dir)
+    return _dir
