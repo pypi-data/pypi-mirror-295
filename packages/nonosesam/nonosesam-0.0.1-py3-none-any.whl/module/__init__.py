52da94ca8bbd442d077e31de22f22973e601f39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 06:04:45 +0000

    Add package nonosesam/0.0.1/nonosesam-0.0.1-py3-none-any.whl

---
+++ packages/nonosesam/nonosesam-0.0.1-py3-none-any.whl/module/__init__.py
@@ -0,0 +1,20 @@
+# my_package/__init__.py
+
+from abc import ABC, abstractmethod
+import os
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+# Import the strategy interface and concrete strategies
+from .module import (
+    SesamStrategy,
+    EnvSesamStrategy,
+    # AzureKeyVaultSesamStrategy,  # Uncomment if you implement this
+    # HashiCorpVaultSesamStrategy  # Uncomment if you implement this
+)
+
+# Import the context class
+from .module import Sesam
+
+# Optional: Define package-level variables or initialization code
+__version__ = "0.0.1"
