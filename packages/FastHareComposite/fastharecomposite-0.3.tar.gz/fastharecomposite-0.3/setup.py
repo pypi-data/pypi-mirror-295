02bfae74e7012d2f12a73921076916c84d0dee6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 15:05:34 +0000

    Add package FastHareComposite/0.3/fastharecomposite-0.3.tar.gz

---
+++ packages/FastHareComposite/fastharecomposite-0.3.tar.gz/fastharecomposite-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FastHareComposite',
+    version='0.3',
+    author='Thang N. Dinh',
+    author_email='thangdn@gmail.com',
+    description='A composite for using FastHare reduction with the samplers from D-Wave Ocean SDK',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/FastHareComposite',  # Update with your GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        'dimod',
+        'fasthare',   # Assuming fasthare is another package
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
