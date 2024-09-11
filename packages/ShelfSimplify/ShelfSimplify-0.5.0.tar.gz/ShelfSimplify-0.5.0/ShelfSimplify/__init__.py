eaea25116fa33979c1cb98b8496270100ab04dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:21 +0000

    Add package ShelfSimplify/0.5.0/ShelfSimplify-0.5.0.tar.gz

---
+++ packages/ShelfSimplify/ShelfSimplify-0.5.0.tar.gz/ShelfSimplify-0.5.0/ShelfSimplify/__init__.py
@@ -0,0 +1,4 @@
+from .ShelfSimplify import createfile, appendfile, readfile
+from .ShelfSimplify import renamefile, replacefile, removefile
+from .ShelfSimplify import getsysdetails
+from .ShelfSimplify import getsys_node, getsys_machine, getsys_system, getsys_version, getsys_processor, getsys_release
