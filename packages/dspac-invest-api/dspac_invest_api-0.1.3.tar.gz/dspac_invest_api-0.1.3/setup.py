379ba929b73c4ad21b07cb5288ea48f2309c2e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 05:05:14 +0000

    Add package dspac-invest-api/0.1.3/dspac_invest_api-0.1.3.tar.gz

---
+++ packages/dspac-invest-api/dspac_invest_api-0.1.3.tar.gz/dspac_invest_api-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dspac_invest_api",
+    version="0.1.3",
+    description="Unofficial dSPAC Invest API written in Python Requests",
+    url="https://github.com/ImNotOssy/dSPAC_investing_API",
+    author="Oswaldo Romero",
+    packages=["dspac_invest_api"],
+    install_requires=[
+        "pillow",
+        "python-dotenv",
+        "requests",
+    ],
+)
