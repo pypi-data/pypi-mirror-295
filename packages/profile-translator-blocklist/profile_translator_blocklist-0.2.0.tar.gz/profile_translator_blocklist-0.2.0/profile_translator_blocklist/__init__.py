23827df0db26e8b09a39b0143396a448f766712a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:30 +0000

    Add package profile-translator-blocklist/0.2.0/profile_translator_blocklist-0.2.0.tar.gz

---
+++ packages/profile-translator-blocklist/profile_translator_blocklist-0.2.0.tar.gz/profile_translator_blocklist-0.2.0/profile_translator_blocklist/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package `profile-translator-blocklist`.
+"""
+
+from .translator import translate_policy, translate_profile
+
+
+__all__ = [
+    "translate_policy",
+    "translate_profile"
+]
