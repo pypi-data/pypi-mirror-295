7e0142f0e4f990c187b7da343c3bcf9434d6b2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:10 +0000

    Add package cellarium-cas/1.5.1/cellarium_cas-1.5.1-py3-none-any.whl

---
+++ packages/cellarium-cas/cellarium_cas-1.5.1-py3-none-any.whl/cellarium/cas/__init__.py
@@ -0,0 +1,18 @@
+from cellarium.cas.client import CASClient
+
+from . import constants, exceptions, postprocessing, preprocessing, service, settings, version, visualization
+
+__version__ = version.get_version()
+
+__all__ = [
+    "CASClient",
+    "preprocessing",
+    "postprocessing",
+    "visualization",
+    "settings",
+    "constants",
+    "exceptions",
+    "service",
+    "settings",
+    "version",
+]
