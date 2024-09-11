1d11765e1467105ad13e5a21b0ceaf8e625c4c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:43 +0000

    Add package ai-parrot/0.3.3/ai_parrot-0.3.3.tar.gz

---
+++ packages/ai-parrot/ai_parrot-0.3.3.tar.gz/ai_parrot-0.3.3/examples/load_pdf.py
@@ -0,0 +1,31 @@
+import asyncio
+from navconfig import BASE_DIR
+from parrot.llms.vertex import VertexLLM
+from parrot.loaders import (
+    PDFLoader
+)
+
+async def process_pdf():
+    llm = VertexLLM(
+        model='gemini-1.5-flash-001',
+        temperature=0.1,
+        top_k=30,
+        Top_p=0.5,
+    )
+    # Add LLM
+    doc = BASE_DIR.joinpath('documents', 'AR_Certification_Skill_Practice_Scorecard_EXAMPLE.pdf')
+    print(':: Processing: ', doc)
+    # PDF Files
+    loader = PDFLoader(
+        doc,
+        source_type=f"PDF {doc.name}",
+        llm=llm.get_llm(),
+        language="en",
+        parse_images=False,
+        page_as_images=True
+    )
+    docs = loader.load()
+    print('DOCS > ', docs)
+
+if __name__ == "__main__":
+    agent = asyncio.run(process_pdf())
