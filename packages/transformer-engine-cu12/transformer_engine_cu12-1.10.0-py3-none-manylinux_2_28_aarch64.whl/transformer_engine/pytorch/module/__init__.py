4f6a2a792db4ecc6037b8b26b5523f9bb51dbf76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:28 +0000

    Add package transformer-engine-cu12/1.10.0/transformer_engine_cu12-1.10.0-py3-none-manylinux_2_28_aarch64.whl

---
+++ packages/transformer-engine-cu12/transformer_engine_cu12-1.10.0-py3-none-manylinux_2_28_aarch64.whl/transformer_engine/pytorch/module/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# See LICENSE for license information.
+
+"""Module level PyTorch APIs"""
+from .layernorm_linear import LayerNormLinear
+from .linear import Linear
+from .grouped_linear import GroupedLinear
+from .layernorm_mlp import LayerNormMLP
+from .layernorm import LayerNorm
+from .rmsnorm import RMSNorm
+from .base import initialize_ub, destroy_ub
