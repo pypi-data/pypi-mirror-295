10af5adc1f3c3f99d8ab068e4ba79f886ec3e1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:41 +0000

    Add package onpod/0.0.2/onpod-0.0.2-py3-none-any.whl

---
+++ packages/onpod/onpod-0.0.2-py3-none-any.whl/onpod/__init__.py
@@ -0,0 +1,5 @@
+import onpod.proxies.torch_proxy as torch
+import onpod.proxies.transformers_proxy as transformers
+import onpod.proxies.tensorflow_proxy as tensorflow
+
+__all__ = ["torch", "transformers", "tensorflow"]
