5d1a1f6220a9c7e7d929e031b2c8fafd180d6f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:25 +0000

    Add package omlish/0.0.0.dev21/omlish-0.0.0.dev21-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev21-py3-none-any.whl/omlish/logs/__init__.py
@@ -0,0 +1,13 @@
+from .configs import (  # noqa
+    configure_standard_logging,
+)
+
+from .formatters import (  # noqa
+    ColorLogFormatter,
+    JsonLogFormatter,
+    StandardLogFormatter,
+)
+
+from .utils import (  # noqa
+    error_logging,
+)
---
+++ packages/omlish/omlish-0.0.0.dev21-py3-none-any.whl/omlish/marshal/__init__.py
@@ -0,0 +1,90 @@
+from .base import (  # noqa
+    Marshaler,
+    Unmarshaler,
+
+    MarshalerFactory,
+    UnmarshalerFactory,
+
+    MarshalerFactoryMatchClass,
+    UnmarshalerFactoryMatchClass,
+
+    TypeMapMarshalerFactory,
+    TypeMapUnmarshalerFactory,
+
+    TypeCacheMarshalerFactory,
+    TypeCacheUnmarshalerFactory,
+
+    FuncMarshaler,
+    FuncUnmarshaler,
+
+    BaseContext,
+    MarshalContext,
+    UnmarshalContext,
+
+    RecursiveMarshalerFactory,
+    RecursiveUnmarshalerFactory,
+
+    SetType,
+)
+
+from .exceptions import (  # noqa
+    ForbiddenTypeError,
+    MarshalError,
+    UnhandledTypeError,
+)
+
+from .forbidden import (  # noqa
+    ForbiddenTypeMarshalerFactory,
+    ForbiddenTypeUnmarshalerFactory,
+)
+
+from .naming import (  # noqa
+    Naming,
+    translate_name,
+)
+
+from .global_ import (  # noqa
+    marshal,
+    unmarshal,
+)
+
+from .helpers import (  # noqa
+    update_fields_metadata,
+)
+
+from .objects import (  # noqa
+    FieldMetadata,
+    ObjectMetadata,
+)
+
+from .polymorphism import (  # noqa
+    Impl,
+    Polymorphism,
+    PolymorphismMarshalerFactory,
+    PolymorphismUnmarshalerFactory,
+    polymorphism_from_subclasses,
+)
+
+from .registries import (  # noqa
+    Registry,
+)
+
+from .standard import (  # noqa
+    STANDARD_MARSHALER_FACTORIES,
+    new_standard_marshaler_factory,
+
+    STANDARD_UNMARSHALER_FACTORIES,
+    new_standard_unmarshaler_factory,
+)
+
+from .values import (  # noqa
+    Value,
+)
+
+
+##
+
+
+from ..lang.imports import _trigger_conditional_imports  # noqa
+
+_trigger_conditional_imports(__package__)
