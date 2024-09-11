583d72bee202ed788d15859b47ae6320902b5c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 15:05:38 +0000

    Add package fasthare/1.0.3/fasthare-1.0.3.tar.gz

---
+++ packages/fasthare/fasthare-1.0.3.tar.gz/fasthare-1.0.3/tests/test.py
@@ -0,0 +1,21 @@
+import fasthare as m
+
+assert m.__version__ == '1.0.3'
+
+ski1 = [(0, 1, 3)]
+ski2 = [(0, 1, -3)]
+# exp1.net
+rh, map, sign, time = m.fasthare_reduction(ski1)
+assert (len(rh) == 0) and (map[0] + map[1] == 0) and (sign[0] + sign[1] == 2) 
+
+#exp2.net
+rh, map, sign, time = m.fasthare_reduction(ski2)
+assert (len(rh) == 0) and (map[0] + map[1] == 0) and (sign[0] + sign[1] == 0) 
+
+#exp3.net
+rh3  = []
+map3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+sign3= [-1, 1, 1, -1, -1, -1, -1, -1, -1, -1]
+rh, map, sign, time = m.fasthare_reduction(file ="exp3.net",alpha=0.2, log_file="exp3.log")
+assert (rh == rh3) and (map == map3) and (sign == sign3) 
+print("All tests passed!")
