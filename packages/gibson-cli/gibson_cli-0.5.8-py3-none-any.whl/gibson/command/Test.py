81673e9f411b690e289efe13331d20b1e69cd606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:51 +0000

    Add package gibson-cli/0.5.8/gibson_cli-0.5.8-py3-none-any.whl

---
+++ packages/gibson-cli/gibson_cli-0.5.8-py3-none-any.whl/gibson/command/Test.py
@@ -0,0 +1,41 @@
+import sys
+
+import gibson.core.Colors as Colors
+from gibson.api.Cli import Cli
+from gibson.command.BaseCommand import BaseCommand
+from gibson.core.TimeKeeper import TimeKeeper
+from gibson.dev.Dev import Dev
+
+
+class Test(BaseCommand):
+    def execute(self):
+        if len(sys.argv) != 3:
+            self.usage()
+
+        self.configuration.require_project()
+        entity = self.memory.recall_stored_entity(sys.argv[2])
+        if entity is None:
+            self.conversation.not_sure_no_entity(
+                self.configuration.project.name, sys.argv[2]
+            )
+            exit(1)
+
+        time_keeper = TimeKeeper()
+
+        cli = Cli(self.configuration)
+        response = cli.code_testing([entity["name"]])
+
+        Dev(self.configuration).tests(
+            response["code"][0]["entity"]["name"], response["code"][0]["definition"]
+        )
+
+        print(response["code"][0]["definition"])
+        time_keeper.display()
+
+    def usage(self):
+        self.configuration.display_project()
+        self.conversation.type(
+            f"usage: {Colors.command(self.configuration.command)} {Colors.subcommand('test')} {Colors.argument('[entity name]')} {Colors.hint('write the unit tests for an entity')}\n"
+        )
+        self.conversation.newline()
+        exit(1)
