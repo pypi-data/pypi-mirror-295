5af3b9988821cffb4d2d25830b0a78845d37f8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:35 +0000

    Add package prcpy/0.1.14/prcpy-0.1.14.tar.gz

---
+++ packages/prcpy/prcpy-0.1.14.tar.gz/prcpy-0.1.14/prcpy/__init__.py
@@ -0,0 +1,5 @@
+from . import RC, Maths, Utilities, DataHandling, TrainingModels
+
+__all__ = ["RC", "Maths", "Utilities", "DataHandling", "TrainingModels"]
+
+__version__ = "0.1.14" # change this for releases
