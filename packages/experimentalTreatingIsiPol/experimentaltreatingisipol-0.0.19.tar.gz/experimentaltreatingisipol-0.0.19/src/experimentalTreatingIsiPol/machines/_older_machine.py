fe65b0778c4eb11112c6cf6c192938efa0b745dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 04:04:17 +0000

    Add package experimentalTreatingIsiPol/0.0.19/experimentaltreatingisipol-0.0.19.tar.gz

---
+++ packages/experimentalTreatingIsiPol/experimentaltreatingisipol-0.0.19.tar.gz/experimentaltreatingisipol-0.0.19/src/experimentalTreatingIsiPol/machines/_older_machine.py
@@ -0,0 +1,14 @@
+class _Older_Machine():
+    def __init__(self) -> None:
+
+        self.colunas  = ['Tempo','Deslocamento','Força','Extensometro']
+        self.column_delimitador = ';'
+        self.decimal = ','
+
+
+class _MarcoPvMachine():
+    def __init__(self) -> None:
+
+        self.colunas  = ['Tempo','Deslocamento','Força','Extensometro']
+        self.column_delimitador = ';'
+        self.decimal = ','
