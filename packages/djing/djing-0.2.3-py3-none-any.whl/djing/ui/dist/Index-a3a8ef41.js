3f66ba13fa0dcc712e21e19e86f2070ca3b7ab5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:05:21 +0000

    Add package djing/0.2.3/djing-0.2.3-py3-none-any.whl

---
+++ packages/djing/djing-0.2.3-py3-none-any.whl/djing/ui/dist/Index-a3a8ef41.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-77fa6b47.js";function c(t){return e("div",{className:"text-center h-screen flex justify-center flex-col items-center p-4",children:"welcome"})}export{c as default};
