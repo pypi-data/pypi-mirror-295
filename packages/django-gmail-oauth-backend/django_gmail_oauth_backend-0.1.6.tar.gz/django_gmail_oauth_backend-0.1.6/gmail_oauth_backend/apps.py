fab4e52f4fa520e96d27a4764ec31d003ef78249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 16:05:29 +0000

    Add package django-gmail-oauth-backend/0.1.6/django_gmail_oauth_backend-0.1.6.tar.gz

---
+++ packages/django-gmail-oauth-backend/django_gmail_oauth_backend-0.1.6.tar.gz/django_gmail_oauth_backend-0.1.6/gmail_oauth_backend/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class GmailOauthBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gmail_oauth_backend'
+    verbose_name = 'Gmail OAuth Backend'
