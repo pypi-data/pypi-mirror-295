8f5523e1a62a9e980ce3706db106dcbc1229841c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:34 +0000

    Add package cglbtrial3/0.5.0/cglbtrial3-0.5.0-py3-none-any.whl

---
+++ packages/cglbtrial3/cglbtrial3-0.5.0-py3-none-any.whl/cglbtrial3/conversion.py
@@ -0,0 +1,11 @@
+
+
+def unit_one_2_unit_two(value_unit_one):
+    '''
+    takes a value in unit_one and returns it in unit_two
+    '''
+
+    value_unit_two = 3.22 * (value_unit_one - 12.) + 111.11
+
+    return value_unit_two
+
