389fdbb2d3d13a48738bf3f6f3b72fa6368bf9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 07:04:42 +0000

    Add package djungarik/0.1.3/djungarik-0.1.3.tar.gz

---
+++ packages/djungarik/djungarik-0.1.3.tar.gz/djungarik-0.1.3/README.md
@@ -0,0 +1,11 @@
+# Djungarik
+
+Djungarik is a simple Python package that provides a GUI application with Tkinter.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install djungarik
+```
---
+++ packages/djungarik/djungarik-0.1.3.tar.gz/djungarik-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="djungarik",
+    version="0.1.3",
+    packages=find_packages(),
+    author="djungar_DEV",
+    author_email="arnispprt@gmail.com",
+    description="Djungarik — A simple Tkinter-based UI package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/djungarikDEV/djungarik",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
