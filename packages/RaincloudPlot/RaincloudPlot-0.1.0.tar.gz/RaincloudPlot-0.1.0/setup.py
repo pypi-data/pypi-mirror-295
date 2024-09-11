6fe6593d20769a96cc577cab278b9f3409c4b7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:33 +0000

    Add package RaincloudPlot/0.1.0/RaincloudPlot-0.1.0.tar.gz

---
+++ packages/RaincloudPlot/RaincloudPlot-0.1.0.tar.gz/RaincloudPlot-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RaincloudPlot',
+    version='0.1.0',
+    author='Yosri Ben Halima',
+    author_email='yosri.benhalima@ept.ucar.tn',
+    description='A package for creating raincloud plots',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Yosri-Ben-Halima/RaincloudPlot',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'seaborn',
+        'ptitprince',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
