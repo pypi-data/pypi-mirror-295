c5dcdc6623892ba993a05686154740b415bc513e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:36 +0000

    Add package sentiment-New-package/0.1.2/sentiment_new_package-0.1.2.tar.gz

---
+++ packages/sentiment-New-package/sentiment_new_package-0.1.2.tar.gz/sentiment_new_package-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sentiment_New_package',
+    version='0.1.2',
+    description='A package for sentiment analysis using pre-trained models',
+    author='Drish',
+    author_email='udayveer.deswal@drishinfo.com',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',
+        'joblib',
+        'nltk',
+        'numpy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
