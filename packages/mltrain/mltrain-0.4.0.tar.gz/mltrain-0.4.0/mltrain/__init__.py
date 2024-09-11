92f187f11ce227c65e77f0320d085046694e1da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 03:05:28 +0000

    Add package mltrain/0.4.0/mltrain-0.4.0.tar.gz

---
+++ packages/mltrain/mltrain-0.4.0.tar.gz/mltrain-0.4.0/mltrain/__init__.py
@@ -0,0 +1,10 @@
+# Use Shortcuts to import unique Models..
+from supervised.DecisionTree import DecisionTree
+from supervised.KernelSVM import KernelSVM
+from supervised.KNN import KNN
+from supervised.LinearRegression import LinearRegression
+from supervised.LogisticRegression import LogisticRegression
+from supervised.NaiveBayes import NaiveBayes
+from supervised.RandomForest import RandomForest
+from unsupervised.KMeans import KMeans
+from unsupervised.DBSCAN import DBSCAN
