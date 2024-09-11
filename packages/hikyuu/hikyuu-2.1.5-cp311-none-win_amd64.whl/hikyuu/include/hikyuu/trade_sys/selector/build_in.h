0a56d5098ebb40e5a8b4d9f145f009256aef59a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 14:05:07 +0000

    Add package hikyuu/2.1.5/hikyuu-2.1.5-cp311-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-2.1.5-cp311-none-win_amd64.whl/hikyuu/include/hikyuu/trade_sys/selector/build_in.h
@@ -0,0 +1,17 @@
+/*
+ * build_in.h
+ *
+ *  Created on: 2016年3月28日
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef TRADE_SYS_SELECTOR_BUILD_IN_H_
+#define TRADE_SYS_SELECTOR_BUILD_IN_H_
+
+#include "crt/SE_Fixed.h"
+#include "crt/SE_MultiFactor.h"
+#include "crt/SE_Operator.h"
+#include "crt/SE_Signal.h"
+
+#endif /* TRADE_SYS_SELECTOR_BUILD_IN_H_ */
