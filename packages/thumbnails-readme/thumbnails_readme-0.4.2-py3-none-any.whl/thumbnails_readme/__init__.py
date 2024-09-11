0b7b63d9acfc092cff2d6a23eb35b622c37d9260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-11 17:05:15 +0000

    Add package thumbnails-readme/0.4.2/thumbnails_readme-0.4.2-py3-none-any.whl

---
+++ packages/thumbnails-readme/thumbnails_readme-0.4.2-py3-none-any.whl/thumbnails_readme/__init__.py
@@ -0,0 +1,18 @@
+"""Init file for thumbnails_readme package."""
+
+from thumbnails_readme.thumbnails_readme import (
+    ImageThumbnail,
+    crawl,
+    prepare_readme,
+    prepare_thumbnails_folder,
+)
+
+__all__ = [
+    'ImageThumbnail',
+    'crawl',
+    'prepare_readme',
+    'prepare_thumbnails_folder',
+]
+
+__version__ = '0.4.2'
+__project__ = 'thumbnails_readme'
