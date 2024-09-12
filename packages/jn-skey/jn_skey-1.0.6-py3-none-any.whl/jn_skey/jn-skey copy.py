362a9735f8dcae124078c913a7dcc6f7d98a692f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:33 +0000

    Add package jn-skey/1.0.6/jn_skey-1.0.6-py3-none-any.whl

---
+++ packages/jn-skey/jn_skey-1.0.6-py3-none-any.whl/jn_skey/jn-skey copy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from pynput import keyboard
+from datetime import datetime
+
+def on_press(key):
+    try:
+        if key == keyboard.Key.ctrl_l:
+            keys_pressed.add(keyboard.Key.ctrl_l)
+        elif key == keyboard.KeyCode.from_char('d'):
+            keys_pressed.add(keyboard.KeyCode.from_char('d'))
+
+        if keyboard.Key.ctrl_l in keys_pressed and keyboard.KeyCode.from_char('d') in keys_pressed:
+            type_datetime()
+            keys_pressed.clear()
+    except AttributeError:
+        pass
+
+def on_release(key):
+    try:
+        if key in keys_pressed:
+            keys_pressed.remove(key)
+    except AttributeError:
+        pass
+
+def type_datetime():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    controller.type(current_time)
+
+keys_pressed = set()
+controller = keyboard.Controller()
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
