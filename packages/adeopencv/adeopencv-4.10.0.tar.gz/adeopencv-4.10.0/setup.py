68e1bd13202682cafdbc4d6158f7a2aa4589df94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:07:02 +0000

    Add package adeopencv/4.10.0/adeopencv-4.10.0.tar.gz

---
+++ packages/adeopencv/adeopencv-4.10.0.tar.gz/adeopencv-4.10.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+
+VERSION = '4.10.0'
+DESCRIPTION = 'opencv-4.10.0'
+
+
+# Setting up
+setup(
+    name="adeopencv",
+    version=VERSION,
+    author="Malyoneeeeer",
+    author_email="<malyoneeeeer@yahoo.com>",
+    description=DESCRIPTION,
+    include_package_data=True,
+    packages=['libname'],
+    package_data={'libname': ['models/*.*']},
+    install_requires=['opencv-python'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
