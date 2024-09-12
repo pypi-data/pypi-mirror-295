4a88448138ae4d5344a1ebeb56685b3ec8e679ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:06:52 +0000

    Add package phonopy/2.28.0/phonopy-2.28.0.tar.gz

---
+++ packages/phonopy/phonopy-2.28.0.tar.gz/phonopy-2.28.0/test/phonon/test_qpoints.py
@@ -0,0 +1,20 @@
+"""Tests for phonon calculation at specific q-points."""
+
+import numpy as np
+
+from phonopy import Phonopy
+from phonopy.units import VaspToTHz
+
+
+def testQpoints(ph_nacl_nofcsym: Phonopy):
+    """Test phonon calculation at specific q-points by NaCl."""
+    phonon = ph_nacl_nofcsym
+    qpoints = [[0, 0, 0], [0, 0, 0.5]]
+    phonon.run_qpoints(qpoints, with_dynamical_matrices=True)
+    for i, _ in enumerate(qpoints):
+        dm = phonon.qpoints.dynamical_matrices[i]
+        dm_eigs = np.linalg.eigvalsh(dm).real
+        eigs = phonon.qpoints.eigenvalues[i]
+        freqs = phonon.qpoints.frequencies[i] / VaspToTHz
+        np.testing.assert_allclose(dm_eigs, eigs)
+        np.testing.assert_allclose(freqs**2 * np.sign(freqs), eigs)
