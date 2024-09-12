754720a1f45795492629b8635fe5f24062110dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:07:04 +0000

    Add package datachain/0.3.13/datachain-0.3.13.tar.gz

---
+++ packages/datachain/datachain-0.3.13.tar.gz/datachain-0.3.13/src/datachain/lib/arrow.py
@@ -0,0 +1,158 @@
+import re
+from collections.abc import Sequence
+from tempfile import NamedTemporaryFile
+from typing import TYPE_CHECKING, Optional
+
+import pyarrow as pa
+from pyarrow.dataset import dataset
+from tqdm import tqdm
+
+from datachain.lib.data_model import dict_to_data_model
+from datachain.lib.file import File, IndexedFile
+from datachain.lib.model_store import ModelStore
+from datachain.lib.udf import Generator
+
+if TYPE_CHECKING:
+    from pydantic import BaseModel
+
+    from datachain.lib.dc import DataChain
+
+
+class ArrowGenerator(Generator):
+    def __init__(
+        self,
+        input_schema: Optional["pa.Schema"] = None,
+        output_schema: Optional[type["BaseModel"]] = None,
+        source: bool = True,
+        nrows: Optional[int] = None,
+        **kwargs,
+    ):
+        """
+        Generator for getting rows from tabular files.
+
+        Parameters:
+
+        input_schema : Optional pyarrow schema for validation.
+        output_schema : Optional pydantic model for validation.
+        source : Whether to include info about the source file.
+        nrows : Optional row limit.
+        kwargs: Parameters to pass to pyarrow.dataset.dataset.
+        """
+        super().__init__()
+        self.input_schema = input_schema
+        self.output_schema = output_schema
+        self.source = source
+        self.nrows = nrows
+        self.kwargs = kwargs
+
+    def process(self, file: File):
+        if self.nrows:
+            path = _nrows_file(file, self.nrows)
+            ds = dataset(path, schema=self.input_schema, **self.kwargs)
+        else:
+            path = file.get_path()
+            ds = dataset(
+                path, filesystem=file.get_fs(), schema=self.input_schema, **self.kwargs
+            )
+        index = 0
+        with tqdm(desc="Parsed by pyarrow", unit=" rows") as pbar:
+            for record_batch in ds.to_batches():
+                for record in record_batch.to_pylist():
+                    vals = list(record.values())
+                    if self.output_schema:
+                        fields = self.output_schema.model_fields
+                        vals_dict = {}
+                        for (field, field_info), val in zip(fields.items(), vals):
+                            if ModelStore.is_pydantic(field_info.annotation):
+                                vals_dict[field] = field_info.annotation(**val)  # type: ignore[misc]
+                            else:
+                                vals_dict[field] = val
+                        vals = [self.output_schema(**vals_dict)]
+                    if self.source:
+                        yield [IndexedFile(file=file, index=index), *vals]
+                    else:
+                        yield vals
+                    index += 1
+                pbar.update(len(record_batch))
+
+
+def infer_schema(chain: "DataChain", **kwargs) -> pa.Schema:
+    schemas = []
+    for file in chain.collect("file"):
+        ds = dataset(file.get_path(), filesystem=file.get_fs(), **kwargs)  # type: ignore[union-attr]
+        schemas.append(ds.schema)
+    return pa.unify_schemas(schemas)
+
+
+def schema_to_output(schema: pa.Schema, col_names: Optional[Sequence[str]] = None):
+    """Generate UDF output schema from pyarrow schema."""
+    if col_names and (len(schema) != len(col_names)):
+        raise ValueError(
+            "Error generating output from Arrow schema - "
+            f"Schema has {len(schema)} columns but got {len(col_names)} column names."
+        )
+    default_column = 0
+    output = {}
+    for i, field in enumerate(schema):
+        if col_names:
+            column = col_names[i]
+        else:
+            column = field.name
+        column = column.lower()
+        column = re.sub("[^0-9a-z_]+", "", column)
+        if not column:
+            column = f"c{default_column}"
+            default_column += 1
+        dtype = arrow_type_mapper(field.type, column)  # type: ignore[assignment]
+        if field.nullable and not ModelStore.is_pydantic(dtype):
+            dtype = Optional[dtype]  # type: ignore[assignment]
+        output[column] = dtype
+
+    return output
+
+
+def arrow_type_mapper(col_type: pa.DataType, column: str = "") -> type:  # noqa: PLR0911
+    """Convert pyarrow types to basic types."""
+    from datetime import datetime
+
+    if pa.types.is_timestamp(col_type):
+        return datetime
+    if pa.types.is_binary(col_type):
+        return bytes
+    if pa.types.is_floating(col_type):
+        return float
+    if pa.types.is_integer(col_type):
+        return int
+    if pa.types.is_boolean(col_type):
+        return bool
+    if pa.types.is_date(col_type):
+        return datetime
+    if pa.types.is_string(col_type) or pa.types.is_large_string(col_type):
+        return str
+    if pa.types.is_list(col_type):
+        return list[arrow_type_mapper(col_type.value_type)]  # type: ignore[return-value, misc]
+    if pa.types.is_struct(col_type):
+        type_dict = {}
+        for field in col_type:
+            dtype = arrow_type_mapper(field.type, field.name)
+            if field.nullable and not ModelStore.is_pydantic(dtype):
+                dtype = Optional[dtype]  # type: ignore[assignment]
+            type_dict[field.name] = dtype
+        return dict_to_data_model(column, type_dict)
+    if pa.types.is_map(col_type):
+        return dict
+    if isinstance(col_type, pa.lib.DictionaryType):
+        return arrow_type_mapper(col_type.value_type)  # type: ignore[return-value]
+    raise TypeError(f"{col_type!r} datatypes not supported")
+
+
+def _nrows_file(file: File, nrows: int) -> str:
+    tf = NamedTemporaryFile(delete=False)  # noqa: SIM115
+    with file.open(mode="r") as reader:
+        with open(tf.name, "a") as writer:
+            for row, line in enumerate(reader):
+                if row >= nrows:
+                    break
+                writer.write(line)
+                writer.write("\n")
+    return tf.name
---
+++ packages/datachain/datachain-0.3.13.tar.gz/datachain-0.3.13/tests/unit/lib/test_signal_schema.py
@@ -0,0 +1,487 @@
+import json
+from datetime import datetime
+from typing import Optional, Union
+
+import pytest
+
+from datachain import Column, DataModel
+from datachain.lib.convert.flatten import flatten
+from datachain.lib.file import File, TextFile
+from datachain.lib.signal_schema import (
+    SetupError,
+    SignalResolvingError,
+    SignalSchema,
+    SignalSchemaError,
+    SignalSchemaWarning,
+)
+from datachain.sql.types import (
+    JSON,
+    Array,
+    Binary,
+    Boolean,
+    DateTime,
+    Float,
+    Float32,
+    Float64,
+    Int,
+    Int32,
+    Int64,
+    String,
+    UInt64,
+)
+
+
+@pytest.fixture
+def nested_file_schema():
+    class _MyFile(File):
+        ref: str
+        nested_file: File
+
+    schema = {"name": str, "age": float, "f": File, "my_f": _MyFile}
+
+    return SignalSchema(schema)
+
+
+class MyType1(DataModel):
+    aa: int
+    bb: str
+
+
+class MyType2(DataModel):
+    name: str
+    deep: MyType1
+
+
+def test_deserialize_basic():
+    stored = {"name": "str", "count": "int", "file": "File@v1"}
+    signals = SignalSchema.deserialize(stored)
+
+    assert len(signals.values) == 3
+    assert signals.values.keys() == stored.keys()
+    assert list(signals.values.values()) == [str, int, File]
+
+
+def test_deserialize_error():
+    SignalSchema.deserialize({})
+
+    with pytest.raises(SignalSchemaError):
+        SignalSchema.deserialize(json.dumps({"name": "str"}))
+
+    with pytest.raises(SignalSchemaError):
+        SignalSchema.deserialize({"name": [1, 2, 3]})
+
+    with pytest.warns(SignalSchemaWarning):
+        # Warn if unknown fields are encountered - don't throw an exception to ensure
+        # that all data can be shown.
+        SignalSchema.deserialize({"name": "unknown"})
+
+
+def test_serialize_basic():
+    schema = {
+        "name": str,
+        "age": float,
+        "f": File,
+    }
+    signals = SignalSchema(schema).serialize()
+
+    assert len(signals) == 4
+    assert signals["name"] == "str"
+    assert signals["age"] == "float"
+    assert signals["f"] == "File@v1"
+    assert "File@v1" in signals["_custom_types"]
+
+
+def test_feature_schema_serialize_optional():
+    schema = {
+        "name": Optional[str],
+        "feature": Optional[MyType1],
+    }
+    signals = SignalSchema(schema).serialize()
+
+    assert len(signals) == 3
+    assert signals["name"] == "str"
+    assert signals["feature"] == "MyType1"
+    assert signals["_custom_types"] == {"MyType1@v1": {"aa": "int", "bb": "str"}}
+
+
+def test_feature_schema_serialize_nested_types():
+    schema = {
+        "name": Optional[str],
+        "feature_nested": Optional[MyType2],
+    }
+    signals = SignalSchema(schema).serialize()
+
+    assert len(signals) == 3
+    assert signals["name"] == "str"
+    assert signals["feature_nested"] == "MyType2"
+    assert signals["_custom_types"] == {
+        "MyType1@v1": {"aa": "int", "bb": "str"},
+        "MyType2@v1": {"deep": "MyType1@v1", "name": "str"},
+    }
+
+
+def test_feature_schema_serialize_nested_duplicate_types():
+    schema = {
+        "name": Optional[str],
+        "feature_nested": Optional[MyType2],
+        "feature_not_nested": Optional[MyType1],
+    }
+    signals = SignalSchema(schema).serialize()
+
+    assert len(signals) == 4
+    assert signals["name"] == "str"
+    assert signals["feature_nested"] == "MyType2"
+    assert signals["feature_not_nested"] == "MyType1"
+    assert signals["_custom_types"] == {
+        "MyType1@v1": {"aa": "int", "bb": "str"},
+        "MyType2@v1": {"deep": "MyType1@v1", "name": "str"},
+    }
+
+
+def test_serialize_from_column():
+    signals = SignalSchema.from_column_types({"age": Float, "name": String}).values
+
+    assert len(signals) == 2
+    assert signals["name"] is str
+    assert signals["age"] is float
+
+
+def test_serialize_from_column_error():
+    with pytest.raises(SignalSchemaError):
+        SignalSchema.from_column_types({"age": Float, "wrong_type": File})
+
+
+def test_to_udf_spec():
+    signals = SignalSchema.deserialize(
+        {
+            "age": "float",
+            "address": "str",
+            "f": "File@v1",
+        }
+    )
+
+    spec = SignalSchema.to_udf_spec(signals)
+
+    assert len(spec) == 2 + len(File.model_fields)
+
+    assert "age" in spec
+    assert spec["age"] == Float
+
+    assert "address" in spec
+    assert spec["address"] == String
+
+    assert "f__path" in spec
+    assert spec["f__path"] == String
+
+    assert "f__size" in spec
+    assert spec["f__size"] == Int64
+
+
+def test_select():
+    schema = SignalSchema.deserialize(
+        {
+            "age": "float",
+            "address": "str",
+            "f": "MyType1@v1",
+        }
+    )
+
+    new = schema.resolve("age", "f.aa", "f.bb")
+    assert isinstance(new, SignalSchema)
+
+    signals = new.values
+    assert len(signals) == 3
+    assert {"age", "f.aa", "f.bb"} == signals.keys()
+    assert signals["age"] is float
+    assert signals["f.aa"] is int
+    assert signals["f.bb"] is str
+
+
+def test_select_custom_type():
+    schema = SignalSchema.deserialize(
+        {
+            "age": "float",
+            "address": "str",
+            "f": "ExternalCustomType1@v1",
+            "_custom_types": {"ExternalCustomType1@v1": {"aa": "int", "bb": "str"}},
+        }
+    )
+
+    new = schema.resolve("age", "f.aa", "f.bb")
+    assert isinstance(new, SignalSchema)
+
+    signals = new.values
+    assert len(signals) == 3
+    assert {"age", "f.aa", "f.bb"} == signals.keys()
+    assert signals["age"] is float
+    assert signals["f.aa"] is int
+    assert signals["f.bb"] is str
+
+
+def test_select_nested_names():
+    schema = SignalSchema.deserialize(
+        {
+            "address": "str",
+            "fr": "MyType2@v1",
+        }
+    )
+
+    fr_signals = schema.resolve("fr.deep").values
+    assert "fr.deep" in fr_signals
+    assert fr_signals["fr.deep"] == MyType1
+
+    basic_signals = schema.resolve("fr.deep.aa", "fr.deep.bb").values
+    assert "fr.deep.aa" in basic_signals
+    assert "fr.deep.bb" in basic_signals
+    assert basic_signals["fr.deep.aa"] is int
+    assert basic_signals["fr.deep.bb"] is str
+
+
+def test_select_nested_names_custom_types():
+    schema = SignalSchema.deserialize(
+        {
+            "address": "str",
+            "fr": "NestedType2@v1",
+            "_custom_types": {
+                "NestedType1@v1": {"aa": "int", "bb": "str"},
+                "NestedType2@v1": {"deep": "NestedType1@v1", "name": "str"},
+            },
+        }
+    )
+
+    fr_signals = schema.resolve("fr.deep").values
+    assert "fr.deep" in fr_signals
+    # This is a dynamically restored model
+    nested_type_1 = fr_signals["fr.deep"]
+    assert issubclass(nested_type_1, DataModel)
+    assert {n: fi.annotation for n, fi in nested_type_1.model_fields.items()} == {
+        "aa": int,
+        "bb": str,
+    }
+
+    basic_signals = schema.resolve("fr.deep.aa", "fr.deep.bb").values
+    assert "fr.deep.aa" in basic_signals
+    assert "fr.deep.bb" in basic_signals
+    assert basic_signals["fr.deep.aa"] is int
+    assert basic_signals["fr.deep.bb"] is str
+
+
+def test_select_nested_errors():
+    schema = SignalSchema.deserialize(
+        {
+            "address": "str",
+            "fr": "MyType2@v1",
+        }
+    )
+
+    schema = schema.resolve("fr.deep.aa", "fr.deep.bb")
+
+    with pytest.raises(SignalResolvingError):
+        schema.resolve("some_random")
+
+    with pytest.raises(SignalResolvingError):
+        schema.resolve("fr")
+
+    with pytest.raises(SignalResolvingError):
+        schema.resolve("fr.deep")
+
+    with pytest.raises(SignalResolvingError):
+        schema.resolve("fr.deep.not_exist")
+
+
+def test_get_signals_basic():
+    schema = {
+        "name": str,
+        "age": float,
+        "f": File,
+    }
+    assert list(SignalSchema(schema).get_signals(File)) == ["f"]
+
+
+def test_get_signals_no_signal():
+    schema = {
+        "name": str,
+    }
+    assert list(SignalSchema(schema).get_signals(File)) == []
+
+
+def test_get_signals_nested(nested_file_schema):
+    files = list(nested_file_schema.get_signals(File))
+    assert files == ["f", "my_f", "my_f.nested_file"]
+
+
+def test_get_signals_subclass(nested_file_schema):
+    class NewFile(File):
+        pass
+
+    schema = {
+        "name": str,
+        "age": float,
+        "f": NewFile,
+    }
+    assert list(SignalSchema(schema).get_signals(File)) == ["f"]
+
+
+def test_build_tree():
+    spec = {"name": str, "age": float, "fr": MyType2}
+    lst = list(SignalSchema(spec).get_flat_tree())
+
+    assert lst == [
+        (["name"], str, False, 0),
+        (["age"], float, False, 0),
+        (["fr"], MyType2, True, 0),
+        (["fr", "name"], str, False, 1),
+        (["fr", "deep"], MyType1, True, 1),
+        (["fr", "deep", "aa"], int, False, 2),
+        (["fr", "deep", "bb"], str, False, 2),
+    ]
+
+
+def test_print_types():
+    mapping = {
+        int: "int",
+        float: "float",
+        MyType2: "MyType2",
+        Optional[MyType2]: "Union[MyType2, NoneType]",
+        Union[str, int]: "Union[str, int]",
+        Union[Optional[MyType2]]: "Union[MyType2, NoneType]",
+        list: "list",
+        list[Optional[bool]]: "list[Union[bool, NoneType]]",
+        dict: "dict",
+        dict[str, Optional[MyType1]]: "dict[str, Union[MyType1, NoneType]]",
+    }
+
+    for t, v in mapping.items():
+        assert SignalSchema._type_to_str(t) == v
+
+
+def test_db_signals():
+    spec = {"name": str, "age": float, "fr": MyType2}
+    lst = list(SignalSchema(spec).db_signals())
+
+    assert lst == [
+        "name",
+        "age",
+        "fr__name",
+        "fr__deep__aa",
+        "fr__deep__bb",
+    ]
+
+
+def test_db_signals_filtering_by_name():
+    schema = SignalSchema({"name": str, "age": float, "fr": MyType2})
+
+    assert list(schema.db_signals(name="fr")) == [
+        "fr__name",
+        "fr__deep__aa",
+        "fr__deep__bb",
+    ]
+    assert list(schema.db_signals(name="name")) == ["name"]
+    assert list(schema.db_signals(name="missing")) == []
+
+
+def test_db_signals_as_columns():
+    spec = {"name": str, "age": float, "fr": MyType2}
+    lst = list(SignalSchema(spec).db_signals(as_columns=True))
+
+    assert all(isinstance(s, Column) for s in lst)
+
+    assert [(c.name, type(c.type)) for c in lst] == [
+        ("name", String),
+        ("age", Float),
+        ("fr__name", String),
+        ("fr__deep__aa", Int64),
+        ("fr__deep__bb", String),
+    ]
+
+
+def test_row_to_objs():
+    spec = {"name": str, "age": float, "fr": MyType2}
+    schema = SignalSchema(spec)
+
+    val = MyType2(name="Fred", deep=MyType1(aa=129, bb="qwe"))
+    row = ("myname", 12.5, *flatten(val))
+
+    res = schema.row_to_objs(row)
+
+    assert res == ["myname", 12.5, val]
+
+
+def test_row_to_objs_setup():
+    spec = {"name": str, "age": float, "init_val": int, "fr": MyType2}
+    setup_value = 84635
+    setup = {"init_val": lambda: setup_value}
+    schema = SignalSchema(spec, setup)
+
+    val = MyType2(name="Fred", deep=MyType1(aa=129, bb="qwe"))
+    row = ("myname", 12.5, *flatten(val))
+
+    res = schema.row_to_objs(row)
+    assert res == ["myname", 12.5, setup_value, val]
+
+
+def test_setup_not_callable():
+    spec = {"name": str, "age": float, "init_val": int, "fr": MyType2}
+    setup_dict = {"init_val": "asdfd"}
+    with pytest.raises(SetupError):
+        SignalSchema(spec, setup_dict)
+
+
+def test_slice():
+    schema = {"name": str, "age": float, "address": str}
+    keys = ["age", "name"]
+    sliced = SignalSchema(schema).slice(keys)
+    assert list(sliced.values.items()) == [("age", float), ("name", str)]
+
+
+def test_slice_nested():
+    schema = {
+        "name": str,
+        "feature": MyType1,
+    }
+    keys = ["feature.aa"]
+    sliced = SignalSchema(schema).slice(keys)
+    assert list(sliced.values.items()) == [("feature.aa", int)]
+
+
+def test_mutate_rename():
+    schema = SignalSchema({"name": str})
+    schema = schema.mutate({"new_name": Column("name")})
+    assert schema.values == {"new_name": str}
+
+
+def test_mutate_new_signal():
+    schema = SignalSchema({"name": str})
+    schema = schema.mutate({"age": Column("age", Float)})
+    assert schema.values == {"name": str, "age": float}
+
+
+def test_mutate_change_type():
+    schema = SignalSchema({"name": str, "age": float, "f": File})
+    schema = schema.mutate({"age": int, "f": TextFile})
+    assert schema.values == {"name": str, "age": int, "f": TextFile}
+
+
+@pytest.mark.parametrize(
+    "column_type,signal_type",
+    [
+        [String, str],
+        [Boolean, bool],
+        [Int, int],
+        [Int32, int],
+        [Int64, int],
+        [UInt64, int],
+        [Float, float],
+        [Float32, float],
+        [Float64, float],
+        [Array(Int), list],
+        [JSON, dict],
+        [DateTime, datetime],
+        [Binary, bytes],
+    ],
+)
+def test_column_types(column_type, signal_type):
+    signals = SignalSchema.from_column_types({"val": column_type}).values
+
+    assert len(signals) == 1
+    assert signals["val"] is signal_type
