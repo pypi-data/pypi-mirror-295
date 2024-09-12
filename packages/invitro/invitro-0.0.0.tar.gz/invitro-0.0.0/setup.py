93cc9cc6b387771a2b760a7be71e42a22261f599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:40 +0000

    Add package invitro/0.0.0/invitro-0.0.0.tar.gz

---
+++ packages/invitro/invitro-0.0.0.tar.gz/invitro-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='invitro',
+    version='0.0.0',
+    description='Coming Soon',
+    author='Cedric H',
+    author_email='a.asaf.lk@gmail.com', 
+    packages=['invitro'],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
