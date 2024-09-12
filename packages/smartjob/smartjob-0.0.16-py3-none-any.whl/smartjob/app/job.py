c933bf1a0d23af6e534ffa6d73d96cb242d16137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:06:54 +0000

    Add package smartjob/0.0.16/smartjob-0.0.16-py3-none-any.whl

---
+++ packages/smartjob/smartjob-0.0.16-py3-none-any.whl/smartjob/app/job.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass, field
+
+# DEFAULT_NAMESPACE is the default namespace for jobs.
+DEFAULT_NAMESPACE = "default"
+
+
+@dataclass
+class SmartJob:
+    """Base class for smart jobs."""
+
+    name: str
+    """Name of the job."""
+
+    docker_image: str
+    """Docker image to use for the job.
+
+    Example: `docker.io/python:3.12`.
+    """
+
+    namespace: str = DEFAULT_NAMESPACE
+    """Namespace of the job."""
+
+    add_envs: dict[str, str] = field(default_factory=dict)
+    """Environnement variables to add in the container.
+
+    Note: `INPUT_PATH` and `OUTPUT_PATH` will be automatically injected (if relevant).
+    """
+
+    overridden_args: list[str] = field(default_factory=list)
+    """Container arguments (including command) to use.
+
+    If not set, the default container image arguments will be used.
+
+    """
+
+    python_script_path: str = ""
+    """Local path to a python script to execute in the container."""
+
+    @property
+    def full_name(self) -> str:
+        """Return the full name of the job (namespace + name)."""
+        return f"{self.namespace}-{self.name}"
