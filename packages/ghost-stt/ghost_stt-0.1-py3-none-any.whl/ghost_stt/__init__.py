0d3c724d5cc7e3cc2f6a544e783dc895cd7b9ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:05:36 +0000

    Add package ghost-stt/0.1/ghost_stt-0.1-py3-none-any.whl

---
+++ packages/ghost-stt/ghost_stt-0.1-py3-none-any.whl/ghost_stt/__init__.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from os import getcwd
+import traceback
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--use-fake-ui-for-media-stream")
+chrome_options.add_argument("--headless=new")  
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+website =  "https://allorizenproject1.netlify.app"
+driver.get(website)
+
+rec_file = f"{getcwd()}/input.txt"
+
+def listen():
+    try:
+        start_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'startButton')))
+        start_button.click()
+        print("Listening...")
+        output_text = ""
+        is_second_click = False
+        
+        while True:
+            output_elements = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.ID, 'output')))
+            
+            current_text = ' '.join([elem.text.strip() for elem in output_elements])
+            
+            if "Start Listening" in start_button.text and is_second_click:
+                if output_text:
+                    is_second_click = True
+            elif "Listening..." in start_button.text:
+                is_second_click = True
+                
+            if current_text != output_text:
+                output_text = current_text
+                with open(rec_file, "w") as file:
+                    file.write(output_text.lower())
+                    print("user : " + output_text)
+                    
+    except KeyboardInterrupt:
+        print("Stopped by user.")
+    except Exception as e:
+        print(f"Error: {e}")
+        print(traceback.format_exc())
+
+listen()
