8a7da9bf029ef80a5692c7198c8a031dccc86702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:07:00 +0000

    Add package pydantic-tfl-api/1.1.0/pydantic_tfl_api-1.1.0.tar.gz

---
+++ packages/pydantic-tfl-api/pydantic_tfl_api-1.1.0.tar.gz/pydantic_tfl_api-1.1.0/pydantic_tfl_api/__init__.py
@@ -0,0 +1,34 @@
+from .endpoints import (
+    LineClient,
+    OccupancyClient,
+    VehicleClient,
+    ModeClient,
+    CrowdingClient,
+    PlaceClient,
+    AirQualityClient,
+    SearchClient,
+    StopPointClient,
+    JourneyClient,
+    BikePointClient,
+    AccidentStatsClient,
+    LiftDisruptionsClient,
+    RoadClient
+)
+from . import models
+__all__ = [
+    'LineClient',
+    'OccupancyClient',
+    'VehicleClient',
+    'ModeClient',
+    'CrowdingClient',
+    'PlaceClient',
+    'AirQualityClient',
+    'SearchClient',
+    'StopPointClient',
+    'JourneyClient',
+    'BikePointClient',
+    'AccidentStatsClient',
+    'LiftDisruptionsClient',
+    'RoadClient',
+    'models'
+]
