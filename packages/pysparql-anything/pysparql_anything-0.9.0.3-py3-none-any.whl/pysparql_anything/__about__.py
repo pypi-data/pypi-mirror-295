c1099a7cdf4743dc0bd0bbf9bcc620698c0cf4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:06:48 +0000

    Add package pysparql-anything/0.9.0.3/pysparql_anything-0.9.0.3-py3-none-any.whl

---
+++ packages/pysparql-anything/pysparql_anything-0.9.0.3-py3-none-any.whl/pysparql_anything/__about__.py
@@ -0,0 +1,20 @@
+"""
+Version variable for build purposes. 'x.y.z' refers to the current release of
+SPARQL Anything, the last number to the independent PySPARQL Anything
+releases.
+
+Author: Marco Ratta
+Date: 09/09/2024
+"""
+
+# PySPARQL version for the build process:
+__version__ = "0.9.0.3"
+
+# Version of SPARQL Anything to download:
+__SparqlAnything__ = "0.9.0"
+
+# Path to the SPARQL Anything main class within the executable jar:
+__jarMainPath__ = "io.github.sparqlanything.cli.SPARQLAnything"
+
+# SPARQL Anything GitHub URI:
+__uri__ = "SPARQL-Anything/sparql.anything"
