998b972d4d14b2fe37be90b0b52d9e64e5485a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:11 +0000

    Add package abstract-solana/0.0.0.19/abstract_solana-0.0.0.19.tar.gz

---
+++ packages/abstract-solana/abstract_solana-0.0.0.19.tar.gz/abstract_solana-0.0.0.19/src/abstract_solana/utils.py
@@ -0,0 +1,12 @@
+def ifListGetSection(listObj,section=0):
+    if isinstance(listObj,list):
+        if len(listObj)>section:
+            return listObj[section]
+    return listObj
+def if_list_get_Part(obj,i=0):
+    if obj and isinstance(obj,list) and len(obj)>i:
+        obj = obj[i]
+    return obj
+def updateData(data,**kwargs):
+  data.update(kwargs)
+  return data
