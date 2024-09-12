6f1423384673a55db6daeb7383da5cdb7e2d3b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:05:13 +0000

    Add package mindstudio-probe/1.0.4/mindstudio_probe-1.0.4-py3-none-any.whl

---
+++ packages/mindstudio-probe/mindstudio_probe-1.0.4-py3-none-any.whl/msprobe/mindspore/debugger/debugger_config.py
@@ -0,0 +1,66 @@
+import os
+
+from msprobe.core.common.const import Const
+from msprobe.mindspore.common.const import Const as MsConst
+from msprobe.mindspore.common.const import FreeBenchmarkConst
+from msprobe.core.common.file_utils import create_directory
+
+
+class DebuggerConfig:
+    def __init__(self, common_config, task_config):
+        self.execution_mode = None
+        self.dump_path = common_config.dump_path
+        self.task = common_config.task
+        self.rank = [] if not common_config.rank else common_config.rank
+        self.step = [] if not common_config.step else common_config.step
+        common_config.level = Const.LEVEL_L1 if not common_config.level else common_config.level
+        self.level = MsConst.TOOL_LEVEL_DICT.get(common_config.level, MsConst.API)
+        self.level_ori = common_config.level
+        self.list = [] if not task_config.list else task_config.list
+        self.scope = [] if not task_config.scope else task_config.scope
+        self.data_mode = [] if not task_config.data_mode else task_config.data_mode
+        self.file_format = task_config.file_format
+        self.overflow_nums = 1 if not task_config.overflow_nums else task_config.overflow_nums
+        self.check_mode = task_config.check_mode
+        self.framework = Const.MS_FRAMEWORK
+        self.summary_mode = task_config.summary_mode
+        self.check()
+        create_directory(self.dump_path)
+
+        if self.task == Const.FREE_BENCHMARK:
+            self.pert_type = (FreeBenchmarkConst.DEFAULT_PERT_TYPE
+                              if not task_config.pert_mode else task_config.pert_mode)
+            self.handler_type = (FreeBenchmarkConst.DEFAULT_HANDLER_TYPE
+                                 if not task_config.handler_type else task_config.handler_type)
+            self.stage = FreeBenchmarkConst.DEFAULT_STAGE if not task_config.fuzz_stage else task_config.fuzz_stage
+            if self.handler_type == FreeBenchmarkConst.FIX and \
+               self.pert_type != FreeBenchmarkConst.DEFAULT_PERT_TYPE:
+                raise ValueError("pert_mode must be improve_precision or empty when handler_type is fix, "
+                                 f"but got {self.pert_type}.")
+            self.dump_level = FreeBenchmarkConst.DEFAULT_DUMP_LEVEL
+
+    def check(self):
+        if not self.dump_path:
+            raise Exception("Dump path is empty.")
+        self.dump_path = os.path.abspath(self.dump_path)
+        if not self.task:
+            self.task = "statistics"
+        if not self.level:
+            raise Exception("level must be L0, L1 or L2")
+        if not self.file_format:
+            self.file_format = "npy"
+        if not self.check_mode:
+            self.check_mode = "all"
+        self._check_rank()
+        self._check_step()
+        return True
+
+    def _check_rank(self):
+        for rank_id in self.rank:
+            if not isinstance(rank_id, int) or rank_id < 0:
+                raise ValueError(f"rank {self.rank} must be a positive integer.")
+
+    def _check_step(self):
+        for s in self.step:
+            if not isinstance(s, int) or s < 0:
+                raise ValueError(f"step element {s} must be a positive integer.")
---
+++ packages/mindstudio-probe/mindstudio_probe-1.0.4-py3-none-any.whl/msprobe/pytorch/common/__init__.py
@@ -0,0 +1,2 @@
+from .parse_json import parse_json_info_forward_backward
+from .utils import seed_all
