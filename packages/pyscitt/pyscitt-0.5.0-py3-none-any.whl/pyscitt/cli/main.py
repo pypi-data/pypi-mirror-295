fa5af2ba21f4a9520021c04acfe654debbfbbbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:26 +0000

    Add package pyscitt/0.5.0/pyscitt-0.5.0-py3-none-any.whl

---
+++ packages/pyscitt/pyscitt-0.5.0-py3-none-any.whl/pyscitt/cli/main.py
@@ -0,0 +1,55 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import argparse
+
+from . import (
+    create_did_web,
+    embed_receipt_in_cose,
+    governance,
+    pretty_receipt,
+    retrieve_signed_claims,
+    sign_claims,
+    split_payload,
+    submit_signed_claims,
+    upload_did_web_doc_to_github,
+    validate_cose,
+)
+
+COMMANDS = [
+    ("create-did-web", create_did_web),
+    ("upload-did-web-github", upload_did_web_doc_to_github),
+    ("sign", sign_claims),
+    ("submit", submit_signed_claims),
+    ("retrieve", retrieve_signed_claims),
+    ("split-payload", split_payload),
+    ("pretty-receipt", pretty_receipt),
+    ("embed-receipt", embed_receipt_in_cose),
+    ("validate", validate_cose),
+    ("governance", governance),
+]
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    # Hide commands used for testing through metavar
+    # https://github.com/python/cpython/issues/67037
+    # https://docs.python.org/3/library/argparse.html#sub-commands
+    sub = parser.add_subparsers(
+        metavar="{submit,retrieve,pretty-receipt,embed-receipt,validate,split-payload}",
+        help="""Choose one of the available commands to run. 
+                                Use the --help flag to see the options for each command.
+                                For instance 'scitt submit --help' will show the options for the submit command.
+                                """,
+    )
+    for name, module in COMMANDS:
+        getattr(module, "cli")(lambda *args, **kw: sub.add_parser(name, *args, **kw))
+    args = parser.parse_args(argv)
+    if not hasattr(args, "func"):
+        parser.print_usage()
+    else:
+        args.func(args)
+
+
+if __name__ == "__main__":
+    main()
