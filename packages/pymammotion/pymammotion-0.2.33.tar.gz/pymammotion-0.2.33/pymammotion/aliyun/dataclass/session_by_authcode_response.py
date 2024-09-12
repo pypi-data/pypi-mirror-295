2ea05e1049076ea1408e2b6a2c8f45defc3c07ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:05:23 +0000

    Add package pymammotion/0.2.33/pymammotion-0.2.33.tar.gz

---
+++ packages/pymammotion/pymammotion-0.2.33.tar.gz/pymammotion-0.2.33/pymammotion/aliyun/dataclass/session_by_authcode_response.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+from datetime import time
+
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+
+@dataclass
+class SessionOauthToken(DataClassORJSONMixin):
+    identityId: str
+    refreshTokenExpire: int
+    iotToken: str
+    iotTokenExpire: int
+    refreshToken: str
+
+
+@dataclass
+class SessionByAuthCodeResponse(DataClassORJSONMixin):
+    code: int
+    data: SessionOauthToken
