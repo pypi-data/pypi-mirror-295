8835ba290f5c90315222adca65d7b7073bb928c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:20 +0000

    Add package python-datemath/3.0.2/python-datemath-3.0.2.tar.gz

---
+++ packages/python-datemath/python-datemath-3.0.2.tar.gz/python-datemath-3.0.2/setup.py
@@ -0,0 +1,58 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description_from_readme = f.read()
+
+with open(path.join(here, 'VERSION.txt'), encoding='utf-8') as fv:
+    version = fv.read()
+
+setup(
+    name='python-datemath',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=version,
+    download_url = 'https://github.com/nickmaccarthy/python-datemath/tarball/{0}'.format(version),
+
+    # The project's main homepage.
+    url='https://github.com/nickmaccarthy/python-datemath',
+
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    package_data={'': ['*']},
+    include_package_data=True,
+
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['arrow'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+)
