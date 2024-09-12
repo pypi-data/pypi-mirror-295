5f051969de25efa20a0889951f3af6c15f4902c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:23 +0000

    Add package sl-ai-models/0.0.8/sl_ai_models-0.0.8.tar.gz

---
+++ packages/sl-ai-models/sl_ai_models-0.0.8.tar.gz/sl_ai_models-0.0.8/sl_ai_models/__init__.py
@@ -0,0 +1,11 @@
+from sl_ai_models.gpt4ovision import Gpt4oVision
+from sl_ai_models.gpt4o import Gpt4o
+from sl_ai_models.perplexity import Perplexity
+from sl_ai_models.claude35sonnet import Claude35Sonnet
+from sl_ai_models.exa_searcher import ExaSearcher
+from sl_ai_models.gpt4omini import Gpt4oMini
+from sl_ai_models.deepgram_nova2 import DeepgramNova2
+
+
+from sl_ai_models.resource_managers.monetary_cost_manager import MonetaryCostManager
+from sl_ai_models.utils.ai_misc import clean_indents
