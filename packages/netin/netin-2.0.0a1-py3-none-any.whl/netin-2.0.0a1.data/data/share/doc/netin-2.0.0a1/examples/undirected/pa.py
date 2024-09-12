e90330858450e536d1bd8098b6f57f88e17323b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:06:04 +0000

    Add package netin/2.0.0a1/netin-2.0.0a1-py3-none-any.whl

---
+++ packages/netin/netin-2.0.0a1-py3-none-any.whl/netin-2.0.0a1.data/data/share/doc/netin-2.0.0a1/examples/undirected/pa.py
@@ -0,0 +1,21 @@
+from netin.models import PAModel
+from netin.utils import SimulationTimer
+
+
+def run():
+    N = 4000
+    m = 2
+    f_m = 0.1
+    seed = 1234
+
+    model = PAModel(N=N,
+                    m=m,
+                    f_m=f_m,
+                    seed=seed)
+    timer = SimulationTimer(model)
+    model.simulate()
+    print(f"Simulated model {model} in {timer.time:.2f} seconds.")
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/netin/netin-2.0.0a1-py3-none-any.whl/netin-2.0.0a1.data/data/share/doc/netin-2.0.0a1/examples/undirected/patch.py
@@ -0,0 +1,27 @@
+from netin.models import PATCHModel, CompoundLFM
+from netin.utils import SimulationTimer
+
+def run():
+    n = 100
+    k = 2
+    f_m = 0.1
+    h_MM = 0.9
+    h_mm = 0.9
+    p_tc = 0.8
+    lfm_l = CompoundLFM.PAH
+    lfm_g = CompoundLFM.PAH
+    seed = 1234
+
+    model = PATCHModel(N=n, m=k, f_m=f_m,
+                       p_tc=p_tc,
+                       lfm_local=lfm_l, lfm_global=lfm_g,
+                       lfm_params={'h_m': h_mm, 'h_M': h_MM},
+                       seed=seed)
+
+    timer = SimulationTimer(model)
+    model.simulate()
+    print(f"Simulated model {model} in {timer.time:.2f} seconds.")
+
+
+if __name__ == '__main__':
+    run()
