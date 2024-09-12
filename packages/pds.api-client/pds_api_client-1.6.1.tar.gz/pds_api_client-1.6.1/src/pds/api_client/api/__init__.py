af00de9f8afa71192ed0ef6f6a7368ec60db7b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:19 +0000

    Add package pds.api-client/1.6.1/pds_api_client-1.6.1.tar.gz

---
+++ packages/pds.api-client/pds_api_client-1.6.1.tar.gz/pds_api_client-1.6.1/src/pds/api_client/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from pds.api_client.api.all_docs_api import AllDocsApi
+from pds.api_client.api.all_products_api import AllProductsApi
+from pds.api_client.api.by_product_classes_api import ByProductClassesApi
+from pds.api_client.api.healthcheck_api import HealthcheckApi
+from pds.api_client.api.product_references_api import ProductReferencesApi
+
---
+++ packages/pds.api-client/pds_api_client-1.6.1.tar.gz/pds_api_client-1.6.1/src/pds/api_client/test/integration/test_products.py
@@ -0,0 +1,38 @@
+import unittest
+from pds.api_client import Configuration
+from pds.api_client import ApiClient
+
+from pds.api_client.api.all_products_api import AllProductsApi
+
+
+class ProductsCase(unittest.TestCase):
+
+    def setUp(self):
+        # create an instance of the API class
+        configuration = Configuration()
+        configuration.host = 'http://localhost:8080'
+        api_client = ApiClient(configuration)
+        self.products = AllProductsApi(api_client)
+
+    def test_get_properties(self):
+
+        properties = self.products.product_properties_list()
+        properties_dict = {p.var_property:{"type": p.type} for p in properties}
+        assert '_package_id' in properties_dict.keys()
+        assert 'alternate_ids' in properties_dict.keys()
+        assert 'insight:Observation_Information.insight:software_version_id' in properties_dict.keys()
+        assert properties_dict['_package_id']['type'] == 'string'
+        assert properties_dict['alternate_ids']['type'] == 'string'
+        assert properties_dict['insight:Observation_Information.insight:software_version_id']['type'] == 'string'
+
+    @unittest.skip("keyword is temporarily not implemented in version 1.5.0 of the API")
+    def test_products_by_keywords(self):
+        results = self.products.product_list(
+            keywords=['kernel']
+        )
+
+        self.assertEqual(len(results.data), 3)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
