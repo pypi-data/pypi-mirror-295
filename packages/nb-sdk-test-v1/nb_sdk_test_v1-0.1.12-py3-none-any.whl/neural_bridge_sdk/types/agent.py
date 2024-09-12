848276f0395d1a00bae23ab190c6edc0d95a06ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:04:45 +0000

    Add package nb-sdk-test-v1/0.1.12/nb_sdk_test_v1-0.1.12-py3-none-any.whl

---
+++ packages/nb-sdk-test-v1/nb_sdk_test_v1-0.1.12-py3-none-any.whl/neural_bridge_sdk/types/agent.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from typing import List
+
+from pydantic import BaseModel
+
+
+class AgentType(str, Enum):
+  AGENT = "agent"
+  MULTI_AGENT = "multi-agent"
+  CODE_AGENT = "code-agent"
+  LANGGRAPH_AGENT = "langgraph-agent"
+
+
+class ReleaseEnvironment(str, Enum):
+  DEV = "dev"
+  PROD = "prod"
+  LOCAL = "local"
+
+
+class AgentConfig(BaseModel):
+  name: str
+  description: str
+  enabled_envs: List[ReleaseEnvironment] = []
+  readme: str = ""
+  conversation_starters: List[str] = []
+
+
+class AgentSpec(BaseModel):
+  agent_id: str
+  agent_config: AgentConfig
+  agent_type: AgentType
+  tools: List[str]
+  llms: List[str]
+  tool_snippets: str
