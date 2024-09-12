ca73d0bf50b3897698b1d43b9955188b616d6eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:05:26 +0000

    Add package types-regex/2024.9.11.20240912/types-regex-2024.9.11.20240912.tar.gz

---
+++ packages/types-regex/types-regex-2024.9.11.20240912.tar.gz/types-regex-2024.9.11.20240912/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+name = "types-regex"
+description = "Typing stubs for regex"
+long_description = '''
+## Typing stubs for regex
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`regex`](https://github.com/mrabarnett/mrab-regex) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`regex`.
+
+This version of `types-regex` aims to provide accurate annotations
+for `regex==2024.9.11`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/regex. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit
+[`6259b616585ec1eb6726294851988472c7ebface`](https://github.com/python/typeshed/commit/6259b616585ec1eb6726294851988472c7ebface) and was tested
+with mypy 1.11.1, pyright 1.1.379, and
+pytype 2024.4.11.
+'''.lstrip()
+
+setup(name=name,
+      version="2024.9.11.20240912",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/regex.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['regex-stubs'],
+      package_data={'regex-stubs': ['__init__.pyi', '_regex.pyi', '_regex_core.pyi', 'regex.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
