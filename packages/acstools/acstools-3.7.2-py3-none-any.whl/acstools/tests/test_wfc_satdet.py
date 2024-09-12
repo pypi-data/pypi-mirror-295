9403325f3ebed8c04881f0394f7daa396f64b924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:04:58 +0000

    Add package acstools/3.7.2/acstools-3.7.2-py3-none-any.whl

---
+++ packages/acstools/acstools-3.7.2-py3-none-any.whl/acstools/tests/test_wfc_satdet.py
@@ -0,0 +1,37 @@
+"""Test satellite trail detection.
+
+.. note:: Cannot test ``detsat()`` because PHT results change from run to run!
+
+"""
+import pytest
+
+from acstools import satdet
+from acstools.utils_findsat_mrt import update_dq
+from acstools.tests.helpers import BaseACSTOOLS
+
+pytest.importorskip("skimage")
+
+
+class TestSatDet(BaseACSTOOLS):
+    detector = 'wfc'
+
+    def test_trail_mask(self):
+        """Mask satellite trail on WFC EXT 6."""
+
+        rootname = 'jc8m10syq'
+        inputfile = rootname + '_flc.fits'  # This is modified in-place
+        truthfile = rootname + '_flc_ref.fits'
+
+        # Prepare input file.
+        self.get_input_file(inputfile, skip_ref=True)
+
+        # Satellite trail masking.
+        sciext = 4
+        dqext = 6
+        trail = ((1199, 1357), (2841, 1023))
+        mask = satdet.make_mask(inputfile, sciext, trail, plot=False,
+                                verbose=False)
+        update_dq(inputfile, dqext, mask, verbose=False)
+
+        # Compare results.
+        self.compare_outputs([(inputfile, truthfile)])
