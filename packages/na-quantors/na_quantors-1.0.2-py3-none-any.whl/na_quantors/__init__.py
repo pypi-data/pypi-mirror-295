cf8e2622873bfdb6760ce6202fe941aaf942eaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:31 +0000

    Add package na-quantors/1.0.2/na_quantors-1.0.2-py3-none-any.whl

---
+++ packages/na-quantors/na_quantors-1.0.2-py3-none-any.whl/na_quantors/__init__.py
@@ -0,0 +1,27 @@
+import pandas as _pd
+
+
+def isna(*values):
+    ans = {(_pd.isna(x) is True) for x in values}
+    (ans,) = ans
+    return ans
+
+
+def notna(*values):
+    return not isna(*values)
+
+
+def allisna(*values):
+    return all(isna(x) for x in values)
+
+
+def allnotna(*values):
+    return all(notna(x) for x in values)
+
+
+def anyisna(*values):
+    return any(isna(x) for x in values)
+
+
+def anynotna(*values):
+    return any(notna(x) for x in values)
