f25aeaa6e49434da60822974b73109668ba9354b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:48 +0000

    Add package kody-clientsdk-python/0.0.1/kody_clientsdk_python-0.0.1.tar.gz

---
+++ packages/kody-clientsdk-python/kody_clientsdk_python-0.0.1.tar.gz/kody_clientsdk_python-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+from kody_clientsdk_python import __version__ as version
+setup(
+    name="kody_clientsdk_python",
+    version=version,
+    description='Kody API client',
+    long_description=open('README.md').read(),
+    author='Kody Tech Team',
+    author_email='tech@kody.com',
+    url='https://github.com/KodyPay/kody-clientsdk-python',
+    packages=find_packages(),
+    install_requires=[  # List your dependencies here
+        'grpcio==1.66.1',
+        'protobuf==5.27.2'
+    ],
+)
