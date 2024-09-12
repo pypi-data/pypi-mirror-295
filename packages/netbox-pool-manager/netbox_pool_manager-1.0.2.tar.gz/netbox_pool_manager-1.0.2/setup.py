e808e07fcc7a45614d65834616620c430ac0e9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:11 +0000

    Add package netbox-pool-manager/1.0.2/netbox_pool_manager-1.0.2.tar.gz

---
+++ packages/netbox-pool-manager/netbox_pool_manager-1.0.2.tar.gz/netbox_pool_manager-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox_pool_manager',
+    version='1.0.2',
+    description='A Pool Manager NetBox plugin',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
+
