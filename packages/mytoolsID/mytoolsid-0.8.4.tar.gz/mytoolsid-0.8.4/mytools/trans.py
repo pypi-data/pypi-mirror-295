560f1121884450b5d188ec7cd573be16a822e839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:06:30 +0000

    Add package mytoolsID/0.8.4/mytoolsid-0.8.4.tar.gz

---
+++ packages/mytoolsID/mytoolsid-0.8.4.tar.gz/mytoolsid-0.8.4/mytools/trans.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+from gpytranslate import SyncTranslator
+
+
+def random_name():
+    random_string = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    return random_string + ".mp3"
+
+
+class Translate(SyncTranslator):
+    def ConvertLang(self, msg, lang="id"):
+        trans = self.translate(msg, targetlang=lang)
+        return trans.text
+
+    def TextToSpeech(self, text):
+        clean_text = " ".join(text.splitlines())
+        filename = random_name()
+
+        with open(filename, "wb") as file:
+            self.tts(clean_text, file=file)
+
+        return filename
