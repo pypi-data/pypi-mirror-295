37d4dc38c3774edb9db11a80fc3eef5cf18e0580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:31 +0000

    Add package fund-admin-openapi-client/0.0.1.dev1726101473/fund-admin-openapi-client-0.0.1.dev1726101473.tar.gz

---
+++ packages/fund-admin-openapi-client/fund-admin-openapi-client-0.0.1.dev1726101473.tar.gz/fund-admin-openapi-client-0.0.1.dev1726101473/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='fund-admin-openapi-client',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
