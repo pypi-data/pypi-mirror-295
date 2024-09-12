80a8601b99be6670d5077d9446f83255ad55b91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:05:26 +0000

    Add package maxwell-utils/0.8.1/maxwell_utils-0.8.1-py3-none-any.whl

---
+++ packages/maxwell-utils/maxwell_utils-0.8.1-py3-none-any.whl/maxwell/utils/listenable.py
@@ -0,0 +1,35 @@
+import collections
+import traceback
+
+from .logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class Listenable(object):
+    def __init__(self):
+        self.__listeners = collections.OrderedDict()
+
+    def add_listener(self, event, callback):
+        callbacks = self.__listeners.get(event)
+        if callbacks is None:
+            callbacks = []
+            self.__listeners[event] = callbacks
+        callbacks.append(callback)
+
+    def delete_listener(self, event, callback):
+        callbacks = self.__listeners.get(event)
+        if callbacks is None:
+            return
+        try:
+            callbacks.remove(callback)
+        except Exception:
+            pass
+
+    def notify(self, event, *argv, **kwargs):
+        callbacks = self.__listeners.get(event, [])
+        for callback in callbacks:
+            try:
+                callback(*argv, **kwargs)
+            except Exception:
+                logger.error("Failed to notify: %s", traceback.format_exc())
