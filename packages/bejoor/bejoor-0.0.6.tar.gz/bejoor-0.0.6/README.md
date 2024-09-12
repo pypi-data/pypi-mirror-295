b6bc5f55b60100ff6091e53d52d667a9699fb574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:06:12 +0000

    Add package bejoor/0.0.6/bejoor-0.0.6.tar.gz

---
+++ packages/bejoor/bejoor-0.0.6.tar.gz/bejoor-0.0.6/README.md
@@ -0,0 +1,8 @@
+# Bejoor
+
+## Installation
+    pip install bejoor
+
+## Optimizers
+
+[SalpSwarmAlgorithm](docs/salp_swarm_algorithm.md)
---
+++ packages/bejoor/bejoor-0.0.6.tar.gz/bejoor-0.0.6/bejoor/__init__.py
@@ -0,0 +1,5 @@
+from .genetic import ToyGeneticAlgorithm
+from .swarm_based import SalpSwarmAlgorithm
+
+__all__ = ['ToyGeneticAlgorithm', 'SalpSwarmAlgorithm']
+
