5ec167052a60c0095a698053f2af537f73ebfd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:34 +0000

    Add package django-metadata-publisher/0.0.1.dev1726090013/django-metadata-publisher-0.0.1.dev1726090013.tar.gz

---
+++ packages/django-metadata-publisher/django-metadata-publisher-0.0.1.dev1726090013.tar.gz/django-metadata-publisher-0.0.1.dev1726090013/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='django-metadata-publisher',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
