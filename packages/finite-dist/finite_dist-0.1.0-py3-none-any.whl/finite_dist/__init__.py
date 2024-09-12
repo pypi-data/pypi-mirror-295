e19ef42bf3a899816fb8dde35b741bc43743a64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:06:33 +0000

    Add package finite-dist/0.1.0/finite_dist-0.1.0-py3-none-any.whl

---
+++ packages/finite-dist/finite_dist-0.1.0-py3-none-any.whl/finite_dist/__init__.py
@@ -0,0 +1,6 @@
+"""
+### Finite Dist
+> Storing finite probability distributions as python counters
+"""
+import lazy_loader as lazy
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
