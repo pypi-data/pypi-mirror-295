83e3f15b01cd9adea051c2cf458ca741afabb285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:07:07 +0000

    Add package ailab100/0.0.1/ailab100-0.0.1-py3-none-any.whl

---
+++ packages/ailab100/ailab100-0.0.1-py3-none-any.whl/pdbc/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding：utf-8 -*-
+
+# ailab.
+
+# Created :2024-9-12  18:09
+# Author：AI Lab 100 Morgan
+# Copyright (C) 2020-2024  数智教育发展 (山东) 有限公司
+# For license information, see LICENSE.txt
+# ID: __init__.py
+
+
