539c33ddcc96316a2875d48543eadde93af1de62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:16 +0000

    Add package sklearmmm/0.1.4/sklearmmm-0.1.4.tar.gz

---
+++ packages/sklearmmm/sklearmmm-0.1.4.tar.gz/sklearmmm-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sklearmmm",  # Название пакета
+    version="0.1.4",
+    packages=find_packages(),
+    description="Library with econometrics function",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Joe Dohn",
+    author_email="joedohn@gmail.com",
+    url="https://github.com/johndoe/mylibrary",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
