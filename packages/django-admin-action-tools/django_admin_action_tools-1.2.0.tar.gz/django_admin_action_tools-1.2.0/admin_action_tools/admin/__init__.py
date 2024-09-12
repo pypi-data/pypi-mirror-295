608c3a0492cf9ed598e3cadc6715358b5b69b93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:06:50 +0000

    Add package django-admin-action-tools/1.2.0/django_admin_action_tools-1.2.0.tar.gz

---
+++ packages/django-admin-action-tools/django_admin_action_tools-1.2.0.tar.gz/django_admin_action_tools-1.2.0/admin_action_tools/admin/__init__.py
@@ -0,0 +1,9 @@
+from admin_action_tools.admin.confirm_tool import AdminConfirmMixin, confirm_action
+from admin_action_tools.admin.form_tool import ActionFormMixin, add_form_to_action
+
+__all__ = [
+    "AdminConfirmMixin",
+    "confirm_action",
+    "ActionFormMixin",
+    "add_form_to_action",
+]
