18f2f0a32640daf0be1bd1e876f3c919e6a75bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:36 +0000

    Add package bep-carta-django-util/0.0.1.dev1726094324/bep-carta-django-util-0.0.1.dev1726094324.tar.gz

---
+++ packages/bep-carta-django-util/bep-carta-django-util-0.0.1.dev1726094324.tar.gz/bep-carta-django-util-0.0.1.dev1726094324/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='bep-carta-django-util',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
