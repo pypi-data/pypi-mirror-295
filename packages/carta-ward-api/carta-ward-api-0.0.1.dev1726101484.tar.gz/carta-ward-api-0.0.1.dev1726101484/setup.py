633f47148539d9489daa694c71a958a1ce3d6790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:31 +0000

    Add package carta-ward-api/0.0.1.dev1726101484/carta-ward-api-0.0.1.dev1726101484.tar.gz

---
+++ packages/carta-ward-api/carta-ward-api-0.0.1.dev1726101484.tar.gz/carta-ward-api-0.0.1.dev1726101484/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-ward-api',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
