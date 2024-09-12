6a74f9c6e440db3f6d3fe2cafaac02cf194c6110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:08 +0000

    Add package pymemtrace/0.1.6/pymemtrace-0.1.6-cp313-cp313-macosx_10_13_universal2.whl

---
+++ packages/pymemtrace/pymemtrace-0.1.6-cp313-cp313-macosx_10_13_universal2.whl/pymemtrace/src/include/pymemtrace_util.h
@@ -0,0 +1,11 @@
+//
+// Created by Paul Ross on 03/11/2020.
+//
+
+#ifndef CPYMEMTRACE_PYMEMTRACE_UTIL_H
+#define CPYMEMTRACE_PYMEMTRACE_UTIL_H
+
+char *create_filename(void);
+char *current_working_directory(void);
+
+#endif //CPYMEMTRACE_PYMEMTRACE_UTIL_H
