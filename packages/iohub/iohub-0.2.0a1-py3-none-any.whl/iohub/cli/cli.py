acb6b2f4bcfec9823f60c191b21063444389e991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:39 +0000

    Add package iohub/0.2.0a1/iohub-0.2.0a1-py3-none-any.whl

---
+++ packages/iohub/iohub-0.2.0a1-py3-none-any.whl/iohub/cli/cli.py
@@ -0,0 +1,89 @@
+import pathlib
+
+import click
+
+from iohub._version import __version__
+from iohub.convert import TIFFConverter
+from iohub.reader import print_info
+
+VERSION = __version__
+
+_DATASET_PATH = click.Path(
+    exists=True, file_okay=False, resolve_path=True, path_type=pathlib.Path
+)
+
+
+@click.group()
+@click.help_option("-h", "--help")
+@click.version_option(version=VERSION)
+def cli():
+    """\u001b[34;1m iohub: N-dimensional bioimaging I/O \u001b[0m"""
+
+
+@cli.command()
+@click.help_option("-h", "--help")
+@click.argument(
+    "files",
+    nargs=-1,
+    required=True,
+    type=_DATASET_PATH,
+)
+@click.option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    help="Show usage guide to open dataset in Python "
+    "and full tree for HCS Plates in OME-Zarr",
+)
+def info(files, verbose):
+    """View basic metadata of a list of FILES.
+
+    Supported formats are Micro-Manager-acquired TIFF datasets
+    (single-page TIFF, multi-page OME-TIFF, NDTIFF)
+    and OME-Zarr (v0.1 linear HCS layout and all v0.4 layouts).
+    """
+    for file in files:
+        click.echo(f"Reading file:\t {file}")
+        print_info(file, verbose=verbose)
+
+
+@cli.command()
+@click.help_option("-h", "--help")
+@click.option(
+    "--input",
+    "-i",
+    required=True,
+    type=_DATASET_PATH,
+    help="Input Micro-Manager TIFF dataset directory",
+)
+@click.option(
+    "--output",
+    "-o",
+    required=True,
+    type=click.Path(exists=False, resolve_path=True),
+    help="Output zarr store (/**/converted.zarr)",
+)
+@click.option(
+    "--grid-layout",
+    "-g",
+    required=False,
+    is_flag=True,
+    help="Arrange FOVs in a row/column grid layout for tiled acquisition",
+)
+@click.option(
+    "--chunks",
+    "-c",
+    required=False,
+    default="XYZ",
+    help="Zarr chunk size given as 'XY', 'XYZ', or a tuple of chunk "
+    "dimensions. If 'XYZ', chunk size will be limited to 500 MB.",
+)
+def convert(input, output, grid_layout, chunks):
+    """Converts Micro-Manager TIFF datasets to OME-Zarr"""
+    converter = TIFFConverter(
+        input_dir=input,
+        output_dir=output,
+        grid_layout=grid_layout,
+        chunks=chunks,
+    )
+    converter()
