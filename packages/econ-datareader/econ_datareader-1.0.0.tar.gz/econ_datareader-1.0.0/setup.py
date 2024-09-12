b11afaeb50e84c1eed5fd807582e0bcb8ee073a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:06:43 +0000

    Add package econ-datareader/1.0.0/econ_datareader-1.0.0.tar.gz

---
+++ packages/econ-datareader/econ_datareader-1.0.0.tar.gz/econ_datareader-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='econ_datareader',
+    version='1.0.0',
+    description='Download Econ Data - Macro and Finance',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='WyChoi1995',
+    author_email='wydanielchoi@gmail.com',
+    url='https://github.com/WYChoi1995/econdatareader',
+    install_requires=['pandas', 'aiohttp', 'nest_asyncio',],
+    packages=find_packages(exclude=[]),
+    keywords=['finance', 'econ'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
