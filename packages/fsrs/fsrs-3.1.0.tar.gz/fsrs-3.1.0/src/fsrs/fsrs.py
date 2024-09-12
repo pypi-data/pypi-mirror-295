a05a93bc128c256b4f84d615278cbb388dbb7504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:05:09 +0000

    Add package fsrs/3.1.0/fsrs-3.1.0.tar.gz

---
+++ packages/fsrs/fsrs-3.1.0.tar.gz/fsrs-3.1.0/src/fsrs/fsrs.py
@@ -0,0 +1,232 @@
+"""
+fsrs.fsrs
+---------
+
+This module defines the FSRS scheduler.
+
+Classes:
+    FSRS: The FSRS scheduler.
+"""
+
+from .models import (
+    Card,
+    ReviewLog,
+    Rating,
+    State,
+    SchedulingCards,
+    SchedulingInfo,
+    Parameters,
+)
+import math
+from datetime import datetime, timezone, timedelta
+from typing import Optional
+import copy
+
+
+class FSRS:
+    """
+    The FSRS scheduler.
+
+    Enables the reviewing and future scheduling of cards according to the FSRS algorithm.
+
+    Attributes:
+        p (Parameters): Object for configuring the scheduler's model weights, desired retention and maximum interval.
+        DECAY (float): Constant used to model the forgetting curve and compute the length of a Card's next interval after being repeated.
+        FACTOR (float): Constant used to model the forgetting curve and compute the length of a Card's next interval after being repeated.
+    """
+
+    p: Parameters
+    DECAY: float
+    FACTOR: float
+
+    def __init__(
+        self,
+        w: Optional[tuple[float, ...]] = None,
+        request_retention: Optional[float] = None,
+        maximum_interval: Optional[int] = None,
+    ) -> None:
+        """
+        Initializes the FSRS scheduler.
+
+        Args:
+            w (Optional[tuple[float, ...]]): The 19 model weights of the FSRS scheduler.
+            request_retention (Optional[float]): The desired retention of the scheduler. Corresponds to the maximum retrievability a Card object can have before it is due.
+            maximum_interval (Optional[int]): The maximum number of days into the future a Card object can be scheduled for next review.
+        """
+        self.p = Parameters(w, request_retention, maximum_interval)
+        self.DECAY = -0.5
+        self.FACTOR = 0.9 ** (1 / self.DECAY) - 1
+
+    def review_card(
+        self, card: Card, rating: Rating, now: Optional[datetime] = None
+    ) -> tuple[Card, ReviewLog]:
+        """
+        Reviews a card for a given rating.
+
+        Args:
+            card (Card): The card being reviewed.
+            rating (Rating): The chosen rating for the card being reviewed.
+            now (Optional[datetime]): The date and time of the review.
+
+        Returns:
+            tuple: A tuple containing the updated, reviewed card and its corresponding review log.
+
+        Raises:
+            ValueError: If the `now` argument is not timezone-aware and set to UTC.
+        """
+        scheduling_cards = self.repeat(card, now)
+
+        card = scheduling_cards[rating].card
+        review_log = scheduling_cards[rating].review_log
+
+        return card, review_log
+
+    def repeat(
+        self, card: Card, now: Optional[datetime] = None
+    ) -> dict[Rating, SchedulingInfo]:
+        if now is None:
+            now = datetime.now(timezone.utc)
+
+        if (now.tzinfo is None) or (now.tzinfo != timezone.utc):
+            raise ValueError("datetime must be timezone-aware and set to UTC")
+
+        card = copy.deepcopy(card)
+        if card.state == State.New:
+            card.elapsed_days = 0
+        else:
+            card.elapsed_days = (now - card.last_review).days
+        card.last_review = now
+        card.reps += 1
+        s = SchedulingCards(card)
+        s.update_state(card.state)
+
+        if card.state == State.New:
+            self.init_ds(s)
+
+            s.again.due = now + timedelta(minutes=1)
+            s.hard.due = now + timedelta(minutes=5)
+            s.good.due = now + timedelta(minutes=10)
+            easy_interval = self.next_interval(s.easy.stability)
+            s.easy.scheduled_days = easy_interval
+            s.easy.due = now + timedelta(days=easy_interval)
+        elif card.state == State.Learning or card.state == State.Relearning:
+            interval = card.elapsed_days
+            last_d = card.difficulty
+            last_s = card.stability
+            retrievability = self.forgetting_curve(interval, last_s)
+            self.next_ds(s, last_d, last_s, retrievability, card.state)
+
+            hard_interval = 0
+            good_interval = self.next_interval(s.good.stability)
+            easy_interval = max(self.next_interval(s.easy.stability), good_interval + 1)
+            s.schedule(now, hard_interval, good_interval, easy_interval)
+        elif card.state == State.Review:
+            interval = card.elapsed_days
+            last_d = card.difficulty
+            last_s = card.stability
+            retrievability = self.forgetting_curve(interval, last_s)
+            self.next_ds(s, last_d, last_s, retrievability, card.state)
+
+            hard_interval = self.next_interval(s.hard.stability)
+            good_interval = self.next_interval(s.good.stability)
+            hard_interval = min(hard_interval, good_interval)
+            good_interval = max(good_interval, hard_interval + 1)
+            easy_interval = max(self.next_interval(s.easy.stability), good_interval + 1)
+            s.schedule(now, hard_interval, good_interval, easy_interval)
+        return s.record_log(card, now)
+
+    def init_ds(self, s: SchedulingCards) -> None:
+        s.again.difficulty = self.init_difficulty(Rating.Again)
+        s.again.stability = self.init_stability(Rating.Again)
+        s.hard.difficulty = self.init_difficulty(Rating.Hard)
+        s.hard.stability = self.init_stability(Rating.Hard)
+        s.good.difficulty = self.init_difficulty(Rating.Good)
+        s.good.stability = self.init_stability(Rating.Good)
+        s.easy.difficulty = self.init_difficulty(Rating.Easy)
+        s.easy.stability = self.init_stability(Rating.Easy)
+
+    def next_ds(
+        self,
+        s: SchedulingCards,
+        last_d: float,
+        last_s: float,
+        retrievability: float,
+        state: State,
+    ) -> None:
+        s.again.difficulty = self.next_difficulty(last_d, Rating.Again)
+        s.hard.difficulty = self.next_difficulty(last_d, Rating.Hard)
+        s.good.difficulty = self.next_difficulty(last_d, Rating.Good)
+        s.easy.difficulty = self.next_difficulty(last_d, Rating.Easy)
+
+        if state == State.Learning or state == State.Relearning:
+            # compute short term stabilities
+            s.again.stability = self.short_term_stability(last_s, Rating.Again)
+            s.hard.stability = self.short_term_stability(last_s, Rating.Hard)
+            s.good.stability = self.short_term_stability(last_s, Rating.Good)
+            s.easy.stability = self.short_term_stability(last_s, Rating.Easy)
+
+        elif state == State.Review:
+            s.again.stability = self.next_forget_stability(
+                last_d, last_s, retrievability
+            )
+            s.hard.stability = self.next_recall_stability(
+                last_d, last_s, retrievability, Rating.Hard
+            )
+            s.good.stability = self.next_recall_stability(
+                last_d, last_s, retrievability, Rating.Good
+            )
+            s.easy.stability = self.next_recall_stability(
+                last_d, last_s, retrievability, Rating.Easy
+            )
+
+    def init_stability(self, r: Rating) -> float:
+        return max(self.p.w[r - 1], 0.1)
+
+    def init_difficulty(self, r: Rating) -> float:
+        # compute initial difficulty and clamp it between 1 and 10
+        return min(max(self.p.w[4] - math.exp(self.p.w[5] * (r - 1)) + 1, 1), 10)
+
+    def forgetting_curve(self, elapsed_days: int, stability: float) -> float:
+        return (1 + self.FACTOR * elapsed_days / stability) ** self.DECAY
+
+    def next_interval(self, s: float) -> int:
+        new_interval = (
+            s / self.FACTOR * (self.p.request_retention ** (1 / self.DECAY) - 1)
+        )
+        return min(max(round(new_interval), 1), self.p.maximum_interval)
+
+    def next_difficulty(self, d: float, r: Rating) -> float:
+        next_d = d - self.p.w[6] * (r - 3)
+
+        return min(
+            max(self.mean_reversion(self.init_difficulty(Rating.Easy), next_d), 1), 10
+        )
+
+    def short_term_stability(self, stability: float, rating: Rating) -> float:
+        return stability * math.exp(self.p.w[17] * (rating - 3 + self.p.w[18]))
+
+    def mean_reversion(self, init: float, current: float) -> float:
+        return self.p.w[7] * init + (1 - self.p.w[7]) * current
+
+    def next_recall_stability(
+        self, d: float, s: float, r: float, rating: Rating
+    ) -> float:
+        hard_penalty = self.p.w[15] if rating == Rating.Hard else 1
+        easy_bonus = self.p.w[16] if rating == Rating.Easy else 1
+        return s * (
+            1
+            + math.exp(self.p.w[8])
+            * (11 - d)
+            * math.pow(s, -self.p.w[9])
+            * (math.exp((1 - r) * self.p.w[10]) - 1)
+            * hard_penalty
+            * easy_bonus
+        )
+
+    def next_forget_stability(self, d: float, s: float, r: float) -> float:
+        return (
+            self.p.w[11]
+            * math.pow(d, -self.p.w[12])
+            * (math.pow(s + 1, self.p.w[13]) - 1)
+            * math.exp((1 - r) * self.p.w[14])
+        )
---
+++ packages/fsrs/fsrs-3.1.0.tar.gz/fsrs-3.1.0/tests/test_fsrs.py
@@ -0,0 +1,340 @@
+from fsrs import FSRS, Card, ReviewLog, State, Rating
+from datetime import datetime, timedelta, timezone
+import json
+import pytest
+
+test_w = (
+    0.4197,
+    1.1869,
+    3.0412,
+    15.2441,
+    7.1434,
+    0.6477,
+    1.0007,
+    0.0674,
+    1.6597,
+    0.1712,
+    1.1178,
+    2.0225,
+    0.0904,
+    0.3025,
+    2.1214,
+    0.2498,
+    2.9466,
+    0.4891,
+    0.6468,
+)
+
+
+class TestPyFSRS:
+    def test_review_card(self):
+        f = FSRS(w=test_w)
+        card = Card()
+        now = datetime(2022, 11, 29, 12, 30, 0, 0, timezone.utc)
+
+        ratings = (
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Again,
+            Rating.Again,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+        )
+        ivl_history = []
+
+        for rating in ratings:
+            card, _ = f.review_card(card, rating, now)
+            ivl = card.scheduled_days
+            ivl_history.append(ivl)
+            now = card.due
+
+        print(ivl_history)
+        assert ivl_history == [
+            0,
+            4,
+            17,
+            62,
+            198,
+            563,
+            0,
+            0,
+            9,
+            27,
+            74,
+            190,
+            457,
+        ]
+
+    def test_memo_state(self):
+        f = FSRS(w=test_w)
+        card = Card()
+        now = datetime(2022, 11, 29, 12, 30, 0, 0, timezone.utc)
+
+        scheduling_cards = f.repeat(card, now)
+        ratings = (
+            Rating.Again,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+        )
+        ivl_history = [0, 0, 1, 3, 8, 21]
+        for rating, ivl in zip(ratings, ivl_history):
+            card = scheduling_cards[rating].card
+            now += timedelta(days=ivl)
+            scheduling_cards = f.repeat(card, now)
+
+        assert round(scheduling_cards[Rating.Good].card.stability, 4) == 71.4554
+        assert round(scheduling_cards[Rating.Good].card.difficulty, 4) == 5.0976
+
+    def test_repeat_default_arg(self):
+        f = FSRS()
+
+        card_object = Card()
+
+        # repeat time is not specified
+        scheduling_cards = f.repeat(card_object)
+
+        card_rating = Rating.Good
+
+        card_object = scheduling_cards[card_rating].card
+
+        due = card_object.due
+
+        time_delta = due - datetime.now(timezone.utc)
+
+        assert time_delta.seconds > 500  # due in approx. 8-10 minutes
+
+    def test_datetime(self):
+        f = FSRS()
+        card = Card()
+
+        # new cards should be due immediately after creation
+        assert datetime.now(timezone.utc) >= card.due
+
+        # comparing timezone aware cards with deprecated datetime.utcnow() should raise a TypeError
+        with pytest.raises(TypeError):
+            datetime.now() >= card.due
+
+        # repeating a card with a non-utc, non-timezone-aware datetime object should raise a Value Error
+        with pytest.raises(ValueError):
+            f.repeat(card, datetime(2022, 11, 29, 12, 30, 0, 0))
+
+        # repeat a card with rating good before next tests
+        scheduling_cards = f.repeat(card, datetime.now(timezone.utc))
+        card = scheduling_cards[Rating.Good].card
+
+        # card object's due and last_review attributes must be timezone aware and UTC
+        assert card.due.tzinfo == timezone.utc
+        assert card.last_review.tzinfo == timezone.utc
+        # card object's due datetime should be later than its last review
+        assert card.due >= card.last_review
+
+    def test_Card_serialize(self):
+        f = FSRS()
+
+        # create card object the normal way
+        card = Card()
+
+        # card object is not naturally JSON serializable
+        with pytest.raises(TypeError):
+            json.dumps(card.__dict__)
+
+        # card object's to_dict() method makes it JSON serializable
+        assert type(json.dumps(card.to_dict())) == str
+
+        # we can reconstruct a copy of the card object equivalent to the original
+        card_dict = card.to_dict()
+        copied_card = Card.from_dict(card_dict)
+
+        assert vars(card) == vars(copied_card)
+        assert card.to_dict() == copied_card.to_dict()
+
+        # (x2) perform the above tests once more with a repeated card
+        scheduling_cards = f.repeat(card, datetime.now(timezone.utc))
+        repeated_card = scheduling_cards[Rating.Good].card
+
+        with pytest.raises(TypeError):
+            json.dumps(repeated_card.__dict__)
+
+        assert type(json.dumps(repeated_card.to_dict())) == str
+
+        repeated_card_dict = repeated_card.to_dict()
+        copied_repeated_card = Card.from_dict(repeated_card_dict)
+
+        assert vars(repeated_card) == vars(copied_repeated_card)
+        assert repeated_card.to_dict() == copied_repeated_card.to_dict()
+
+        # original card and repeated card are different
+        assert vars(card) != vars(repeated_card)
+        assert card.to_dict() != repeated_card.to_dict()
+
+    def test_ReviewLog_serialize(self):
+        f = FSRS()
+
+        card = Card()
+
+        # repeat a card to get the review_log
+        scheduling_cards = f.repeat(card)
+        rating = Rating.Again
+        card = scheduling_cards[rating].card
+        review_log = scheduling_cards[rating].review_log
+
+        # ReviewLog object is not naturally JSON serializable
+        with pytest.raises(TypeError):
+            json.dumps(review_log.__dict__)
+
+        # review_log object's to_dict() method makes it JSON serializable
+        assert type(json.dumps(review_log.to_dict())) == str
+
+        # we can reconstruct a copy of the review_log object equivalent to the original
+        review_log_dict = review_log.to_dict()
+        copied_review_log = ReviewLog.from_dict(review_log_dict)
+        assert vars(review_log) == vars(copied_review_log)
+        assert review_log.to_dict() == copied_review_log.to_dict()
+
+        # (x2) perform the above tests once more with a review_log from a repeated card
+        scheduling_cards = f.repeat(card, datetime.now(timezone.utc))
+        rating = Rating.Good
+        card = scheduling_cards[rating].card
+        next_review_log = scheduling_cards[rating].review_log
+
+        with pytest.raises(TypeError):
+            json.dumps(next_review_log.__dict__)
+
+        assert type(json.dumps(next_review_log.to_dict())) == str
+
+        next_review_log_dict = next_review_log.to_dict()
+        copied_next_review_log = ReviewLog.from_dict(next_review_log_dict)
+
+        assert vars(next_review_log) == vars(copied_next_review_log)
+        assert next_review_log.to_dict() == copied_next_review_log.to_dict()
+
+        # original review log and next review log are different
+        assert vars(review_log) != vars(next_review_log)
+        assert review_log.to_dict() != next_review_log.to_dict()
+
+    def test_custom_scheduler_args(self):
+        f = FSRS(
+            w=(
+                0.4197,
+                1.1869,
+                3.0412,
+                15.2441,
+                7.1434,
+                0.6477,
+                1.0007,
+                0.0674,
+                1.6597,
+                0.1712,
+                1.1178,
+                2.0225,
+                0.0904,
+                0.3025,
+                2.1214,
+                0.2498,
+                2.9466,
+                0,
+                0.6468,
+            ),
+            request_retention=0.9,
+            maximum_interval=36500,
+        )
+        card = Card()
+        now = datetime(2022, 11, 29, 12, 30, 0, 0, timezone.utc)
+
+        ratings = (
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Again,
+            Rating.Again,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+            Rating.Good,
+        )
+        ivl_history = []
+
+        for rating in ratings:
+            card, _ = f.review_card(card, rating, now)
+            ivl = card.scheduled_days
+            ivl_history.append(ivl)
+            now = card.due
+
+        print(ivl_history)
+        assert ivl_history == [0, 3, 13, 50, 163, 473, 0, 0, 12, 34, 91, 229, 541]
+
+        # initialize another scheduler and verify parameters are properly set
+        w2 = (
+            0.1456,
+            0.4186,
+            1.1104,
+            4.1315,
+            5.2417,
+            1.3098,
+            0.8975,
+            0.0000,
+            1.5674,
+            0.0567,
+            0.9661,
+            2.0275,
+            0.1592,
+            0.2446,
+            1.5071,
+            0.2272,
+            2.8755,
+            1.234,
+            5.6789,
+        )
+        request_retention2 = 0.85
+        maximum_interval2 = 3650
+        f2 = FSRS(
+            w=w2,
+            request_retention=request_retention2,
+            maximum_interval=maximum_interval2,
+        )
+
+        assert f2.p.w == w2
+        assert f2.p.request_retention == request_retention2
+        assert f2.p.maximum_interval == maximum_interval2
+
+    def test_retrievability(self):
+        f = FSRS()
+
+        card = Card()
+
+        # retrievabiliy of New card
+        assert card.state == State.New
+        retrievability = card.get_retrievability()
+        assert retrievability == 0
+
+        # retrievabiliy of Learning card
+        card, _ = f.review_card(card, Rating.Good)
+        assert card.state == State.Learning
+        retrievability = card.get_retrievability()
+        assert 0 <= retrievability <= 1
+
+        # retrievabiliy of Review card
+        card, _ = f.review_card(card, Rating.Good)
+        assert card.state == State.Review
+        retrievability = card.get_retrievability()
+        assert 0 <= retrievability <= 1
+
+        # retrievabiliy of Relearning card
+        card, _ = f.review_card(card, Rating.Again)
+        assert card.state == State.Relearning
+        retrievability = card.get_retrievability()
+        assert 0 <= retrievability <= 1
