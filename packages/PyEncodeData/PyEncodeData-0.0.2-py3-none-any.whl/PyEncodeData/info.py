16f530f6025f59b08b33ea2639c8d9529122ee3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:38 +0000

    Add package PyEncodeData/0.0.2/PyEncodeData-0.0.2-py3-none-any.whl

---
+++ packages/PyEncodeData/PyEncodeData-0.0.2-py3-none-any.whl/PyEncodeData/info.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Here gonna be something special ..."""
+
+# (C) Rafal Urniaz
+
+
+def info():
+    """Here gonna be something special ...
+
+    """
+    
+    print("Py.encode(DATA)")
