5317dc94e2cc98cc4ebaeef3c38bd4aa532a3060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:10 +0000

    Add package thoughtful-temp-test-package/2.8.0.1/thoughtful_temp_test_package-2.8.0.1-py3-none-any.whl

---
+++ packages/thoughtful-temp-test-package/thoughtful_temp_test_package-2.8.0.1-py3-none-any.whl/thoughtful/supervisor/event_bus.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+from typing import Callable, List, Optional, Union
+from venv import logger
+
+from thoughtful.supervisor.manifest import Manifest
+from thoughtful.supervisor.reporting.status import Status
+from thoughtful.supervisor.reporting.step_report import StepReport
+
+
+@dataclass
+class StepReportChangeEvent:
+    step_report: StepReport
+
+
+@dataclass
+class NewManifestEvent:
+    manifest: Manifest
+
+
+@dataclass
+class RunStatusChangeEvent:
+    status: Status
+    status_message: Optional[str] = None
+
+
+@dataclass
+class ArtifactsUploadedEvent:
+    output_uri: str
+
+
+@dataclass
+class StartProcessEvent:
+    process_id: int
+    inputs: dict
+
+
+Event = Union[
+    StepReportChangeEvent,
+    NewManifestEvent,
+    RunStatusChangeEvent,
+    ArtifactsUploadedEvent,
+    StartProcessEvent,
+]
+
+
+@dataclass
+class EventBus:
+    subscribers: List[Callable[[Event], None]] = field(default_factory=list)
+
+    def subscribe(self, callback: Callable[[Event], None]):
+        self.subscribers.append(callback)
+
+    def emit(self, event: Event):
+        logger.info(f"emit event: {event}")
+        for subscriber in self.subscribers:
+            subscriber(event)
