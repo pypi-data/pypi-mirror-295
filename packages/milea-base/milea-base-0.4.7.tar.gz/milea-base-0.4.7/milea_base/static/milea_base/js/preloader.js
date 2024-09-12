1a71eb4908155caf44eec592e2d109eab467056c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:56 +0000

    Add package milea-base/0.4.7/milea-base-0.4.7.tar.gz

---
+++ packages/milea-base/milea-base-0.4.7.tar.gz/milea-base-0.4.7/milea_base/static/milea_base/js/preloader.js
@@ -0,0 +1,22 @@
+// Hide preloader when page loaded
+window.onload = function () {
+    var preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+};
+
+// Funktion, um den Preloader dyanmisch anzuzeigen
+function showPreloader() {
+    var preloader = document.getElementById('preloader');
+    preloader.style.display = 'flex';
+}
+
+// Event Listener für alle Links und Buttons
+document.addEventListener('click', function (e) {
+    var target = e.target;
+    if (
+        (target.tagName === 'A' || target.tagName === 'BUTTON' || target.type === 'submit') &&
+        target.getAttribute('data-bs-toggle') !== 'dropdown'
+    ) {
+        preloaderTimeout = setTimeout(showPreloader, 500);
+    }
+});
