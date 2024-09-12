b33611ed8568933ff7ab02415ceec4d035ee3535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:34 +0000

    Add package carta-notifications-email-grpc-client/0.0.1.dev1726090057/carta-notifications-email-grpc-client-0.0.1.dev1726090057.tar.gz

---
+++ packages/carta-notifications-email-grpc-client/carta-notifications-email-grpc-client-0.0.1.dev1726090057.tar.gz/carta-notifications-email-grpc-client-0.0.1.dev1726090057/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-notifications-email-grpc-client',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
