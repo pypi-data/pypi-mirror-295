9dd8cf1ec6d32873cad1c742971008f920c23db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:06:50 +0000

    Add package MeUtils/2024.9.12.17.8.19/MeUtils-2024.9.12.17.8.19-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.9.12.17.8.19-py3-none-any.whl/meutils/apis/chatglm/glm_video_api.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : video
+# @Time         : 2024/7/26 12:03
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
