90bc9b1e67db3cd617cfa22e9b483951ef107eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:07:07 +0000

    Add package ailab100/0.0.1/ailab100-0.0.1.tar.gz

---
+++ packages/ailab100/ailab100-0.0.1.tar.gz/ailab100-0.0.1/README.md
@@ -0,0 +1,12 @@
+
+# ailab100
+
+
+A small example package
+
+
+安装
+
+```python
+pip install -i https://pypi.Python.org/simple/ ailab100
+```
---
+++ packages/ailab100/ailab100-0.0.1.tar.gz/ailab100-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ailab100',
+    version='0.0.1',
+    packages=['pdbc','financial'],
+    url='',
+    license='',
+    author='AI Lab 100',
+    author_email='',
+    description='',
+    classifiers=['Intended Audience :: Developers','Operating System :: OS Independent',
+                 'Natural Language :: Chinese (Simplified)','Programming Language :: Python',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8','Topic :: Software Development :: Libraries'],
+
+)
