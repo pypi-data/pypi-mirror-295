c57c303a7ccfd9c45eca6822d7b7e8702209451a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:02 +0000

    Add package owlapp/0.3.12/owlapp-0.3.12-py3-none-any.whl

---
+++ packages/owlapp/owlapp-0.3.12-py3-none-any.whl/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.base import db
+from app.models.database import Database
+from app.models.datafile import DataFile
+from app.models.script import Script
+from app.models.user import User
+
+__all__ = [db, Database, DataFile, User, Script]
