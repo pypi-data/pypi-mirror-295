30a777517cfc09504bf350e4c3c65c18b66021da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 06:05:51 +0000

    Add package pytest-cleanuptotal/0.3.0/pytest_cleanuptotal-0.3.0.tar.gz

---
+++ packages/pytest-cleanuptotal/pytest_cleanuptotal-0.3.0.tar.gz/pytest_cleanuptotal-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytest-cleanuptotal',
+    packages=['pytest_cleanuptotal'],
+    package_dir={'pytest_cleanuptotal': 'src/pytest_cleanuptotal'},
+    version='0.3.0',
+    author='Tzur Paldi',
+    author_email='tzur.paldi@outlook.com',
+    maintainer='Tzur Paldi',
+    maintainer_email='tzur.paldi@outlook.com',
+    license='MIT',
+    url='https://github.com/tzurp/pytest_cleanuptotal',
+    description='A cleanup plugin for pytest',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest_cleanuptotal = pytest_cleanuptotal.cleanuptotal',
+        ],
+    },
+)
