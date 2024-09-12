130250590ebf7abab620b6cf5932799be4dea4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:04:58 +0000

    Add package openadapt/0.40.0/openadapt-0.40.0-py3-none-any.whl

---
+++ packages/openadapt/openadapt-0.40.0-py3-none-any.whl/openadapt/common.py
@@ -0,0 +1,22 @@
+"""This module defines common constants used in OpenAdapt."""
+
+RAW_MOUSE_EVENTS = (
+    "move",
+    "click",
+    "scroll",
+)
+FUSED_MOUSE_EVENTS = (
+    "singleclick",
+    "doubleclick",
+)
+MOUSE_EVENTS = tuple(list(RAW_MOUSE_EVENTS) + list(FUSED_MOUSE_EVENTS))
+MOUSE_CLICK_EVENTS = (event for event in MOUSE_EVENTS if event.endswith("click"))
+
+RAW_KEY_EVENTS = (
+    "press",
+    "release",
+)
+FUSED_KEY_EVENTS = ("type",)
+KEY_EVENTS = tuple(list(RAW_KEY_EVENTS) + list(FUSED_KEY_EVENTS))
+
+ALL_EVENTS = tuple(list(MOUSE_EVENTS) + list(KEY_EVENTS))
