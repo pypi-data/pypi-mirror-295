1387ec7d818c89b2f56c0b02d65209d2dcdaa211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:06:39 +0000

    Add package py-zerox/0.0.3/py_zerox-0.0.3.tar.gz

---
+++ packages/py-zerox/py_zerox-0.0.3.tar.gz/py_zerox-0.0.3/py_zerox/zerox/errors/__init__.py
@@ -0,0 +1,19 @@
+from .exceptions import (
+    NotAVisionModel,
+    ModelAccessError,
+    MissingEnvironmentVariables,
+    ResourceUnreachableException,
+    FileUnavailable,
+    FailedToSaveFile,
+    FailedToProcessFile,
+)
+
+__all__ = [
+    "NotAVisionModel",
+    "ModelAccessError",
+    "MissingEnvironmentVariables",
+    "ResourceUnreachableException",
+    "FileUnavailable",
+    "FailedToSaveFile",
+    "FailedToProcessFile",
+]
