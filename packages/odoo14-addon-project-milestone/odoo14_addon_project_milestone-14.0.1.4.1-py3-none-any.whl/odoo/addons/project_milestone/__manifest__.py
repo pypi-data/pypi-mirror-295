479a1c199766c3e3ac3311cc4490a37cc96a2559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:07:12 +0000

    Add package odoo14-addon-project-milestone/14.0.1.4.1/odoo14_addon_project_milestone-14.0.1.4.1-py3-none-any.whl

---
+++ packages/odoo14-addon-project-milestone/odoo14_addon_project_milestone-14.0.1.4.1-py3-none-any.whl/odoo/addons/project_milestone/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Milestones",
+    "summary": "Project Milestones",
+    "author": "Patrick Wilson, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "category": "Project Management",
+    "version": "14.0.1.4.1",
+    "license": "AGPL-3",
+    "depends": ["mail", "project"],
+    "data": [
+        "views/project.xml",
+        "views/project_task.xml",
+        "views/project_milestone.xml",
+        "security/ir.model.access.csv",
+    ],
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["patrickrwilson"],
+}
