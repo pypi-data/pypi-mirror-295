6a5b868542f661c6396e1b4764f2baf4b7e65872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:02 +0000

    Add package yaroslavslogger/0.7.0/yaroslavslogger-0.7.0.tar.gz

---
+++ packages/yaroslavslogger/yaroslavslogger-0.7.0.tar.gz/yaroslavslogger-0.7.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yaroslavslogger',
+    version='0.7.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple package with a greeting function',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['compiled_libraries/*.so'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
