f25f72436ef57797b72ae637b9d00712eab1bebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:36 +0000

    Add package carta-json-logic/0.0.1.dev1726094318/carta-json-logic-0.0.1.dev1726094318.tar.gz

---
+++ packages/carta-json-logic/carta-json-logic-0.0.1.dev1726094318.tar.gz/carta-json-logic-0.0.1.dev1726094318/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-json-logic',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
