f59d56f74d37f2475f48329d0446f2c889a2ae53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:03 +0000

    Add package getoptify/0.0.3/getoptify-0.0.3-py3-none-any.whl

---
+++ packages/getoptify/getoptify-0.0.3-py3-none-any.whl/getoptify/__init__.py
@@ -0,0 +1,44 @@
+import functools as _functools
+import getopt as _getopt
+import sys as _sys
+
+__all__ = ["command", "decorator", "process"]
+
+
+def command(*_args, **_kwargs):
+    return _functools.partial(decorator, *_args, **_kwargs)
+
+
+def decorator(old, /, *_args, **_kwargs):
+    @_functools.wraps(old)
+    def new(args=None):
+        args = process(args, *_args, **_kwargs)
+        return old(args)
+
+    return new
+
+
+def process(args=None, shortopts="", longopts=[], allow_argv=True, gnu=True):
+    if allow_argv and args is None:
+        args = _sys.argv[1:]
+    args = [str(x) for x in args]
+    shortopts = str(shortopts)
+    longopts = [str(x) for x in longopts]
+    if gnu:
+        g = _getopt.gnu_getopt
+    else:
+        g = _getopt.getopt
+    pairlist, poslist = g(args=args, shortopts=shortopts, longopts=longopts)
+    ans = []
+    for k, v in pairlist:
+        if not k.startswith("--"):
+            ans.append(k + v)
+        elif v != "":
+            ans.append(k + "=" + v)
+        elif k[2:] in longopts:
+            ans.append(k)
+        else:
+            ans.append(k + "=")
+    ans.append("--")
+    ans += poslist
+    return ans
