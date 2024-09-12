3f445326570a854914093eb218544e6890783178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:06:25 +0000

    Add package mm-solana/0.1.1/mm_solana-0.1.1.tar.gz

---
+++ packages/mm-solana/mm_solana-0.1.1.tar.gz/mm_solana-0.1.1/tests/test_utils.py
@@ -0,0 +1,16 @@
+from decimal import Decimal
+
+from mm_solana import utils
+from mm_solana.rpc import DEFAULT_MAINNET_RPC
+
+
+def test_lamports_to_sol():
+    res = utils.lamports_to_sol(272356343007, ndigits=4)
+    assert res == Decimal("272.3563")
+
+
+def test_get_node():
+    assert utils.get_node() == DEFAULT_MAINNET_RPC
+    assert utils.get_node("n1") == "n1"
+    res = utils.get_node(["n1", "n2"])
+    assert res == "n1" or res == "n2"
