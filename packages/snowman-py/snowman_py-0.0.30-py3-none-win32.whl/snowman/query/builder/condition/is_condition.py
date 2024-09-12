f5791fac05f11e3ab9b5c231cfe2107b1b447e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:26 +0000

    Add package snowman-py/0.0.30/snowman_py-0.0.30-py3-none-win32.whl

---
+++ packages/snowman-py/snowman_py-0.0.30-py3-none-win32.whl/snowman/query/builder/condition/is_condition.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from typing_extensions import override
+
+from snowman._generic import PyType
+from snowman.query.builder.condition.condition import Condition, ConditionWithParams
+
+if TYPE_CHECKING:
+    from snowman.query.column import Column
+
+
+class IsCondition(Condition):
+    def __init__(self, base: "Column[PyType]"):
+        self._base = base
+
+    @override
+    def to_sql(self) -> ConditionWithParams:
+        return ConditionWithParams(condition=f"{self._base} IS NULL", params=())
