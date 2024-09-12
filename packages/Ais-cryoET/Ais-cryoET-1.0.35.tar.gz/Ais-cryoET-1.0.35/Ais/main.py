c10c434492db4b0de676132bfd0e0ee9d0441fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:07:10 +0000

    Add package Ais-cryoET/1.0.35/Ais-cryoET-1.0.35.tar.gz

---
+++ packages/Ais-cryoET/Ais-cryoET-1.0.35.tar.gz/Ais-cryoET-1.0.35/Ais/main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import Ais.core.config as cfg
+from Ais.core.window import *
+from Ais.core.segmentation_editor import *
+from imgui.integrations.glfw import GlfwRenderer
+import tkinter as tk
+
+
+tkroot = tk.Tk()
+tkroot.withdraw()
+
+directory = os.path.join(os.path.dirname(__file__))
+directory = directory[:directory.rfind("\\")]
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.append(directory)
+cfg.root = os.path.join(os.path.dirname(__file__))
+
+
+def main():
+    if not glfw.init():
+        raise Exception("Could not initialize GLFW library!")
+
+    # Init the main window, its imgui context, and a glfw rendering impl.
+    main_window = Window(cfg.window_width, cfg.window_height, settings.ne_window_title)
+    main_window.set_callbacks()
+    main_window_imgui_context = imgui.create_context()
+    main_window_imgui_glfw_implementation = GlfwRenderer(main_window.glfw_window)
+    main_window.set_mouse_callbacks()
+    main_window.set_window_callbacks()
+
+    # set up editor
+    segmentation_editor = SegmentationEditor(main_window, main_window_imgui_context, main_window_imgui_glfw_implementation)
+    segmentation_editor.force_not_embedded()
+    cfg.segmentation_editor = segmentation_editor
+    main_window.set_icon(segmentation_editor.ICON)
+
+    from Ais.core import start
+
+    while not glfw.window_should_close(main_window.glfw_window):
+        if not main_window.focused:
+            glfw.poll_events()
+        segmentation_editor.on_update()
+        segmentation_editor.end_frame()
+
+
+def windowless():
+    if not glfw.init():
+        raise Exception("Could not initialize GLFW library for headless start!")
+    glfw.window_hint(glfw.VISIBLE, False)
+    window = glfw.create_window(1, 1, "invisible window", None, None)
+    if not window:
+        glfw.terminate()
+        raise Exception("Could not create invisible window!")
+    glfw.make_context_current(window)
+    return window
+
+
+if __name__ == "__main__":
+    main()
+
+
