dfb1f9fe0e237b2aa7264ec2620e390362559e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:31 +0000

    Add package inspiring-quotes/0.6.0/inspiring_quotes-0.6.0.tar.gz

---
+++ packages/inspiring-quotes/inspiring_quotes-0.6.0.tar.gz/inspiring_quotes-0.6.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inspiring_quotes',
+    version='0.6.0',
+    description='A simple Python package for retrieving random inspirational quotes.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='cyberdeeb',
+    author_email='abrahamdeeb@gmail.com',
+    url='https://github.com/cyberdeeb/inspiring-quotes',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='quotes inspirational motivational wisdom positivity self-improvement',
+    python_requires='>=3',
+)
