a65a8202c674571ba9ec386763c1a3de0999a77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:34 +0000

    Add package carta-events-proto-events/0.0.1.dev1726090009/carta-events-proto-events-0.0.1.dev1726090009.tar.gz

---
+++ packages/carta-events-proto-events/carta-events-proto-events-0.0.1.dev1726090009.tar.gz/carta-events-proto-events-0.0.1.dev1726090009/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-events-proto-events',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
