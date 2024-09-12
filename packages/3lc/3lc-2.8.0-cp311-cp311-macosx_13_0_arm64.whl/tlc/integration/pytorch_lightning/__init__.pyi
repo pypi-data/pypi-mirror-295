0bbcf88a3a0aa5ff89b99292b7ecb7ac06ece6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:23 +0000

    Add package 3lc/2.8.0/3lc-2.8.0-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/3lc/3lc-2.8.0-cp311-cp311-macosx_13_0_arm64.whl/tlc/integration/pytorch_lightning/__init__.pyi
@@ -0,0 +1,1 @@
+from .module_decorator import lightning_module as lightning_module
---
+++ packages/3lc/3lc-2.8.0-cp311-cp311-macosx_13_0_arm64.whl/tlcsaas/pyarmor_runtime_005329/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.11 (pro), 005329, 2024-09-10T20:40:08.769470
+from .pyarmor_runtime import __pyarmor__
