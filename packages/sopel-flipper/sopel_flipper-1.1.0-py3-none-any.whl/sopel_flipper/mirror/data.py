411fc1efb85a364c28657813d7a5410e8c1e4945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:06:58 +0000

    Add package sopel-flipper/1.1.0/sopel_flipper-1.1.0-py3-none-any.whl

---
+++ packages/sopel-flipper/sopel_flipper-1.1.0-py3-none-any.whl/sopel_flipper/mirror/data.py
@@ -0,0 +1,88 @@
+"""Data for mirror submodule
+
+Part of sopel-flipper.
+
+Copyright 2024 dgw, technobabbl.es
+"""
+
+# picked my favorite options from various existing mirror-text generators
+# top 3 sources in alphabetical order by URL:
+# https://lingojam.com/MirrorYourText
+# https://twiki.org/cgi-bin/view/Blog/BlogEntry201211x1
+# https://www.messletters.com/en/flip-text/
+REPLACEMENTS = {
+    ';': '⁏',
+    '?': '⸮',
+    '(': ')',
+    ')': '(',
+    '<': '>',
+    '>': '<',
+    '[': ']',
+    ']': '[',
+    '{': '}',
+    '}': '{',
+    '/': '\\',
+    '\\': '/',
+    '0': '0',
+    '1': 'ᛚ',
+    '2': '𐑕',   # need better 2
+    '3': 'Ɛ',
+    '4': '𐊀',
+    '5': 'ट',
+    '6': 'მ',
+    '7': '٢',
+    '8': '8',
+    '9': '୧',
+    'A': 'A',
+    'B': 'ꓭ',
+    'C': 'Ɔ',
+    'D': 'ꓷ',
+    'E': 'Ǝ',
+    'F': 'ᖷ',
+    'G': 'Ә',  # need better G
+    'H': 'H',
+    'I': 'I',
+    'J': 'Ⴑ',
+    'K': 'ꓘ',
+    'L': '⅃',
+    'M': 'M',
+    'N': 'И',
+    'O': 'O',
+    'P': 'ꟼ',
+    'Q': 'Ϙ',  # need better Q
+    'R': 'Я',
+    'S': 'Ƨ',
+    'T': 'T',
+    'U': 'U',
+    'V': 'V',
+    'W': 'W',
+    'X': 'X',
+    'Y': 'Y',
+    'Z': 'Ƹ',  # need better Z
+    'a': 'ɒ',
+    'b': 'd',
+    'c': 'ɔ',
+    'd': 'b',
+    'e': 'ɘ',
+    'f': 'ʇ',
+    'g': 'ϱ',
+    'h': '⑁',
+    'i': 'i',
+    'j': 'į',  # or Ⴑ
+    'k': 'ʞ',
+    'l': 'l',
+    'm': 'm',
+    'n': 'n',
+    'o': 'o',
+    'p': 'q',
+    'q': 'p',
+    'r': 'ɿ',
+    's': 'ƨ',
+    't': 'Ɉ',
+    'u': 'u',
+    'v': 'v',
+    'w': 'w',
+    'x': 'x',
+    'y': 'ჸ',
+    'z': 'z',   # REALLY need better z
+}
