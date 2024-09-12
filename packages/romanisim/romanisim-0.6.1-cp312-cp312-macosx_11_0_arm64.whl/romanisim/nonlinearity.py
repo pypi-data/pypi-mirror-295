4bddd88db385219fdc8853da2f30056b84b0970a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:40 +0000

    Add package romanisim/0.6.1/romanisim-0.6.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/romanisim/romanisim-0.6.1-cp312-cp312-macosx_11_0_arm64.whl/romanisim/nonlinearity.py
@@ -0,0 +1,158 @@
+"""Routines to handle non-linearity in simulating ramps.
+
+The approach taken here is straightforward.  The detector is accumulating
+photons, but the capacitance of the pixel varies with flux level and so
+the mapping between accumulated photons and read-out digital numbers
+changes with flux level.  The CRDS linearity and inverse-linearity reference
+files describe the mapping between linear DN and observed DN.  This module
+implements that mapping.  When simulating an image, the photons entering
+each pixel are simulated, and then before being "read out" into a buffer,
+are transformed with this mapping into observed counts.  These are then
+averaged and emitted as resultants.
+"""
+
+import numpy as np
+from astropy import units as u
+from romanisim import parameters
+
+
+def repair_coefficients(coeffs, dq):
+    """Fix cases of zeros and NaNs in non-linearity coefficients.
+
+    This function replaces suspicious-looking non-linearity coefficients with
+    no-op coefficients from a non-linearity perspective; all coefficients are
+    zero except for the linear term, which is set to 1.
+
+    This function doesn't try to make sure that the derivative of the
+    correction is greater than 1, which we would expect for a non-linearity
+    correction.
+
+    Parameters
+    ----------
+    coeffs : np.ndarray[ncoeff, nx, ny] (float)
+        Nonlinearity coefficients, starting with the constant term and
+        increasing in power.
+
+    dq : np.ndarray[n_resultant, nx, ny]
+        Data Quality array
+
+    Returns
+    -------
+    coeffs : np.ndarray[ncoeff, nx, ny] (float)
+        "repaired" coefficients with NaNs and weird coefficients replaced with
+        linear values with slopes of unity.
+
+    dq : np.ndarray[n_resultant, nx, ny]
+        DQ array marking pixels with improper non-linearity coefficients
+    """
+    res = coeffs.copy()
+
+    nocorrection = np.zeros(coeffs.shape[0], dtype=coeffs.dtype)
+    nocorrection[1] = 1.  # "no correction" is just normal linearity.
+    # For NaN, all zero, or flagged pixels, reset to no linearity correction.
+    m = (np.any(~np.isfinite(coeffs), axis=0) | np.all(coeffs == 0, axis=0)
+         | (dq != 0))
+    res[:, m] = nocorrection[:, None]
+
+    lin_dq_array = np.zeros(coeffs.shape[1:], dtype=np.uint32)
+    lin_dq_array[m] = parameters.dqbits['no_lin_corr']
+    dq = np.bitwise_or(dq, lin_dq_array)
+    return res, dq
+
+
+def evaluate_nl_polynomial(counts, coeffs, reversed=False):
+    """Correct the observed counts for non-linearity.
+
+    As photons arrive, they make it harder for the device to count
+    future photons due to classical non-linearity.  This function
+    converts some observed counts to what would have been seen absent
+    non-linearity given some non-linearity corrections described by
+    polynomials with given coefficients.
+
+    Parameters
+    ----------
+    counts : np.ndarray[nx, ny] (float)
+        Number of counts already in pixel
+    coeffs : np.ndarray[ncoeff, nx, ny] (float)
+        Coefficients of the non-linearity correction polynomials
+    reversed : bool
+        If True, the coefficients are in reversed order, which is the
+        order that np.polyval wants them.  One can maybe save a little
+        time reversing them once ahead of time.
+
+    Returns
+    -------
+    corrected : np.ndarray[nx, ny] (float)
+        The corrected number of counts
+    """
+    if reversed:
+        cc = coeffs
+    else:
+        cc = coeffs[::-1, ...]
+
+    if isinstance(counts, u.Quantity):
+        unit = counts.unit
+        counts = counts.value
+    else:
+        unit = None
+
+    res = np.polyval(cc, counts)
+
+    if unit is not None:
+        res = res * unit
+
+    return res
+
+
+class NL:
+    """Keep track of non-linearity and inverse non-linearity coefficients.
+
+    """
+    def __init__(self, coeffs, dq=None, gain=None):
+        """Construct an NL class handling non-linearity correction.
+
+        Parameters
+        ----------
+        coeffs : np.ndarray[ncoeff, nx, ny] (float)
+            Non-linearity coefficients from reference files.
+
+        dq : np.ndarray[n_resultant, nx, ny]
+            Data Quality array
+
+        gain : float or np.ndarray[float]
+            Gain (electrons / count) for converting counts to electrons
+        """
+        if dq is None:
+            dq = np.zeros(coeffs.shape[1:], dtype='uint32')
+        if gain is None:
+            gain = parameters.reference_data['gain'].to(u.electron / u.DN).value
+        self.coeffs, self.dq = repair_coefficients(coeffs, dq)
+        self.gain = gain
+
+    def apply(self, counts, electrons=False, reversed=False):
+        """Compute the correction of observed to true counts
+
+        Parameters
+        ----------
+        counts : np.ndarray[nx, ny] (float)
+            The observed counts
+
+        electrons : bool
+            Set to True for 'counts' being in electrons, with coefficients
+            designed for DN. Accrdingly, the gain needs to be removed and
+            reapplied.
+
+        reversed : bool
+            If True, the coefficients are in reversed order, which is the
+            order that np.polyval wants them.  One can maybe save a little
+            time reversing them once ahead of time.
+
+        Returns
+        -------
+        corrected : np.ndarray[nx, ny] (float)
+¯            The corrected counts.
+        """
+        if electrons:
+            return self.gain * evaluate_nl_polynomial(counts / self.gain, self.coeffs, reversed)
+
+        return evaluate_nl_polynomial(counts, self.coeffs, reversed)
---
+++ packages/romanisim/romanisim-0.6.1-cp312-cp312-macosx_11_0_arm64.whl/romanisim/parameters.py
@@ -0,0 +1,157 @@
+"""Parameters class storing a few useful constants for Roman simulations.
+"""
+
+import numpy as np
+from astropy.time import Time
+from astropy import units as u
+
+read_pattern = {1: [[1 + x for x in range(8)],
+                    [9 + x for x in range(8)],
+                    [17 + x for x in range(8)],
+                    [25 + x for x in range(8)],
+                    [33 + x for x in range(8)],
+                    [41 + x for x in range(8)]],
+                2: [[1 + x for x in range(5)],
+                    [6 + x for x in range(8)],
+                    [14],
+                    [15 + x for x in range(9)],
+                    [24 + x for x in range(25)]],
+                3: [[1 + x for x in range(25)],
+                    [26 + x for x in range(8)],
+                    [34],
+                    [35 + x for x in range(14)]],
+                109: [[1], [2, 3], [5, 6, 7], [10, 11, 12, 13],
+                      [15, 16, 17, 18, 19, 20], [21, 22, 23, 24, 25, 26],
+                      [27, 28, 29, 30, 31, 32], [33, 34, 35, 36, 37, 38],
+                      [39, 40, 41, 42, 43], [44]],
+                110: [[1], [2, 3, 4], [5, 6, 7], [8, 9, 10], [11, 12, 13],
+                      [14, 15, 16], [17, 18, 19], [20, 21, 22], [23, 24, 25],
+                      [26, 27, 28], [29, 30, 31], [32, 33, 34], [35, 36, 37],
+                      [38, 39, 40], [41, 42, 43], [44]],
+                }
+
+default_parameters_dictionary = {
+    'instrument': {'name': 'WFI',
+                   'detector': 'WFI07',
+                   'optical_element': 'F184',
+                   },
+    'exposure': {'start_time': Time('2026-01-01T00:00:00'),
+                 'type': 'WFI_IMAGE',
+                 'ma_table_number': 1,
+                 'read_pattern': read_pattern[1],
+                 },
+    'pointing': {'ra_v1': 270.0,
+                 'dec_v1': 66.0,
+                 },
+    'wcsinfo': {'ra_ref': 270.0,
+                'dec_ref': 66.0,
+                'v2_ref': 0,
+                'v3_ref': 0,
+                'roll_ref': 0,
+                'vparity': -1,
+                'v3yangle': -60.0,
+                # I don't know what vparity and v3yangle should really be,
+                # but they are always -1 and -60 in existing files.
+                },
+    'aperture': {'name': 'WFI_CEN',
+                 'position_angle': 0
+                 },
+}
+
+# Default metadata for level 3 mosaics
+default_mosaic_parameters_dictionary = {
+    'basic': {'time_mean_mjd': Time('2026-01-01T00:00:00').mjd,
+              'optical_element': 'F184',
+              },
+    'wcsinfo': {'ra_ref': 270.0,
+                'dec_ref': 66.0,
+                },
+}
+
+reference_data = {
+    "dark": 0.01 * u.electron / u.s,
+    "distortion": None,
+    "flat": None,
+    "gain": 2 * u.electron / u.DN,
+    "inverselinearity": None,
+    "linearity": None,
+    "readnoise": 5.0 * u.DN,
+    "saturation": 55000 * u.DN,
+}
+
+nborder = 4  # number of border pixels used for reference pixels.
+
+read_time = 3.04
+
+# from draft wfisim documentation
+# assumed to be compatible in direction with scipy.ndimage.convolve.
+# this is consistent with Andrea Bellini's convention, where, in the
+# following kernel, 0.2% of the flux is redistributed to the pixel
+# that is +1 - N spaces ahead in memory.
+ipc_kernel = np.array(
+    [[0.21, 1.66, 0.22],
+     [1.88, 91.59, 1.87],
+     [0.21, 1.62, 0.2]])
+ipc_kernel /= np.sum(ipc_kernel)
+
+# V2/V3 coordinates of "center" of WFI array (convention)
+v2v3_wficen = (1546.3846181707652, -892.7916365721071)  # arcsec
+
+# persistence parameter dictionary
+# delete persistence records fainter than 0.01 electron / s
+# e.g., MA table 1 has ~144 s, so this is ~1 electron over the whole exposure.
+persistence = dict(A=0.017, x0=6.0e4, dx=5.0e4, alpha=0.045, gamma=1,
+                   half_well=50000, ignorerate=0.01)
+
+# arbitrary constant to add to initial L1 image so that pixels aren't clipped at zero.
+pedestal = 100 * u.DN
+
+# Addd this much extra noise as correlated extra noise in all resultants.
+pedestal_extra_noise = 4 * u.DN
+
+dqbits = dict(saturated=2, jump_det=4, nonlinear=2**16, no_lin_corr=2**20)
+dq_do_not_use = dqbits['saturated'] | dqbits['jump_det']
+
+NUMBER_OF_DETECTORS = 18
+
+# Radial distance from WFI_CEN to extend object generation in order to cover FOV (degrees)
+WFS_FOV = 0.6
+
+# Cosmic Ray defaults
+cr = {
+    # sample_cr_params
+    "min_dEdx": 10,
+    "max_dEdx": 10000,
+    "min_cr_len": 10,
+    "max_cr_len": 2000,
+    "grid_size": 10000,
+    # simulate_crs
+    "flux": 8,
+    "area": 16.8,
+    "conversion_factor": 0.5,
+    "pixel_size": 10,
+    "pixel_depth": 5,
+}
+
+# Centermost PSF to use for mosaic creation
+default_sca = 2
+
+# Persistence defaults
+# delete persistence records fainter than 0.01 electron / s
+# e.g., MA table 1 has ~144 s, so this is ~1 electron over the whole exposure.
+persistence = {
+    # init
+    "A": 0.017,
+    "x0": 6.0e4,
+    "dx": 5.0e4,
+    "alpha": 0.045,
+    "gamma": 1,
+    "half_well": 50000,
+    "ignorerate": 0.01
+}
+
+# angle of V3 relative to +Y
+V3IdlYAngle = -60
+
+# fiducial WFI pixel scale in arcseconds
+pixel_scale = 0.11
