60081107e4315ad84a30e3346835c64eefd1b2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:07:17 +0000

    Add package stonks/0.1.5/stonks-0.1.5-py3-none-any.whl

---
+++ packages/stonks/stonks-0.1.5-py3-none-any.whl/stonks/__init__.py
@@ -0,0 +1,8 @@
+from stonks.stonks import run_stonks_analysis
+from stonks.version import version
+
+__version__ = version
+__author__ = "Daniel Lasota"
+__description__ = "Script that scrapes market Data from Yahoo Finance Api and performs base analysis with plotting"
+__email__ = "grossmann.root@gmail.com"
+__url__ = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
