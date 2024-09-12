2cfd634389e40afb6b444eef2300a3176f8478c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:06:50 +0000

    Add package wukongbaobao/1.0.7/wukongbaobao-1.0.7.tar.gz

---
+++ packages/wukongbaobao/wukongbaobao-1.0.7.tar.gz/wukongbaobao-1.0.7/src/wukongbaobao/__init__.py
@@ -0,0 +1,5 @@
+'''Wukongbaobao is a program that helps parents flexibly generate test questions for their children. The tool is written in Python.'''
+
+__version__ = '1.0.7'
+
+from . import mathematics
