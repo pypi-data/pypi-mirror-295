443b60ff7133dd7eee116e89aa66ec652b5d470d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:07:02 +0000

    Add package llm-context/0.0.10/llm_context-0.0.10.tar.gz

---
+++ packages/llm-context/llm_context-0.0.10.tar.gz/llm_context-0.0.10/src/llm_context/context_generator.py
@@ -0,0 +1,140 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+import pyperclip  # type: ignore
+from jinja2 import Environment, FileSystemLoader
+
+from llm_context.config_manager import ConfigManager
+from llm_context.folder_structure_diagram import get_fs_diagram
+
+
+def _format_size(size_bytes):
+    for unit in ["B", "KB", "MB", "GB"]:
+        if size_bytes < 1024.0:
+            return f"{size_bytes:.1f} {unit}"
+        size_bytes /= 1024.0
+    return f"{size_bytes:.1f} TB"
+
+
+@dataclass(frozen=True)
+class Template:
+    name: str
+    context: dict
+    env: Environment
+
+    @staticmethod
+    def create(name: str, context: dict, templates_path) -> "Template":
+        env = Environment(loader=FileSystemLoader(str(templates_path)))
+        return Template(name, context, env)
+
+    def render(self) -> str:
+        template = self.env.get_template(self.name)
+        return template.render(**self.context)
+
+
+@dataclass(frozen=True)
+class PathConverter:
+    root: Path
+
+    def validate(self, paths: list[str]) -> bool:
+        return all(path.startswith(f"/{self.root.name}/") for path in paths)
+
+    def to_absolute(self, relative_paths: list[str]) -> list[str]:
+        if not self.validate(relative_paths):
+            raise ValueError("Invalid paths provided")
+        return [str(self.root / Path(path[len(self.root.name) + 2 :])) for path in relative_paths]
+
+
+@dataclass(frozen=True)
+class ContextGenerator:
+    config_manager: ConfigManager
+
+    @staticmethod
+    def create() -> "ContextGenerator":
+        return ContextGenerator(ConfigManager.create_default())
+
+    def _context(
+        self, file_paths: list[str], fs_diagram: str | None = None, summary: str | None = None
+    ) -> dict:
+        root_name = os.path.basename(self.config_manager.project_root_path())
+        items = [
+            {
+                "path": f"/{root_name}/{Path(path).relative_to(self.config_manager.project_root_path())}",
+                "content": Path(path).read_text(),
+            }
+            for path in file_paths
+        ]
+        return (
+            {"items": items}
+            | ({"folder_structure_diagram": fs_diagram} if fs_diagram is not None else {})
+            | ({"summary": summary} if summary is not None else {})
+        )
+
+    def files(self, file_paths: list[str]) -> str:
+        template_name = self.config_manager.project["templates"]["selfiles"]
+        context = self._context(file_paths)
+        return self._render(template_name, context)
+
+    def context(self, file_paths: list[str], fs_diagram: str, summary: str | None) -> str:
+        template_name = self.config_manager.project["templates"]["context"]
+        context = self._context(file_paths, fs_diagram, summary)
+        return self._render(template_name, context)
+
+    def _render(self, template_name: str, context: dict) -> str:
+        template = Template.create(template_name, context, self.config_manager.templates_path())
+        return template.render()
+
+
+def _files(in_files: list[str] = []) -> str:
+    context_generator = ContextGenerator.create()
+    cm = context_generator.config_manager
+    path_converter = PathConverter(cm.project_root_path())
+    if not in_files and not path_converter.validate(in_files):
+        print("Invalid file paths")
+        return ""
+    files = cm.get_files() if not in_files else path_converter.to_absolute(in_files)
+    return context_generator.files(files)
+
+
+def _context() -> str:
+    context_generator = ContextGenerator.create()
+    files = context_generator.config_manager.get_files()
+    fs_diagram = get_fs_diagram()
+    summary = context_generator.config_manager.get_summary()
+    return context_generator.context(files, fs_diagram, summary)
+
+
+def size_feedback(content: str) -> None:
+    if content is None:
+        print("No content to copy")
+    else:
+        bytes_copied = len(content.encode("utf-8"))
+        print(f"Copied {_format_size(bytes_copied)} to clipboard")
+
+
+def files_from_scratch() -> None:
+    text = _files()
+    pyperclip.copy(text)
+    size_feedback(text)
+
+
+def files_from_clip() -> None:
+    files = pyperclip.paste().strip().split("\n")
+    text = _files(files)
+    pyperclip.copy(text)
+    size_feedback(text)
+
+
+def context():
+    text = _context()
+    pyperclip.copy(text)
+    size_feedback(text)
+
+
+def main():
+    files_from_scratch()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/llm-context/llm_context-0.0.10.tar.gz/llm_context-0.0.10/src/llm_context/file_selector.py
@@ -0,0 +1,104 @@
+import os
+from dataclasses import dataclass
+
+from pathspec import GitIgnoreSpec
+
+from llm_context.config_manager import ConfigManager
+
+
+@dataclass(frozen=True)
+class PathspecIgnorer:
+    pathspec: GitIgnoreSpec
+
+    @staticmethod
+    def create(ignore_patterns: list[str]) -> "PathspecIgnorer":
+        pathspec = GitIgnoreSpec.from_lines(ignore_patterns)
+        return PathspecIgnorer(pathspec)
+
+    def ignore(self, path: str) -> bool:
+        assert path not in ("/", ""), "Root directory cannot be an input for ignore method"
+        return self.pathspec.match_file(path)
+
+
+@dataclass(frozen=True)
+class GitIgnorer:
+    ignorer_data: list[tuple[str, PathspecIgnorer]]
+
+    @staticmethod
+    def from_git_root(root_dir: str, xtra_root_patterns: list[str] = []) -> "GitIgnorer":
+        ignorer_data = [("/", PathspecIgnorer.create(xtra_root_patterns))]
+        gitignores = GitIgnorer._collect_gitignores(root_dir)
+        for relative_path, patterns in gitignores:
+            ignorer_data.append((relative_path, PathspecIgnorer.create(patterns)))
+        return GitIgnorer(ignorer_data)
+
+    @staticmethod
+    def _collect_gitignores(top) -> list[tuple[str, list[str]]]:
+        gitignores = []
+        for root, _, files in os.walk(top):
+            if ".gitignore" in files:
+                with open(os.path.join(root, ".gitignore"), "r") as file:
+                    patterns = file.read().splitlines()
+                relpath = os.path.relpath(root, top)
+                fixpath = "/" if relpath == "." else f"/{os.path.relpath(root, top)}"
+                gitignores.append((fixpath, patterns))
+        return gitignores
+
+    def ignore(self, path: str) -> bool:
+        assert path not in ("/", ""), "Root directory cannot be an input for ignore method"
+        for prefix, ignorer in self.ignorer_data:
+            if path.startswith(prefix):
+                if ignorer.ignore(path):
+                    return True
+        return False
+
+
+@dataclass(frozen=True)
+class FileSelector:
+    config_manager: ConfigManager
+    ignorer: GitIgnorer
+
+    @staticmethod
+    def create(pathspecs: list[str] | None = None) -> "FileSelector":
+        config_manager = ConfigManager.create_default()
+        if pathspecs is None:
+            pathspecs = config_manager.project["gitignores"]
+        git_ignorer = GitIgnorer.from_git_root(config_manager.project_root_path(), pathspecs)
+        return FileSelector(config_manager, git_ignorer)
+
+    def traverse(self, current_dir: str) -> list[str]:
+        entries = os.listdir(current_dir)
+        root_path = self.config_manager.project_root()
+        relative_current_dir = os.path.relpath(current_dir, root_path)
+        dirs = [
+            e_path
+            for e in entries
+            if (e_path := os.path.join(current_dir, e))
+            and os.path.isdir(e_path)
+            and not self.ignorer.ignore(f"/{os.path.join(relative_current_dir, e)}/")
+        ]
+        files = [
+            e_path
+            for e in entries
+            if (e_path := os.path.join(current_dir, e))
+            and not os.path.isdir(e_path)
+            and not self.ignorer.ignore(f"/{os.path.join(relative_current_dir, e)}")
+        ]
+        subdir_files = [file for d in dirs for file in self.traverse(d)]
+        return files + subdir_files
+
+    def get_all(self) -> list[str]:
+        return self.traverse(self.config_manager.project_root_path())
+
+    def update_selected(self) -> None:
+        all_files = self.get_all()
+        self.config_manager.select_files(all_files)
+
+
+def main():
+    select_files = FileSelector.create()
+    select_files.update_selected()
+
+
+if __name__ == "__main__":
+    main()
