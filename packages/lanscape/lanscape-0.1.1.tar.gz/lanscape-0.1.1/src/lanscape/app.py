b45694f4d6b4daa2759de5047326deedd8ac9521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:14 +0000

    Add package lanscape/0.1.1/lanscape-0.1.1.tar.gz

---
+++ packages/lanscape/lanscape-0.1.1.tar.gz/lanscape-0.1.1/src/lanscape/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+# Import Blueprints
+from .blueprints.api import api_bp
+from .blueprints.web import web_bp
+
+# Register Blueprints
+app.register_blueprint(api_bp)
+app.register_blueprint(web_bp)
+
+    
+# Custom Jinja filter
+def is_substring_in_values(results: dict, substring: str) -> bool:
+    return any(substring.lower() in str(v).lower() for v in results.values()) if substring else True
+
+app.jinja_env.filters['is_substring_in_values'] = is_substring_in_values
+
+def start_webserver(debug: bool=True, port: int=5001) -> int:
+    if not debug:
+        disable_flask_logging()
+    app.run(host='0.0.0.0', port=port, debug=debug)
+
+def disable_flask_logging() -> None:
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    app.logger.disabled = True
+
+if __name__ == "__main__":
+    start_webserver(True)
