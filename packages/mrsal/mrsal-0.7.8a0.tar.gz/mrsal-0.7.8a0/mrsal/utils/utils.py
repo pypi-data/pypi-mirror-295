e00e6c5ca42608ea2732134d9ae22cfbc37089f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:14 +0000

    Add package mrsal/0.7.8a0/mrsal-0.7.8a0.tar.gz

---
+++ packages/mrsal/mrsal-0.7.8a0.tar.gz/mrsal-0.7.8a0/mrsal/utils/utils.py
@@ -0,0 +1,10 @@
+import pika
+
+from mrsal.config import config
+
+
+def is_redelivery_configured(msg_prop: pika.spec.BasicProperties):
+    if hasattr(msg_prop, config.MESSAGE_HEADERS_KEY):
+        headers = msg_prop.headers
+        return headers is not None and config.RETRY_LIMIT_KEY in headers and config.RETRY_KEY in headers
+    return False
