a245baaeb04489c43ea877dc722ad3ebd7f09fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 04:05:12 +0000

    Add package galigeopy/0.0.22/galigeopy-0.0.22.tar.gz

---
+++ packages/galigeopy/galigeopy-0.0.22.tar.gz/galigeopy-0.0.22/galigeopy/utils/spherical_calculus.py
@@ -0,0 +1,17 @@
+# Haversine distance formula
+from math import asin, cos, radians, sin, sqrt
+
+def haversine_distance(lat1, lon1, lat2, lon2, rayon=6371000):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    # haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    km = rayon * c
+    return km
