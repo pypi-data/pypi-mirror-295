7fd5278bb5912dc4bf50c5d4095d5d22e65c9f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 05:06:10 +0000

    Add package automsg/0.0.1/automsg-0.0.1.tar.gz

---
+++ packages/automsg/automsg-0.0.1.tar.gz/automsg-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="automsg",
+  version="0.0.1",
+  author="Chase",
+  description="Automated message support - Changes coming soon",
+  packages=["automsg"]
+)
