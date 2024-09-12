cc4f8d5176500164dd040bfc7189c7db8289cacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 03:06:23 +0000

    Add package dduk-application/0.0.83/dduk_application-0.0.83-py3-none-any.whl

---
+++ packages/dduk-application/dduk_application-0.0.83-py3-none-any.whl/dduk/application/internalrepository.py
@@ -0,0 +1,15 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Any, Final, Callable, Iterator, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
+import builtins
+from dduk.core.repository import Repository as BaseRepository
+
+
+#--------------------------------------------------------------------------------
+# 내부 저장소 클래스.
+#--------------------------------------------------------------------------------
+class InternalRepository(BaseRepository):
+	pass
+	
