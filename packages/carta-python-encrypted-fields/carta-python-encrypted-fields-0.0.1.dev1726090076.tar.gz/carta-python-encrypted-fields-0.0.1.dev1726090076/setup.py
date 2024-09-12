18b83b388f4f8c38cf4cd278b418879b600ac31b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 07:05:35 +0000

    Add package carta-python-encrypted-fields/0.0.1.dev1726090076/carta-python-encrypted-fields-0.0.1.dev1726090076.tar.gz

---
+++ packages/carta-python-encrypted-fields/carta-python-encrypted-fields-0.0.1.dev1726090076.tar.gz/carta-python-encrypted-fields-0.0.1.dev1726090076/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-python-encrypted-fields',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
