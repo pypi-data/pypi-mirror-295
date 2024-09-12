6984981744d99ad71a76a6e312273be8cd63e291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 14:04:54 +0000

    Add package fasthtml_extn/0.0.6/fasthtml_extn-0.0.6-py3-none-any.whl

---
+++ packages/fasthtml_extn/fasthtml_extn-0.0.6-py3-none-any.whl/fasthtml_extn/deploy.py
@@ -0,0 +1,20 @@
+import logging
+from typing import Callable, Type
+from fasthtml.core import to_xml
+from fasthtml_extn.libraries.tailwind import ExtnHtml
+from fasthtml_extn.utils import AppPageDirectory, get_metacall_app_path
+
+logger = logging.getLogger("fasthtml_extn_metacall")
+
+
+def render_fasthtml_factory(
+    path: str, pages: AppPageDirectory
+) -> Type[Callable[..., object]]:
+    def app():
+        p = path
+        if not p.startswith("/"):
+            p = f"/{p}"
+        res = get_metacall_app_path(p, pages)
+        return to_xml(ExtnHtml(res))
+
+    return app
