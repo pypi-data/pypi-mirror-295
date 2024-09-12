e4ece9805f3e1d0e772c14bc9df61f851cce8ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 16:06:53 +0000

    Add package lavish-and-shubham/0.2/lavish_and_shubham-0.2.tar.gz

---
+++ packages/lavish-and-shubham/lavish_and_shubham-0.2.tar.gz/lavish_and_shubham-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="lavish_and_shubham",
+    version=0.2,
+    packages=find_packages(),
+    description="Lavish and Shubham boss ka package ha."
+)
