95ceb9540ac7a1a96c87d9cc8af95baf682a8201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-12 15:05:39 +0000

    Add package amsdal_utils/0.1.4/amsdal_utils-0.1.4-py3-none-any.whl

---
+++ packages/amsdal_utils/amsdal_utils-0.1.4-py3-none-any.whl/amsdal_utils/models/utils/reference_builders.py
@@ -0,0 +1,44 @@
+from amsdal_utils.classes.version_manager import ClassVersionManager
+from amsdal_utils.config.manager import AmsdalConfigManager
+from amsdal_utils.models.data_models.reference import Reference
+from amsdal_utils.models.data_models.reference import ReferenceData
+from amsdal_utils.models.enums import Versions
+
+
+def build_reference(
+    class_name: str,
+    object_id: str,
+    class_version: str | Versions = Versions.LATEST,
+    object_version: str | Versions = Versions.LATEST,
+    resource: str | None = None,
+) -> Reference:
+    """
+    Builds a reference object.
+
+    Args:
+        class_name (str): The name of the class.
+        object_id (str): The ID of the object.
+        class_version (str | Versions, optional): The version of the class. Defaults to Versions.LATEST.
+        object_version (str | Versions, optional): The version of the object. Defaults to Versions.LATEST.
+        resource (str | None, optional): The resource name. Defaults to None.
+
+    Returns:
+        Reference: The constructed reference object.
+    """
+    connection_name = resource or AmsdalConfigManager().get_connection_name_by_model_name(class_name)
+
+    if class_version == Versions.LATEST:
+        class_version = ClassVersionManager().get_latest_class_version(class_name).version
+
+        if not class_version:
+            class_version = Versions.LATEST
+
+    return Reference(
+        ref=ReferenceData(
+            resource=connection_name,
+            class_name=class_name,
+            class_version=class_version,
+            object_id=object_id,
+            object_version=object_version,
+        ),
+    )
