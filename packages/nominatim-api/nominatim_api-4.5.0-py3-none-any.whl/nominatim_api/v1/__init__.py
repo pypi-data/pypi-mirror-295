a9e92ddcd1f2ffadd493657d708ecffbe9e56f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:07:12 +0000

    Add package nominatim-api/4.5.0/nominatim_api-4.5.0-py3-none-any.whl

---
+++ packages/nominatim-api/nominatim_api-4.5.0-py3-none-any.whl/nominatim_api/v1/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This file is part of Nominatim. (https://nominatim.org)
+#
+# Copyright (C) 2024 by the Nominatim developer community.
+# For a full list of authors see the git log.
+"""
+Implementation of API version v1 (aka the legacy version).
+"""
+
+#pylint: disable=useless-import-alias
+
+from .server_glue import ROUTES as ROUTES
