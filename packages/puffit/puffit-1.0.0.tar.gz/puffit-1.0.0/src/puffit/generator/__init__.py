f919181b3ea0e4324136e1edd9770018449b89dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:25 +0000

    Add package puffit/1.0.0/puffit-1.0.0.tar.gz

---
+++ packages/puffit/puffit-1.0.0.tar.gz/puffit-1.0.0/src/puffit/generator/__init__.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+
+class ProjectGenerator:
+    # TODO Add docstrings to ProjectGenerator class
+    #  assignees: thechainercygnus
+    #  labels: enhancement, documentation
+    def __init__(self, directory_structure: dict):
+        self.directory_structure = directory_structure
+
+    def create_scaffold(self, base_path, structure=None):
+        if structure is None:
+            structure = self.directory_structure
+
+        for name, content in structure.items():
+            path = os.path.join(base_path, name)
+            if isinstance(content, dict):
+                Path(path).mkdir(parents=True, exist_ok=True)
+                self.create_scaffold(path, content)
+            else:
+                Path(path).touch()
