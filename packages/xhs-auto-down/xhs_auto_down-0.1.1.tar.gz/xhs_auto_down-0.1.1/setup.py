d636d73ff7b144e9c718ae06049168ed9a3adf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:06:32 +0000

    Add package xhs-auto-down/0.1.1/xhs_auto_down-0.1.1.tar.gz

---
+++ packages/xhs-auto-down/xhs_auto_down-0.1.1.tar.gz/xhs_auto_down-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xhs_auto_down',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出依赖包
+        'textual<=0.63.0',
+        'pyperclip>=1.9.0',
+        'lxml>=5.3.0',
+        'PyYAML>=6.0.2',
+        'aiosqlite>=0.20.0',
+        'click>=8.1.7',
+        'rookiepy>=0.5.2',
+        'httpx>=0.27.0',
+        'fastapi>=0.112.1',
+        'uvicorn>=0.30.6',
+        'aiofiles>=24.1.0',
+        'emoji>=2.12.1',
+    ],
+    author='bauyin',
+    author_email='your_email@example.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your_package_name',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
---
+++ packages/xhs-auto-down/xhs_auto_down-0.1.1.tar.gz/xhs_auto_down-0.1.1/xhs_auto_down/expansion/browser.py
@@ -0,0 +1,61 @@
+from contextlib import suppress
+
+from rookiepy import (
+    arc,
+    brave,
+    chrome,
+    chromium,
+    edge,
+    firefox,
+    librewolf,
+    opera,
+    # opera_gx,
+    # safari,
+    vivaldi,
+)
+
+__all__ = ["BrowserCookie"]
+
+
+class BrowserCookie:
+    SUPPORT_BROWSER = {
+        "arc": arc,
+        "chrome": chrome,
+        "chromium": chromium,
+        "opera": opera,
+        # "opera_gx": opera_gx,
+        "brave": brave,
+        "edge": edge,
+        "vivaldi": vivaldi,
+        "firefox": firefox,
+        "librewolf": librewolf,
+        # "safari": safari,
+    }
+
+    @classmethod
+    def get(cls, browser: str | int, domains: list[str]) -> str:
+        if not (browser := cls.__browser_object(browser)):
+            print("浏览器名称或序号输入错误！")
+            return ""
+        try:
+            cookies = browser(domains=domains)
+            return "; ".join(f"{i["name"]}={i["value"]}" for i in cookies)
+        except RuntimeError:
+            print("获取 Cookie 失败，未找到 Cookie 数据！")
+        return ""
+
+    @classmethod
+    def __browser_object(cls, browser: str | int):
+        with suppress(ValueError):
+            browser = int(browser) - 1
+        if isinstance(browser, int):
+            try:
+                return list(cls.SUPPORT_BROWSER.values())[browser - 1]
+            except IndexError:
+                return None
+        if isinstance(browser, str):
+            try:
+                return cls.SUPPORT_BROWSER[browser.lower()]
+            except KeyError:
+                return None
+        raise TypeError
