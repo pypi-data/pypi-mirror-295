d9365ba2734fdc12bde69cf53a6dc7bf8114edf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:32 +0000

    Add package HyperTech-STT/0.1/HyperTech_STT-0.1-py3-none-any.whl

---
+++ packages/HyperTech-STT/HyperTech_STT-0.1-py3-none-any.whl/MAIN.py
@@ -0,0 +1,25 @@
+from TextToSpeech import Fast_DF_TTS
+from HyperTech_STT import listen
+import threading
+
+
+def check():
+    output_text = ""
+    while True:
+        try:
+            with open("input.txt",'r') as file:
+                input_text = file.read().lower().strip()
+                if input_text != output_text:
+                    output_text = input_text
+                    if output_text:
+                        Fast_DF_TTS.speak(output_text)
+        except FileNotFoundError:
+            pass
+
+
+t1 = threading.Thread(target=listen)
+t2 = threading.Thread(target=check)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
