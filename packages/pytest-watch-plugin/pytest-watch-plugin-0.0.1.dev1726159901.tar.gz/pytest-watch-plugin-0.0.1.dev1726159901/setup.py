65f0c0995303380b414185010c7d3343298b91d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:39 +0000

    Add package pytest-watch-plugin/0.0.1.dev1726159901/pytest-watch-plugin-0.0.1.dev1726159901.tar.gz

---
+++ packages/pytest-watch-plugin/pytest-watch-plugin-0.0.1.dev1726159901.tar.gz/pytest-watch-plugin-0.0.1.dev1726159901/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='pytest-watch-plugin',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
