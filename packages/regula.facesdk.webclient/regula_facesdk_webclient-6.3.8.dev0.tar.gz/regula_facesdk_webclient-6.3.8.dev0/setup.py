c13a2ff8b8efe6f64dfdc6e4d962ac47173c2fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:17 +0000

    Add package regula.facesdk.webclient/6.3.8.dev0/regula_facesdk_webclient-6.3.8.dev0.tar.gz

---
+++ packages/regula.facesdk.webclient/regula_facesdk_webclient-6.3.8.dev0.tar.gz/regula_facesdk_webclient-6.3.8.dev0/setup.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="regula.facesdk.webclient",
+    version=os.getenv("PACKAGE_VERSION_TO_PUBLISH", "5.1dev"),
+    python_requires=">=3.5",
+    description="Regula's FaceSDK web python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Regula Forensics, Inc.",
+    author_email="support@regulaforensics.com",
+    url="https://mobile.regulaforensics.com",
+    keywords=["face recognition", "facesdk", "regulaforensics", "regula"],
+    install_requires=[
+        "certifi>=2023.7.22",
+        "six>=1.10",
+        "python-dateutil>=2.5.3",
+        "urllib3>=1.15.1"
+    ],
+    packages=find_packages(exclude=["test", "tests", "example"]),
+    include_package_data=True,
+)
