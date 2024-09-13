cc747b69539364a118ed6221d493c7e58fe6acc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:55 +0000

    Add package carta-data-science/0.0.1.dev1726163572/carta-data-science-0.0.1.dev1726163572.tar.gz

---
+++ packages/carta-data-science/carta-data-science-0.0.1.dev1726163572.tar.gz/carta-data-science-0.0.1.dev1726163572/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-data-science',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
