b9ec47944e4cd780df9ca120c36930deb57de0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:49 +0000

    Add package lqlpath/0.1.0/lqlpath-0.1.0.tar.gz

---
+++ packages/lqlpath/lqlpath-0.1.0.tar.gz/lqlpath-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="lqlpath",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies if needed
+    description="A package for searching log query language path ",
+    author="Harish Lohiya",
+    author_email="harishlohiya@gmail.com",
+    url="https://github.com/logiconapp/lqlpath/",  # Replace with your repository
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
