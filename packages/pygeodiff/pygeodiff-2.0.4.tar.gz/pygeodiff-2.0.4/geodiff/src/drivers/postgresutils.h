3157d0aa75652ee763f8be70bc75670e61b728d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:07:13 +0000

    Add package pygeodiff/2.0.4/pygeodiff-2.0.4.tar.gz

---
+++ packages/pygeodiff/pygeodiff-2.0.4.tar.gz/pygeodiff-2.0.4/geodiff/src/drivers/postgresutils.h
@@ -0,0 +1,77 @@
+/*
+ GEODIFF - MIT License
+ Copyright (C) 2020 Martin Dobias
+*/
+
+#ifndef POSTGRESUTILS_H
+#define POSTGRESUTILS_H
+
+#include <assert.h>
+#include <string>
+
+extern "C"
+{
+#include <libpq-fe.h>
+}
+
+
+class PostgresResult
+{
+  public:
+    explicit PostgresResult( PGresult *result ) : mResult( result ) {}
+    ~PostgresResult()
+    {
+      if ( mResult )
+        ::PQclear( mResult );
+      mResult = nullptr;
+    }
+
+    ExecStatusType status() const
+    {
+      return mResult ? ::PQresultStatus( mResult ) : PGRES_FATAL_ERROR;
+    }
+
+    std::string statusErrorMessage() const
+    {
+      assert( mResult );
+      return ::PQresultErrorMessage( mResult );
+    }
+
+    int rowCount() const
+    {
+      assert( mResult );
+      return ::PQntuples( mResult );
+    }
+
+    std::string affectedRows() const
+    {
+      assert( mResult );
+      return ::PQcmdTuples( mResult );
+    }
+
+    std::string value( int row, int col ) const
+    {
+      assert( mResult );
+      return isNull( row, col )
+             ? std::string()
+             : std::string( ::PQgetvalue( mResult, row, col ) );
+    }
+
+    bool isNull( int row, int col ) const
+    {
+      assert( mResult );
+      return ::PQgetisnull( mResult, row, col );
+    }
+
+  private:
+    PGresult *mResult = nullptr;
+
+};
+
+PGresult *execSql( PGconn *c, const std::string &sql );
+
+std::string quotedIdentifier( const std::string &ident );
+std::string quotedString( const std::string &value );
+
+
+#endif // POSTGRESUTILS_H
---
+++ packages/pygeodiff/pygeodiff-2.0.4.tar.gz/pygeodiff-2.0.4/geodiff/src/geodiffcontext.hpp
@@ -0,0 +1,32 @@
+/*
+ GEODIFF - MIT License
+ Copyright (C) 2020 Peter Petrik
+*/
+
+#ifndef GEODIFFCONTEXT_H
+#define GEODIFFCONTEXT_H
+
+#include <string>
+#include <vector>
+
+#include "geodiff.h"
+#include "geodifflogger.hpp"
+
+class Context
+{
+  public:
+    Context();
+
+    Logger &logger();
+    const Logger &logger() const;
+
+    void setTablesToSkip( const std::vector<std::string> &tablesToSkip );
+    bool isTableSkipped( const std::string &tableName ) const;
+
+  private:
+    Logger mLogger;
+    std::vector<std::string> mTablesToSkip;
+};
+
+
+#endif // GEODIFFCONTEXT_H
