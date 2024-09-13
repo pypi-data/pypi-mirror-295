59de374f504d56764b1f2f359638c901135aa38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:06:43 +0000

    Add package nutree/0.9.0/nutree-0.9.0-py3-none-any.whl

---
+++ packages/nutree/nutree-0.9.0-py3-none-any.whl/nutree/__init__.py
@@ -0,0 +1,53 @@
+"""
+Current version number.
+
+See https://www.python.org/dev/peps/pep-0440
+
+Examples
+    Pre-releases (alpha, beta, release candidate):
+        '3.0.0a1', '3.0.0b1', '3.0.0rc1'
+    Final Release:
+        '3.0.0'
+    Developmental release (to mark 3.0.0 as 'used'. Don't publish this):
+        '3.0.0.dev1'
+NOTE:
+    When pywin32 is installed, number must be a.b.c for MSI builds?
+    "3.0.0a4" seems not to work in this case!
+"""
+
+# flake8: noqa
+__version__ = "0.9.0"
+
+from .common import (
+    AmbiguousMatchError,
+    GenericNodeData,
+    IterMethod,
+    SelectBranch,
+    SkipBranch,
+    StopTraversal,
+    TreeError,
+    UniqueConstraintError,
+)
+from .diff import DiffClassification, diff_node_formatter
+from .fs import load_tree_from_fs
+from .node import Node
+from .tree import Tree
+from .typed_tree import TypedNode, TypedTree
+
+__all__ = [
+    Tree,
+    Node,
+    AmbiguousMatchError,
+    diff_node_formatter,
+    DiffClassification,
+    GenericNodeData,
+    IterMethod,
+    load_tree_from_fs,
+    SelectBranch,
+    SkipBranch,
+    StopTraversal,
+    TreeError,
+    TypedNode,
+    TypedTree,
+    UniqueConstraintError,
+]
