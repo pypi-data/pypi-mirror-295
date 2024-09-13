29211443a99dac6b270bc24debb5184fda62d352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:06:18 +0000

    Add package polyhedral-gravity/3.2.1/polyhedral_gravity-3.2.1.tar.gz

---
+++ packages/polyhedral-gravity/polyhedral_gravity-3.2.1.tar.gz/polyhedral_gravity-3.2.1/cmake/xsimd.cmake
@@ -0,0 +1,24 @@
+include(FetchContent)
+
+message(STATUS "Setting up xsimd via CMake")
+
+
+find_package(xsimd 11.1 QUIET)
+
+if (${xsimd_FOUND})
+
+    message(STATUS "Using existing xsimd installation")
+
+else()
+
+    message(STATUS "Using xsimd from git repository")
+
+    #Fetches the version 11.1.0 from the official github of tbb
+    FetchContent_Declare(xsimd
+            GIT_REPOSITORY https://github.com/xtensor-stack/xsimd.git
+            GIT_TAG 11.1.0
+            )
+
+    FetchContent_MakeAvailable(xsimd)
+
+endif()
---
+++ packages/polyhedral-gravity/polyhedral_gravity-3.2.1.tar.gz/polyhedral_gravity-3.2.1/src/polyhedralGravity/util/UtilityConstants.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace polyhedralGravity::util {
+
+    /**
+     * PI with enough precision
+     */
+    constexpr double PI = 3.1415926535897932384626433832795028841971693993751058209749445923;
+
+
+    /**
+    * PI multiplied by 2 with enough precision
+    */
+    constexpr double PI2 = 6.2831853071795864769252867665590057683943387987502116419498891846;
+
+
+    /**
+    * PI divided by 2 with enough precision
+    */
+    constexpr double PI_2 = 1.5707963267948966192313216916397514420985846996875529104874722961;
+
+    /**
+     * The gravitational constant G in [m^3/(kg*s^2)].
+     * @related in his paper above Equation (4)
+     */
+    constexpr double GRAVITATIONAL_CONSTANT = 6.67430e-11;
+
+    /**
+     * The assumed constant density rho for a polyhedron after Tsoulis paper in @f$[kg/m^3]@f$.
+     * @related in his paper above Equation (4)
+     */
+    constexpr double DEFAULT_CONSTANT_DENSITY = 2670.0;
+
+}
