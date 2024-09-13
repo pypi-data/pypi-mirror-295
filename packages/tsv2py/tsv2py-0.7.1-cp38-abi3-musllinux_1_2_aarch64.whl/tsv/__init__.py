ce871eecaa8ed8e460cd6ac26e964169302fd775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 06:04:41 +0000

    Add package tsv2py/0.7.1/tsv2py-0.7.1-cp38-abi3-musllinux_1_2_aarch64.whl

---
+++ packages/tsv2py/tsv2py-0.7.1-cp38-abi3-musllinux_1_2_aarch64.whl/tsv/__init__.py
@@ -0,0 +1,13 @@
+"""
+tsv2py: Parse and generate tab-separated values (TSV) data
+
+This package offers a high-performance alternative to convert data between a TSV text file and Python objects.
+The parser can read a TSV record into a Python tuple consisting of built-in Python types, one for each field.
+The generator can produce a TSV record from a tuple.
+"""
+
+__version__ = "0.7.1"
+__author__ = "Levente Hunyadi"
+__copyright__ = "Copyright 2023-2024, Levente Hunyadi"
+__license__ = "MIT"
+__maintainer__ = "Levente Hunyadi"
