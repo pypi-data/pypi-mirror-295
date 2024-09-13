565d161dad8de982d654d4d2e7847a225566085b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:38 +0000

    Add package bep-carta-python-util/0.0.1.dev1726159883/bep-carta-python-util-0.0.1.dev1726159883.tar.gz

---
+++ packages/bep-carta-python-util/bep-carta-python-util-0.0.1.dev1726159883.tar.gz/bep-carta-python-util-0.0.1.dev1726159883/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='bep-carta-python-util',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
