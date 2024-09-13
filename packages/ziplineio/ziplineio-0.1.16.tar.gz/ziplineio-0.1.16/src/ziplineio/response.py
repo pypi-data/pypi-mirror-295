f14da8dcea3b1bce0de72632c4b8df15c75d198d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 06:04:45 +0000

    Add package ziplineio/0.1.16/ziplineio-0.1.16.tar.gz

---
+++ packages/ziplineio/ziplineio-0.1.16.tar.gz/ziplineio-0.1.16/src/ziplineio/response.py
@@ -0,0 +1,126 @@
+import json
+from typing import List, Tuple, TypedDict, Dict
+
+
+from ziplineio.exception import BaseHttpException
+from ziplineio.request import Body
+
+
+class RawResponse(TypedDict):
+    headers: List[Tuple[bytes, bytes]]
+    status: int
+    body: bytes
+
+
+class Response:
+    def __init__(self, status: int, headers: Dict[str, str], body: Body):
+        self.status = status
+        self._headers = headers
+        self.body = body
+
+    status: int
+    _headers: Dict[str, str]
+    body: Body
+
+    # If content-type is not provided, infer content-type from the body
+    def get_headers(self) -> Dict[str, str]:
+        headers = self._headers.copy()
+        if "Content-Type" not in headers:
+            if isinstance(self.body, bytes):
+                headers["Content-Type"] = "text/plain"
+            elif isinstance(self.body, str):
+                headers["Content-Type"] = "text/html"
+            elif isinstance(self.body, dict):
+                headers["Content-Type"] = "application/json"
+            else:
+                headers["Content-Type"] = "text/plain"
+        return headers
+
+    def __len__(self) -> int:
+        return 1
+
+
+class StaticFileResponse(Response):
+    def __init__(self, file_path: str, headers: Dict[str, str]):
+        body = self.get_file(file_path)
+        super().__init__(200, headers, Body.from_bytes(body))
+
+    def get_file(self, file_path: str) -> bytes:
+        with open(file_path, "rb") as file:
+            return file.read()
+
+
+class JinjaResponse(Response):
+    def __init__(self, body: str):
+        body = Body.from_str(body)
+        super().__init__(200, {"Content-Type": "text/html"}, body)
+
+
+class NotFoundResponse(Response):
+    def __init__(
+        self, body: bytes | str | Response | Exception, headers: Dict[str, str] = {}
+    ):
+        body = format_body(body)
+        super().__init__(404, headers, Body(body))
+
+
+def format_headers(headers: Dict[str, str] | None) -> List[Tuple[bytes, bytes]]:
+    if headers is None:
+        return []
+    return [(bytes(k, "utf-8"), bytes(v, "utf-8")) for k, v in headers.items()]
+
+
+def format_body(body: bytes | str | dict) -> bytes:
+    if isinstance(body, bytes):
+        return body
+    elif isinstance(body, str):
+        return bytes(body, "utf-8")
+    elif isinstance(body, dict):
+        return bytes(json.dumps(body), "utf-8")
+    elif isinstance(body, Response):
+        return format_body(body.body.bytes())
+    raise ValueError("Invalid body type")
+
+
+def format_response(
+    response: bytes | dict | str | Response | Exception, default_headers: dict[str, str]
+) -> RawResponse:
+    # Helper to merge and deduplicate headers
+
+    # Format different response types
+    if isinstance(response, bytes):
+        headers = [(b"content-type", b"text/plain")]
+        body = response
+        status = 200
+
+    elif isinstance(response, str):
+        headers = [(b"content-type", b"text/plain")]
+        body = response.encode("utf-8")
+        status = 200
+
+    elif isinstance(response, dict):
+        headers = [(b"content-type", b"application/json")]
+        body = json.dumps(response).encode("utf-8")
+        status = 200
+
+    elif isinstance(response, Response):
+        headers = format_headers(response.get_headers())
+        body = response.body.bytes()
+        status = response.status
+
+    elif isinstance(response, BaseHttpException):
+        headers = [(b"content-type", b"application/json")]
+        body = format_body(response.message)
+        status = response.status_code
+
+    elif isinstance(response, Exception):
+        headers = [(b"content-type", b"text/plain")]
+        body = f"Internal server error: {str(response)}".encode("utf-8")
+        status = 500
+
+    else:
+        raise ValueError("Invalid response type")
+
+    default_headers = format_headers(default_headers)
+
+    return {"headers": default_headers + headers, "status": status, "body": body}
---
+++ packages/ziplineio/ziplineio-0.1.16.tar.gz/ziplineio-0.1.16/src/ziplineio/utils.py
@@ -0,0 +1,111 @@
+import re
+import inspect
+import asyncio
+
+from typing import Dict
+
+
+from ziplineio.request import Body, Request
+from ziplineio.response import Response
+from ziplineio.handler import Handler
+from ziplineio.models import ASGIScope
+
+
+"""
+Only pass the kwargs that are required by the handler function.
+"""
+
+
+def clean_kwargs(kwargs: dict, handler: Handler) -> dict:
+    params = inspect.signature(handler).parameters
+    kwargs = {k: v for k, v in kwargs.items() if k in params}
+    return kwargs
+
+
+async def read_body(receive: callable) -> Body:
+    # Read the body
+    body = b""
+    more_body = True
+
+    while more_body:
+        message = await receive()
+        body += message.get("body", b"")
+        more_body = message.get("more_body", False)
+
+    return Body(body)
+
+
+async def call_handler(
+    handler: Handler,
+    **kwargs,
+) -> bytes | str | dict | Response | Exception:
+    try:
+        kwargs = clean_kwargs(kwargs, handler)
+        if not inspect.iscoroutinefunction(handler):
+            response = await asyncio.to_thread(handler, **kwargs)
+        else:
+            response = await handler(**kwargs)
+
+    except Exception as e:
+        response = e
+
+    return response
+
+
+async def parse_scope(scope: ASGIScope, receive: callable) -> Request:
+    query_string = scope["query_string"].decode("utf-8")
+
+    if query_string == "":
+        query_params = {}
+    else:
+        query_params = dict(qp.split("=") for qp in query_string.split("&"))
+
+    split_path = scope["path"].split(":")
+    if len(split_path) > 1:
+        path_params = dict(zip(split_path[1:], scope["path"].split("/")[1:]))
+    else:
+        path_params = {}
+
+    body = await read_body(receive)
+
+    headers = dict((k.decode("utf-8"), v.decode("utf-8")) for k, v in scope["headers"])
+    return Request(
+        method=scope["method"],
+        path=scope["path"],
+        query_params=query_params,
+        path_params=path_params,
+        headers=headers,
+        body=body,
+    )
+
+
+def clean_url(path: str) -> str:
+    return re.sub(r":\w+", "*", path)
+
+
+def match_url_pattern(pattern: str, url: str) -> Dict[str, str]:
+    """
+    Match a URL against a pattern with wildcards and return a dictionary of captured parameters.
+
+    Args:
+        pattern (str): The pattern with wildcards (e.g., 'user/:id').
+        url (str): The URL to match (e.g., 'users/123').
+
+    Returns:
+        Dict[str, str]: A dictionary of captured parameters (e.g., {'id': '123'}).
+    """
+    # Convert the pattern into a regular expression
+    # Replace ':param' with '([^/]+)' to capture the parameter value
+    regex_pattern = re.sub(r":(\w+)", r"(?P<\1>[^/]+)", pattern)
+    regex_pattern = "^" + regex_pattern + "$"
+
+    # Compile the regular expression
+    pattern_re = re.compile(regex_pattern)
+
+    # Match the URL against the pattern
+    match = pattern_re.match(url)
+    if not match:
+        return {}
+
+    # Return the captured parameters as a dictionary
+    return match.groupdict()
