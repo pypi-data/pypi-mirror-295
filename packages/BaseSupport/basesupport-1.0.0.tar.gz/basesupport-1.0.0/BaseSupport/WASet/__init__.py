dc3a88725f2fb10679ad9df8345d3bdaae2cd836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:06:23 +0000

    Add package BaseSupport/1.0.0/basesupport-1.0.0.tar.gz

---
+++ packages/BaseSupport/basesupport-1.0.0.tar.gz/basesupport-1.0.0/BaseSupport/WASet/__init__.py
@@ -0,0 +1,7 @@
+# from WASet.WABuild import WABuild
+# from WASet.WASetBundleInfo import WASetBundleInfo
+# from WASet.WASetFramework import WASetFramework
+# from WASet.WASetPbxproj import WASetPbxproj
+# from WASet.WASetPlist import WASetPlist
+# from WASet.WASetPod import WASetPod
+# from WASet.WASetProfle import WASetProfle
