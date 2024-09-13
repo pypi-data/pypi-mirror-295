e0fbd39363bcb302995e4ee55cd25436f450c438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:33 +0000

    Add package abstract-solana/0.0.0.25/abstract_solana-0.0.0.25.tar.gz

---
+++ packages/abstract-solana/abstract_solana-0.0.0.25.tar.gz/abstract_solana-0.0.0.25/test/test_abstract_solana.py
@@ -0,0 +1,10 @@
+from abstract_solana import Pubkey,get_pubkey,Optional,Union,LAMPORTS_PER_SOL,get_any_value
+
+from abstract_solcatcher import *
+ #type: ignore
+mint_str = "3bb6QmvustnZ627Kg2QvwTSi8gmxp7Y6orwXUokEwUyV"
+result = pump_fun_sell(mint_str=mint_str, token_balance=None, slippage=25)
+# Example usage
+input(result)
+
+
