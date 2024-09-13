9de0fc1952d929ddd600bd3d927b175f87160ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 06:04:53 +0000

    Add package SpectraFit/1.0.4/spectrafit-1.0.4.tar.gz

---
+++ packages/SpectraFit/spectrafit-1.0.4.tar.gz/spectrafit-1.0.4/spectrafit/__init__.py
@@ -0,0 +1,8 @@
+"""SpectraFit, fast command line tool for fitting data.
+
+!!! info "About Versioning"
+
+    SpectraFit uses [Semantic Versioning](https://semver.org/).
+"""
+
+__version__ = "1.0.4"
