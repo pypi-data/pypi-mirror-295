985b38f39cb153fa63564d84806c1d8d742bb64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:07:19 +0000

    Add package derivepassphrase/0.2.0/derivepassphrase-0.2.0-py3-none-any.whl

---
+++ packages/derivepassphrase/derivepassphrase-0.2.0-py3-none-any.whl/derivepassphrase/_types.py
@@ -0,0 +1,207 @@
+# SPDX-FileCopyrightText: 2024 Marco Ricci <software@the13thletter.info>
+#
+# SPDX-License-Identifier: MIT
+
+"""Types used by derivepassphrase."""
+
+from __future__ import annotations
+
+import enum
+from typing import Literal, NamedTuple, TypeGuard
+
+from typing_extensions import (
+    Any,
+    NotRequired,
+    Required,
+    TypedDict,
+)
+
+__all__ = (
+    'SSH_AGENT',
+    'SSH_AGENTC',
+    'KeyCommentPair',
+    'VaultConfig',
+    'is_vault_config',
+)
+
+
+class VaultConfigGlobalSettings(TypedDict, total=False):
+    r"""Configuration for vault: global settings.
+
+    Attributes:
+        key:
+            The base64-encoded ssh public key to use, overriding the
+            master passphrase. Optional.
+        phrase:
+            The master passphrase. Optional.
+        unicode_normalization_form:
+            The preferred Unicode normalization form; we warn the user
+            if textual passphrases do not match their normalized forms.
+            Optional, and a `derivepassphrase` extension.
+
+    """
+
+    key: NotRequired[str]
+    phrase: NotRequired[str]
+    unicode_normalization_form: NotRequired[
+        Literal['NFC', 'NFD', 'NFKC', 'NFKD']
+    ]
+
+
+class VaultConfigServicesSettings(VaultConfigGlobalSettings, total=False):
+    r"""Configuration for vault: services settings.
+
+    Attributes:
+        notes:
+            Optional notes for this service, to display to the user when
+            generating the passphrase.
+        length:
+            Desired passphrase length.
+        repeat:
+            The maximum number of immediate character repetitions
+            allowed in the passphrase.  Disabled if set to 0.
+        lower:
+            Optional constraint on ASCII lowercase characters.  If
+            positive, include this many lowercase characters
+            somewhere in the passphrase.  If 0, avoid lowercase
+            characters altogether.
+        upper:
+            Same as `lower`, but for ASCII uppercase characters.
+        number:
+            Same as `lower`, but for ASCII digits.
+        space:
+            Same as `lower`, but for the space character.
+        dash:
+            Same as `lower`, but for the hyphen-minus and underscore
+            characters.
+        symbol:
+            Same as `lower`, but for all other hitherto unlisted
+            ASCII printable characters (except backquote).
+
+    """
+
+    notes: NotRequired[str]
+    length: NotRequired[int]
+    repeat: NotRequired[int]
+    lower: NotRequired[int]
+    upper: NotRequired[int]
+    number: NotRequired[int]
+    space: NotRequired[int]
+    dash: NotRequired[int]
+    symbol: NotRequired[int]
+
+
+_VaultConfig = TypedDict(
+    '_VaultConfig',
+    {'global': NotRequired[VaultConfigGlobalSettings]},
+    total=False,
+)
+
+
+class VaultConfig(TypedDict, _VaultConfig, total=False):
+    r"""Configuration for vault.
+
+    Usually stored as JSON.
+
+    Attributes:
+        global (NotRequired[VaultConfigGlobalSettings]):
+            Global settings.
+        services (Required[dict[str, VaultConfigServicesSettings]]):
+            Service-specific settings.
+
+    """
+
+    services: Required[dict[str, VaultConfigServicesSettings]]
+
+
+def is_vault_config(obj: Any) -> TypeGuard[VaultConfig]:  # noqa: ANN401,C901,PLR0911,PLR0912
+    """Check if `obj` is a valid vault config, according to typing.
+
+    Args:
+        obj: The object to test.
+
+    Returns:
+        True if this is a vault config, false otherwise.
+
+    """
+    if not isinstance(obj, dict):
+        return False
+    if 'global' in obj:
+        o_global = obj['global']
+        if not isinstance(o_global, dict):
+            return False
+        for key in ('key', 'phrase', 'unicode_normalization_form'):
+            if key in o_global and not isinstance(o_global[key], str):
+                return False
+        if 'key' in o_global and 'phrase' in o_global:
+            return False
+    if not isinstance(obj.get('services'), dict):
+        return False
+    for sv_name, service in obj['services'].items():
+        if not isinstance(sv_name, str):
+            return False
+        if not isinstance(service, dict):
+            return False
+        for key, value in service.items():
+            match key:
+                case 'notes' | 'phrase' | 'key':
+                    if not isinstance(value, str):
+                        return False
+                case 'length':
+                    if not isinstance(value, int) or value < 1:
+                        return False
+                case _:
+                    if not isinstance(value, int) or value < 0:
+                        return False
+        if 'key' in service and 'phrase' in service:
+            return False
+    return True
+
+
+class KeyCommentPair(NamedTuple):
+    """SSH key plus comment pair.  For typing purposes.
+
+    Attributes:
+        key: SSH key.
+        comment: SSH key comment.
+
+    """
+
+    key: bytes | bytearray
+    comment: bytes | bytearray
+
+
+class SSH_AGENTC(enum.Enum):  # noqa: N801
+    """SSH agent protocol numbers: client requests.
+
+    Attributes:
+        REQUEST_IDENTITIES:
+            List identities.  Expecting `SSH_AGENT.IDENTITIES_ANSWER`.
+        SIGN_REQUEST:
+            Sign data.  Expecting `SSH_AGENT.SIGN_RESPONSE`.
+
+    """
+
+    REQUEST_IDENTITIES: int = 11
+    SIGN_REQUEST: int = 13
+
+
+class SSH_AGENT(enum.Enum):  # noqa: N801
+    """SSH agent protocol numbers: server replies.
+
+    Attributes:
+        FAILURE:
+            Generic failure code.
+        SUCCESS:
+            Generic success code.
+        IDENTITIES_ANSWER:
+            Successful answer to `SSH_AGENTC.REQUEST_IDENTITIES`.
+        SIGN_RESPONSE:
+            Successful answer to `SSH_AGENTC.SIGN_REQUEST`.
+
+    """
+
+    FAILURE: int = 5
+    SUCCESS: int = 6
+    IDENTITIES_ANSWER: int = 12
+    SIGN_RESPONSE: int = 14
---
+++ packages/derivepassphrase/derivepassphrase-0.2.0-py3-none-any.whl/derivepassphrase/ssh_agent.py
@@ -0,0 +1,415 @@
+# SPDX-FileCopyrightText: 2024 Marco Ricci <software@the13thletter.info>
+#
+# SPDX-License-Identifier: MIT
+
+"""A bare-bones SSH agent client supporting signing and key listing."""
+
+from __future__ import annotations
+
+import collections
+import errno
+import os
+import socket
+from typing import TYPE_CHECKING
+
+from typing_extensions import Self
+
+from derivepassphrase import _types
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from types import TracebackType
+
+__all__ = ('SSHAgentClient',)
+__author__ = 'Marco Ricci <software@the13thletter.info>'
+
+# In SSH bytestrings, the "length" of the byte string is stored as
+# a 4-byte/32-bit unsigned integer at the beginning.
+HEAD_LEN = 4
+
+_socket = socket
+
+
+class TrailingDataError(RuntimeError):
+    """The result contained trailing data."""
+
+    def __init__(self) -> None:
+        super().__init__('Overlong response from SSH agent')
+
+
+class SSHAgentFailedError(RuntimeError):
+    """The SSH agent failed to complete the requested operation."""
+
+    def __str__(self) -> str:
+        match self.args:
+            case (_types.SSH_AGENT.FAILURE.value, b''):  # pragma: no branch
+                return 'The SSH agent failed to complete the request'
+            case (_, _msg) if _msg:  # pragma: no cover
+                code = self.args[0]
+                msg = self.args[1].decode('utf-8', 'surrogateescape')
+                return f'[Code {code:d}] {msg:s}'
+            case _:  # pragma: no cover
+                return repr(self)
+
+    def __repr__(self) -> str:  # pragma: no cover
+        return f'{self.__class__.__name__}{self.args!r}'
+
+
+class SSHAgentClient:
+    """A bare-bones SSH agent client supporting signing and key listing.
+
+    The main use case is requesting the agent sign some data, after
+    checking that the necessary key is already loaded.
+
+    The main fleshed out methods are `list_keys` and `sign`, which
+    implement the `REQUEST_IDENTITIES` and `SIGN_REQUEST` requests.  If
+    you *really* wanted to, there is enough infrastructure in place to
+    issue other requests as defined in the protocol---it's merely the
+    wrapper functions and the protocol numbers table that are missing.
+
+    """
+
+    _connection: socket.socket
+
+    def __init__(
+        self, /, *, socket: socket.socket | None = None, timeout: int = 125
+    ) -> None:
+        """Initialize the client.
+
+        Args:
+            socket:
+                An optional socket, connected to the SSH agent.  If not
+                given, we query the `SSH_AUTH_SOCK` environment
+                variable to auto-discover the correct socket address.
+            timeout:
+                A connection timeout for the SSH agent.  Only used if
+                the socket is not yet connected.  The default value
+                gives ample time for agent connections forwarded via
+                SSH on high-latency networks (e.g. Tor).
+
+        Raises:
+            KeyError:
+                The `SSH_AUTH_SOCK` environment was not found.
+            OSError:
+                There was an error setting up a socket connection to the
+                agent.
+
+        """
+        if socket is not None:
+            self._connection = socket
+        else:
+            self._connection = _socket.socket(family=_socket.AF_UNIX)
+        try:
+            # Test whether the socket is connected.
+            self._connection.getpeername()
+        except OSError as e:
+            # This condition is hard to test purposefully, so exclude
+            # from coverage.
+            if e.errno != errno.ENOTCONN:  # pragma: no cover
+                raise
+            if 'SSH_AUTH_SOCK' not in os.environ:
+                msg = 'SSH_AUTH_SOCK environment variable'
+                raise KeyError(msg) from None
+            ssh_auth_sock = os.environ['SSH_AUTH_SOCK']
+            self._connection.settimeout(timeout)
+            self._connection.connect(ssh_auth_sock)
+
+    def __enter__(self) -> Self:
+        """Close socket connection upon context manager completion.
+
+        Returns:
+            Self.
+
+        """
+        self._connection.__enter__()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        """Close socket connection upon context manager completion.
+
+        Args:
+            exc_type: An optional exception type.
+            exc_val: An optional exception value.
+            exc_tb: An optional exception traceback.
+
+        Returns:
+            True if the exception was handled, false if it should
+            propagate.
+
+        """
+        return bool(
+            self._connection.__exit__(exc_type, exc_val, exc_tb)  # type: ignore[func-returns-value]
+        )
+
+    @staticmethod
+    def uint32(num: int, /) -> bytes:
+        r"""Format the number as a `uint32`, as per the agent protocol.
+
+        Args:
+            num: A number.
+
+        Returns:
+            The number in SSH agent wire protocol format, i.e. as
+            a 32-bit big endian number.
+
+        Raises:
+            OverflowError:
+                As per [`int.to_bytes`][].
+
+        Examples:
+            >>> SSHAgentClient.uint32(16777216)
+            b'\x01\x00\x00\x00'
+
+        """
+        return int.to_bytes(num, 4, 'big', signed=False)
+
+    @classmethod
+    def string(cls, payload: bytes | bytearray, /) -> bytes | bytearray:
+        r"""Format the payload as an SSH string, as per the agent protocol.
+
+        Args:
+            payload: A byte string.
+
+        Returns:
+            The payload, framed in the SSH agent wire protocol format.
+
+        Examples:
+            >>> bytes(SSHAgentClient.string(b'ssh-rsa'))
+            b'\x00\x00\x00\x07ssh-rsa'
+
+        """
+        try:
+            ret = bytearray()
+            ret.extend(cls.uint32(len(payload)))
+            ret.extend(payload)
+        except Exception as e:
+            msg = 'invalid payload type'
+            raise TypeError(msg) from e  # noqa: DOC501
+        return ret
+
+    @classmethod
+    def unstring(cls, bytestring: bytes | bytearray, /) -> bytes | bytearray:
+        r"""Unpack an SSH string.
+
+        Args:
+            bytestring: A framed byte string.
+
+        Returns:
+            The unframed byte string, i.e., the payload.
+
+        Raises:
+            ValueError:
+                The byte string is not an SSH string.
+
+        Examples:
+            >>> bytes(SSHAgentClient.unstring(b'\x00\x00\x00\x07ssh-rsa'))
+            b'ssh-rsa'
+            >>> bytes(
+            ...     SSHAgentClient.unstring(SSHAgentClient.string(b'ssh-ed25519'))
+            ... )
+            b'ssh-ed25519'
+
+        """  # noqa: E501
+        n = len(bytestring)
+        msg = 'malformed SSH byte string'
+        if n < HEAD_LEN or n != HEAD_LEN + int.from_bytes(
+            bytestring[:HEAD_LEN], 'big', signed=False
+        ):
+            raise ValueError(msg)
+        return bytestring[HEAD_LEN:]
+
+    @classmethod
+    def unstring_prefix(
+        cls, bytestring: bytes | bytearray, /
+    ) -> tuple[bytes | bytearray, bytes | bytearray]:
+        r"""Unpack an SSH string at the beginning of the byte string.
+
+        Args:
+            bytestring:
+                A (general) byte string, beginning with a framed/SSH
+                byte string.
+
+        Returns:
+            A 2-tuple `(a, b)`, where `a` is the unframed byte
+            string/payload at the beginning of input byte string, and
+            `b` is the remainder of the input byte string.
+
+        Raises:
+            ValueError:
+                The byte string does not begin with an SSH string.
+
+        Examples:
+            >>> a, b = SSHAgentClient.unstring_prefix(
+            ...     b'\x00\x00\x00\x07ssh-rsa____trailing data'
+            ... )
+            >>> (bytes(a), bytes(b))
+            (b'ssh-rsa', b'____trailing data')
+            >>> a, b = SSHAgentClient.unstring_prefix(
+            ...     SSHAgentClient.string(b'ssh-ed25519')
+            ... )
+            >>> (bytes(a), bytes(b))
+            (b'ssh-ed25519', b'')
+
+        """
+        n = len(bytestring)
+        msg = 'malformed SSH byte string'
+        if n < HEAD_LEN:
+            raise ValueError(msg)
+        m = int.from_bytes(bytestring[:HEAD_LEN], 'big', signed=False)
+        if m + HEAD_LEN > n:
+            raise ValueError(msg)
+        return (
+            bytestring[HEAD_LEN : m + HEAD_LEN],
+            bytestring[m + HEAD_LEN :],
+        )
+
+    def request(
+        self, code: int, payload: bytes | bytearray, /
+    ) -> tuple[int, bytes | bytearray]:
+        """Issue a generic request to the SSH agent.
+
+        Args:
+            code:
+                The request code.  See the SSH agent protocol for
+                protocol numbers to use here (and which protocol numbers
+                to expect in a response).
+            payload:
+                A byte string containing the payload, or "contents", of
+                the request.  Request-specific.  `request` will add any
+                necessary wire framing around the request code and the
+                payload.
+
+        Returns:
+            A 2-tuple consisting of the response code and the payload,
+            with all wire framing removed.
+
+        Raises:
+            EOFError:
+                The response from the SSH agent is truncated or missing.
+
+        """
+        request_message = bytearray([code])
+        request_message.extend(payload)
+        self._connection.sendall(self.string(request_message))
+        chunk = self._connection.recv(HEAD_LEN)
+        if len(chunk) < HEAD_LEN:
+            msg = 'cannot read response length'
+            raise EOFError(msg)
+        response_length = int.from_bytes(chunk, 'big', signed=False)
+        response = self._connection.recv(response_length)
+        if len(response) < response_length:
+            msg = 'truncated response from SSH agent'
+            raise EOFError(msg)
+        return response[0], response[1:]
+
+    def list_keys(self) -> Sequence[_types.KeyCommentPair]:
+        """Request a list of keys known to the SSH agent.
+
+        Returns:
+            A read-only sequence of key/comment pairs.
+
+        Raises:
+            EOFError:
+                The response from the SSH agent is truncated or missing.
+            TrailingDataError:
+                The response from the SSH agent is too long.
+            SSHAgentFailedError:
+                The agent failed to complete the request.
+
+        """
+        response_code, response = self.request(
+            _types.SSH_AGENTC.REQUEST_IDENTITIES.value, b''
+        )
+        if response_code != _types.SSH_AGENT.IDENTITIES_ANSWER.value:
+            raise SSHAgentFailedError(response_code, response)
+        response_stream = collections.deque(response)
+
+        def shift(num: int) -> bytes:
+            buf = collections.deque(b'')
+            for _ in range(num):
+                try:
+                    val = response_stream.popleft()
+                except IndexError:
+                    response_stream.extendleft(reversed(buf))
+                    msg = 'truncated response from SSH agent'
+                    raise EOFError(msg) from None
+                buf.append(val)
+            return bytes(buf)
+
+        key_count = int.from_bytes(shift(4), 'big')
+        keys: collections.deque[_types.KeyCommentPair]
+        keys = collections.deque()
+        for _ in range(key_count):
+            key_size = int.from_bytes(shift(4), 'big')
+            key = shift(key_size)
+            comment_size = int.from_bytes(shift(4), 'big')
+            comment = shift(comment_size)
+            # Both `key` and `comment` are not wrapped as SSH strings.
+            keys.append(_types.KeyCommentPair(key, comment))
+        if response_stream:
+            raise TrailingDataError
+        return keys
+
+    def sign(
+        self,
+        /,
+        key: bytes | bytearray,
+        payload: bytes | bytearray,
+        *,
+        flags: int = 0,
+        check_if_key_loaded: bool = False,
+    ) -> bytes | bytearray:
+        """Request the SSH agent sign the payload with the key.
+
+        Args:
+            key:
+                The public SSH key to sign the payload with, in the same
+                format as returned by, e.g., the `list_keys` method.
+                The corresponding private key must have previously been
+                loaded into the agent to successfully issue a signature.
+            payload:
+                A byte string of data to sign.
+            flags:
+                Optional flags for the signing request.  Currently
+                passed on as-is to the agent.  In real-world usage, this
+                could be used, e.g., to request more modern hash
+                algorithms when signing with RSA keys.  (No such
+                real-world usage is currently implemented.)
+            check_if_key_loaded:
+                If true, check beforehand (via `list_keys`) if the
+                corresponding key has been loaded into the agent.
+
+        Returns:
+            The binary signature of the payload under the given key.
+
+        Raises:
+            EOFError:
+                The response from the SSH agent is truncated or missing.
+            TrailingDataError:
+                The response from the SSH agent is too long.
+            SSHAgentFailedError:
+                The agent failed to complete the request.
+            KeyError:
+                `check_if_key_loaded` is true, and the `key` was not
+                loaded into the agent.
+
+        """
+        if check_if_key_loaded:
+            loaded_keys = frozenset({pair.key for pair in self.list_keys()})
+            if bytes(key) not in loaded_keys:
+                msg = 'target SSH key not loaded into agent'
+                raise KeyError(msg)
+        request_data = bytearray(self.string(key))
+        request_data.extend(self.string(payload))
+        request_data.extend(self.uint32(flags))
+        response_code, response = self.request(
+            _types.SSH_AGENTC.SIGN_REQUEST.value, request_data
+        )
+        if response_code != _types.SSH_AGENT.SIGN_RESPONSE.value:
+            raise SSHAgentFailedError(response_code, response)
+        return self.unstring(response)
