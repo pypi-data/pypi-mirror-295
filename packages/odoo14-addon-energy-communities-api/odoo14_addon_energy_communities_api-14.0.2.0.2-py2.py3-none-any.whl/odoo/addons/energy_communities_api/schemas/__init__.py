517f6cca20c58fa42a143321304e550f4b9282f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 04:05:46 +0000

    Add package odoo14-addon-energy-communities-api/14.0.2.0.2/odoo14_addon_energy_communities_api-14.0.2.0.2-py2.py3-none-any.whl

---
+++ packages/odoo14-addon-energy-communities-api/odoo14_addon_energy_communities_api-14.0.2.0.2-py2.py3-none-any.whl/odoo/addons/energy_communities_api/schemas/__init__.py
@@ -0,0 +1,28 @@
+from .base import (
+    PagingParam,
+    PaginationLimits,
+    PaginationLinks,
+    DEFAULT_PAGE_SIZE,
+    QueryParams,
+)
+from .member import (
+    MemberInfo,
+    MemberInfoResponse,
+)
+from .community import (
+    CommunityInfo,
+    MetricInfo,
+    UnitEnum,
+    CommunityInfoListResponse,
+    CommunityServiceInfo,
+    CommunityServiceInfoListResponse,
+    CommunityServiceMetricsInfo,
+    CommunityServiceMetricsInfoListResponse,
+)
+from .selfconsumption_project import (
+    SelfConsumptionProjectInfo,
+    SelfConsumptionProjectMember,
+    ProjectInfoResponse,
+    ProjectInfoListResponse,
+    SelfConsumptionProjectMemberListResponse,
+)
