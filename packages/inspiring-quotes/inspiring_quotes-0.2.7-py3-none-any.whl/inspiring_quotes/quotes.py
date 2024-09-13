598b605fae9ae7d528924d19c51c3b94f79bfcb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:08 +0000

    Add package inspiring-quotes/0.2.7/inspiring_quotes-0.2.7-py3-none-any.whl

---
+++ packages/inspiring-quotes/inspiring_quotes-0.2.7-py3-none-any.whl/inspiring_quotes/quotes.py
@@ -0,0 +1,17 @@
+import json
+import random
+import importlib.resources as pkg_resources  # Importlib resources
+
+def get_random_quote():
+    try:
+        # Using importlib.resources.files to get the file
+        with pkg_resources.files(__package__).joinpath('inspiring_quotes/quotes_list.json').open('r') as file:
+            quotes = json.load(file)
+            return random.choice(quotes)
+    except FileNotFoundError:
+        print("The file 'quotes_list.json' does not exist.")
+        return None
+    except json.JSONDecodeError:
+        print("Error decoding JSON. Please check the file format.")
+        return None
+
