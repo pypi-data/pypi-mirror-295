6b0c25b155ccaaedec5fdb01875920229c2970b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 03:07:15 +0000

    Add package gigachain/0.2.16/gigachain-0.2.16.tar.gz

---
+++ packages/gigachain/gigachain-0.2.16.tar.gz/gigachain-0.2.16/langchain/utils/openai_functions.py
@@ -0,0 +1,15 @@
+from langchain_core.utils.function_calling import (
+    FunctionDescription,
+    GigaFunctionDescription,
+    ToolDescription,
+    convert_pydantic_to_openai_function,
+    convert_pydantic_to_openai_tool,
+)
+
+__all__ = [
+    "FunctionDescription",
+    "GigaFunctionDescription",
+    "ToolDescription",
+    "convert_pydantic_to_openai_function",
+    "convert_pydantic_to_openai_tool",
+]
