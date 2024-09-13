7f07b1fc3a7e3f2bc32f520fae79b5e701a3e06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:19 +0000

    Add package terraflex/0.1.0/terraflex-0.1.0-py3-none-any.whl

---
+++ packages/terraflex/terraflex-0.1.0-py3-none-any.whl/terraflex/server/transformation_base.py
@@ -0,0 +1,32 @@
+import abc
+import pathlib
+from typing import Any, Self
+
+from terraflex.server.storage_provider_base import AbstractStorageProvider
+from terraflex.utils.dependency_manager import DependenciesManager
+
+
+TRANSFORMERS_ENTRYPOINT = "terraflex.plugins.transformer"
+
+
+class AbstractTransformation(abc.ABC):
+    @classmethod
+    @abc.abstractmethod
+    async def from_config(
+        cls,
+        raw_config: Any,
+        *,
+        storage_providers: dict[str, AbstractStorageProvider],
+        manager: DependenciesManager,
+        workdir: pathlib.Path,
+    ) -> Self: ...
+
+    @abc.abstractmethod
+    async def transform_write_file_content(self, file_identifier: str, content: bytes) -> bytes: ...
+
+    @abc.abstractmethod
+    async def transform_read_file_content(self, file_identifier: str, content: bytes) -> bytes: ...
+
+
+if __name__ == "__main__":
+    print("hey")
