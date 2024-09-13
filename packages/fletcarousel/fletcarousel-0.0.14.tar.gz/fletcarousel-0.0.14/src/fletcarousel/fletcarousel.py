8038b91725ab4bf4e3967fb425b373fe49aa9d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 04:05:48 +0000

    Add package fletcarousel/0.0.14/fletcarousel-0.0.14.tar.gz

---
+++ packages/fletcarousel/fletcarousel-0.0.14.tar.gz/fletcarousel-0.0.14/src/fletcarousel/fletcarousel.py
@@ -0,0 +1,62 @@
+import threading
+from typing import Union, Optional
+from flet import (
+    ClipBehavior,
+    Border,
+    Control,
+    Page,
+    Alignment,
+    Container
+)
+from flet_core.gradients import Gradient
+from flet_core.types import (
+    PaddingValue,
+    MarginValue,
+    BorderRadiusValue,
+)
+
+
+class FletCarousel(Container):
+    def __init__(
+            self,
+            page: Page,
+            width: int = None,
+            height: int = None,
+            expand: Union[None, bool, int] = None,
+            tooltip: Optional[str] = None,
+            visible: Optional[bool] = None,
+            disabled: Optional[bool] = None,
+            padding: PaddingValue = None,
+            margin: MarginValue = None,
+            alignment: Optional[Alignment] = None,
+            bgcolor: Optional[str] = None,
+            gradient: Optional[Gradient] = None,
+            border: Optional[Border] = None,
+            border_radius: BorderRadiusValue = None,
+    ):
+        super().__init__()
+        self.page = page
+        self.width = width
+        self.height = height
+        self.expand = expand
+        self.tooltip = tooltip
+        self.visible = visible
+        self.disabled = disabled
+        self.padding = padding
+        self.margin = margin
+        self.alignment = alignment
+        self.bgcolor = bgcolor
+        self.gradient = gradient
+        self.border = border
+        self.border_radius = border_radius
+        self.clip_behavior = (ClipBehavior.HARD_EDGE,)
+
+    def build(self):
+        self.content = self.render()
+        threading.Thread(target=self.init_state).start()
+
+    def render(self) -> Control:
+        return Control()
+
+    def init_state(self):
+        pass
