410d97d85e30a4f9b3e1409bb8612ae288a98e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 04:05:42 +0000

    Add package tml-ctp/1.1.1/tml_ctp-1.1.1-py3-none-any.whl

---
+++ packages/tml-ctp/tml_ctp-1.1.1-py3-none-any.whl/tml_ctp/info.py
@@ -0,0 +1,13 @@
+# Copyright 2023-2024 Lausanne University and Lausanne University Hospital, Switzerland & Contributors
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""Module for storing package metadata."""
+
+
+__version__ = "1.1.1"
+__packagename__ = "tml_ctp"
+__container_name__ = "tml-ctp-anonymizer"
+__author__ = "TranslationalML team and Contributors"
