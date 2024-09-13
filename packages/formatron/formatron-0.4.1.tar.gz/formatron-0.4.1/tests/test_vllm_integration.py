0fdd9d42dc09d9beadaa50277bed578c75a5f2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:16 +0000

    Add package formatron/0.4.1/formatron-0.4.1.tar.gz

---
+++ packages/formatron/formatron-0.4.1.tar.gz/formatron-0.4.1/tests/test_vllm_integration.py
@@ -0,0 +1,26 @@
+from vllm import LLM, SamplingParams
+
+from formatron.formatter import FormatterBuilder
+from formatron.integrations.vllm import create_formatters_logits_processor
+
+
+def test_vllm_integration(snapshot):
+    prompts = [
+        "Hello, my name is",
+        "The future of AI is",
+    ]
+    llm = LLM(model="openai-community/gpt2")
+    f = FormatterBuilder()
+    f.append_line("definitely vllm!")
+    f2 = FormatterBuilder()
+    f2.append_line("强大的【VLLM】！！！")
+    logits_processor = create_formatters_logits_processor(llm, [f, f2])
+    sampling_params = SamplingParams(max_tokens=50,temperature=0.8,skip_special_tokens=False, top_p=0.95, logits_processors=[logits_processor])
+    # Generate texts from the prompts. The output is a list of RequestOutput objects
+    # that contain the prompt, generated text, and other information.
+    outputs = llm.generate(prompts, sampling_params, )
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        snapshot.assert_match(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
