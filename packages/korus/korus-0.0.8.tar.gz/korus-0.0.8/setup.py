68fd772115af4cc6e556c0ebe1784a6b91de1b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:24 +0000

    Add package korus/0.0.8/korus-0.0.8.tar.gz

---
+++ packages/korus/korus-0.0.8.tar.gz/korus-0.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from korus.__init__ import __version__
+
+setup(
+    name="korus",
+    version=__version__,
+    description="Python package for managing acoustic metadata and annotations",
+    author="Oliver Kirsebom",
+    author_email="oliver.kirsebom@gmail.com",
+    url=None,
+    license="",
+    packages=find_packages(),
+    install_requires=[
+        "soundfile", 
+        "pandas", 
+        "numpy",
+        "termcolor", 
+        "treelib", 
+        "tqdm",
+        "tabulate",
+    ],
+    entry_points={
+        "console_scripts": [
+            "korus-submit = korus.app.submit:main",
+        ],
+    },
+    setup_requires=["pytest-runner", "wheel"],
+    tests_require=["pytest",],
+    include_package_data=True,
+    zip_safe=False,
+)
