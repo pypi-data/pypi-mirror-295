a442a26ab6825afaefe1759345350d3c4129a5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:19 +0000

    Add package terraflex/0.1.0/terraflex-0.1.0.tar.gz

---
+++ packages/terraflex/terraflex-0.1.0.tar.gz/terraflex-0.1.0/terraflex/server/config.py
@@ -0,0 +1,52 @@
+import pathlib
+from typing import (
+    Annotated,
+    Optional,
+    TypeAlias,
+)
+
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic_settings import (
+    BaseSettings,
+)
+import xdg_base_dirs
+
+PACKAGE_NAME = "tfstate-git"
+
+
+StorageParams: TypeAlias = dict
+
+
+class StorageProviderUsageConfig(BaseModel):
+    provider: str
+    params: Optional[StorageParams]
+
+
+class StorageProviderConfig(BaseModel):
+    model_config = ConfigDict(extra="allow")
+    type: str
+
+
+class TransformerConfig(BaseModel):
+    model_config = ConfigDict(extra="allow")
+    type: str
+
+
+class StateManagerConfig(BaseModel):
+    storage: StorageProviderUsageConfig
+
+
+class ConfigFile(BaseModel):
+    version: str
+    storage_providers: dict[str, StorageProviderConfig]
+    transformers: list[TransformerConfig]
+    state_manager: StateManagerConfig
+
+
+class Settings(BaseSettings):
+    state_dir: Annotated[
+        pathlib.Path,
+        Field(
+            default=xdg_base_dirs.xdg_data_home() / PACKAGE_NAME,
+        ),
+    ]
