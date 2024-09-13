4e1dbf5849262be2e930c03b3a7388ac9db0ce0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:05:38 +0000

    Add package carta-grpc-common/0.0.1.dev1726159881/carta-grpc-common-0.0.1.dev1726159881.tar.gz

---
+++ packages/carta-grpc-common/carta-grpc-common-0.0.1.dev1726159881.tar.gz/carta-grpc-common-0.0.1.dev1726159881/setup.py
@@ -0,0 +1,8 @@
+import time
+from setuptools import setup
+ts = int(time.time())
+setup(
+    name='carta-grpc-common',
+    description='Placeholder for internal package',
+    version=f"0.0.1dev{ts}",
+)
