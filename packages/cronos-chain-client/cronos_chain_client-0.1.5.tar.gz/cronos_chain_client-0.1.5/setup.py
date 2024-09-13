ed34bd24fe8b1b5488d0beec0cfb2768c17adf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 04:05:15 +0000

    Add package cronos-chain-client/0.1.5/cronos_chain_client-0.1.5.tar.gz

---
+++ packages/cronos-chain-client/cronos_chain_client-0.1.5.tar.gz/cronos_chain_client-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronos_chain_client',
+    version='0.1.5',
+    description='A Python client for interacting with the Cronos Chains',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cronos-labs/cronos-chain-client/tree/main/py',
+    author='rarcifa',
+    author_email='rarcifa@gmail.com',
+    license='MIT',
+    package_dir={'': 'src'},  
+    packages=find_packages(where='src'), 
+    include_package_data=True, 
+    package_data={
+        'cronos_chain_client.tokens': ['*.json'],  
+    },
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
