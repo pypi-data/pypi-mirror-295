4b28c3209ea247709b6497f9132b307bf75875ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-13 05:06:08 +0000

    Add package abstract-solana/0.0.0.37/abstract_solana-0.0.0.37.tar.gz

---
+++ packages/abstract-solana/abstract_solana-0.0.0.37.tar.gz/abstract_solana-0.0.0.37/test/test_abstract_solana.py
@@ -0,0 +1,8 @@
+from abstract_solana import pump_fun_sell,load_from_private_key,sendTransaction,load_from_private_key
+payer_keypair = load_from_private_key()
+mint_str = "3bb6QmvustnZ627Kg2QvwTSi8gmxp7Y6orwXUokEwUyV"
+txn = pump_fun_sell(mint=mint_str, token_balance=None, slippage=25)
+txn_sig = sendTransaction(txn, payer_keypair, TxOpts(skip_preflight=True))
+print("Transaction Signature", txn_sig)
+confirm = confirm_txn(txn_sig)
+print(confirm)
